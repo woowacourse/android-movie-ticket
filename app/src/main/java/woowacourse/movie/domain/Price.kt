4c79b1a37363589790716711e2949c62ae43a0a1
whk06061 <gju06061@naver.com>
2023-04-11 14:40:30 +0900

    feat: Price 클래스 구현

---
+++ app/src/main/java/woowacourse/movie/domain/Price.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.domain
+
+data class Price(val price: Int = DEFAULT) {
+    init {
+        require(price >= 0) { throw IllegalArgumentException(MINUS_ERROR) }
+    }
+
+    companion object {
+        private const val DEFAULT = 13000
+        private const val MINUS_ERROR = "가격은 음수가 될 수 없습니다."
+    }
+}
