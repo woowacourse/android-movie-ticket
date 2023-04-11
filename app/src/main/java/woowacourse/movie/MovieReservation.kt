ffe8f7f618e5551b06388dd65f17e46f04e0be6c
Doyeop Kim <dygames34@gmail.com>
2023-04-11 17:10:16 +0900

    feat: 각 액티비티의 ActionBar에서 뒤로가기 버튼 구현

--- app/src/main/java/woowacourse/movie/MovieReservation.kt
+++ app/src/main/java/woowacourse/movie/MovieReservation.kt
@@ -1,11 +1,20 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MovieReservation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_reservation)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/woowacourse/movie/ReservationResult.kt
+++ app/src/main/java/woowacourse/movie/ReservationResult.kt
@@ -1,11 +1,20 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class ReservationResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation_result)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
