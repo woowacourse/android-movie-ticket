b0d2f551ec02ad1ac74c289b86826b743f9bd0ef
briandr97 <briandr@naver.com>
2023-04-11 21:30:37 +0900

    feat(MainActivity): 메인 액티비티 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,23 @@
 package woowacourse.movie
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val movieAdapter by lazy { MovieAdapter(this) { clickBook(it) } }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ListView>(R.id.listMovie).adapter = movieAdapter
+        movieAdapter.initMovies(MovieData.movies)
+    }
+
+    private fun clickBook(movieId: Long) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("ID", movieId)
+        startActivity(intent)
     }
 }
