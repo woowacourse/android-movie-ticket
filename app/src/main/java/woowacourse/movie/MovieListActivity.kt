1b5e90cafa287ceeb947566b8d6c2494e9e6e962
changhwan <chang5hustle@gmail.com>
2023-04-11 14:36:49 +0900

    feat(movie_list_item): 리스트뷰 아이템 xml 생성

--- app/src/main/java/woowacourse/movie/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieListActivity.kt
@@ -3,9 +3,9 @@ package woowacourse.movie
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_list)
     }
 }
