adf25b7d6bae8efc154be8f5f6ea6f4e38d98fbf
whk06061 <gju06061@naver.com>
2023-04-11 17:26:26 +0900

    refactor: activity_movie_ticket.xml 글자 볼드 처리 수정

--- app/src/main/java/woowacourse/movie/MovieTicketActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieTicketActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.domain.TicketingInfo
+import java.text.DecimalFormat
 
 class MovieTicketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +25,15 @@ class MovieTicketActivity : AppCompatActivity() {
         val title = findViewById<TextView>(R.id.text_title)
         val playingDate = findViewById<TextView>(R.id.text_playing_date)
         val count = findViewById<TextView>(R.id.text_person_count)
-        val pricePayment = findViewById<TextView>(R.id.text_price)
+        val price = findViewById<TextView>(R.id.text_price)
+        val pricePayment = findViewById<TextView>(R.id.text_price_payment)
 
         title.text = info.title
         playingDate.text = info.playingDate
-        count.text = getText(R.string.normal_count).toString().format(info.count)
-        pricePayment.text = getText(R.string.price_payment).toString().format(info.price.price, info.payment)
+        count.text = info.count.toString()
+        val totalPrice = DecimalFormat("#,###").format(info.price.price * info.count)
+        price.text = totalPrice.toString()
+        pricePayment.text = getText(R.string.price_payment).toString().format(info.payment)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
