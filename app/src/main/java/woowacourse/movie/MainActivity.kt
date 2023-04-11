988d22c368ddaa2187f20d6b25093c8b32a35889
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-11 15:56:32 +0900

    feat: 영화 목록 adpater 연결해서 화면에 영화 목록 출력

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,23 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.domain.Movie
+import woowacourse.movie.domain.Ticket
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movieListView = findViewById<ListView>(R.id.movie_listView)
+
+        val movie = Movie("해리포터", 200, "dfdfdfd", R.drawable.movie_harry)
+        val ticket = Ticket(13000, Date(20230202), movie)
+        val movieListViewAdapter = MovieListViewAdapter(mutableListOf(ticket))
+
+        movieListView.adapter = movieListViewAdapter
     }
 }
