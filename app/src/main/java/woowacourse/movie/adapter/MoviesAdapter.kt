b51ad7bd3985af35100a4d1d15ad963ca164a92f
otter <a33397397@gmail.com>
2023-04-11 18:06:22 +0900

    refactor: 불필요 파일 제거

--- app/src/main/java/woowacourse/movie/adapter/MoviesAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MoviesAdapter.kt
@@ -26,15 +26,21 @@ class MoviesAdapter(
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View = LayoutInflater.from(context).inflate(R.layout.item_movie, null)
         val movieImageView: ImageView = view.findViewById(R.id.movie_image_view)
-        val screeningTimeTextView: TextView = view.findViewById(R.id.movie_screening_date_text_view)
+        val screeningDateTextView: TextView = view.findViewById(R.id.movie_screening_date_text_view)
         val runningTimeTextView: TextView = view.findViewById(R.id.movie_running_time_text_view)
 
+        // todo: 코드 줄이기
         val movie: Movie = movies[position]
 
         movie.posterImage?.let { movieImageView.setImageResource(it) }
-        screeningTimeTextView.text = "상영일: %d.%d.%d".format(movie.screeningDate.year, movie.screeningDate.monthValue, movie.screeningDate.dayOfMonth)
-        runningTimeTextView.text = "러닝타임: %d분".format(movie.runningTime)
+        screeningDateTextView.text = SCREENING_TIME.format(movie.screeningDate.year, movie.screeningDate.monthValue, movie.screeningDate.dayOfMonth)
+        runningTimeTextView.text = RUNNING_TIME.format(movie.runningTime)
 
         return view
+    }
+
+    companion object {
+        private const val SCREENING_TIME = "상영일: %d.%d.%d"
+        private const val RUNNING_TIME = "러닝타임: %d분"
     }
 }
