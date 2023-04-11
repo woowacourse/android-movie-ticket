282882bce92a26075eea63033bc9d77777d4362b
whk06061 <gju06061@naver.com>
2023-04-11 15:33:51 +0900

    refactor: TicketingInfo name->title로 수정

--- app/src/main/java/woowacourse/movie/domain/TicketingInfo.kt
+++ app/src/main/java/woowacourse/movie/domain/TicketingInfo.kt
@@ -1,3 +1,3 @@
 package woowacourse.movie.domain
 
-data class TicketingInfo(val name: String, val playingDate: String, val count: Int, val price: Price, val payment: String)
+data class TicketingInfo(val title: String, val playingDate: String, val count: Int, val price: Price, val payment: String)
