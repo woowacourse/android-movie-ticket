581286103284e8847013b77c5b0c98a65d89a32f
briandr97 <briandr@naver.com>
2023-04-11 21:27:36 +0900

    feat(Movie): Movie 변경 (이미지 추가)

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -3,9 +3,11 @@ package woowacourse.movie
 import java.time.LocalDate
 
 data class Movie(
-    val id: Int,
+    val id: Long,
     val title: String,
     val screeningDate: LocalDate,
     val runningTime: Int,
     val description: String,
+    val thumbnail: Int,
+    val poster: Int,
 )
