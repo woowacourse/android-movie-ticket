e6630a8b91e1f86fcae9cf5cab78beba545e2765
SangHyun Moon <no1msh1217@gmail.com>
2023-04-11 17:36:28 +0900

    feat: 영화 리스트뷰 연결 및 데이터 추가

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,21 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListView()
+    }
+
+    private fun setListView() {
+        val listView = findViewById<ListView>(R.id.lv_movie_list)
+        val movies = listOf(Movie(R.drawable.harry_poter, "해리포터", "2023.04.11", "123분"))
+        val adapter = MovieListAdapter(movies)
+        listView.adapter = adapter
     }
 }
