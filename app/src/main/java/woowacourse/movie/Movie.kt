9750f15a835342623ea594d0d1bbf24b38d6cd7f
briandr97 <briandr@naver.com>
2023-04-11 14:48:31 +0900

    feat(Movie): 영화 데이터 생성

---
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -0,0 +1,10 @@
+package woowacourse.movie
+
+import java.util.*
+
+data class Movie(
+    val title: String,
+    val screeningDate: Date,
+    val runningTime: Int,
+    val description: String,
+)
