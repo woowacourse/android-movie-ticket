15cf193ba78007a6b5782d5bb304bb45971f7e42
whk06061 <gju06061@naver.com>
2023-04-11 15:50:36 +0900

    feat: MovieListActivity 구현

--- app/src/main/java/woowacourse/movie/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieListActivity.kt
@@ -1,11 +1,19 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.domain.Movie
 
 class MovieListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        val movies = mutableListOf<Movie>()
+        movies.add(Movie(R.drawable.img, "해리포터와 마법사의 돌", "2024.3.1", 152, "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."))
+        val adapter = MovieListAdapter(this, movies)
+        listView.adapter = adapter
     }
 }
