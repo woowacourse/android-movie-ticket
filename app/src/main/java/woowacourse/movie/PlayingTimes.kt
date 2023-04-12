aa74fa907dc0394301e13a418fd9d348876b3d67
whk06061 <gju06061@naver.com>
2023-04-12 15:25:15 +0900

    feat: PlayingTimes 구현

---
+++ app/src/main/java/woowacourse/movie/PlayingTimes.kt
@@ -0,0 +1,29 @@
+package woowacourse.movie
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalTime
+
+class PlayingTimes(val startDate: LocalDate, val endDate: LocalDate) {
+    private val _times: MutableMap<LocalDate, List<LocalTime>> = mutableMapOf()
+    val times: Map<LocalDate, List<LocalTime>>
+        get() = _times.toMap()
+
+    init {
+        var date = startDate
+        while (!date.isEqual(endDate.plusDays(1))) {
+            _times[date] = makeTimes(date)
+            date = date.plusDays(1)
+        }
+    }
+
+    private fun makeTimes(date: LocalDate) = buildList<LocalTime> {
+        var startHour = 10
+        if (isWeekends(date.dayOfWeek)) startHour = 9
+        for (hour in startHour until 24 step 2) {
+            add(LocalTime.of(hour, 0))
+        }
+    }
+
+    private fun isWeekends(dayOfWeek: DayOfWeek): Boolean = dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
+}
---
+++ app/src/test/java/woowacourse/movie/PlayingTimesTest.kt
@@ -0,0 +1,35 @@
+package woowacourse.movie
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalTime
+
+class PlayingTimesTest {
+
+    @Test
+    fun `평일 상영일은 오전 10시부터 두 시간 간격으로 상영 시간을 생성한다`() {
+        val date = LocalDate.of(2023, 4, 11)
+        val actual = PlayingTimes(date, date).times
+        val expected = mapOf(date to makeTimes(date))
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `주말 상영일은 오전 9시부터 두 시간 간격으로 상영 시간을 생성한다`() {
+        val date = LocalDate.of(2023, 4, 15)
+        val actual = PlayingTimes(date, date).times
+        val expected = mapOf(date to makeTimes(date))
+        assertEquals(actual, expected)
+    }
+
+    private fun makeTimes(date: LocalDate) = buildList<LocalTime> {
+        var hour = 10
+        if (date.dayOfWeek == DayOfWeek.SATURDAY || date.dayOfWeek == DayOfWeek.SUNDAY) hour = 9
+        while (hour < 24) {
+            add(LocalTime.of(hour, 0))
+            hour += 2
+        }
+    }
+}
