119a0db0fd3fd61c0b345aa11c9989c68120ba91
changhwan <chang5hustle@gmail.com>
2023-04-12 16:58:57 +0900

    feat(datetime): 시간 관련 리펙토링용 class 생성

---
+++ app/src/main/java/woowacourse/movie/domain/datetime/ScreeningDateTime.kt
@@ -0,0 +1,3 @@
+package woowacourse.movie.domain.datetime
+
+class ScreeningDateTime
---
+++ app/src/main/java/woowacourse/movie/domain/datetime/ScreeningPeriod.kt
@@ -0,0 +1,72 @@
+package woowacourse.movie.domain.datetime
+
+import woowacourse.movie.domain.datetime.DayOfWeekStandard.WEEKDAY
+import woowacourse.movie.domain.datetime.DayOfWeekStandard.WEEKEND
+import java.time.DayOfWeek.FRIDAY
+import java.time.DayOfWeek.MONDAY
+import java.time.DayOfWeek.SATURDAY
+import java.time.DayOfWeek.SUNDAY
+import java.time.DayOfWeek.THURSDAY
+import java.time.DayOfWeek.TUESDAY
+import java.time.DayOfWeek.WEDNESDAY
+import java.time.LocalDate
+import java.time.LocalTime
+
+class ScreeningPeriod(val start: LocalDate, val end: LocalDate) {
+    init {
+        validatePeriod()
+    }
+
+    private fun validatePeriod() {
+        require(start < end) { SCREENING_PERIOD_INIT_ERROR }
+    }
+
+    private fun getDayOfWeekStandard(selectedDate: LocalDate): DayOfWeekStandard {
+        return when (selectedDate.dayOfWeek) {
+            MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> WEEKDAY
+            SATURDAY, SUNDAY -> WEEKEND
+        }
+    }
+
+    fun getScreeningTime(selectedDate: LocalDate): List<LocalTime> {
+        val dayOfWeekStandard: DayOfWeekStandard = getDayOfWeekStandard(selectedDate)
+        return when (dayOfWeekStandard) {
+            WEEKDAY -> WEEKDAY_TIME_TABLE
+            WEEKEND -> WEEKEND_TIME_TABLE
+        }
+    }
+
+    companion object {
+        private const val SCREENING_PERIOD_INIT_ERROR = "기간설정 단위가 올바르지 않습니다."
+        private val WEEKDAY_TIME_TABLE =
+            convertStringListToLocalTimeList(
+                listOf(
+                    "09:00",
+                    "11:00",
+                    "13:00",
+                    "15:00",
+                    "17:00",
+                    "19:00",
+                    "21:00",
+                    "23:00"
+                )
+            )
+
+        private val WEEKEND_TIME_TABLE =
+            convertStringListToLocalTimeList(
+                listOf(
+                    "10:00",
+                    "12:00",
+                    "14:00",
+                    "16:00",
+                    "18:00",
+                    "20:00",
+                    "22:00",
+                    "24:00"
+                )
+            )
+
+        private fun convertStringListToLocalTimeList(times: List<String>): List<LocalTime> =
+            times.map { LocalTime.parse(it) }
+    }
+}
