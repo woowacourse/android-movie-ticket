22fcb50e5390fff543b8394e6094042ee5b7f758
SangHyun Moon <no1msh1217@gmail.com>
2023-04-12 21:40:54 +0900

    feat: 영화 상영일을 관리하는 클래스 구현

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie
 
 import java.io.Serializable
+import java.time.LocalDate
 
 data class Movie(
     val poster: Int,
@@ -8,4 +9,6 @@ data class Movie(
     val releaseDate: String,
     val runningTime: String,
     val synopsis: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate
 ) : Serializable
