ef93679d0c9a9cccdd809809e11aaf39156a4b59
re4rk <37167652+re4rk@users.noreply.github.com>
2023-04-12 17:52:11 +0900

    feat (ScreeningDate): 날짜 및 시간을 관리하는 클래스 생성 및 구현

---
+++ app/src/main/java/woowacourse/movie/ScreeningDate.kt
@@ -0,0 +1,30 @@
+package woowacourse.movie
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
+
+class ScreeningDate(
+    private val startDate: LocalDate,
+    private val endDate: LocalDate,
+) {
+    val dateList = (0..ChronoUnit.DAYS.between(startDate, endDate))
+        .map { startDate.plusDays(it).toString() }
+
+    fun getScreeningTime(date: LocalDate): List<LocalTime> = when {
+        isWeekend(date) -> WEEKEND_SCHEDULE
+        else -> WEEKDAY_SCHEDULE
+    }
+
+    private fun isWeekend(date: LocalDate): Boolean {
+        val dayOfWeek = date.dayOfWeek
+        return dayOfWeek == java.time.DayOfWeek.SATURDAY || dayOfWeek == java.time.DayOfWeek.SUNDAY
+    }
+
+    companion object {
+        private val WEEKEND_SCHEDULE: List<LocalTime> =
+            (9..24 step 2).map { time -> LocalTime.of(time, 0) }
+        private val WEEKDAY_SCHEDULE: List<LocalTime> =
+            (10..24 step 2).map { time -> LocalTime.of(time % 24, 0) }
+    }
+}
