a255211a22f4fa73ef001094b4018343b8c6c045
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-12 20:29:48 +0900

    feat: 영화를 선택한 후 날짜와 시간을 정하는 기능 구현

--- app/src/main/java/woowacourse/movie/MovieDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieDetailActivity.kt
@@ -3,14 +3,24 @@ package woowacourse.movie
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.domain.DayOfWeek
 import woowacourse.movie.domain.Movie
+import woowacourse.movie.domain.ReservationDate
+import woowacourse.movie.domain.ReservationTime
+import woowacourse.movie.domain.RunningDate
 import woowacourse.movie.domain.Ticket
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class MovieDetailActivity : AppCompatActivity() {
@@ -20,7 +30,8 @@ class MovieDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_detail)
 
         val movie = intent.getSerializableExtra("movie") as Movie
-        var numberOfBooker = 1
+
+        setDateSpinner(movie.runningDate)
 
         val moviePoster = findViewById<ImageView>(R.id.movie_poster)
         val movieTitle = findViewById<TextView>(R.id.movie_title)
@@ -58,11 +69,70 @@ class MovieDetailActivity : AppCompatActivity() {
         }
 
         bookBtn.setOnClickListener {
+            val selectedDate = LocalDate.parse(selectDateSpinner.selectedItem.toString())
+            val selectedTime = LocalTime.parse(selectTimeSpinner.selectedItem.toString())
             val ticket =
-                Ticket(13000, LocalDateTime.of(2024, 3, 1, 3, 15), movie.title, numberOfBooker)
+                Ticket(
+                    13000,
+                    LocalDateTime.of(selectedDate, selectedTime),
+                    movie.title,
+                    numberOfBooker,
+                )
             val intent = Intent(this, TicketActivity::class.java)
             intent.putExtra("ticket", ticket)
             startActivity(intent)
+        }
+    }
+
+    fun setDateSpinner(date: RunningDate) {
+        val dateAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            ReservationDate(date).getScreeningDays(),
+        )
+
+        dateAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        selectDateSpinner.adapter = dateAdapter
+        selectDateSpinner.setSelection(dateSpinnerPosition)
+
+        selectDateSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                setTimeSpinner(LocalDate.parse(selectDateSpinner.getItemAtPosition(position) as String))
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+    }
+
+    fun setTimeSpinner(selectedDay: LocalDate) {
+        val timeAdapter = ArrayAdapter(
+            this@MovieDetailActivity,
+            android.R.layout.simple_spinner_item,
+            ReservationTime(DayOfWeek.checkDayOfWeek(selectedDay)).getScreeningTimes(),
+        )
+
+        timeAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        selectTimeSpinner.adapter = timeAdapter
+        selectTimeSpinner.setSelection(timeSpinnerPosition)
+
+        selectTimeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                timeSpinnerPosition = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
         }
     }
 }
