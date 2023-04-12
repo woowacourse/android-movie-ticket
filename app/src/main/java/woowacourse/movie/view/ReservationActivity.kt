89ad568a7b67acc3cf59656cb823722b5bbeeba7
ki960213 <ki960213@github.com>
2023-04-12 14:36:46 +0900

    feat(activity_reservation): 예약 액티비티의 레이아웃 구현

---
+++ app/src/main/java/woowacourse/movie/view/ReservationActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import woowacourse.movie.R
+
+class ReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation)
+    }
+}
