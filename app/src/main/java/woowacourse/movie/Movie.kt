b8c9431689ab70f18b95eb15709671da7187c03a
SangHyun Moon <no1msh1217@gmail.com>
2023-04-12 17:02:36 +0900

    feat: movie 임시 데이터 중 시놉시스 데이터 추가

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -1,8 +1,11 @@
 package woowacourse.movie
 
+import java.io.Serializable
+
 data class Movie(
     val poster: Int,
     val title: String,
     val releaseDate: String,
     val runningTime: String,
-)
+    val synopsis: String,
+) : Serializable
