84169f668a6016e7bef17300e16029cb8ffe676b
ki960213 <ki960213@github.com>
2023-04-12 17:13:46 +0900

    feat(ReservationCompletedActivity): 예매 완료 액티비티에서 받은 예매 데이터를 보여 주는 기능 구현

--- app/src/main/java/woowacourse/movie/view/ReservationCompletedActivity.kt
+++ app/src/main/java/woowacourse/movie/view/ReservationCompletedActivity.kt
@@ -1,12 +1,41 @@
 package woowacourse.movie.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import woowacourse.movie.R
+import woowacourse.movie.domain.Reservation
+import java.text.DecimalFormat
 
 class ReservationCompletedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation_completed)
+
+        val reservation = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra(ReservationActivity.RESERVATION, Reservation::class.java)
+        } else {
+            intent.getSerializableExtra(ReservationActivity.RESERVATION) as? Reservation
+        }
+        requireNotNull(reservation) { "인텐트로 받아온 데이터가 널일 수 없습니다." }
+        initViewData(reservation)
+    }
+
+    private fun initViewData(reservation: Reservation) {
+        val titleView = findViewById<TextView>(R.id.movie_title)
+        titleView.text = reservation.movieTitle
+        val screeningDateView = findViewById<TextView>(R.id.movie_screening_date)
+        screeningDateView.text = reservation.movieScreeningDate.toString()
+        val peopleCountView = findViewById<TextView>(R.id.people_count)
+        peopleCountView.text = getString(R.string.reservation_people_count_format)
+            .format(getString(R.string.general_person), reservation.peopleCount)
+        val totalPriceView = findViewById<TextView>(R.id.total_price)
+        totalPriceView.text =
+            getString(R.string.total_price_format).format(DECIMAL_FORMAT.format(reservation.totalReservationFee.amount))
+    }
+
+    companion object {
+        private val DECIMAL_FORMAT = DecimalFormat("#,###")
     }
 }
