edfdd1f20d03487ef2349354e392463fd7099a1b
otter <a33397397@gmail.com>
2023-04-12 11:37:02 +0900

    feat(movie): 티켓 개수 선택 기능 구현

--- app/src/main/java/woowacourse/movie/ReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/ReservationActivity.kt
@@ -1,8 +1,10 @@
 package woowacourse.movie
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,22 +12,45 @@ import domain.Movie
 import java.io.Serializable
 
 class ReservationActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
 
-        val posterImageView: ImageView = findViewById(R.id.movie_image_view1)
-        val nameTextView: TextView = findViewById(R.id.movie_name_text_view1)
-        val screeningDateTextView: TextView = findViewById(R.id.movie_screening_date_text_view1)
-        val runningTimeTextView: TextView = findViewById(R.id.movie_running_time_text_view1)
-        val descriptionTextView: TextView = findViewById(R.id.movie_description_text_view1)
+        val posterImageView: ImageView = findViewById(R.id.reservation_movie_image_view)
+        val nameTextView: TextView = findViewById(R.id.reservation_movie_name_text_view)
+        val screeningDateTextView: TextView =
+            findViewById(R.id.reservation_movie_screening_date_text_view)
+        val runningTimeTextView: TextView =
+            findViewById(R.id.reservation_movie_running_time_text_view)
+        val descriptionTextView: TextView =
+            findViewById(R.id.reservation_movie_description_text_view)
 
         intent.customGetSerializable<Movie>("movie")?.let {
             it.posterImage?.let { id -> posterImageView.setImageResource(id) }
             nameTextView.text = it.name
-            screeningDateTextView.text = SCREENING_TIME.format(it.screeningDate.year, it.screeningDate.monthValue, it.screeningDate.dayOfMonth)
+            screeningDateTextView.text = SCREENING_TIME.format(
+                it.screeningDate.year,
+                it.screeningDate.monthValue,
+                it.screeningDate.dayOfMonth
+            )
             runningTimeTextView.text = RUNNING_TIME.format(it.runningTime)
             descriptionTextView.text = it.description
+        }
+
+        val minusButton: Button = findViewById(R.id.reservation_ticket_count_minus_button)
+        val plusButton: Button = findViewById(R.id.reservation_ticket_count_plus_button)
+        val ticketCountTextView: TextView = findViewById(R.id.reservation_ticket_count_text_view)
+
+        minusButton.setOnClickListener {
+            val ticketCount = ticketCountTextView.text.toString().toInt()
+            if (ticketCount > 1) {
+                ticketCountTextView.text = (ticketCount - 1).toString()
+            }
+        }
+        plusButton.setOnClickListener {
+            val ticketCount = ticketCountTextView.text.toString().toInt()
+            ticketCountTextView.text = (ticketCount + 1).toString()
         }
     }
 
