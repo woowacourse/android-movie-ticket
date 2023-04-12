5cc667dfd2356bcec7f9e6c4e23bc2ecfc903990
SangHyun Moon <no1msh1217@gmail.com>
2023-04-12 17:05:47 +0900

    feat: 예매 완료 화면 기능 추가

--- app/src/main/java/woowacourse/movie/BookCompleteActivity.kt
+++ app/src/main/java/woowacourse/movie/BookCompleteActivity.kt
@@ -1,11 +1,22 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class BookCompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_complete)
+
+        val movieData = intent.customGetSerializable<Movie>("movieData")
+        val ticketCount = intent.getIntExtra("ticketCount", 0)
+
+        findViewById<TextView>(R.id.tv_book_movie_title).text = movieData?.title
+        findViewById<TextView>(R.id.tv_book_date).text = movieData?.releaseDate
+        findViewById<TextView>(R.id.tv_book_person_count).text =
+            getString(R.string.book_person_count).format(ticketCount)
+        findViewById<TextView>(R.id.tv_book_total_pay).text =
+            getString(R.string.book_total_pay).format(ticketCount * 13000)
     }
 }
