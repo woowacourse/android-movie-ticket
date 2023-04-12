7c42e7d73a0f8b590147a6839785fe216fecae81
changhwan <chang5hustle@gmail.com>
2023-04-12 17:20:27 +0900

    feat(ScreeningDateTime): class 생성

--- app/src/main/java/woowacourse/movie/domain/datetime/ScreeningDateTime.kt
+++ app/src/main/java/woowacourse/movie/domain/datetime/ScreeningDateTime.kt
@@ -1,3 +1,17 @@
 package woowacourse.movie.domain.datetime
 
-class ScreeningDateTime
+import java.time.LocalDateTime
+
+class ScreeningDateTime(val value: LocalDateTime, private val screeningPeriod: ScreeningPeriod) {
+    init {
+        validateDateTime()
+    }
+
+    private fun validateDateTime() {
+        require(value.toLocalDate() in screeningPeriod.start..screeningPeriod.end) { SELECTED_SCREENING_DATE_TIME_ERROR }
+    }
+
+    companion object {
+        private const val SELECTED_SCREENING_DATE_TIME_ERROR = "선택된 날짜가 영화 상영 기간 범위에서 벗어났습니다."
+    }
+}
