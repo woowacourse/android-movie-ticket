8d83daa751626611d11ee7f420ba5dab3671edc5
Sujin Kim <sujinkim919@gmail.com>
2023-04-12 14:51:54 +0900

    feat: 메인 페이지에서 영화 상세 페이지로 이동 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,5 +1,6 @@
 package woowacourse.movie
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,7 +20,23 @@ class MainActivity : AppCompatActivity() {
             "직업도 없이 허름한 반지하에 사는 기택 가족에게 돈을 벌 기회가 찾아온다. 친구의 소개로 부잣집 딸 다혜의 과외 선생님을 맡게 된 기택의 아들, 기우는 기대감에 부푼 채 글로벌 IT기업을 이끄는 박 사장의 저택에 들어간다."
         )
         val movies = listOf(movie)
-        val movieList = findViewById<ListView>(R.id.main_movie_list)
-        movieList.adapter = MovieListAdapter(this, movies)
+
+        val moviesView = findViewById<ListView>(R.id.main_movie_list)
+        moviesView.adapter = MovieListAdapter(
+            this, movies,
+            object : MovieListAdapter.ItemClickListener {
+                override fun onItemClick(position: Int) {
+                    moveToDetailActivity()
+                }
+            }
+        )
+        moviesView.setOnItemClickListener { parent, view, position, id ->
+            moveToDetailActivity()
+        }
+    }
+
+    private fun moveToDetailActivity() {
+        val intent = Intent(this, MovieDetailActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/woowacourse/movie/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/MovieListAdapter.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import woowacourse.movie.domain.Movie
 
 class MovieListAdapter(
     private val context: Context,
-    private val movies: List<Movie>
+    private val movies: List<Movie>,
+    private val itemClickListener: ItemClickListener,
 ) : BaseAdapter() {
+    interface ItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
     override fun getCount(): Int = movies.size
 
     override fun getItem(position: Int): Any = movies[position]
@@ -27,6 +33,7 @@ class MovieListAdapter(
         itemView.findViewById<TextView>(R.id.item_title).text = movie.title
         itemView.findViewById<TextView>(R.id.item_date).text = "상영일: ${movie.date.year}.${movie.date.month}.${movie.date.day}"
         itemView.findViewById<TextView>(R.id.item_running_time).text = "러닝타임: ${movie.time}분"
+        itemView.findViewById<Button>(R.id.item_booking_button).setOnClickListener { itemClickListener.onItemClick(position) }
 
         return itemView
     }
