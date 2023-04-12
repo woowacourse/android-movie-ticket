c05ff0e24628565419b4f5d89964ea72e7abe0aa
whk06061 <gju06061@naver.com>
2023-04-12 16:30:44 +0900

    refactor: PlayintDate, PlayingTime 따로 받도록 수정

--- app/src/main/java/woowacourse/movie/domain/TicketingInfo.kt
+++ app/src/main/java/woowacourse/movie/domain/TicketingInfo.kt
@@ -1,3 +1,3 @@
 package woowacourse.movie.domain
 
-data class TicketingInfo(val title: String, val playingDate: String, val count: Int, val price: Price, val payment: String) : java.io.Serializable
+data class TicketingInfo(val title: String, val playingDate: String, val playingTime: String, val count: Int, val price: Price, val payment: String) : java.io.Serializable
