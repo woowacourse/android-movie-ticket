e12243403a1b7b4f87a99b46fa6b71ba51de85d2
tmdgh1592 <tmdgh1592@naver.com>
2023-04-12 14:54:27 +0900

    refactor: 영화 날짜 정보 프로퍼티 타입을 Date에서 LocalDate으로 변경

--- app/src/main/java/woowacourse/movie/data/Movie.kt
+++ app/src/main/java/woowacourse/movie/data/Movie.kt
@@ -1,28 +1,25 @@
 package woowacourse.movie.data
 
-import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
 import woowacourse.movie.R
 import java.io.Serializable
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class Movie(
     val title: String,
-    val date: Date,
+    val date: LocalDate,
     val runningTime: Int,
     val introduce: String,
     @DrawableRes val thumbnail: Int,
 ) : Serializable {
-    @SuppressLint("SimpleDateFormat")
-    val formattedDate: String = SimpleDateFormat("yyyy.MM.dd").format(date)
+    val formattedDate: String = date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
 
     companion object {
         fun provideDummy(): List<Movie> = listOf(
             Movie(
                 "해리 포터와 마법사의 돌",
-                Date(Calendar.getInstance().apply { set(2024, Calendar.MARCH, 1) }.timeInMillis),
+                LocalDate.of(2024, 3, 1),
                 152,
                 "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
                 R.drawable.img_default_movie_thumbnail
