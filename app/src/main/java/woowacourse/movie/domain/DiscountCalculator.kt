c96771343e98c033792e31d895d263aec36cde79
mendel <rhthrhrl0@naver.com>
2023-04-13 11:23:40 +0900

    refactor(DiscountCalculator): 장 당 할인으로 수정

--- app/src/main/java/woowacourse/movie/domain/DiscountCalculator.kt
+++ app/src/main/java/woowacourse/movie/domain/DiscountCalculator.kt
@@ -6,21 +6,20 @@ import java.time.LocalDateTime
 
 class DiscountCalculator {
     fun discount(count: Count, dateTime: LocalDateTime): Money {
-        var money = count.value * TICKET_MONEY
-        money = when (dateTime.dayOfMonth) {
-            10, 20, 30 -> (money * MOVIE_DAY_DISCOUNT).toInt()
-            else -> money
+        val money = when (dateTime.dayOfMonth) {
+            10, 20, 30 -> TICKET_MONEY * MOVIE_DAY_DISCOUNT
+            else -> TICKET_MONEY
         }
         return when (dateTime.hour) {
-            in TIME_MORNING_NIGHT -> Money(money - TIME_DISCOUNT)
-            else -> Money(money)
+            in TIME_MORNING_NIGHT -> (money - TIME_DISCOUNT) * count
+            else -> money * count
         }
     }
 
     companion object {
-        private const val TICKET_MONEY = 13000
-        private const val MOVIE_DAY_DISCOUNT = 0.9
-        private const val TIME_DISCOUNT = 2000
+        private val TICKET_MONEY = Money(13000)
+        private const val MOVIE_DAY_DISCOUNT = 0.9f
+        private val TIME_DISCOUNT = Money(2000)
         private val TIME_MORNING_NIGHT = listOf(0, 9, 10, 11, 20, 21, 22, 23)
     }
 }
--- app/src/main/java/woowacourse/movie/entity/Money.kt
+++ app/src/main/java/woowacourse/movie/entity/Money.kt
@@ -6,6 +6,19 @@ value class Money(val value: Int) {
         require(value > 0) { MONEY_ERROR_MESSAGE }
     }
 
+    operator fun times(other: Float): Money {
+        val result = (value * other).toInt()
+        return Money(result)
+    }
+
+    operator fun times(other: Count): Money {
+        return Money(value * other.value)
+    }
+
+    operator fun minus(other: Money): Money {
+        return Money(value - other.value)
+    }
+
     companion object {
         private const val MONEY_ERROR_MESSAGE = "[ERROR} 예매 금액은 0원보다 커야 합니다."
     }
