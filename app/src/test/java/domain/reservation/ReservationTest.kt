e05401cad4f7b50da99dbd8f55ca04b234c0bbd7
otter <a33397397@gmail.com>
2023-04-13 11:48:33 +0900

    refactor(reservation): 도메인 변경에 따른 테스트 변경 반영

--- app/src/test/java/domain/reservation/ReservationTest.kt
+++ app/src/test/java/domain/reservation/ReservationTest.kt
@@ -1,25 +1,31 @@
-package domain
+package domain.reservation
 
 import domain.movie.Movie
+import domain.movie.Name
+import domain.movie.ScreeningPeriod
 import domain.payment.PaymentAmount
-import domain.reservation.Reservation
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 class ReservationTest {
     @Test
     fun `영화와 티켓 개수를 받아서 예매 정보를 반환한다`() {
         val movie = Movie(
-            name = "해리포터",
+            name = Name("해리포터"),
             posterImage = null,
-            screeningPeriod = LocalDate.of(2000, 10, 1),
+            screeningPeriod = ScreeningPeriod(
+                LocalDate.of(2000, 10, 1),
+                LocalDate.of(2000, 10, 30)
+            ),
             runningTime = 120,
             description = "마법영화"
         )
-        val reservation = Reservation.from(movie, 3)
+        val reservation = Reservation.from(movie, 3, LocalDateTime.of(2000, 10, 1, 13, 0))
         val result = Reservation(
             movie = movie,
+            screeningDateTime = LocalDateTime.of(2000, 10, 1, 13, 0),
             ticketCount = 3,
             paymentAmount = PaymentAmount(39000),
         )
