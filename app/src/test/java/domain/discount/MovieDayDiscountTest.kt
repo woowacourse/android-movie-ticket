9ac8805ccd6d1635a62c0006b3027d4de17dcf7e
otter <a33397397@gmail.com>
2023-04-13 13:46:02 +0900

    test(discount): 무비데이 할인 기능 검증 테스트 추가

---
+++ app/src/test/java/domain/discount/MovieDayDiscountTest.kt
@@ -0,0 +1,63 @@
+package domain.discount
+
+import domain.payment.PaymentAmount
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDateTime
+
+internal class MovieDayDiscountTest {
+
+    lateinit var movieDayDiscount: MovieDayDiscount
+    val paymentAmount: PaymentAmount = PaymentAmount(13000)
+
+    @Before
+    fun setUp() {
+        movieDayDiscount = MovieDayDiscount()
+    }
+
+    @Test
+    fun `10일인_경우_10%_할인이_적용된다`() {
+        val resultDiscountedPaymentAmount = movieDayDiscount.getPaymentAmountResult(
+            paymentAmount,
+            LocalDateTime.of(2023, 4, 10, 13, 0)
+        )
+        val expectedDiscountedPaymentAmount = PaymentAmount(11700)
+
+        Assert.assertEquals(resultDiscountedPaymentAmount, expectedDiscountedPaymentAmount)
+    }
+
+    @Test
+    fun `20일인_경우_10%_할인이_적용된다`() {
+        val resultDiscountedPaymentAmount = movieDayDiscount.getPaymentAmountResult(
+            paymentAmount,
+            LocalDateTime.of(2023, 4, 20, 13, 0)
+        )
+        val expectedDiscountedPaymentAmount = PaymentAmount(11700)
+
+        Assert.assertEquals(resultDiscountedPaymentAmount, expectedDiscountedPaymentAmount)
+    }
+
+    @Test
+    fun `30일_경우_10%_할인이_적용된다`() {
+        val resultDiscountedPaymentAmount = movieDayDiscount.getPaymentAmountResult(
+            paymentAmount,
+            LocalDateTime.of(2023, 4, 30, 13, 0)
+        )
+        val expectedDiscountedPaymentAmount = PaymentAmount(11700)
+
+        Assert.assertEquals(resultDiscountedPaymentAmount, expectedDiscountedPaymentAmount)
+    }
+
+    @Test
+    fun `10일_20일_30일이_아닌_경우_할인이_적용되지_않는다`() {
+        val paymentAmount = PaymentAmount(13000)
+        val resultDiscountedPaymentAmount = movieDayDiscount.getPaymentAmountResult(
+            paymentAmount,
+            LocalDateTime.of(2023, 4, 13, 15, 0)
+        )
+        val expectedDiscountedPaymentAmount = PaymentAmount(13000)
+
+        Assert.assertEquals(resultDiscountedPaymentAmount, expectedDiscountedPaymentAmount)
+    }
+}
