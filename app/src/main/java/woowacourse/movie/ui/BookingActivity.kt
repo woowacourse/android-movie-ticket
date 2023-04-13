b1609015f45a2cd6d001b49d474ebdb2d92c3fae
briandr97 <briandr@naver.com>
2023-04-13 15:59:20 +0900

    feat(AppBar): 액션 바의 뒤로 가기 버튼 기능 재정의

--- app/src/main/java/woowacourse/movie/ui/BookingActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/BookingActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
@@ -42,6 +43,14 @@ class BookingActivity : AppCompatActivity() {
         initView(movie)
         gatherClickListeners()
         initDateSpinnerSelectedListener(movie)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun restoreData(savedInstanceState: Bundle?) {
--- app/src/main/java/woowacourse/movie/ui/CompletedActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/CompletedActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
@@ -24,6 +25,14 @@ class CompletedActivity : AppCompatActivity() {
             intent.getParcelableExtra(TICKET) ?: throw IllegalArgumentException()
         }
         initView(ticket)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initView(ticket: Ticket) {
