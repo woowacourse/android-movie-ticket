558d28e98a05cc89750997260e1788d3989a04cf
Doyeop Kim <dygames34@gmail.com>
2023-04-13 15:34:56 +0900

    fix: 회전시 카운터 값이 유지되지 않던 오류 수정

--- app/src/main/java/woowacourse/movie/SaveStateCounter.kt
+++ app/src/main/java/woowacourse/movie/SaveStateCounter.kt
@@ -14,6 +14,7 @@ class SaveStateCounter(val counter: Counter, override val saveStateKey: String) 
     override fun load(savedInstanceState: Bundle?) {
         if (savedInstanceState != null) {
             counter.count = savedInstanceState.getInt(saveStateKey)
+            applyToView()
         }
     }
 
