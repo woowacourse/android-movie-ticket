fe771afd2edfb4a462eda6874b2c432e4954ab75
tmdgh1592 <tmdgh1592@naver.com>
2023-04-13 17:37:44 +0900

    refactor: 영화 리스트 어댑터에 ViewHolder 패턴 적용

--- app/src/main/java/woowacourse/movie/view/adapter/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/view/adapter/MovieListAdapter.kt
@@ -1,6 +1,5 @@
 package woowacourse.movie.view.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,23 +18,46 @@ class MovieListAdapter(
 
     override fun getItemId(position: Int): Long = position.toLong()
 
-    @SuppressLint("ViewHolder")
-    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
-        val context = parent?.context
-        val binding = ItemMovieBinding.inflate(LayoutInflater.from(context), parent, false)
-        with(movies[position]) {
-            binding.ivPoster.setImageResource(thumbnail)
-            binding.tvTitle.text = title
-            binding.tvDate.text = context?.getString(
-                R.string.movie_release_date,
-                startDate.formattedDate,
-                endDate.formattedDate
-            )
-            binding.tvRunningTime.text =
-                context?.getString(R.string.movie_running_time, runningTime)
-            binding.btnBook.setOnClickListener { onBookClickListener.onClick(this) }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view: View? = convertView
+        val viewHolder: ViewHolder
+
+        if (view == null) {
+            val binding =
+                ItemMovieBinding.inflate(LayoutInflater.from(parent?.context), parent, false)
+            view = binding.root
+            viewHolder = ViewHolder(binding)
+            view.tag = viewHolder
+        } else {
+            viewHolder = view.tag as ViewHolder
         }
-        return binding.root
+
+        viewHolder.bind(movies[position])
+        return view
+    }
+
+    private inner class ViewHolder(private val binding: ItemMovieBinding) {
+        private val ivPoster = binding.ivPoster
+        private val tvTitle = binding.tvTitle
+        private val tvDate = binding.tvDate
+        private val tvRunningTime = binding.tvRunningTime
+        private val btnBook = binding.btnBook
+
+        fun bind(item: Movie) {
+            val context = binding.root.context
+            with(item) {
+                ivPoster.setImageResource(thumbnail)
+                tvTitle.text = title
+                tvDate.text = context.getString(
+                    R.string.movie_release_date,
+                    startDate.formattedDate,
+                    endDate.formattedDate
+                )
+                tvRunningTime.text =
+                    context.getString(R.string.movie_running_time, runningTime)
+                btnBook.setOnClickListener { onBookClickListener.onClick(this) }
+            }
+        }
     }
 
     interface OnBookClickListener {
