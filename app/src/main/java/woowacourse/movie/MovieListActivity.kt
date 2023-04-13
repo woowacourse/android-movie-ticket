520d74b74c93b45344505532052af6ae7d29e079
whk06061 <gju06061@naver.com>
2023-04-13 11:06:30 +0900

    refactor: 영화 더미 데이터 가져오는 함수 분리

--- app/src/main/java/woowacourse/movie/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieListActivity.kt
@@ -12,6 +12,11 @@ class MovieListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_list)
 
         val listView = findViewById<ListView>(R.id.list_view)
+        val adapter = MovieListAdapter(this, getMovieData())
+        listView.adapter = adapter
+    }
+
+    private fun getMovieData(): List<Movie> {
         val movies = mutableListOf<Movie>()
         movies.add(
             Movie(
@@ -22,7 +27,6 @@ class MovieListActivity : AppCompatActivity() {
                 "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
             )
         )
-        val adapter = MovieListAdapter(this, movies)
-        listView.adapter = adapter
+        return movies
     }
 }
