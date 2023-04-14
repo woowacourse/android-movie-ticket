3dc5f84a4a6a20647c2a21e8208b5e3a72cf4224
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-14 15:33:35 +0900

    style(MovieDayDiscount): return문을 중괄호 안에 넣기

--- app/src/main/java/domain/discount/MovieDayDiscount.kt
+++ app/src/main/java/domain/discount/MovieDayDiscount.kt
@@ -5,12 +5,16 @@ import java.time.LocalDateTime
 
 class MovieDayDiscount : DiscountRule {
     override fun getPaymentAmountResult(paymentAmount: PaymentAmount, screeningDateTime: LocalDateTime): PaymentAmount {
-        if (!isDiscountCondition(screeningDateTime)) return paymentAmount
+        if (!isDiscountCondition(screeningDateTime)) {
+            return paymentAmount
+        }
         return PaymentAmount((paymentAmount.value * DISCOUNT_PERCENTAGE).toInt())
     }
 
     private fun isDiscountCondition(screeningDateTime: LocalDateTime): Boolean {
-        if (screeningDateTime.dayOfMonth in MOVIE_DAYS) return true
+        if (screeningDateTime.dayOfMonth in MOVIE_DAYS) {
+            return true
+        }
         return false
     }
 
