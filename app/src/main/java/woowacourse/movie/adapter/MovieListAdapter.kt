452126cc48e4768f1a3c87d8fa5292f15e74ef48
hyemdooly <hyemdooly@gmail.com>
2023-04-14 15:10:06 +0900

    refactor: ViewHolder 필드들 nullable인 것 수정

--- app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
@@ -36,26 +36,25 @@ class MovieListAdapter(private val context: Context, private val movies: List<Mo
         setViewHolder(holder, movie)
         return view
     }
-    private fun getViewHolder(view: View): ViewHolder {
-        val holder = ViewHolder()
-        holder.image = view.findViewById(R.id.img_movie)
-        holder.title = view.findViewById(R.id.text_title)
-        holder.playingDate = view.findViewById(R.id.text_playing_date)
-        holder.runningTime = view.findViewById(R.id.text_running_time)
-        holder.ticketingButton = view.findViewById(R.id.btn_ticketing)
 
-        return holder
-    }
+    private fun getViewHolder(view: View): ViewHolder = ViewHolder(
+        view.findViewById(R.id.img_movie),
+        view.findViewById(R.id.text_title),
+        view.findViewById(R.id.text_playing_date),
+        view.findViewById(R.id.text_running_time),
+        view.findViewById(R.id.btn_ticketing)
+    )
+
 
     private fun setViewHolder(holder: ViewHolder, movie: Movie) {
-        holder.image?.setImageResource(movie.image)
-        holder.title?.text = movie.title
-        holder.playingDate?.text = getString(R.string.playing_time).format(
+        holder.image.setImageResource(movie.image)
+        holder.title.text = movie.title
+        holder.playingDate.text = getString(R.string.playing_time).format(
             Formatter.dateFormat(movie.playingTimes.startDate),
             Formatter.dateFormat(movie.playingTimes.endDate)
         )
-        holder.runningTime?.text = getString(R.string.running_time).format(movie.runningTime)
-        holder.ticketingButton?.setOnClickListener {
+        holder.runningTime.text = getString(R.string.running_time).format(movie.runningTime)
+        holder.ticketingButton.setOnClickListener {
             val intent = Intent(context, MovieDetailActivity::class.java)
             intent.putExtra(MOVIE_KEY, movie)
             context.startActivity(intent)
@@ -63,13 +62,13 @@ class MovieListAdapter(private val context: Context, private val movies: List<Mo
     }
 
     private fun getString(string: Int): String = context.getString(string)
-    private class ViewHolder {
-        var image: ImageView? = null
-        var title: TextView? = null
-        var playingDate: TextView? = null
-        var runningTime: TextView? = null
-        var ticketingButton: Button? = null
-    }
+    private class ViewHolder(
+        val image: ImageView,
+        val title: TextView,
+        val playingDate: TextView,
+        val runningTime: TextView,
+        val ticketingButton: Button
+    )
 
     companion object {
         private const val MOVIE_KEY = "movie"
