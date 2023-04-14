7d387a162c31728204089e5d834cbf769f51a00d
Doyeop Kim <dygames34@gmail.com>
2023-04-14 17:14:27 +0900

    refactor: Bundle의 확장함수를 제공하는 파일 이름 변경

--- app/src/main/java/woowacourse/movie/BundleExtensions.kt
+++ app/src/main/java/woowacourse/movie/BundleExtensions.kt
@@ -8,6 +8,6 @@ inline fun <reified T : Serializable> Bundle.getSerializable(key: String): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getSerializable(key, T::class.java)
     } else {
-        getSerializable(key) as T
+        getSerializable(key) as? T
     }
 }
