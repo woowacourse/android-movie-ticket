d36f0b8da975af92979095c863ec725093e3eda0
tmdgh1592 <tmdgh1592@naver.com>
2023-04-14 17:23:19 +0900

    refactor: bundle extension 파일 분리

---
+++ app/src/main/java/woowacourse/movie/presentation/extensions/BundleExt.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.presentation.extensions
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+
+inline fun <reified T : Parcelable> Bundle.getParcelableExtraCompat(key: String): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelable(key, T::class.java)
+    } else {
+        getParcelable(key) as T?
+    }
--- app/src/main/java/woowacourse/movie/presentation/extensions/IntentExt.kt
+++ app/src/main/java/woowacourse/movie/presentation/extensions/IntentExt.kt
@@ -2,7 +2,6 @@ package woowacourse.movie.presentation.extensions
 
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
 import android.os.Parcelable
 
 inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? =
@@ -10,11 +9,4 @@ inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String)
         getParcelableExtra(key, T::class.java)
     } else {
         getParcelableExtra(key) as T?
-    }
-
-inline fun <reified T : Parcelable> Bundle.getParcelableExtraCompat(key: String): T? =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelable(key, T::class.java)
-    } else {
-        getParcelable(key) as T?
     }
