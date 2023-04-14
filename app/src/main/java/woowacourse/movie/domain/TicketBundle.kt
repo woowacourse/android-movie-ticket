45c67a1a0e81accbf8d3607896295080a179b1fa
SangHyun Moon <no1msh1217@gmail.com>
2023-04-14 17:24:06 +0900

    refactor: 부생성자 최적화

--- app/src/main/java/woowacourse/movie/domain/TicketBundle.kt
+++ app/src/main/java/woowacourse/movie/domain/TicketBundle.kt
@@ -1,13 +1,8 @@
 package woowacourse.movie.domain
 
 class TicketBundle(private val tickets: List<Ticket>) {
-
     constructor(count: Int) : this(
-        mutableListOf<Ticket>().apply {
-            repeat(count) {
-                add(Ticket())
-            }
-        }.toList()
+        List(count) { Ticket() }
     )
 
     fun calculateTotalPrice(date: String, time: String): Int = tickets.sumOf {
