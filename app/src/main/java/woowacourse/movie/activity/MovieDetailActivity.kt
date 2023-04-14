77afa958224e205474262edb39af94cab7a02cb0
hyemdooly <hyemdooly@gmail.com>
2023-04-14 15:45:04 +0900

    refactor: PlayingTimes 리팩터링

--- app/src/main/java/woowacourse/movie/activity/MovieDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MovieDetailActivity.kt
@@ -3,8 +3,6 @@ package woowacourse.movie.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
--- app/src/main/java/woowacourse/movie/model/PlayingTimes.kt
+++ app/src/main/java/woowacourse/movie/model/PlayingTimes.kt
@@ -4,26 +4,43 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalTime
 
-class PlayingTimes(val startDate: LocalDate, val endDate: LocalDate) : java.io.Serializable {
-    private val _times: MutableMap<LocalDate, List<LocalTime>> = mutableMapOf()
+class PlayingTimes(startDate: LocalDate, endDate: LocalDate) : java.io.Serializable {
+    private val _times: Map<LocalDate, List<LocalTime>> = makeDates(startDate, endDate).associateWith { date -> makeTimes(date) }
     val times: Map<LocalDate, List<LocalTime>>
         get() = _times.toMap()
+    val startDate: LocalDate
+        get() = times.keys.max()
+    val endDate: LocalDate
+        get() = times.keys.min()
 
-    init {
+    private fun makeDates(startDate: LocalDate, endDate: LocalDate): List<LocalDate> {
         var date = startDate
-        while (!date.isEqual(endDate.plusDays(1))) {
-            _times[date] = makeTimes(date)
-            date = date.plusDays(1)
+        return buildList {
+            while (date.isBefore(endDate.plusDays(ONE_DAY.toLong()))) {
+                add(date)
+                date = date.plusDays(ONE_DAY.toLong())
+            }
         }
     }
 
-    private fun makeTimes(date: LocalDate) = buildList<LocalTime> {
-        var startHour = 10
-        if (isWeekends(date.dayOfWeek)) startHour = 9
-        for (hour in startHour until 24 step 2) {
-            add(LocalTime.of(hour, 0))
-        }
+    private fun makeTimes(date: LocalDate): List<LocalTime> {
+        val startHour = getStartHour(date)
+        return (startHour until MAX_HOUR step STEP).map { LocalTime.of(it, MINUTE_DEFAULT) }
+    }
+
+    private fun getStartHour(date: LocalDate): Int {
+        if (isWeekends(date.dayOfWeek)) return WEEKEND_START_HOUR
+        return WEEKDAY_START_HOUR
     }
 
     private fun isWeekends(dayOfWeek: DayOfWeek): Boolean = dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
+
+    companion object {
+        private const val WEEKEND_START_HOUR = 9
+        private const val WEEKDAY_START_HOUR = 10
+        private const val MINUTE_DEFAULT = 0
+        private const val MAX_HOUR = 24
+        private const val STEP = 2
+        private const val ONE_DAY = 1
+    }
 }
