00395da08e540ae251d55a2c703641a899f79215
ki960213 <ki960213@github.com>
2023-04-14 16:32:39 +0900

    refactor(Reservation): 예매 클래스에서 에러 메세지를 문자열 템플릿을 사용한 문자열로 변경

--- app/src/main/java/woowacourse/movie/domain/Reservation.kt
+++ app/src/main/java/woowacourse/movie/domain/Reservation.kt
@@ -23,7 +23,8 @@ data class Reservation(
         const val MIN_PEOPLE_COUNT = 1
         const val MAX_PEOPLE_COUNT = 200
         private const val TICKET_PRICE = 13_000
-        private const val PEOPLE_COUNT_RANGE_ERROR = "[ERROR] 예매 인원은 최소 1명 이상 최대 200명 이하여야 합니다."
+        private const val PEOPLE_COUNT_RANGE_ERROR =
+            "[ERROR] 예매 인원은 최소 ${MIN_PEOPLE_COUNT}명 이상 최대 ${MAX_PEOPLE_COUNT}명 이하여야 합니다."
         private const val SCREENING_DATE_RANGE_ERROR = "[ERROR] 예매할 날짜가 상영 기간이 아닙니다."
     }
 }
