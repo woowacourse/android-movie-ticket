2e672941e89d1b0ca7042ec0b7874edff5477ca6
hyemdooly <hyemdooly@gmail.com>
2023-04-14 16:39:46 +0900

    test: DiscountPolicy 클래스 변경으로 인한 테스트 수정 및 추가

--- app/src/test/java/woowacourse/movie/TicketingInfoTest.kt
+++ app/src/test/java/woowacourse/movie/TicketingInfoTest.kt
@@ -4,16 +4,30 @@ import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import woowacourse.movie.model.Price
 import woowacourse.movie.model.TicketingInfo
+import woowacourse.movie.model.policy.DiscountPolicy
+import woowacourse.movie.model.policy.MorningPolicy
+import woowacourse.movie.model.policy.MovieDayPolicy
+import woowacourse.movie.model.policy.NightPolicy
 import java.time.LocalDate
 import java.time.LocalTime
 
 class TicketingInfoTest {
     @Test
     fun `조조에 해당하지만 무비데이면 무비데이 할인이 선적용된다`() {
-        val actual = TicketingInfo(LocalDate.of(2023, 4, 30), LocalTime.of(9, 0), Price()).price
+        val policies = listOf(MovieDayPolicy(), MorningPolicy())
+        val actual = TicketingInfo(policies, LocalDate.of(2023, 4, 30), LocalTime.of(9, 0), Price()).price
         val expected = Price(9700)
         assertEquals(actual, expected)
     }
 
-    private fun TicketingInfo(playingDate: LocalDate, playingTime: LocalTime, price: Price): TicketingInfo = TicketingInfo.of("해리포터와 마법사의 돌", playingDate, playingTime, 1, price, "현장")
+    @Test
+    fun `야간에 해당하지만 무비데이면 무비데이 할인이 선적용된다`() {
+        val policies = listOf(MovieDayPolicy(), NightPolicy())
+        val actual = TicketingInfo(policies, LocalDate.of(2023, 4, 30), LocalTime.of(21, 0), Price()).price
+        val expected = Price(9700)
+        assertEquals(actual, expected)
+    }
+
+    private fun TicketingInfo(policies: List<DiscountPolicy>, playingDate: LocalDate, playingTime: LocalTime, price: Price): TicketingInfo =
+        TicketingInfo.of(policies, "해리포터와 마법사의 돌", playingDate, playingTime, 1, price, "현장")
 }
