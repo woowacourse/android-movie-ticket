ee2d52007eed65af01b06f368f3f5ef35c48a58f
whk06061 <gju06061@naver.com>
2023-04-15 23:28:07 +0900

    refactor: 의미있는 단위로 함수 묶어주는 리팩토링
    - 한가지 함수가 하나의 역할을 하도록 함수를 분리한다
    - 의미있는 함수끼리 묶어줌으로써 onCreate 함수의 가독성을 높인다

--- app/src/main/java/woowacourse/movie/activity/TicketResultActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/TicketResultActivity.kt
@@ -5,33 +5,63 @@ import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
+import woowacourse.movie.model.Payment
+import woowacourse.movie.model.Price
 import woowacourse.movie.model.TicketingInfo
 import woowacourse.movie.util.Formatter
 import woowacourse.movie.util.customGetSerializable
+import java.time.LocalDate
+import java.time.LocalTime
 
 class TicketResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_result)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        initTicketDataView()
+        setActionBar()
+    }
 
+    private fun initTicketDataView() {
         val info: TicketingInfo = intent.customGetSerializable(INFO_KEY)
+        initTitle(info.title)
+        initPlayingDate(info.playingDate, info.playingTime)
+        initCount(info.count)
+        initPrice(info.price, info.count)
+        initPricePayment(info.payment)
+    }
 
-        val title = findViewById<TextView>(R.id.text_title)
-        title.text = info.title
+    private fun initTitle(title: String) {
+        val titleView = findViewById<TextView>(R.id.text_title)
+        titleView.text = title
+    }
 
-        val playingDate = findViewById<TextView>(R.id.text_playing_date)
-        playingDate.text = getString(R.string.date_time, Formatter.dateFormat(info.playingDate), Formatter.timeFormat(info.playingTime))
+    private fun initPlayingDate(playingDate: LocalDate, playingTime: LocalTime) {
+        val playingDateView = findViewById<TextView>(R.id.text_playing_date)
+        playingDateView.text = getString(
+            R.string.date_time,
+            Formatter.dateFormat(playingDate),
+            Formatter.timeFormat(playingTime)
+        )
+    }
 
-        val count = findViewById<TextView>(R.id.text_person_count)
-        count.text = info.count.toString()
+    private fun initCount(count: Int) {
+        val countView = findViewById<TextView>(R.id.text_person_count)
+        countView.text = count.toString()
+    }
 
-        val price = findViewById<TextView>(R.id.text_price)
-        price.text = Formatter.decimalFormat(info.price.price * info.count)
+    private fun initPrice(price: Price, count: Int) {
+        val priceView = findViewById<TextView>(R.id.text_price)
+        priceView.text = Formatter.decimalFormat(price.price * count)
+    }
 
+    private fun initPricePayment(payment: Payment) {
         val pricePayment = findViewById<TextView>(R.id.text_price_payment)
-        pricePayment.text = getText(R.string.price_payment).toString().format(info.payment.string)
+        pricePayment.text = getString(R.string.price_payment, payment.string)
+    }
+
+    private fun setActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
