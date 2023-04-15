7752f379f828ab99165cfcf5fd54fcb0f9c86047
EmilyCh0 <emily9803@ewhain.net>
2023-04-15 17:02:29 +0900

    test(ReservationTest): 구체적인 테스트명으로 변경

--- app/src/test/java/woowacourse/movie/domain/ReservationTest.kt
+++ app/src/test/java/woowacourse/movie/domain/ReservationTest.kt
@@ -3,26 +3,33 @@ package woowacourse.movie.domain
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-@RunWith(Parameterized::class)
-class ReservationTest(private val illegalPeopleCount: Int) {
+class ReservationTest() {
 
     @Test
-    fun `예매 인원이 1명 이상 200명 이하가 아니면 에러가 발생한다`() {
+    fun `예매 인원이 0명이면 에러가 발생한다`() {
         assertThrows(
             "[ERROR] 예매 인원은 최소 1명 이상 최대 200명 이하여야 합니다.",
             IllegalArgumentException::class.java
         ) {
-            Reservation(getAnyMovie(), illegalPeopleCount, LocalDateTime.now())
+            Reservation(getAnyMovie(), 0, LocalDateTime.now())
         }
     }
 
     @Test
-    fun `처음 총 예매 금액은 인원수에 영화 티켓 한 장 가격을 곱한 값이다`() {
+    fun `예매 인원이 201명이면 에러가 발생한다`() {
+        assertThrows(
+            "[ERROR] 예매 인원은 최소 1명 이상 최대 200명 이하여야 합니다.",
+            IllegalArgumentException::class.java
+        ) {
+            Reservation(getAnyMovie(), 201, LocalDateTime.now())
+        }
+    }
+
+    @Test
+    fun `13,000원 영화를 2명이 예약하면 초기 총 예매 금액은 26,000원이다`() {
         val peopleCount = 2
 
         val actual =
@@ -32,7 +39,7 @@ class ReservationTest(private val illegalPeopleCount: Int) {
     }
 
     @Test
-    fun `예약 생성 시 상영 날짜가 영화의 상영 기간 범위를 벗어나면 에러가 발생한다`() {
+    fun `상영 기간이 2일부터 31일인데 범위를 벗어난 1일에 예약하면 에러가 발생한다`() {
         val movie = Movie(
             "아바타",
             LocalDate.of(2024, 3, 2),
@@ -60,13 +67,4 @@ class ReservationTest(private val illegalPeopleCount: Int) {
             1,
             "줄거리"
         )
-
-    companion object {
-
-        @JvmStatic
-        @Parameterized.Parameters
-        fun peopleCount(): Collection<Int> {
-            return listOf(0, 201)
-        }
-    }
 }
