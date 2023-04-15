258b98cbd58bf55bec4aeca9c1535641ebcf9cab
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-15 19:36:49 +0900

    feat(MoiveItemViewInitializer): ListView에 존재하는 item에 대한 view를 initialize하는 클래스 생성

--- app/src/main/java/woowacourse/movie/adapter/MoviesAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MoviesAdapter.kt
@@ -6,16 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import domain.movie.Movie
 import woowacourse.movie.R
-import java.time.format.DateTimeFormatter
 
 class MoviesAdapter(
     private val movies: List<Movie>,
     private val reservationEvent: (movie: Movie) -> Unit
 ) : BaseAdapter() {
+
+    private val movieItemViewInitializer = MovieItemViewInitializer()
 
     override fun getCount(): Int = movies.size
 
@@ -29,7 +28,7 @@ class MoviesAdapter(
         val view =
             convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.item_movie, null)
 
-        initView(movie, view)
+        movieItemViewInitializer.init(movie, view)
         initReservationButtonClickListener(view, movie)
 
         return view
@@ -41,25 +40,5 @@ class MoviesAdapter(
         reservationButton.setOnClickListener {
             reservationEvent(movie)
         }
-    }
-
-    private fun initView(movie: Movie, view: View) {
-        val movieNameTextView: TextView = view.findViewById(R.id.movie_name_text_view)
-        val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        val movieImageView: ImageView = view.findViewById(R.id.movie_image_view)
-        val screeningDateTextView: TextView = view.findViewById(R.id.movie_screening_date_text_view)
-        val runningTimeTextView: TextView = view.findViewById(R.id.movie_running_time_text_view)
-
-        movieNameTextView.text = movie.name.value
-        movie.posterImage?.let { movieImageView.setImageResource(it) }
-        screeningDateTextView.text = view.context
-            .getString(R.string.screening_period_form)
-            .format(
-                movie.screeningPeriod.startDate.format(dateFormat),
-                movie.screeningPeriod.endDate.format(dateFormat)
-            )
-        runningTimeTextView.text = view.context
-            .getString(R.string.running_time_form)
-            .format(movie.runningTime)
     }
 }
