b07927d806819fc4f994e5ac01f8539235102591
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-04-15 18:29:26 +0900

    refactor: viewHolder 패턴 적용

---
+++ app/src/main/java/woowacourse/movie/MovieListViewHolder.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie
+
+import android.content.Intent
+import woowacourse.movie.databinding.ItemMovieListBinding
+
+class MovieListViewHolder(binding: ItemMovieListBinding) {
+    private val context = binding.root.context
+    private val ivMoviePoster = binding.ivMoviePoster
+    private val tvMovieTitle = binding.tvMovieTitle
+    private val tvMovieReleaseDate = binding.tvMovieReleaseDate
+    private val tvMovieRunningTime = binding.tvMovieRunningTime
+    private val btBookNow = binding.btBookNow
+    fun bind(movie: Movie) {
+        ivMoviePoster.setImageResource(movie.poster)
+        tvMovieTitle.text = movie.title
+        tvMovieReleaseDate.text = movie.releaseDate
+        tvMovieRunningTime.text = movie.runningTime
+        btBookNow.setOnClickListener {
+            val intent = Intent(context, MovieDetailActivity::class.java)
+            intent.putExtra("movieData", movie)
+            context.startActivity(intent)
+        }
+    }
+}
