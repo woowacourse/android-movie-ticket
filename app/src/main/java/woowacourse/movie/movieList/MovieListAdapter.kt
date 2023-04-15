4c196e8393a88f39cbdff56efe302e4db09c5cf8
re4rk <37167652+re4rk@users.noreply.github.com>
2023-04-15 22:09:31 +0900

    refactor: 영화 목록에서 영화 객체 분리

--- app/src/main/java/woowacourse/movie/movieList/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/movieList/MovieListAdapter.kt
@@ -1,15 +1,10 @@
 package woowacourse.movie.movieList
 
-import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import androidx.core.content.ContextCompat.startActivity
 import movie.Cinema
 import movie.MovieSchedule
-import woowacourse.movie.R
-import woowacourse.movie.movieReservation.MovieReservationActivity
-import woowacourse.movie.utils.DateUtil
 
 class MovieListAdapter(
     private val Cinema: Cinema,
@@ -27,39 +22,6 @@ class MovieListAdapter(
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: inflateMovieListItemView(parent)
-        val viewHolder = view.tag as MovieListViewHolder
-
-        with(Cinema[position]) {
-            viewHolder.bind(
-                posterResource = poster,
-                title = title,
-                date = DateUtil(view.context).getDateRange(startDate, endDate),
-                runningTime = view.context.getString(R.string.movie_running_time).format(runningTime),
-                reservationListener = {
-                    val intent = Intent(view.context, MovieReservationActivity::class.java)
-                    intent.putExtra(MovieReservationActivity.KEY_MOVIE_SCHEDULE, this)
-                    startActivity(view.context, intent, null)
-                },
-            )
-        }
-
-        return view
-    }
-
-    private fun inflateMovieListItemView(parent: ViewGroup?): View {
-        return View.inflate(
-            parent?.context,
-            R.layout.include_movie_list_item,
-            null,
-        ).apply {
-            tag = MovieListViewHolder(
-                posterView = this.findViewById(R.id.movie_poster),
-                titleView = this.findViewById(R.id.movie_title),
-                releaseDateView = this.findViewById(R.id.movie_release_date),
-                runningTimeView = this.findViewById(R.id.movie_running_time),
-                reservationButton = this.findViewById(R.id.movie_reservation_button),
-            )
-        }
+        return MovieListItemView.getView(Cinema[position], convertView, parent)
     }
 }
---
+++ app/src/main/java/woowacourse/movie/movieList/MovieListItemView.kt
@@ -0,0 +1,51 @@
+package woowacourse.movie.movieList
+
+import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import movie.MovieSchedule
+import woowacourse.movie.R
+import woowacourse.movie.movieReservation.MovieReservationActivity
+import woowacourse.movie.utils.DateUtil
+
+object MovieListItemView {
+    fun getView(movieSchedule: MovieSchedule, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: initMovieListItemView(parent)
+        view.tag = view.tag ?: initViewHolder(view)
+        bindViewHolder(view, movieSchedule)
+        return view
+    }
+
+    private fun initMovieListItemView(parent: ViewGroup?): View = View.inflate(
+        parent?.context,
+        R.layout.include_movie_list_item,
+        null,
+    )
+
+    private fun initViewHolder(view: View): MovieListViewHolder = MovieListViewHolder(
+        posterView = view.findViewById(R.id.movie_poster),
+        titleView = view.findViewById(R.id.movie_title),
+        releaseDateView = view.findViewById(R.id.movie_release_date),
+        runningTimeView = view.findViewById(R.id.movie_running_time),
+        reservationButton = view.findViewById(R.id.movie_reservation_button),
+    )
+
+    private fun bindViewHolder(view: View, movieSchedule: MovieSchedule) {
+        val viewHolder = (view.tag) as MovieListViewHolder
+
+        with(movieSchedule) {
+            viewHolder.bind(
+                posterResource = movieSchedule.poster,
+                title = movieSchedule.title,
+                date = DateUtil(view.context).getDateRange(startDate, endDate),
+                runningTime = view.context.getString(R.string.movie_running_time).format(runningTime),
+                reservationListener = {
+                    val intent = Intent(view.context, MovieReservationActivity::class.java)
+                    intent.putExtra(MovieReservationActivity.KEY_MOVIE_SCHEDULE, this)
+                    startActivity(view.context, intent, null)
+                },
+            )
+        }
+    }
+}
