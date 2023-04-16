e8c0344b7f6a46efb530d94dea73f74425b2af46
vrexpert <vrexpert2302@gmail.com>
2023-04-16 17:40:18 +0900

    refactor(DateTimeDiscountAdapter): list 로 할인 정책들을 관리

--- app/src/main/java/woowacourse/movie/domain/discount/discountpolicy/DateTimeDiscountAdapter.kt
+++ app/src/main/java/woowacourse/movie/domain/discount/discountpolicy/DateTimeDiscountAdapter.kt
@@ -3,15 +3,20 @@ package woowacourse.movie.domain.discount.discountpolicy
 import woowacourse.movie.domain.model.Money
 import java.time.LocalDateTime
 
-class DateTimeDiscountAdapter(override val dateTime: LocalDateTime) : DateTimeDiscountPolicy {
+class DateTimeDiscountAdapter(
+    override val dateTime: LocalDateTime,
+) : DateTimeDiscountPolicy {
+
+    val policies = listOf(
+        MovieDayTimeDiscountPolicy(dateTime, 0.1),
+        EarlyBirdTimeDiscountPolicy(dateTime, 2000),
+        LateNightTimeDiscountPolicy(dateTime, 2000),
+    )
 
     override fun discount(price: Money): Money {
-        val movieDayPrice = MovieDayTimeDiscountPolicy(dateTime, 0.1).discount(price)
-        return timeDiscount(movieDayPrice)
-    }
+        var discountedMoney = price
 
-    private fun timeDiscount(price: Money): Money {
-        val nextPrice = EarlyBirdTimeDiscountPolicy(dateTime, 2000).discount(price)
-        return LateNightTimeDiscountPolicy(dateTime, 2000).discount(nextPrice)
+        policies.forEach { discountedMoney = it.discount(discountedMoney) }
+        return discountedMoney
     }
 }
