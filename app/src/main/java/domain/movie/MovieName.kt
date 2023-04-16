adb9aa53f44cd720c8b5c20fc46daab10a2391b4
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-16 23:36:54 +0900

    refactor(MovieName): 예외 발생 오류 문구 추가

--- app/src/main/java/domain/movie/MovieName.kt
+++ app/src/main/java/domain/movie/MovieName.kt
@@ -3,11 +3,14 @@ package domain.movie
 @JvmInline
 value class MovieName(val value: String) {
     init {
-        require(value.length >= MINIMUM_LENGTH)
+        require(value.length >= MINIMUM_LENGTH) {
+            MOVIE_NAME_LENGTH_ERROR
+        }
     }
 
     companion object {
 
+        private const val MOVIE_NAME_LENGTH_ERROR = "[ERROR] 영화 이름의 길이는 1이상이다."
         private const val MINIMUM_LENGTH = 1
     }
 }
