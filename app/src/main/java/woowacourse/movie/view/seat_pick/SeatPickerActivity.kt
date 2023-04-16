b3290fb3027c3aeab2d35619d9a0fa5fe23f45b3
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-04-16 19:19:30 +0900

    feat: 좌석 선택화면 구현

---
+++ app/src/main/java/woowacourse/movie/view/seat_pick/SeatPickerActivity.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie.view.seat_pick
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.databinding.ActivitySeatPickerBinding
+
+class SeatPickerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySeatPickerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySeatPickerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
