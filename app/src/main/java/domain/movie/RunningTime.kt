2e2b9f5132158c07eacc6718ad93abf32557b11a
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-16 23:41:44 +0900

    refactor(RunningTime): 매직넘버를 상수로 관리

--- app/src/main/java/domain/movie/RunningTime.kt
+++ app/src/main/java/domain/movie/RunningTime.kt
@@ -4,12 +4,13 @@ package domain.movie
 value class RunningTime(val value: Int) {
 
     init {
-        require(value > 0) {
+        require(value >= MINIMUM_MINUTE) {
             RUNNING_TIME_ERROR
         }
     }
 
     companion object {
         private const val RUNNING_TIME_ERROR = "[ERROR] 러닝타임은 1분 이상이다."
+        private const val MINIMUM_MINUTE = 1
     }
 }
