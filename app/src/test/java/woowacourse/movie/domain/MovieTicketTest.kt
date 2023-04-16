c8823a3db37c7e37fe26d8892890a1ca10db24af
Gloria <ippnsj@gmail.com>
2023-04-16 20:21:57 +0900

    test: 타입 변경에 따른 테스트 코드 수정

--- app/src/test/java/woowacourse/movie/domain/MovieTicketTest.kt
+++ app/src/test/java/woowacourse/movie/domain/MovieTicketTest.kt
@@ -2,17 +2,14 @@ package woowacourse.movie.domain
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 class MovieTicketTest {
     @Test
     fun `인원이 2명일 경우 예매 금액은 26000원이다`() {
         val ticket = MovieTicket(
             "title",
-            TicketTime(
-                LocalDate.of(2023, 4, 13),
-                Time(12)
-            ),
+            TicketTime(LocalDateTime.of(2023, 4, 13, 12, 0)),
             PeopleCount(2)
         )
         assertEquals(26000, ticket.getPrice())
@@ -22,10 +19,7 @@ class MovieTicketTest {
     fun `인원이 2명이고 무비데이인 경우 예매 금액은 23400원이다`() {
         val ticket = MovieTicket(
             "title",
-            TicketTime(
-                LocalDate.of(2023, 4, 10),
-                Time(12)
-            ),
+            TicketTime(LocalDateTime.of(2023, 4, 10, 12, 0)),
             PeopleCount(2)
         )
         assertEquals(23400, ticket.getPrice())
@@ -35,10 +29,7 @@ class MovieTicketTest {
     fun `인원이 2명이고 조조 영화인 경우 예매 금액은 24000원이다`() {
         val ticket = MovieTicket(
             "title",
-            TicketTime(
-                LocalDate.of(2023, 4, 11),
-                Time(10)
-            ),
+            TicketTime(LocalDateTime.of(2023, 4, 11, 10, 0)),
             PeopleCount(2)
         )
         assertEquals(24000, ticket.getPrice())
@@ -48,10 +39,7 @@ class MovieTicketTest {
     fun `인원이 2명이고 심야 영화인 경우 예매 금액은 24000원이다`() {
         val ticket = MovieTicket(
             "title",
-            TicketTime(
-                LocalDate.of(2023, 4, 11),
-                Time(23)
-            ),
+            TicketTime(LocalDateTime.of(2023, 4, 11, 23, 0)),
             PeopleCount(2)
         )
         assertEquals(24000, ticket.getPrice())
@@ -61,10 +49,7 @@ class MovieTicketTest {
     fun `인원이 2명이고 무비 데이고 조조 영화인 경우 예매 금액은 21400원이다`() {
         val ticket = MovieTicket(
             "title",
-            TicketTime(
-                LocalDate.of(2023, 4, 20),
-                Time(9)
-            ),
+            TicketTime(LocalDateTime.of(2023, 4, 20, 9, 0)),
             PeopleCount(2)
         )
         assertEquals(21400, ticket.getPrice())
--- app/src/test/java/woowacourse/movie/domain/TicketTimeTest.kt
+++ app/src/test/java/woowacourse/movie/domain/TicketTimeTest.kt
@@ -3,46 +3,46 @@ package woowacourse.movie.domain
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 class TicketTimeTest() {
     @Test
     fun `10일은 무비데이 이다`() {
-        assertTrue(TicketTime(LocalDate.of(2023, 4, 10), Time(15)).isMovieDay())
+        assertTrue(TicketTime(LocalDateTime.of(2023, 4, 10, 15, 0)).isMovieDay())
     }
 
     @Test
     fun `20일은 무비데이 이다`() {
-        assertTrue(TicketTime(LocalDate.of(2023, 4, 20), Time(15)).isMovieDay())
+        assertTrue(TicketTime(LocalDateTime.of(2023, 4, 20, 15, 0)).isMovieDay())
     }
 
     @Test
     fun `30일은 무비데이 이다`() {
-        assertTrue(TicketTime(LocalDate.of(2023, 4, 30), Time(15)).isMovieDay())
+        assertTrue(TicketTime(LocalDateTime.of(2023, 4, 30, 15, 0)).isMovieDay())
     }
 
     @Test
     fun `14일은 무비데이가 아니다`() {
-        assertFalse(TicketTime(LocalDate.of(2023, 4, 14), Time(15)).isMovieDay())
+        assertFalse(TicketTime(LocalDateTime.of(2023, 4, 14, 15, 0)).isMovieDay())
     }
 
     @Test
     fun `11시는 영화가 할인되는 시간이다`() {
-        assertTrue(TicketTime(LocalDate.of(2023, 4, 14), Time(11)).isSaleTime())
+        assertTrue(TicketTime(LocalDateTime.of(2023, 4, 14, 11, 0)).isSaleTime())
     }
 
     @Test
     fun `20시는 영화가 할인되는 시간이다`() {
-        assertTrue(TicketTime(LocalDate.of(2023, 4, 14), Time(20)).isSaleTime())
+        assertTrue(TicketTime(LocalDateTime.of(2023, 4, 14, 20, 0)).isSaleTime())
     }
 
     @Test
     fun `12시는 영화가 할인되는 시간이 아니다`() {
-        assertFalse(TicketTime(LocalDate.of(2023, 4, 14), Time(12)).isSaleTime())
+        assertFalse(TicketTime(LocalDateTime.of(2023, 4, 14, 12, 0)).isSaleTime())
     }
 
     @Test
     fun `19시는 영화가 할인되는 시간이다`() {
-        assertFalse(TicketTime(LocalDate.of(2023, 4, 14), Time(19)).isSaleTime())
+        assertFalse(TicketTime(LocalDateTime.of(2023, 4, 14, 19, 0)).isSaleTime())
     }
 }
