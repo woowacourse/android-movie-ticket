93b97cfaf6a4276a5308d7f79de685ccbdd05661
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-16 23:33:55 +0900

    refactor: 더미데이터 외부로 추출

--- app/src/main/java/woowacourse/movie/movielist/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/movielist/MainActivity.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
-import woowacourse.movie.domain.movieinfo.Movie
 import woowacourse.movie.dto.MovieDto
-import woowacourse.movie.mapper.mapToMovieDto
+import woowacourse.movie.dto.MovieDummy
 import woowacourse.movie.moviedetail.MovieDetailActivity
-import java.time.LocalDate
 
 class MainActivity : AppCompatActivity(), OnMovieClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,19 +17,6 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
         setUpMovieDatas()
     }
 
-    private fun setMovieData(): List<MovieDto> = listOf(
-        mapToMovieDto(
-            Movie(
-                "해리포터",
-                LocalDate.of(2024, 3, 1),
-                LocalDate.of(2024, 4, 1),
-                200,
-                getString(R.string.description),
-                R.drawable.img,
-            ),
-        ),
-    )
-
     override fun onMovieClick(movie: MovieDto) {
         val intent = Intent(this, MovieDetailActivity::class.java)
         intent.putExtra(MOVIE_KEY, movie)
@@ -40,7 +25,7 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
 
     private fun setUpMovieDatas() {
         val movieListView = findViewById<ListView>(R.id.movie_listView)
-        val movieListViewAdapter = MovieListViewAdapter(this, setMovieData(), this)
+        val movieListViewAdapter = MovieListViewAdapter(this, MovieDummy.movieDatas, this)
 
         movieListView.adapter = movieListViewAdapter
     }
