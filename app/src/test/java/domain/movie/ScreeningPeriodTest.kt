7c653505d72729b97f125e2d87ed4eed2ca9fdcf
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-16 23:23:09 +0900

    refactor(ScreeningPeriodTest): 부생성자를 활용해 생성자를 간소화

--- app/src/test/java/domain/movie/ScreeningPeriodTest.kt
+++ app/src/test/java/domain/movie/ScreeningPeriodTest.kt
@@ -9,24 +9,24 @@ internal class ScreeningPeriodTest {
     @Test
     fun `상영_시작일은_상영_종료일_이전이어야_한다`() {
         ScreeningPeriod(
-            startDate = ScreeningDate(LocalDate.of(2023, 4, 12)),
-            endDate = ScreeningDate(LocalDate.of(2023, 4, 12)),
+            startDate = LocalDate.of(2023, 4, 12),
+            endDate = LocalDate.of(2023, 4, 12),
         )
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `상영_시작일_보다_상영_종료일이_이전일_경우_에러를_발생시킨다`() {
         ScreeningPeriod(
-            startDate = ScreeningDate(LocalDate.of(2023, 4, 12)),
-            endDate = ScreeningDate(LocalDate.of(2023, 4, 11)),
+            startDate = LocalDate.of(2023, 4, 12),
+            endDate = LocalDate.of(2023, 4, 11),
         )
     }
 
     @Test
     fun `상영_시작일과_상영_종료일_사이의_모든_상영일을_반환`() {
         val screeningDates: List<ScreeningDate> = ScreeningPeriod(
-            startDate = ScreeningDate(LocalDate.of(2023, 4, 10)),
-            endDate = ScreeningDate(LocalDate.of(2023, 4, 12)),
+            startDate = LocalDate.of(2023, 4, 10),
+            endDate = LocalDate.of(2023, 4, 12),
         ).getScreeningDates()
 
         val expectedDates: List<ScreeningDate> = listOf(
