f0737a357520162ed05b318e0179fed6e2269831
whk06061 <gju06061@naver.com>
2023-04-16 17:30:38 +0900

    refactor: ViewHolder 패턴 수정
    - ViewHolder 객체 생성시 inflate 된 뷰 넘겨주도록 수정
    - 불필요한 convertView 엘비스 연산자 제거

--- app/src/main/java/woowacourse/movie/activity/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MovieListActivity.kt
@@ -20,8 +20,7 @@ class MovieListActivity : AppCompatActivity() {
     }
 
     private fun getMovieData(): List<Movie> {
-        val movies = mutableListOf<Movie>()
-        movies.add(
+        val movies = List(1000) {
             Movie(
                 R.drawable.img,
                 "해리포터와 마법사의 돌",
@@ -29,7 +28,7 @@ class MovieListActivity : AppCompatActivity() {
                 152,
                 "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
             )
-        )
+        }
         return movies
     }
 }
--- app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
@@ -14,7 +14,8 @@ import woowacourse.movie.activity.MovieDetailActivity
 import woowacourse.movie.model.Movie
 import woowacourse.movie.util.Formatter
 
-class MovieListAdapter(private val context: Context, private val movies: List<Movie>) : BaseAdapter() {
+class MovieListAdapter(private val context: Context, private val movies: List<Movie>) :
+    BaseAdapter() {
     override fun getCount(): Int {
         return movies.size
     }
@@ -28,47 +29,58 @@ class MovieListAdapter(private val context: Context, private val movies: List<Mo
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.movie_item, null)
-        if (convertView == null) view.tag = getViewHolder(view)
-
-        val holder = view.tag as ViewHolder
-        val movie = getItem(position) as Movie
-        setViewHolder(holder, movie)
+        val view: View
+        val viewHolder: ViewHolder
+        // parent에는 ListView
+        // convertView에는 LinearLayout, 즉 item view 들어있음
+        if (convertView == null) {
+            // convertView 가 null 이면 holder 객체를 생성하고, 생성한 holder 객체에 inflating한 뷰의 참조값 저장
+            view = LayoutInflater.from(parent?.context).inflate(R.layout.movie_item, null)
+            viewHolder = getViewHolder(view)
+            // view의 태그에 holder 객체를 저장
+            view.tag = viewHolder
+        } else {
+            // convertView가 null이 아니면 뷰를 생성할 때 태그에 저장했던 holder 객체가 존재
+            // 이 holder 객체는 자신을 inflating 한 참조값 (즉 다시 infalte 할 필요가 없다.)
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+        setViewHolder(viewHolder, movies[position])
         return view
     }
-    private fun getViewHolder(view: View): ViewHolder {
-        val holder = ViewHolder()
-        holder.image = view.findViewById(R.id.img_movie)
-        holder.title = view.findViewById(R.id.text_title)
-        holder.playingDate = view.findViewById(R.id.text_playing_date)
-        holder.runningTime = view.findViewById(R.id.text_running_time)
-        holder.ticketingButton = view.findViewById(R.id.btn_ticketing)
 
-        return holder
+    private fun getViewHolder(view: View): ViewHolder {
+        val image: ImageView = view.findViewById(R.id.img_movie)
+        val title: TextView = view.findViewById(R.id.text_title)
+        val playingDate: TextView = view.findViewById(R.id.text_playing_date)
+        val runningTime: TextView = view.findViewById(R.id.text_running_time)
+        val ticketingButton: Button = view.findViewById(R.id.btn_ticketing)
+
+        return ViewHolder(image, title, playingDate, runningTime, ticketingButton)
     }
 
     private fun setViewHolder(holder: ViewHolder, movie: Movie) {
-        holder.image?.setImageResource(movie.image)
-        holder.title?.text = movie.title
-        holder.playingDate?.text = context.getString(
+        holder.image.setImageResource(movie.image)
+        holder.title.text = movie.title
+        holder.playingDate.text = context.getString(
             R.string.playing_time, Formatter.dateFormat(movie.playingTimes.startDate),
             Formatter.dateFormat(movie.playingTimes.endDate)
         )
-        holder.runningTime?.text = context.getString(R.string.running_time, movie.runningTime)
-        holder.ticketingButton?.setOnClickListener {
+        holder.runningTime.text = context.getString(R.string.running_time, movie.runningTime)
+        holder.ticketingButton.setOnClickListener {
             val intent = Intent(context, MovieDetailActivity::class.java)
             intent.putExtra(MOVIE_KEY, movie)
             context.startActivity(intent)
         }
     }
 
-    private class ViewHolder {
-        var image: ImageView? = null
-        var title: TextView? = null
-        var playingDate: TextView? = null
-        var runningTime: TextView? = null
-        var ticketingButton: Button? = null
-    }
+    private class ViewHolder(
+        val image: ImageView,
+        val title: TextView,
+        val playingDate: TextView,
+        val runningTime: TextView,
+        val ticketingButton: Button
+    )
 
     companion object {
         private const val MOVIE_KEY = "movie"
