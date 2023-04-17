8c8e5230c86978351138d2295380d4a70bc97605
briandr97 <briandr@naver.com>
2023-04-17 17:13:52 +0900

    refactor(MovieAdapter): MovieAdapter에서 뷰홀더 활용
    - convertView를 통해 뷰를 재활용하는 것에 더하여 뷰홀더를 사용하여 findViewById의 반복 호출을 막음

--- app/src/main/java/woowacourse/movie/ui/MovieAdapter.kt
+++ app/src/main/java/woowacourse/movie/ui/MovieAdapter.kt
@@ -12,10 +12,10 @@ import woowacourse.movie.R
 import woowacourse.movie.domain.Movie
 import woowacourse.movie.formatScreenDate
 
-class MovieAdapter(context: Context, private val clickBook: (Long) -> Unit) :
-    BaseAdapter() {
+class MovieAdapter(context: Context, private val clickBook: (Long) -> Unit) : BaseAdapter() {
     private val movies = mutableListOf<Movie>()
     private val layoutInflater = LayoutInflater.from(context)
+    private val viewHolders = mutableMapOf<Int, MovieViewHolder>()
 
     override fun getCount(): Int = movies.size
 
@@ -26,36 +26,46 @@ class MovieAdapter(context: Context, private val clickBook: (Long) -> Unit) :
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View =
             convertView ?: layoutInflater.inflate(R.layout.movie_list_item, parent, false)
-        initView(position, view)
-        clickBookButton(view, position)
+        onBindViewHolder(getItem(position), view.hashCode(), view)
         return view
     }
 
-    private fun clickBookButton(view: View, position: Int) {
-        view.findViewById<Button>(R.id.buttonItemBook).setOnClickListener {
-            clickBook(movies[position].id)
+    private fun onBindViewHolder(movie: Movie, key: Int, view: View) {
+        if (viewHolders[key] == null) {
+            viewHolders[key] = MovieViewHolder(view)
         }
-    }
-
-    private fun initView(position: Int, view: View) {
-        view.findViewById<ImageView>(R.id.imageItemThumbnail)
-            .setImageResource(movies[position].thumbnail)
-        view.findViewById<TextView>(R.id.textItemTitle).text = movies[position].title
-        view.findViewById<TextView>(R.id.textBookingScreeningDate).apply {
-            text = context.getString(R.string.screening_date)
-                .format(
-                    movies[position].startDate.formatScreenDate(),
-                    movies[position].endDate.formatScreenDate(),
-                )
-        }
-        view.findViewById<TextView>(R.id.textBookingRunningTime).apply {
-            text = context.getString(R.string.running_time).format(movies[position].runningTime)
-        }
+        viewHolders[key]?.onBind(movie)
+        viewHolders[key]?.clickBookButton(movie, clickBook)
     }
 
     fun initMovies(items: List<Movie>) {
         movies.clear()
         movies.addAll(items)
         notifyDataSetChanged()
+    }
+
+    class MovieViewHolder(private val view: View) {
+        private val thumbnail: ImageView = view.findViewById(R.id.imageItemThumbnail)
+        private val title: TextView = view.findViewById(R.id.textItemTitle)
+        private val date: TextView = view.findViewById(R.id.textBookingScreeningDate)
+        private val runningTime: TextView = view.findViewById(R.id.textBookingRunningTime)
+        private val button: Button = view.findViewById(R.id.buttonItemBook)
+
+        fun onBind(movie: Movie) {
+            thumbnail.setImageResource(movie.thumbnail)
+            title.text = movie.title
+            date.text = view.context.getString(
+                R.string.screening_date,
+                movie.startDate.formatScreenDate(),
+                movie.endDate.formatScreenDate(),
+            )
+            runningTime.text = view.context.getString(R.string.running_time, movie.runningTime)
+        }
+
+        fun clickBookButton(movie: Movie, clickBook: (Long) -> Unit) {
+            button.setOnClickListener {
+                clickBook(movie.id)
+            }
+        }
     }
 }
