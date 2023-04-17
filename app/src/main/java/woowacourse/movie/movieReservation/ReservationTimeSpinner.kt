021347e9dfc4106a65a5123a1a0886ba981f52e0
re4rk <37167652+re4rk@users.noreply.github.com>
2023-04-17 19:47:20 +0900

    refactor: 스피너 시간 저장 방식 개선

--- app/src/main/java/woowacourse/movie/movieReservation/ReservationTimeSpinner.kt
+++ app/src/main/java/woowacourse/movie/movieReservation/ReservationTimeSpinner.kt
@@ -9,31 +9,53 @@ import woowacourse.movie.R
 import java.time.LocalDate
 import java.time.LocalTime
 
-class ReservationTimeSpinner(
-    private val view: View,
-) {
-    private val timeSpinner: Spinner = view.findViewById(R.id.reservation_screening_time_spinner)
+class ReservationTimeSpinner(view: View) {
+    private var date = LocalDate.MIN
+
+    private val timeList = mutableListOf<LocalTime>()
+
+    private val arrayAdapter = ArrayAdapter(
+        view.context,
+        android.R.layout.simple_spinner_item,
+        timeList,
+    )
+
+    private val timeSpinner: Spinner =
+        view.findViewById<Spinner>(R.id.reservation_screening_time_spinner)
+            .apply { adapter = arrayAdapter }
 
     val selectedTime: LocalTime
         get() = LocalTime.parse(timeSpinner.selectedItem.toString())
 
     fun load(savedInstanceState: Bundle) {
-        timeSpinner.setSelection(savedInstanceState.getInt(KEY_TIME))
+        savedInstanceState.getStringArrayList(KEY_TIME_LIST)?.let { newTimeList ->
+            updateTimeList(newTimeList.map { LocalTime.parse(it) })
+        }
+
+        date = LocalDate.parse(savedInstanceState.getString(KEY_DATE))
     }
 
     fun save(outState: Bundle) {
-        outState.putInt(KEY_TIME, timeSpinner.selectedItemPosition)
+        outState.putString(KEY_DATE, date.toString())
+        outState.putStringArrayList(KEY_TIME_LIST, timeList.map { it.toString() }.toCollection(ArrayList()))
     }
 
     fun initTimeSpinner(date: LocalDate) {
-        timeSpinner.adapter = ArrayAdapter(
-            view.context,
-            android.R.layout.simple_spinner_item,
-            ScreeningDate.getScreeningTimes(date),
-        )
+        if (date == this.date) return
+
+        this.date = date
+        timeSpinner.setSelection(0)
+        updateTimeList(ScreeningDate.getScreeningTimes(date))
+    }
+
+    private fun updateTimeList(newTimeList: List<LocalTime>) {
+        timeList.clear()
+        timeList.addAll(newTimeList)
+        arrayAdapter.notifyDataSetChanged()
     }
 
     companion object {
-        private const val KEY_TIME = "timeSpinner"
+        private const val KEY_DATE = "timeSpinnerDate"
+        private const val KEY_TIME_LIST = "timeList"
     }
 }
