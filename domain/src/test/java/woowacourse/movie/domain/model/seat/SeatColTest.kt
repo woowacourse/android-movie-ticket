47d817bd3f4c060bfc40dde8c660da646b950187
tmdgh1592 <tmdgh1592@naver.com>
2023-04-17 15:07:21 +0900

    feat: 좌석 행(column) 기능 구현

---
+++ domain/src/test/java/woowacourse/movie/domain/model/seat/SeatColTest.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie.domain.model.seat
+
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(JUnitParamsRunner::class)
+internal class SeatColTest {
+    @Test
+    @Parameters("1", "2", "3", "4")
+    internal fun `좌석은 1열부터 4열까지이다`(column: Int) {
+        SeatCol(column)
+    }
+
+    @Test
+    @Parameters("0", "5")
+    internal fun `좌석은 1열과 4열 사이를 벗어나면 예외가 발생한다`(column: Int) {
+        assertThrows(IllegalArgumentException::class.java) {
+            SeatCol(column)
+        }
+    }
+}
