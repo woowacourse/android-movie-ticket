ba82f35f8f00ddebbad48c4e013a4ac801933cec
briandr97 <briandr@naver.com>
2023-04-18 16:54:15 +0900

    refactor(policy): 필요없어진 정책 삭제

--- app/src/main/java/woowacourse/movie/domain/discountpolicy/DateTimeDiscountPolicy.kt
+++
@@ -1,7 +0,0 @@
-package woowacourse.movie.domain.discountpolicy
-
-import java.time.LocalDateTime
-
-interface DateTimeDiscountPolicy : DiscountPolicy {
-    val dateTime: LocalDateTime
-}
--- app/src/main/java/woowacourse/movie/domain/discountpolicy/DiscountPolicy.kt
+++
@@ -1,5 +0,0 @@
-package woowacourse.movie.domain.discountpolicy
-
-interface DiscountPolicy {
-    fun discount(price: Int): Int
-}
