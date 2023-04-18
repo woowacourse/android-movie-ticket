0cf44da49941fc8836c9312602bd09420c3166bf
pingu244 <pingu244@naver.com>
2023-04-18 17:23:06 +0900

    refactor: 오버로딩에 맞게 테스트 수정

--- app/src/test/java/woowacourse/movie/domain/CountNumberOfPeopleTest.kt
+++ app/src/test/java/woowacourse/movie/domain/CountNumberOfPeopleTest.kt
@@ -8,7 +8,7 @@ class CountNumberOfPeopleTest {
     @Test
     fun `사람 수를 한 명 빼서 반환한다`() {
         val numberOfPeople = 5
-        val actual = CountNumberOfPeople().minusNumberOfPeople(numberOfPeople)
+        val actual = CountNumberOfPeople().minus(numberOfPeople)
 
         assertEquals(actual, 4)
     }
@@ -16,7 +16,7 @@ class CountNumberOfPeopleTest {
     @Test
     fun `사람 수가 1명인 경우에 뺀다면 계속 1이다`() {
         val numberOfPeople = 1
-        val actual = CountNumberOfPeople().minusNumberOfPeople(numberOfPeople)
+        val actual = CountNumberOfPeople().minus(numberOfPeople)
 
         assertEquals(actual, 1)
     }
@@ -24,7 +24,7 @@ class CountNumberOfPeopleTest {
     @Test
     fun `사람 수를 한 명 더해서 반환한다`() {
         val numberOfPeople = 5
-        val actual = CountNumberOfPeople().plusNumberOfPeople(numberOfPeople)
+        val actual = CountNumberOfPeople().plus(numberOfPeople)
 
         assertEquals(actual, 6)
     }
@@ -32,7 +32,7 @@ class CountNumberOfPeopleTest {
     @Test
     fun `사람 수가 10명인 경우에 더한다면 계속 10이다`() {
         val numberOfPeople = 10
-        val actual = CountNumberOfPeople().plusNumberOfPeople(numberOfPeople)
+        val actual = CountNumberOfPeople().plus(numberOfPeople)
 
         assertEquals(actual, 10)
     }
