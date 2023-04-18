364f375930e8f5360ed7f14b02c846be265c31fd
krrong <khk9664@naver.com>
2023-04-18 19:23:43 +0900

    feat(MovieSeatActivity): 좌석 선택 액티비티 생성

---
+++ app/src/main/java/woowacourse/movie/movieSeat/MovieSeatActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.movieSeat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.R
+
+class MovieSeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_seat)
+    }
+}
