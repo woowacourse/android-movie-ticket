3043a9c501c4d61dab9987c9d10818e6cd6e7780
inseonyun <yuninseon@naver.com>
2023-04-18 17:47:31 +0900

    refactor(DiscountPolicy): 추상 메서드 접근 제한자 수정

--- domain/src/main/java/com/woowacourse/movie/domain/policy/DiscountPolicy.kt
+++ domain/src/main/java/com/woowacourse/movie/domain/policy/DiscountPolicy.kt
@@ -1,8 +1,8 @@
 package com.woowacourse.movie.domain.policy
 
 abstract class DiscountPolicy {
-    abstract fun isDiscount(): Boolean
-    abstract fun determineDiscount(price: Int): Int
+    protected abstract fun isDiscount(): Boolean
+    protected abstract fun determineDiscount(price: Int): Int
     fun calculatePrice(price: Int): Int =
         if (isDiscount()) determineDiscount(price) else price
 }
