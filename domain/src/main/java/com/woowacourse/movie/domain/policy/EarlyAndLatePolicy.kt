d5253d419c7a85c928cd00d0bef5d4b70deaeca4
inseonyun <yuninseon@naver.com>
2023-04-19 00:17:44 +0900

    fix(EarlyAndLatePolicy): 할인 가능 여부 시간 계산 로직 수정

--- domain/src/main/java/com/woowacourse/movie/domain/policy/EarlyAndLatePolicy.kt
+++ domain/src/main/java/com/woowacourse/movie/domain/policy/EarlyAndLatePolicy.kt
@@ -6,15 +6,19 @@ class EarlyAndLatePolicy(private val time: LocalTime) : DiscountPolicy() {
     private fun calculateTime(localTime: LocalTime): Int = localTime.hour * HOUR_TO_MIN + localTime.minute
     override fun isDiscount(): Boolean {
         val totalTime = calculateTime(time)
-        return totalTime < AM_DISCOUNT_CLOSE_TIME || totalTime >= PM_DISCOUNT_OPEN_TIME
+        val amDiscountTime = calculateTime(LocalTime.of(AM_DISCOUNT_CLOSE_HOUR, AM_DISCOUNT_CLOSE_MIN))
+        val pmDiscountTime = calculateTime(LocalTime.of(PM_DISCOUNT_OPEN_HOUR, PM_DISCOUNT_OPEN_MIN))
+        return totalTime < amDiscountTime || totalTime >= pmDiscountTime
     }
 
     override fun determineDiscount(price: Int): Int = price - DISCOUNT_PRICE
 
     companion object {
         private const val HOUR_TO_MIN = 60
-        private const val AM_DISCOUNT_CLOSE_TIME = 11
-        private const val PM_DISCOUNT_OPEN_TIME = 20
+        private const val AM_DISCOUNT_CLOSE_HOUR = 11
+        private const val AM_DISCOUNT_CLOSE_MIN = 0
+        private const val PM_DISCOUNT_OPEN_HOUR = 20
+        private const val PM_DISCOUNT_OPEN_MIN = 0
         private const val DISCOUNT_PRICE = 2_000
     }
 }
--- domain/src/test/java/com/woowacourse/movie/domain/policy/EarlyAndLatePolicyTest.kt
+++ domain/src/test/java/com/woowacourse/movie/domain/policy/EarlyAndLatePolicyTest.kt
@@ -15,4 +15,14 @@ class EarlyAndLatePolicyTest {
 
         assertThat(actual).isEqualTo(expected)
     }
+    @Test
+    fun `심야 영화이고, 영화 가격이 13_000원일 때, 영화 가격은 2_000원 할인 된 11_000원이다`() {
+        val movieTime = LocalTime.of(20, 0)
+        val earlyAndLatePolicy = EarlyAndLatePolicy(movieTime)
+        val ticketPrice = 13_000
+        val actual = earlyAndLatePolicy.calculatePrice(ticketPrice)
+        val expected = 11_000
+
+        assertThat(actual).isEqualTo(expected)
+    }
 }
