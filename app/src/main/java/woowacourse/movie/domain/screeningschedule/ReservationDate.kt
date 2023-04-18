9b88cad0ed5b5b510cca36e77563a2f7d5d042ac
pingu244 <pingu244@naver.com>
2023-04-18 15:25:13 +0900

    refactor: 시퀀스를 사용하지 않게 코드 수정

--- app/src/main/java/woowacourse/movie/domain/screeningschedule/ReservationDate.kt
+++ app/src/main/java/woowacourse/movie/domain/screeningschedule/ReservationDate.kt
@@ -1,14 +1,13 @@
 package woowacourse.movie.domain.screeningschedule
 
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class ReservationDate(private val startDay: LocalDate, private val endDay: LocalDate) {
 
     fun getIntervalDays(): List<String> {
 
-        return generateSequence(startDay) { it.plusDays(1) }
-            .takeWhile { !it.isAfter(endDay) }
-            .map { it.toString() }
-            .toList()
+        val days = ChronoUnit.DAYS.between(startDay, endDay.plusDays(1))
+        return List(days.toInt()) { startDay.plusDays(it.toLong()).toString() }
     }
 }
