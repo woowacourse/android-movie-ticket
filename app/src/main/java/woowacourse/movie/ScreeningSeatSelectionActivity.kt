f967e26f75bf3560dc4e732273d79da149111a54
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-19 17:51:36 +0900

    ui(ScreeningSeatSelectionActivity): 좌석을 선택하는 액티비티 xml 구성하기

---
+++ app/src/main/java/woowacourse/movie/ScreeningSeatSelectionActivity.kt
@@ -0,0 +1,11 @@
+package woowacourse.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreeningSeatSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screening_seat_selection)
+    }
+}
