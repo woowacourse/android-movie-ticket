179c8a63a54f743b52a57f134cecb424045b359b
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-19 23:30:07 +0900

    feat: Seats UI모델 생성 및 mapping하는 함수 추가

---
+++ app/src/main/java/woowacourse/movie/dto/SeatsDto.kt
@@ -0,0 +1,5 @@
+package woowacourse.movie.dto
+
+import java.io.Serializable
+
+data class SeatsDto(val seats: List<SeatDto>) : Serializable
---
+++ app/src/main/java/woowacourse/movie/mapper/SeatsMapper.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie.mapper
+
+import domain.Seat
+import domain.Seats
+import woowacourse.movie.dto.SeatDto
+import woowacourse.movie.dto.SeatsDto
+
+fun SeatsDto.mapToSeats(): Seats {
+    return Seats(this.seats.map { Seat(it.position, it.price.mapToTicketPrice()) })
+}
+
+fun Seats.mapToSeatsDto(): SeatsDto {
+    return SeatsDto(this.seats.map { SeatDto(it.position, it.price.mapToTicketPriceDto()) })
+}
