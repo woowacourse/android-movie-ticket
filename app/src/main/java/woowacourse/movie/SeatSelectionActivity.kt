d14b7628a3b380aa2f3eb97ac2312f8f87e3574e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-20 00:10:47 +0900

    feat: 좌석을 선택하면 배경색이 바뀌는 기능 추가

--- app/src/main/java/woowacourse/movie/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/SeatSelectionActivity.kt
@@ -1,12 +1,57 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import woowacourse.movie.dto.MovieDateDto
+import woowacourse.movie.dto.MovieDto
+import woowacourse.movie.dto.MovieTimeDto
+import woowacourse.movie.dto.TicketCountDto
 
 class SeatSelectionActivity : AppCompatActivity() {
+
+    private val date by lazy { intent.getSerializableExtra(DATE_KEY) as MovieDateDto }
+    private val time by lazy { intent.getSerializableExtra(TIME_KEY) as MovieTimeDto }
+    private val movie by lazy { intent.getSerializableExtra(MOVIE_KEY) as MovieDto }
+    private val ticketCount by lazy { intent.getSerializableExtra(TICKET_KEY) as TicketCountDto }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
+        setMovieTitle()
+
+        onSeatClickListener()
+    }
+
+    private fun setMovieTitle() {
+        val movieTtile = findViewById<TextView>(R.id.movie_title)
+        movieTtile.text = movie.title
+    }
+
+    private fun getSeatView(): List<TextView> {
+        return findViewById<TableLayout>(R.id.seat)
+            .children
+            .filterIsInstance<TableRow>()
+            .flatMap { it.children }
+            .filterIsInstance<TextView>()
+            .toList()
+    }
+
+    private fun onSeatClickListener() {
+        getSeatView().forEachIndexed { index, textView ->
+            textView.setOnClickListener {
+                textView.setBackgroundColor(getColor(R.color.select_seat))
+            }
+        }
+    }
+
+    companion object {
+        private const val TICKET_KEY = "ticket"
+        private const val MOVIE_KEY = "movie"
+        private const val DATE_KEY = "movie_date"
+        private const val TIME_KEY = "movie_time"
     }
 }
