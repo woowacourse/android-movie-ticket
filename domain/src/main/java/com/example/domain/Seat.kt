6ab0606f0dc38f85a63bcc39f61ce15867fb470f
EmilyCh0 <emily9803@ewhain.net>
2023-04-19 17:45:05 +0900

    fix(Seat): 좌석 행, 열 수정

--- domain/src/main/java/com/example/domain/Seat.kt
+++ domain/src/main/java/com/example/domain/Seat.kt
@@ -5,7 +5,7 @@ data class Seat(val column: Int, val row: Int) {
     val price = type.price
 
     private val type
-        get() : SeatType = when (column) {
+        get() : SeatType = when (row) {
             1, 2 -> SeatType.BType
             3, 4 -> SeatType.SType
             5 -> SeatType.AType
@@ -23,9 +23,9 @@ data class Seat(val column: Int, val row: Int) {
 
     companion object {
         private const val MIN_ROW = 1
-        private const val MAX_ROW = 4
+        const val MAX_ROW = 5
         private const val MIN_COLUMN = 1
-        private const val MAX_COLUMN = 5
+        const val MAX_COLUMN = 4
         private const val ERROR_INVALID_ROW = "[ERROR] 올바른 좌석(행)이 아닙니다."
         private const val ERROR_INVALID_COLUMN = "[ERROR] 올바른 좌석(열)이 아닙니다."
     }
