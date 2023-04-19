ce7550b6c19662ed2f69d77e45b9d67a5a1ec5f1
Sujin Kim <sujinkim919@gmail.com>
2023-04-19 11:48:24 +0900

    refactor: 주말 상영시간 범위 수정

--- app/src/main/java/woowacourse/movie/domain/TimesGenerator.kt
+++ app/src/main/java/woowacourse/movie/domain/TimesGenerator.kt
@@ -5,7 +5,7 @@ import java.time.LocalTime
 
 object TimesGenerator {
     private val WEEKDAYS = (9..23 step 2).map { LocalTime.of(it, 0) }
-    private val WEEKENDS = (10..22 step 2).map { LocalTime.of(it, 0) }.plus(LocalTime.of(0, 0))
+    private val WEEKENDS = (10..22 step 2).map { LocalTime.of(it, 0) }
 
     fun getTimesByDate(date: LocalDate): List<LocalTime> {
         return when (date.dayOfWeek.value) {
