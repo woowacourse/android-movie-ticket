6dc9f1b50870500719d2f735978fdaa0e089f8ff
tmdgh1592 <tmdgh1592@naver.com>
2023-04-19 13:53:38 +0900

    refactor: 영화 목록 ListView를 RecyclerView로 변경

--- app/src/main/java/woowacourse/movie/presentation/activities/movielist/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/presentation/activities/movielist/MovieListAdapter.kt
@@ -1,9 +1,8 @@
 package woowacourse.movie.presentation.activities.movielist
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ItemMovieBinding
 import woowacourse.movie.presentation.model.Movie
@@ -11,33 +10,24 @@ import woowacourse.movie.presentation.model.Movie
 class MovieListAdapter(
     private val movies: List<Movie>,
     private val onBookBtnClick: (Movie) -> Unit,
-) : BaseAdapter() {
-    override fun getCount(): Int = movies.size
+) : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
 
-    override fun getItem(position: Int): Movie = movies[position]
-
-    override fun getItemId(position: Int): Long = position.toLong()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var view: View? = convertView
-        val viewHolder: ViewHolder
-
-        if (view == null) {
-            val binding =
-                ItemMovieBinding.inflate(LayoutInflater.from(parent?.context), parent, false)
-            binding.btnBook.setOnClickListener { onBookBtnClick(getItem(position)) }
-            viewHolder = ViewHolder(binding)
-            view = binding.root.also { it.tag = viewHolder }
-        } else {
-            viewHolder = view.tag as ViewHolder
-        }
-
-        viewHolder.bind(movies[position])
-        return view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val movieView = ItemMovieBinding.inflate(layoutInflater, parent, false)
+        return MovieViewHolder(movieView, onBookBtnClick)
     }
 
-    private class ViewHolder(private val binding: ItemMovieBinding) {
+    override fun getItemCount(): Int = movies.size
 
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    class MovieViewHolder(
+        private val binding: ItemMovieBinding,
+        private val onBookBtnClick: (Movie) -> Unit = {},
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Movie) {
             val context = binding.root.context
             with(item) {
@@ -50,6 +40,7 @@ class MovieListAdapter(
                 )
                 binding.tvRunningTime.text =
                     context.getString(R.string.movie_running_time, runningTime)
+                binding.btnBook.setOnClickListener { onBookBtnClick(item) }
             }
         }
     }
--- app/src/main/java/woowacourse/movie/presentation/model/Movie.kt
+++ app/src/main/java/woowacourse/movie/presentation/model/Movie.kt
@@ -22,14 +22,14 @@ data class Movie(
     companion object {
         private const val MOVIE_DATE_PATTERN = "yyyy.MM.dd"
 
-        fun provideDummy(): List<Movie> = List(10) {
+        fun provideDummy(): List<Movie> = List(10_000) {
             Movie(
                 "해리 포터와 마법사의 돌 $it",
                 LocalDate.of(2023, 4, 1),
                 LocalDate.of(2023, 4, 30),
                 152,
                 "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
-                R.drawable.img_default_movie_thumbnail1
+                R.drawable.img_sample_movie_thumbnail1
             )
         }
     }
