f9e941d63bf7904b9f86cb03098ef2b6a817ad92
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-04-19 21:59:39 +0900

    feat: 광고 기능 추가

--- app/src/main/java/woowacourse/movie/presentation/view/main/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/MovieListAdapter.kt
@@ -1,32 +1,42 @@
 package woowacourse.movie.presentation.view.main
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.databinding.ItemAdvertiseBinding
 import woowacourse.movie.databinding.ItemMovieListBinding
 import woowacourse.movie.model.Movie
 
-class MovieListAdapter(private val movies: List<Movie>) : BaseAdapter() {
-    private lateinit var binding: ItemMovieListBinding
-    override fun getCount(): Int = movies.size
-
-    override fun getItem(position: Int): Movie = movies[position]
-
-    override fun getItemId(position: Int): Long = position.toLong()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        if (convertView != null) {
-            binding = binding.root.tag as ItemMovieListBinding
-        } else {
-            binding = ItemMovieListBinding.inflate(LayoutInflater.from(parent?.context))
-            binding.root.tag = binding
+class MovieListAdapter(private val movies: List<Movie>, private val ad: Drawable) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_AD) {
+            val binding =
+                ItemAdvertiseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return AdListViewHolder(binding)
         }
-        val movie = movies[position]
-        val viewHolder = MovieListViewHolder(binding)
+        val binding =
+            ItemMovieListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieListViewHolder(binding)
+    }
 
-        viewHolder.bind(movie)
+    override fun getItemViewType(position: Int): Int {
+        return if (position % 3 == 0 && position > 0) VIEW_TYPE_AD else VIEW_TYPE_MOVIE
+    }
 
-        return binding.root
+    override fun getItemCount() = (movies.size) + (movies.size / 3)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position % 3 == 0 && position > 0) {
+            (holder as AdListViewHolder).bind(ad)
+        } else {
+            (holder as MovieListViewHolder).bind(movies[position - (position / 3)])
+        }
+    }
+
+    companion object {
+        const val VIEW_TYPE_AD = 0
+        const val VIEW_TYPE_MOVIE = 1
     }
 }
---
+++ app/src/main/java/woowacourse/movie/presentation/view/main/ViewType.kt
@@ -0,0 +1,6 @@
+package woowacourse.movie.presentation.view.main
+
+enum class ViewType {
+    MOVIE,
+    AD
+}
