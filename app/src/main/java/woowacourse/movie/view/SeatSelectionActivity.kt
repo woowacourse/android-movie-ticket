e9cce0def307976c5eb6d052973532b6115479a9
pingu244 <pingu244@naver.com>
2023-04-19 15:38:23 +0900

    feat: 좌석 선택시 배경색 바뀌는 기능 구현

--- app/src/main/java/woowacourse/movie/view/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/view/SeatSelectionActivity.kt
@@ -1,12 +1,28 @@
 package woowacourse.movie.view
 
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import woowacourse.movie.R
 
 class SeatSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
+
+        val seats = findViewById<TableLayout>(R.id.seat_selection)
+            .children
+            .filterIsInstance<TableRow>()
+            .flatMap { it.children }
+            .filterIsInstance<TextView>()
+
+        seats.forEachIndexed { index, view ->
+            view.setOnClickListener {
+                view.setBackgroundColor(getColor(R.color.seat_select_state))
+            }
+        }
     }
 }
