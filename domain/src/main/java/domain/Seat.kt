d5f876ac80301e5b754cc9d83e3e6d37e841d22e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-19 18:23:26 +0900

    feat: equals와 hashCode 재정의

--- domain/src/main/java/domain/Seat.kt
+++ domain/src/main/java/domain/Seat.kt
@@ -13,19 +13,11 @@ data class Seat(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Seat
-
-        if (position != other.position) return false
-        if (price != other.price) return false
-
-        return true
+        if (other !is Seat) return false
+        return position == other.position
     }
 
     override fun hashCode(): Int {
-        var result = position.hashCode()
-        result = 31 * result + price.hashCode()
-        return result
+        return position.hashCode()
     }
 }
