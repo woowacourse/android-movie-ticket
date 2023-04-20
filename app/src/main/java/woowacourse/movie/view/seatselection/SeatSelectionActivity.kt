064265bdba29a5a948a14d9bc7e4d7721ffa795c
pingu244 <pingu244@naver.com>
2023-04-20 15:41:36 +0900

    refactor: 다이얼로그 관련 로직 함수화

--- app/src/main/java/woowacourse/movie/view/seatselection/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/view/seatselection/SeatSelectionActivity.kt
@@ -21,59 +21,54 @@ class SeatSelectionActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
+        setBackToBefore(R.id.seat_selection_toolbar)
 
         val selectedDate = intent.getSerializableExtra(DATE_KEY) as LocalDateTime
         val selectedNumberOfPeople = intent.getIntExtra(NUMBER_OF_PEOPLE_KEY, 0)
         val movieUI = intent.getSerializableExtra(MOVIE_KEY) as MovieUIModel
 
-        setBackToBefore(R.id.seat_selection_toolbar)
-
         findViewById<TextView>(R.id.seat_selection_title).text = movieUI.title
         val price = findViewById<TextView>(R.id.seat_selection_price)
-        price.text = this.getString(R.string.price, 0)
-        val nextBtn = findViewById<Button>(R.id.seat_selection_check)
+        val goToTicketBtn = findViewById<Button>(R.id.seat_selection_check)
         val seatsView = findViewById<TableLayout>(R.id.seat_selection)
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<TextView>().toList()
 
+        price.text = this.getString(R.string.price, 0)
         val seatState = SeatState(selectedDate)
+
         seatsView.forEachIndexed { index, view ->
             view.setOnClickListener {
                 seatState.setSeatState(index, view, this)
                 price.text = getString(R.string.price, seatState.priceNum)
 
-                nextBtn.isEnabled = (selectedNumberOfPeople == seatState.countPeople)
+                goToTicketBtn.isEnabled = (selectedNumberOfPeople == seatState.countPeople)
             }
         }
-        nextBtn.setOnClickListener {
 
-            val seats = mutableListOf<Int>()
-            seatsView.forEachIndexed { index, view ->
-                if (view.isSelected) seats.add(index)
-            }
-            val ticket = Ticket(
-                seatState.priceNum,
-                selectedDate,
-                seatState.countPeople,
-                seats.map { indexToPosition(it) }
-            )
-
-            AlertDialog.Builder(this)
-                .setTitle("예매 확인")
-                .setMessage("정말 예매하시겠습니까?")
-                .setPositiveButton("예매 완료") { _, _ ->
-                    val intent = Intent(this, TicketActivity::class.java)
-                    intent.putExtra(MOVIE_KEY, movieUI)
-                    intent.putExtra(TICKET_KEY, ticket.toUIModel())
-                    startActivity(intent)
-                }
-                .setNegativeButton("취소") { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .show()
+        goToTicketBtn.setOnClickListener {
+            val seats = seatsView.withIndex().filter { it.value.isSelected }.map { indexToPosition(it.index) }
+            val ticket = Ticket(seatState.priceNum, selectedDate, seatState.countPeople, seats)
+            checkAgain(ticket, movieUI)
         }
+    }
+
+    private fun checkAgain(ticket: Ticket, movieUI: MovieUIModel) {
+        AlertDialog.Builder(this)
+            .setTitle(RESERVATION_CHECK)
+            .setMessage(ARE_YOU_SURE)
+            .setPositiveButton(RESERVATION_COMPLETE) { _, _ ->
+                val intent = Intent(this, TicketActivity::class.java)
+                intent.putExtra(MOVIE_KEY, movieUI)
+                intent.putExtra(TICKET_KEY, ticket.toUIModel())
+                startActivity(intent)
+            }
+            .setNegativeButton(RESERVATION_CANCEL) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
     companion object {
@@ -81,5 +76,9 @@ class SeatSelectionActivity : BaseActivity() {
         private const val DATE_KEY = "date"
         private const val TICKET_KEY = "ticket"
         private const val NUMBER_OF_PEOPLE_KEY = "numberOfPeople"
+        private const val RESERVATION_CHECK = "예매 확인"
+        private const val ARE_YOU_SURE = "정말 예매하시겠습니까?"
+        private const val RESERVATION_COMPLETE = "예매 완료"
+        private const val RESERVATION_CANCEL = "취소"
     }
 }
