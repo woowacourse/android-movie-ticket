d2191cb86bcf8b2f3fcbcf855e850e01d6e8f138
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-20 15:55:51 +0900

    feat(ScreeningSeats): 주입받은 티켓 개수에 따른 좌석 선택을 담당하는 클래스 생성

---
+++ domain/src/main/java/domain/reservation/SeatReservation.kt
@@ -0,0 +1,48 @@
+package domain.reservation
+
+import domain.movie.MovieName
+import domain.seat.ScreeningSeat
+import domain.seat.ScreeningSeats
+import java.time.LocalDateTime
+
+data class SeatReservation(
+    val movieName: MovieName,
+    val screeningTime: LocalDateTime,
+    val selectingCount: TicketCount,
+    val screeningSeats: ScreeningSeats = ScreeningSeats()
+) {
+
+    private val _selectedSeats = mutableListOf<ScreeningSeat>()
+    val selectedSeats: List<ScreeningSeat>
+        get() = _selectedSeats.toList()
+
+    fun selectSeat(seat: ScreeningSeat) {
+        if (_selectedSeats.size == selectingCount.value) {
+            throw IllegalStateException(ERROR_TICKET_COUNT)
+        }
+        val selectedSeat = screeningSeats.selectSeat(seat)
+
+        selectedSeat?.let {
+            _selectedSeats.add(it)
+        }
+    }
+
+    fun cancelSeat(seat: ScreeningSeat) {
+        val selectedSeat = screeningSeats.cancelSeat(seat)
+
+        selectedSeat?.let {
+            _selectedSeats.remove(it)
+        }
+    }
+
+    fun selectingComplete(): List<ScreeningSeat> {
+        if (selectingCount.value == _selectedSeats.size) {
+            return _selectedSeats.toList()
+        }
+        throw IllegalStateException(ERROR_TICKET_COUNT)
+    }
+
+    companion object {
+        private const val ERROR_TICKET_COUNT = "[ERROR]: 티켓 개수만큼 좌석을 선택해야합니다."
+    }
+}
---
+++ domain/src/test/kotlin/domain/reservation/SeatReservationTest.kt
@@ -0,0 +1,58 @@
+package domain.reservation
+
+import domain.movie.MovieName
+import domain.seat.ScreeningSeat
+import domain.seat.SeatColumn
+import domain.seat.SeatRow
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDateTime
+
+class SeatReservationTest {
+
+    private lateinit var seatReservation: SeatReservation
+
+    @Before
+    fun setUp() {
+        seatReservation = SeatReservation(
+            movieName = MovieName("해리포터"),
+            screeningTime = LocalDateTime.of(1999, 10, 6, 22, 0),
+            selectingCount = TicketCount(1)
+        )
+    }
+
+    @Test
+    fun `좌석_선택에_성공하면_선택된_좌석_목록에_추가된다`() {
+        // given
+        seatReservation.selectSeat(ScreeningSeat(SeatRow.A, SeatColumn.FIRST))
+
+        // when
+        val selectedSeats: List<ScreeningSeat> = seatReservation.selectedSeats
+
+        // then
+        val expected = listOf(ScreeningSeat(SeatRow.A, SeatColumn.FIRST))
+
+        assertEquals(selectedSeats, expected)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `티켓수만큼_좌석_선택을_하지_않고_좌석_선택을_완료하게되면_예외가_발생한다`() {
+        seatReservation.selectingComplete()
+    }
+
+    @Test
+    fun `좌석_취소에_성공하면_선택된_좌석_목록에서_삭제된다`() {
+        // given
+        seatReservation.selectSeat(ScreeningSeat(SeatRow.A, SeatColumn.FIRST))
+
+        // when
+        seatReservation.cancelSeat(ScreeningSeat(SeatRow.A, SeatColumn.FIRST))
+        val selectedSeats: List<ScreeningSeat> = seatReservation.selectedSeats
+
+        // then
+        val expected: List<ScreeningSeat> = listOf()
+
+        assertEquals(selectedSeats, expected)
+    }
+}
