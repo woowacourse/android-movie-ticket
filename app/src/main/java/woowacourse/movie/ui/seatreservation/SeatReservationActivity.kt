c56719d5a2129b941f9ce22416d2aaaa214ba410
s9hn <tnghqn@naver.com>
2023-04-20 15:52:52 +0900

    [FEAT(SeatReservationActivity)] 좌선선택 액티비티 추가

---
+++ app/src/main/java/woowacourse/movie/ui/seatreservation/SeatReservationActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.ui.seatreservation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.R
+
+class SeatReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_reservation)
+    }
+}
