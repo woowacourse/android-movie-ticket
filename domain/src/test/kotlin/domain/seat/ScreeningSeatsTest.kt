490330afbd5468faa51f46e10036aa428e51a4a0
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-20 14:14:30 +0900

    test(ScreeningSeats): 좌석 선택과 취소에 대한 테스트 케이스 추가

--- domain/src/test/kotlin/domain/seat/ScreeningSeatsTest.kt
+++ domain/src/test/kotlin/domain/seat/ScreeningSeatsTest.kt
@@ -23,7 +23,36 @@ class ScreeningSeatsTest {
     }
 
     @Test
-    fun `선택된_좌석을_취소하면_해당_좌석은_이용_가능한_상태가_된다`() {
+    fun `이용가능한_좌석을_선택하면_해당_좌석을_반환한다`() {
+        // given
+        val screeningSeats = ScreeningSeats()
+        val seat = ScreeningSeat(SeatRow.A, SeatColumn.FIRST)
+
+        // when
+        val selectedSeat = screeningSeats.selectSeat(seat)
+        val expected = ScreeningSeat(SeatRow.A, SeatColumn.FIRST)
+
+        // then
+        assertEquals(expected, selectedSeat)
+    }
+
+    @Test
+    fun `이미_선택된_좌석을_선택하면_null을_반환한다`() {
+        // given
+        val screeningSeats = ScreeningSeats()
+        val seat = ScreeningSeat(SeatRow.A, SeatColumn.FIRST)
+        screeningSeats.selectSeat(seat)
+
+        // when
+        val selectedSeat = screeningSeats.selectSeat(seat)
+        val expected = null
+
+        // then
+        assertEquals(expected, selectedSeat)
+    }
+
+    @Test
+    fun `선택된_좌석을_취소하면_해당_좌석은_이용가능한_상태가_된다`() {
         // given
         val screeningSeats = ScreeningSeats()
 
@@ -32,9 +61,35 @@ class ScreeningSeatsTest {
         screeningSeats.cancelSeat(seat)
 
         // then
-        assertTrue(
-            screeningSeats.values[seat] == SeatState.AVAILABLE
-        )
+        assertTrue(screeningSeats.values[seat] == SeatState.AVAILABLE)
+    }
+
+    @Test
+    fun `선택된_좌석을_취소하면_해당_좌석을_반환한다`() {
+        // given
+        val screeningSeats = ScreeningSeats()
+        val seat = ScreeningSeat(SeatRow.A, SeatColumn.FIRST)
+        screeningSeats.selectSeat(seat)
+
+        // when
+        val canceledSeat = screeningSeats.cancelSeat(seat)
+        val expected = ScreeningSeat(SeatRow.A, SeatColumn.FIRST)
+
+        // then
+        assertEquals(expected, canceledSeat)
+    }
+
+    @Test
+    fun `이용가능한_좌석을_취소하면_null을_반환한다`() {
+        // given
+        val screeningSeats = ScreeningSeats()
+
+        // when
+        val canceledSeat = screeningSeats.cancelSeat(ScreeningSeat(SeatRow.A, SeatColumn.FIRST))
+        val expected = null
+
+        // then
+        assertEquals(expected, canceledSeat)
     }
 
     @Test
