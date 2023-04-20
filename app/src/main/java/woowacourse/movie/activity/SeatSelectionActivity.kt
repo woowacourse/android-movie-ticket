6b6db8027ae0abdbfd5762791fd496eb1d0f1ab8
otter <a33397397@gmail.com>
2023-04-20 15:59:19 +0900

    feat: 좌석 선택시 색깔 변환 기능 구현

--- app/src/main/java/woowacourse/movie/activity/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/SeatSelectionActivity.kt
@@ -20,32 +20,54 @@ class SeatSelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setSeatsViews()
+        val seats: List<List<TextView>> = setSeatsViews()
+        setSeatSelectEvent(seats)
     }
 
-    private fun setSeatsViews() {
+    private fun setSeatsViews(): List<List<TextView>> {
+        val seats: MutableList<MutableList<TextView>> = mutableListOf()
+
         for (row in MIN_ROW..MAX_ROW) {
             val tableRow = TableRow(this)
             tableRow.layoutParams = TableRow.LayoutParams(
                 TableRow.LayoutParams.MATCH_PARENT, 180, 1f
             )
+            seats.add(mutableListOf())
 
             for (col in MIN_COLUMN..MAX_COLUMN) {
                 val textView = TextView(this)
                 textView.layoutParams = TableRow.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT,
-                    180,
-                    1f
+                    TableRow.LayoutParams.MATCH_PARENT, 180, 1f
                 )
 
                 textView.text = getString(R.string.seat_name_form).format(row, col)
-                textView.setBackgroundColor(getColor(R.color.white))
+                textView.setBackgroundColor(getColor(R.color.not_selected_seat_color))
                 textView.setTextColor(getColor(R.color.black))
                 textView.gravity = Gravity.CENTER
+
+                seats[row - MIN_ROW].add(textView)
                 tableRow.addView(textView)
             }
 
             binding.seatTableLayout.addView(tableRow)
+        }
+
+        return seats
+    }
+
+    private fun setSeatSelectEvent(seats: List<List<TextView>>) {
+        seats.forEach {
+            it.forEach { seat ->
+                seat.setOnClickListener {
+                    if (seat.isSelected) {
+                        seat.setBackgroundColor(getColor(R.color.not_selected_seat_color))
+                        seat.isSelected = false
+                    } else if (!seat.isSelected) {
+                        seat.setBackgroundColor(getColor(R.color.selected_seat_color))
+                        seat.isSelected = true
+                    }
+                }
+            }
         }
     }
 }
