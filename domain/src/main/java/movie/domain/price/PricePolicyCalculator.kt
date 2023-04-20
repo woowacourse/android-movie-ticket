48674b62e4525d6ce60771ef220a17c027555160
Yerin <myheaven_b@naver.com>
2023-04-21 01:03:36 +0900

    refactor: 가격 정책 로직 변경

--- domain/src/main/java/movie/domain/price/PricePolicyCalculator.kt
+++ domain/src/main/java/movie/domain/price/PricePolicyCalculator.kt
@@ -1,9 +1,14 @@
 package movie.domain.price
 
-class PricePolicyCalculator(private val discountPolicies: List<DiscountPolicy> = listOf()) :
-    PricePolicy {
-    override fun totalPriceCalculate(ticketPrice: Int, ticketCount: Int): Int =
-        discountCalculate(ticketPrice) * ticketCount
+import movie.domain.seat.Seat
+
+class PricePolicyCalculator(private val discountPolicies: List<DiscountPolicy>) : PricePolicy {
+
+    override fun totalPriceCalculate(selectedSeats: List<Seat>): Int {
+        return selectedSeats.fold(0) { totalPrice, seat ->
+            totalPrice + discountCalculate(seat.seatRank.price)
+        }
+    }
 
     override fun discountCalculate(price: Int): Int {
         return discountPolicies.fold(price) { discountedPrice, discountPolicy ->
