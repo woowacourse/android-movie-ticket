2827bc8d5602035f6e01a410e6e56e5015b033af
s9hn <tnghqn@naver.com>
2023-04-20 19:59:00 +0900

    [CHORE] roboto font 에러 수정

--- app/src/main/java/woowacourse/movie/ui/seatreservation/SeatReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/seatreservation/SeatReservationActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.ui.seatreservation
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
 
@@ -8,5 +9,10 @@ class SeatReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_reservation)
+
+        val button = findViewById<TextView>(R.id.tv_seat_reservation_check_btn)
+        button.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
     }
 }
