6cdc26e8e02d19b0ad382ac5d78f189047a93b5f
changhwan <chang5hustle@gmail.com>
2023-04-20 14:02:22 +0900

    refactor(ScreeningDateTime): String + 연산 제거

--- app/src/main/java/woowacourse/movie/domain/datetime/ScreeningDateTime.kt
+++ app/src/main/java/woowacourse/movie/domain/datetime/ScreeningDateTime.kt
@@ -13,7 +13,8 @@ class ScreeningDateTime(val time: LocalDateTime, private val screeningPeriod: Sc
         require(time.toLocalDate() in screeningPeriod.start..screeningPeriod.end) {
             SELECTED_SCREENING_DATE_TIME_ERROR.format(
                 time.toString(),
-                screeningPeriod.start.toString() + screeningPeriod.end.toString()
+                screeningPeriod.start.toString(),
+                screeningPeriod.end.toString()
             )
         }
     }
@@ -28,7 +29,7 @@ class ScreeningDateTime(val time: LocalDateTime, private val screeningPeriod: Sc
 
     companion object {
         private const val SELECTED_SCREENING_DATE_TIME_ERROR =
-            "선택된 날짜(%s)가 영화 상영 기간(%s) 범위에서 벗어났습니다."
+            "선택된 날짜(%s)가 영화 상영 기간(%s~%s) 범위에서 벗어났습니다."
         private val MOVIE_DAYS = listOf(10, 20, 30)
         private const val EARLY_MORNING_STANDARD = 11
         private const val LATE_NIGHT_STANDARD = 20
