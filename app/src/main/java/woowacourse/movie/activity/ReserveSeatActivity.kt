967ec73b53118ea6033494ff5d31c8b869fb2ad1
whk06061 <gju06061@naver.com>
2023-04-21 02:55:07 +0900

    refactor: ReserveSeatActivity xml 구현

---
+++ app/src/main/java/woowacourse/movie/activity/ReserveSeatActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.R
+
+class ReserveSeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve_seat)
+    }
+}
