76c0a90aacea286bbd3755977329d42b06180976
hyemdooly <hyemdooly@gmail.com>
2023-04-20 16:07:29 +0900

    feat: SeatView 구현

--- app/src/main/java/woowacourse/movie/activity/seatselect/SeatSelectActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/seatselect/SeatSelectActivity.kt
@@ -8,5 +8,7 @@ class SeatSelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_select)
+        val seatView = SeatView(findViewById(R.id.layout_seat_area))
+        seatView.set()
     }
 }
