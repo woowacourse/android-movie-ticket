cd736ceada85b9864b457dee4388a1f5593be78d
changhwan <chang5hustle@gmail.com>
2023-04-21 17:38:23 +0900

    feat(MovieSelectSeatActivity): UI 작업

---
+++ app/src/main/java/woowacourse/movie/ui/movieselectseatactivity/MovieSelectSeatActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.ui.movieselectseatactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import woowacourse.movie.R
+
+class MovieSelectSeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_select_seat)
+    }
+}
