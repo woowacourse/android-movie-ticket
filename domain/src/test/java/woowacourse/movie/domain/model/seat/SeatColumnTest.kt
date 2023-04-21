bf5386885764acdeeb9135c0ceb0750548862830
tmdgh1592 <tmdgh1592@naver.com>
2023-04-21 14:10:57 +0900

    test: 불필요한 테스트 코드 제거

--- domain/src/test/java/woowacourse/movie/domain/model/seat/SeatColumnTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/model/seat/SeatColumnTest.kt
@@ -1,19 +1,12 @@
 package woowacourse.movie.domain.model.seat
 
 import junitparams.JUnitParamsRunner
-import junitparams.Parameters
 import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(JUnitParamsRunner::class)
 internal class SeatColumnTest {
-    @Test
-    @Parameters("1", "2", "3", "4")
-    internal fun `좌석은 1열부터 4열까지이다`(column: Int) {
-        SeatColumn(column)
-    }
-
     @Test
     internal fun `열이 1보다 작으면 예외가 발생한다`() {
         assertThrows(IllegalArgumentException::class.java) {
--- domain/src/test/java/woowacourse/movie/domain/model/seat/SeatRowTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/model/seat/SeatRowTest.kt
@@ -1,19 +1,12 @@
 package woowacourse.movie.domain.model.seat
 
 import junitparams.JUnitParamsRunner
-import junitparams.Parameters
 import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(JUnitParamsRunner::class)
 internal class SeatRowTest {
-    @Test
-    @Parameters("1", "2", "3", "4", "5")
-    internal fun `좌석은 1행부터 5행까지이다`(row: Int) {
-        SeatRow(row)
-    }
-
     @Test
     internal fun `행이 1보다 작으면 예외가 발생한다`() {
         assertThrows(IllegalArgumentException::class.java) {
