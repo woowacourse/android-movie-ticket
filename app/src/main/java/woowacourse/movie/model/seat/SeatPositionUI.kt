cdf0976e5ccab772bd0c29101e444a9d7480bbac
inseonyun <yuninseon@naver.com>
2023-04-21 17:28:27 +0900

    refactor(SeatPositionUI): 포지션 format 함수 구현

--- app/src/main/java/woowacourse/movie/model/seat/SeatPositionUI.kt
+++ app/src/main/java/woowacourse/movie/model/seat/SeatPositionUI.kt
@@ -7,4 +7,12 @@ import kotlinx.parcelize.Parcelize
 data class SeatPositionUI(
     val row: RowUI,
     val col: ColUI
-) : Parcelable
+) : Parcelable {
+    fun getSeatPositionUIFormat() =
+        POSITION_UI_FORMAT.format(('A' + row.x), (col.y + COLUMN_UI_CONDITION))
+
+    companion object {
+        private const val POSITION_UI_FORMAT = "%c%d"
+        private const val COLUMN_UI_CONDITION = 1
+    }
+}
