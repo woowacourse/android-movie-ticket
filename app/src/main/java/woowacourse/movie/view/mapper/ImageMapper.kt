e0ac9e4d06260f0fa71dfeb6be247b8fc138668d
Doyeop Kim <dygames34@gmail.com>
2023-04-21 20:26:30 +0900

    feat: ImageMapper에 에러 메시지 표시

--- app/src/main/java/woowacourse/movie/view/mapper/ImageMapper.kt
+++ app/src/main/java/woowacourse/movie/view/mapper/ImageMapper.kt
@@ -5,14 +5,20 @@ import woowacourse.movie.domain.Image
 import woowacourse.movie.view.data.ImageViewData
 
 object ImageMapper : Mapper<Image, ImageViewData> {
+    private const val ERROR_IMAGE_NOT_FOUND = "이미지 매핑 정보가 없습니다."
+
     private val images: List<Pair<Image, ImageViewData>> =
         listOf(Image(0) to ImageViewData(R.drawable.poster_harrypotter))
 
     override fun Image.toView(): ImageViewData {
-        return images.find { it.first == this }?.second ?: throw IllegalStateException()
+        return images.find { it.first == this }?.second ?: throw IllegalStateException(
+            ERROR_IMAGE_NOT_FOUND
+        )
     }
 
     override fun ImageViewData.toDomain(): Image {
-        return images.find { it.second == this }?.first ?: throw IllegalStateException()
+        return images.find { it.second == this }?.first ?: throw IllegalStateException(
+            ERROR_IMAGE_NOT_FOUND
+        )
     }
 }
