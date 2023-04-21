21c892b2475b006daf21912d08a1e06fabe23a90
hyemdooly <hyemdooly@gmail.com>
2023-04-21 15:53:43 +0900

    feat: TheaterInfo 생성

---
+++ domain/src/main/java/woowacourse/movie/domain/theater/TheaterInfo.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie.domain.theater
+
+import woowacourse.movie.domain.seat.Grade
+import woowacourse.movie.domain.ticket.Price
+
+class TheaterInfo(
+    private val rowGrade: Map<Int, Grade>,
+    private val seatRow: Int,
+    private val seatCol: Int
+) {
+    fun getGradePrice(row: Int): Price? = rowGrade[row]?.price
+
+    fun isValidSeat(selectRow: Int, selectCol: Int): Boolean = selectRow < seatRow && selectCol < seatCol
+}
