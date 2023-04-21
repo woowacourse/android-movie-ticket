57264bfd54aed0bee00e12f640dd35ec24801a41
briandr97 <briandr@naver.com>
2023-04-21 14:08:04 +0900

    refactor(Position): 위치를 인덱스 기반으로 생각해 0부터 가능하도록 수정

--- domain/src/main/java/woowacourse/movie/ticket/Position.kt
+++ domain/src/main/java/woowacourse/movie/ticket/Position.kt
@@ -1,11 +1,11 @@
 package woowacourse.movie.ticket
 
 data class Position(
-    val column: Int,
     val row: Int,
+    val column: Int,
 ) {
     init {
-        require(column > 0) { "좌석은 1행 부터 존재합니다. 현재 열: $column" }
-        require(row > 0) { "좌석은 1열 부터 존재합니다. 현재 열: $column" }
+        require(column >= 0) { "좌석의 행은 음수일 수 없습니다. 현재 열: $column" }
+        require(row >= 0) { "좌석의 열은 음수일 수 없습니다. 현재 열: $column" }
     }
 }
