1d8de39a675c7dd819ed64e624ffb1dddb2a0fa4
s9hn <tnghqn@naver.com>
2023-04-21 19:50:18 +0900

    [FEAT(Calculator)] 좌석값 총계 계산 기능 구현

---
+++ app/src/main/java/woowacourse/movie/ui/seatreservation/domain/Calculator.kt
@@ -0,0 +1,18 @@
+package woowacourse.movie.ui.seatreservation.domain
+
+class Calculator {
+    var totalAmount: Int = 0
+        private set
+
+    fun plus(selectSeatPrice: Money) {
+        totalAmount += selectSeatPrice.value
+    }
+
+    fun minus(selectSeatPrice: Money) {
+        totalAmount -= selectSeatPrice.value
+    }
+
+    companion object {
+        fun create() = Calculator()
+    }
+}
