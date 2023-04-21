037cd7e63985c7b9a6a0dc45082ab4ac39c5d95e
briandr97 <briandr@naver.com>
2023-04-21 15:31:29 +0900

    feat(Theater): app 모듈에서 도메인을 통해 상영관을 가져오는 로직 추가

---
+++ domain/src/main/java/woowacourse/movie/TheaterRepository.kt
@@ -0,0 +1,25 @@
+package woowacourse.movie
+
+import woowacourse.movie.theater.TheaterDatabase
+import woowacourse.movie.theater.TheaterEntity
+
+object TheaterRepository {
+    fun getTheaters(): List<Theater> {
+        return TheaterDatabase.theaters.map { it.toTheater() }
+    }
+
+    fun getTheater(theaterId: Long): Theater {
+        return TheaterDatabase.selectTheater(theaterId).toTheater()
+    }
+
+    private fun TheaterEntity.toTheater(): Theater {
+        return Theater(
+            id = this.id,
+            rowSize = this.rowSize,
+            columnSize = this.columnSize,
+            sRankRange = this.sRankRange,
+            aRankRange = this.aRankRange,
+            bRankRange = this.bRankRange,
+        )
+    }
+}
