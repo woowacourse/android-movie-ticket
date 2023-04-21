eb0f2efdd606d69cf0da5cd54fb434714a33a206
briandr97 <briandr@naver.com>
2023-04-21 15:12:23 +0900

    feat(Theater): 상영관에 좌석을 전달하면 해당 좌석의 Seat객체를 반환하는 기능 작성

--- domain/src/main/java/woowacourse/movie/Theater.kt
+++ domain/src/main/java/woowacourse/movie/Theater.kt
@@ -1,5 +1,9 @@
 package woowacourse.movie
 
+import woowacourse.movie.ticket.Position
+import woowacourse.movie.ticket.Seat
+import woowacourse.movie.ticket.SeatRank
+
 data class Theater(
     val id: Int,
     val rowSize: Int,
@@ -7,4 +11,27 @@ data class Theater(
     val sRankRange: List<IntRange>,
     val aRankRange: List<IntRange>,
     val bRankRange: List<IntRange>,
-)
+) {
+    private val rankMap: Map<SeatRank, List<IntRange>>
+        get() = mapOf(
+            SeatRank.S to sRankRange,
+            SeatRank.A to aRankRange,
+            SeatRank.B to bRankRange,
+        )
+
+    fun selectSeat(position: Position): Seat {
+        return Seat(getSeatRank(position.row), position)
+    }
+
+    private fun getSeatRank(row: Int): SeatRank {
+        rankMap.entries.forEach { return it.matchedKey(row) }
+        throw IllegalArgumentException()
+    }
+
+    private fun Map.Entry<SeatRank, List<IntRange>>.matchedKey(row: Int): SeatRank {
+        if (value.any { range -> row in range }) {
+            return key
+        }
+        throw IllegalArgumentException()
+    }
+}
