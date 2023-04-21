94b9ff95af4b868d5f9c889dc76bf67b376cbf88
vrexpert <vrexpert2302@gmail.com>
2023-04-21 16:53:48 +0900

    refactor(MovieModel): 영화 UI 모델 reserve 가 seatList 를 받도록 구현

--- app/src/main/java/woowacourse/movie/presentation/model/MovieModel.kt
+++ app/src/main/java/woowacourse/movie/presentation/model/MovieModel.kt
@@ -2,8 +2,9 @@ package woowacourse.movie.presentation.model
 
 import androidx.annotation.DrawableRes
 import woowacourse.movie.TicketCount
+import woowacourse.movie.model.Seat
+import woowacourse.movie.presentation.Reservation
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class MovieModel(
     val id: Long,
@@ -15,8 +16,9 @@ data class MovieModel(
     @DrawableRes val thumbnail: Int?,
     @DrawableRes val poster: Int?,
 ) {
-    fun reserve(dateTime: LocalDateTime, ticketCount: TicketCount): TicketModel =
-        toDomainModel().reserve(dateTime, ticketCount).toPresentation()
+    fun reserve(reservation: Reservation, seats: List<Seat>): TicketModel =
+        toDomainModel().reserve(reservation.bookedDateTime, TicketCount(reservation.count), seats)
+            .toPresentation()
 
     fun getScreeningDates(): List<LocalDate> = toDomainModel().getScreeningDates()
 }
