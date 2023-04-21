c41f0a8a1b5f430812d97e53228d9c9422a4bc66
krrong <khk9664@naver.com>
2023-04-22 04:40:11 +0900

    refactor(MovieTicket): 역할이 모호한 클래스 삭제

--- app/src/main/java/woowacourse/movie/uimodel/Mapper.kt
+++ app/src/main/java/woowacourse/movie/uimodel/Mapper.kt
@@ -1,22 +1,13 @@
 package woowacourse.movie.utils
 
-import movie.data.Movie
 import movie.data.MovieDetail
 import movie.data.MovieSchedule
-import movie.data.MovieTicket
 import movie.screening.ScreeningDate
-import movie.seat.Seat
 import woowacourse.movie.uimodel.MovieDetailUi
 import woowacourse.movie.uimodel.MovieModelUi
-import woowacourse.movie.uimodel.MovieTicketUi
 
 fun MovieModelUi.MovieScheduleUi.toDomain(): MovieSchedule {
     return MovieSchedule(
-        Movie(
-            title,
-            runningTime,
-            summary,
-        ),
         ScreeningDate(
             startDate,
             endDate,
@@ -26,12 +17,4 @@ fun MovieModelUi.MovieScheduleUi.toDomain(): MovieSchedule {
 
 fun MovieDetailUi.toDomain(): MovieDetail {
     return MovieDetail(count, title, date, time)
-}
-
-fun MovieTicketUi.toDomain(): MovieTicket {
-    val movieTicket = MovieTicket(totalPrice, count, title, date, time)
-    seats.forEach {
-        movieTicket.addSeat(Seat.of(it))
-    }
-    return movieTicket
 }
--- domain/src/main/java/movie/data/MovieSchedule.kt
+++ domain/src/main/java/movie/data/MovieSchedule.kt
@@ -3,7 +3,6 @@ package movie.data
 import movie.screening.ScreeningDate
 
 class MovieSchedule(
-    private val movie: Movie,
     private val screeningDate: ScreeningDate,
 ) {
     fun getScreeningDate(): List<String> = screeningDate.getScreeningDate()
