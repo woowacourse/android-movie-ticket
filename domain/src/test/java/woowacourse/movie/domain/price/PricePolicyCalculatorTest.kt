6867d21970e28812239aa578db488fc92bdd916e
changhwan <chang5hustle@gmail.com>
2023-04-23 07:31:41 +0900

    test: 변경에따른 테스트 수정

--- domain/src/test/java/woowacourse/movie/domain/price/PricePolicyCalculatorTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/price/PricePolicyCalculatorTest.kt
@@ -5,14 +5,13 @@ import org.junit.Test
 
 class PricePolicyCalculatorTest {
     @Test
-    fun `할인이 없는경우 13000원짜리 표 3장을 구매하면 39000원이다`() {
+    fun `할인이 없는경우 13000원짜리 표를 구매하면 13000원이다`() {
         val pricePolicy = woowacourse.movie.domain.price.pricecalculate.PricePolicyCalculator()
         assertEquals(
-            pricePolicy.totalPriceCalculate(
-                TicketPrice(13000),
-                TicketCount(3)
+            pricePolicy.discountCalculate(
+                TicketPrice(13000)
             ),
-            TicketPrice(39000)
+            TicketPrice(13000)
         )
     }
 }
