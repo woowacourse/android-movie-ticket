b16fbf4444df4b056862566eff0426ad729157df
briandr97 <briandr@naver.com>
2023-04-22 18:27:32 +0900

    feat(Extension): Parcelable과 Toast의 확장함수 제작

---
+++ app/src/main/java/woowacourse/movie/util/ContextExtension.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
--- app/src/main/java/woowacourse/movie/util/IntentExtension.kt
+++ app/src/main/java/woowacourse/movie/util/IntentExtension.kt
@@ -2,6 +2,7 @@ package woowacourse.movie.util
 
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import java.io.Serializable
 
 inline fun <reified T : Serializable> Intent.getSerializable(key: String, data: Class<T>): T {
@@ -17,5 +18,13 @@ fun <T> Intent.getParcelable(key: String, data: Class<T>): T {
         getParcelableExtra(key, data) ?: throw IllegalArgumentException()
     } else {
         getParcelableExtra(key) as T? ?: throw IllegalArgumentException()
+    }
+}
+
+fun <T> Bundle.getParcelableBundle(key: String, data: Class<T>): T {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelable(key, data) ?: throw IllegalArgumentException()
+    } else {
+        getParcelable(key) as T? ?: throw IllegalArgumentException()
     }
 }
