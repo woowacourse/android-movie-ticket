f481ca0487d1549f002145b1c6945a06c1f54763
whk06061 <gju06061@naver.com>
2023-04-22 23:26:52 +0900

    test: RankTest 구현

---
+++ app/src/test/java/woowacourse/movie/RankTest.kt
@@ -0,0 +1,26 @@
+package woowacourse.movie
+
+import com.example.domain.model.model.Rank
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RankTest {
+
+    @Test
+    fun `0 또는 1 번째 열이면 B 등급이다`() {
+        val actual: Rank = Rank.map(0)
+        assertEquals(actual, Rank.B)
+    }
+
+    @Test
+    fun `2 또는 3 번째 열이면 S 등급이다`() {
+        val actual: Rank = Rank.map(2)
+        assertEquals(actual, Rank.S)
+    }
+
+    @Test
+    fun `4 번째 열이면 A 등급이다`() {
+        val actual: Rank = Rank.map(4)
+        assertEquals(actual, Rank.A)
+    }
+}
