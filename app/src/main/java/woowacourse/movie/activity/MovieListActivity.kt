2b49666dba56b02b857dbc7534abc97620c0627d
whk06061 <gju06061@naver.com>
2023-04-22 19:35:26 +0900

    refactor: 함수 분리 및 아이템 리스트 생성 로직 간소화

--- app/src/main/java/woowacourse/movie/activity/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MovieListActivity.kt
@@ -1,7 +1,6 @@
 package woowacourse.movie.activity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,37 +21,44 @@ class MovieListActivity : AppCompatActivity() {
 
     private fun initRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val adapter = MovieListAdapter(getMovieData())
+        val adapter = MovieListAdapter(getMovieListData())
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
     }
 
-    private fun getMovieData(): List<MovieListItem> {
-        val movies = List(1000) {
-            Movie(
-                R.drawable.movie_poster,
-                "해리포터 $it",
-                LocalDate.of(2023, 3, 1),
-                LocalDate.of(2023, 3, 31),
-                152,
-                "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
-            ).toMovieModel()
-        }
-        val ads = List(500) {
-            MovieListItem.AdModel(R.drawable.advertisement)
-        }
+    private fun getMovieListData(): List<MovieListItem> {
+        val movies = getMovieData()
+        val ads = getAdData()
         val items = mutableListOf<MovieListItem>()
-        var movieIndex = 0
         var adIndex = 0
-        while (movieIndex < movies.size) {
-            Log.d("혜경 movieIndex", movieIndex.toString())
-            Log.d("혜경 adIndex", adIndex.toString())
-            items.add(movies[movieIndex++])
-            if (movieIndex % 3 == 0) {
-                items.add(ads[adIndex++ % ads.size])
-                Log.d("혜경 광고 들어감", "")
+
+        movies.forEachIndexed { movieIndex, movie ->
+            items.add(movie)
+            if ((movieIndex + 1) % AD_CYCLE == 0) {
+                items.add(ads[adIndex % ads.size])
+                adIndex++
             }
         }
+
         return items
+    }
+
+    private fun getMovieData() = List(1000) {
+        Movie(
+            R.drawable.movie_poster,
+            "해리포터 $it",
+            LocalDate.of(2023, 3, 1),
+            LocalDate.of(2023, 3, 31),
+            152,
+            "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
+        ).toMovieModel()
+    }
+
+    private fun getAdData() = List(500) {
+        MovieListItem.AdModel(R.drawable.advertisement)
+    }
+
+    companion object {
+        private const val AD_CYCLE = 3
     }
 }
