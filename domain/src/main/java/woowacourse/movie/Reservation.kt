dca40aebaeadd28592a2476f17ea676e7100fc92
briandr97 <briandr@naver.com>
2023-04-22 18:06:35 +0900

    feat(Reservation): 가독성을 좋게 해주는 기능 추가

--- domain/src/main/java/woowacourse/movie/Reservation.kt
+++ domain/src/main/java/woowacourse/movie/Reservation.kt
@@ -1,11 +1,14 @@
 package woowacourse.movie
 
 import woowacourse.movie.ticket.Ticket
+import java.time.LocalDateTime
 
 data class Reservation(
-    val tickets: List<Ticket>,
+    val tickets: Set<Ticket>,
     val paymentType: PaymentType = PaymentType.OFFLINE,
 ) {
     val payment: Int get() = tickets.sumOf { it.price }
-    val movieId: Long get() = tickets[0].movieId
+    val movieId: Long get() = tickets.first().movieId
+    val bookedDateTime: LocalDateTime get() = tickets.first().bookedDateTime
+    val count: Int get() = tickets.size
 }
