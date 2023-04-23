37838d09480f3668b5ae2478db223467fa1a1524
EmilyCh0 <emily9803@ewhain.net>
2023-04-23 18:46:04 +0900

    test(SeatSelectionActivityTest, ReservationActivityTest): newIntent 메서드 사용해서 인텐트 생성하도록 수정

--- app/src/androidTest/java/woowacourse/movie/view/ReservationActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/ReservationActivityTest.kt
@@ -1,6 +1,5 @@
 package woowacourse.movie.view
 
-import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -28,12 +27,10 @@ class ReservationActivityTest {
         "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
     )
 
-    private val intent = Intent(
+    private val intent = ReservationActivity.newIntent(
         ApplicationProvider.getApplicationContext(),
-        ReservationActivity::class.java
-    ).apply {
-        putExtra("MOVIE_ITEM", movie)
-    }
+        movie
+    )
 
     @get:Rule
     val activityRule = ActivityScenarioRule<ReservationActivity>(intent)
--- app/src/androidTest/java/woowacourse/movie/view/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/SeatSelectionActivityTest.kt
@@ -1,6 +1,5 @@
 package woowacourse.movie.view
 
-import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -45,13 +44,11 @@ class SeatSelectionActivityTest {
         "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
     )
 
-    private val intent = Intent(
+    private val intent = SeatSelectionActivity.newIntent(
         ApplicationProvider.getApplicationContext(),
-        SeatSelectionActivity::class.java
-    ).apply {
-        putExtra("RESERVATION_OPTIONS", reservationOptions)
-        putExtra("MOVIE", movie.toUiModel())
-    }
+        reservationOptions,
+        movie.toUiModel()
+    )
 
     @get:Rule
     val activityRule = ActivityScenarioRule<SeatSelectionActivity>(intent)
