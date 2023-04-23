846973f2e1cfd14f1b60c212974513a923df21a8
ki960213 <ki960213@github.com>
2023-04-23 19:56:34 +0900

    refactor(DiscountPolicy): DiscountPolicy 클래스를 enum class에서 abstract class로 변경

--- domain/src/main/java/woowacourse/movie/domain/DiscountPolicy.kt
+++ domain/src/main/java/woowacourse/movie/domain/DiscountPolicy.kt
@@ -1,58 +1,17 @@
 package woowacourse.movie.domain
 
 import java.time.LocalDateTime
-import java.time.LocalTime
 
-enum class DiscountPolicy {
-    MOVIE_DAY {
-        override val discountCondition: DiscountCondition = createDiscountCondition()
+abstract class DiscountPolicy(
+    private val discountConditions: List<DiscountCondition>,
+    val priority: Int
+) {
 
-        private fun createDiscountCondition(): DiscountCondition {
-            val movieDays: List<Int> = listOf(10, 20, 30)
+    fun getDiscountedFee(screeningDateTime: LocalDateTime, initFee: Money): Money =
+        if (discountConditions.any { it.isSatisfiedBy(screeningDateTime) }) {
+            val discountFee = calculateDiscountFee(initFee)
+            if (initFee < discountFee) Money(0) else initFee - discountFee
+        } else initFee
 
-            return DayDiscountCondition(movieDays)
-        }
-
-        override fun calculateDiscountFee(movieFee: Money): Money {
-            val movieDaysDiscountRate = 10
-            return movieFee / movieDaysDiscountRate
-        }
-    },
-    SCREENING_TIME {
-        override val discountCondition: DiscountCondition = createDiscountCondition()
-
-        private fun createDiscountCondition(): DiscountCondition {
-            val earlyMorningDiscountTimeRange =
-                TimeRange(LocalTime.MIN, LocalTime.of(11, 0))
-            val nightDiscountTimeRange = TimeRange(LocalTime.of(20, 0), LocalTime.MAX)
-
-            return ScreeningTimeDiscountCondition(
-                listOf(earlyMorningDiscountTimeRange, nightDiscountTimeRange)
-            )
-        }
-
-        override fun calculateDiscountFee(movieFee: Money): Money {
-            val screeningTimeDiscountAmount = 2000
-            return Money(screeningTimeDiscountAmount)
-        }
-    };
-
-    protected abstract val discountCondition: DiscountCondition
-
-    protected abstract fun calculateDiscountFee(movieFee: Money): Money
-
-    private fun getDiscountFee(screeningDateTime: LocalDateTime, movieFee: Money): Money =
-        if (discountCondition.isSatisfiedBy(screeningDateTime)) calculateDiscountFee(movieFee)
-        else Money(0)
-
-    companion object {
-        fun getDiscountedFee(screeningDateTime: LocalDateTime, movieFee: Money): Money {
-            var totalFee = movieFee
-            values().forEach {
-                totalFee -= it.getDiscountFee(screeningDateTime, movieFee)
-                println(totalFee)
-            }
-            return totalFee
-        }
-    }
+    protected abstract fun calculateDiscountFee(initFee: Money): Money
 }
--- domain/src/test/java/woowacourse/movie/domain/DiscountPolicyTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/DiscountPolicyTest.kt
@@ -4,60 +4,39 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
-class DiscountPolicyTest {
+internal class DiscountPolicyTest {
 
     @Test
-    fun `무비 데이이고 조조나 야간이 아닐 때 예매하면 영화 요금을 10퍼센트 할인 받는다`() {
-        val screeningDateTime = LocalDateTime.of(2021, 3, 10, 15, 0)
-        val movieFee = Money(13_000)
+    fun `할인 조건을 만족하지 않는다면 할인 적용된 금액은 원래 금액과 같다`() {
+        val discountCondition = DayDiscountCondition(listOf(1))
+        val discountPolicy = FixDiscountPolicy(listOf(discountCondition), 1, Money(10_000))
+        val initFee = Money(5_000)
 
-        val actual = DiscountPolicy.getDiscountedFee(screeningDateTime, movieFee)
+        val actual =
+            discountPolicy.getDiscountedFee(LocalDateTime.of(2024, 3, 31, 0, 0), initFee)
 
-        val expected = Money(11_700)
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo(initFee)
     }
 
     @Test
-    fun `무비 데이가 아니고 조조일 때 예매하면 영화 요금을 2000원 할인 받는다`() {
-        val screeningDateTime = LocalDateTime.of(2021, 3, 1, 10, 0)
-        val movieFee = Money(13_000)
+    fun `할인 조건을 만족할 때 할인 금액이 원래 금액보다 크다면 할인이 적용된 금액은 0원이다`() {
+        val discountCondition = DayDiscountCondition(listOf(1))
+        val discountPolicy = FixDiscountPolicy(listOf(discountCondition), 1, Money(10_000))
 
-        val actual = DiscountPolicy.getDiscountedFee(screeningDateTime, movieFee)
+        val actual =
+            discountPolicy.getDiscountedFee(LocalDateTime.of(2024, 3, 1, 0, 0), Money(5_000))
 
-        val expected = Money(11_000)
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo(Money(0))
     }
 
     @Test
-    fun `무비 데이가 아니고 야간일 때 예매하면 영화 요금을 2000원 할인을 받는다`() {
-        val screeningDateTime = LocalDateTime.of(2021, 3, 1, 20, 0)
-        val movieFee = Money(13_000)
+    fun `할인 조건을 만족할 때 할인 금액이 원래 금액보다 작거나 같아면 할인이 적용된 금액은 원래 금액에서 할인 금액을 뺀 금액이다`() {
+        val discountCondition = DayDiscountCondition(listOf(1))
+        val discountPolicy = FixDiscountPolicy(listOf(discountCondition), 1, Money(5_000))
 
-        val actual = DiscountPolicy.getDiscountedFee(screeningDateTime, movieFee)
+        val actual =
+            discountPolicy.getDiscountedFee(LocalDateTime.of(2024, 3, 1, 0, 0), Money(10_000))
 
-        val expected = Money(11_000)
-        assertThat(actual).isEqualTo(expected)
-
-    }
-
-    @Test
-    fun `무비 데이 할인과 상영 시간 할인을 둘 다 받으면 무비 데이 할인을 먼저 적용된다`() {
-        val screeningDateTime = LocalDateTime.of(2021, 3, 10, 20, 0)
-        val movieFee = Money(13_000)
-
-        val actual = DiscountPolicy.getDiscountedFee(screeningDateTime, movieFee)
-
-        val expected = Money(9700)
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `어떤 할인 조건도 만족하지 않는다면 할인된 금액은 초기 예매 금액과 같다`() {
-        val screeningDateTime = LocalDateTime.of(2021, 3, 1, 15, 0)
-        val movieFee = Money(13_000)
-
-        val actual = DiscountPolicy.getDiscountedFee(screeningDateTime, movieFee)
-
-        assertThat(actual).isEqualTo(movieFee)
+        assertThat(actual).isEqualTo(Money(5_000))
     }
 }
