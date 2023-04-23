166dfb7bbcec5894b4f83db2c32452dfc3609934
Yerin <myheaven_b@naver.com>
2023-04-23 15:07:26 +0900

    feat: 티켓 정보를 클래스로 관리

---
+++ app/src/main/java/woowacourse/movie/model/TicketState.kt
@@ -0,0 +1,13 @@
+package woowacourse.movie.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class TicketState(
+    val count: CountState,
+    val movieData: MovieDataState,
+    val screeningDateTime: ScreeningDateTimeState,
+    var price: Int = 0,
+    val seatSelection: MutableList<SeatState> = mutableListOf()
+) : Parcelable
---
+++ domain/src/main/java/movie/domain/Ticket.kt
@@ -0,0 +1,30 @@
+package movie.domain
+
+import movie.domain.datetime.ScreeningDateTime
+import movie.domain.price.DiscountPolicy
+import movie.domain.price.EarlyMorningLateNightDiscount
+import movie.domain.price.MovieDayDiscount
+import movie.domain.price.PricePolicyCalculator
+import movie.domain.seat.Seat
+
+class Ticket(
+    val count: Count,
+    val movieData: MovieData,
+    val screeningDateTime: ScreeningDateTime,
+    price: Int,
+    val seatSelection: MutableList<Seat> = mutableListOf()
+) {
+    val price
+        get() = calculatePrice()
+
+    private fun calculatePrice(): Int {
+        val discountPolicies = mutableListOf<DiscountPolicy>()
+        if (screeningDateTime.checkMovieDay()) {
+            discountPolicies.add(MovieDayDiscount())
+        }
+        if (screeningDateTime.checkEarlyMorningLateNight()) {
+            discountPolicies.add(EarlyMorningLateNightDiscount())
+        }
+        return PricePolicyCalculator(discountPolicies).totalPriceCalculate(seatSelection)
+    }
+}
