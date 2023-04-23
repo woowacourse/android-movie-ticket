c2335700e236b78ef72e912d46181e041d44d626
briandr97 <briandr@naver.com>
2023-04-24 06:23:24 +0900

    feat(recyclerview): 리스트뷰에서 리사이클러뷰로 교체

--- app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
@@ -2,15 +2,16 @@ package woowacourse.movie.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.advertisement.AdvertisementPolicy
 import woowacourse.movie.advertisement.AdvertisementRepository
-import woowacourse.movie.model.main.MainMapper.toUiModel
+import woowacourse.movie.model.main.MainMapper.toUiAdvertisements
+import woowacourse.movie.model.main.MainMapper.toUiMovies
 import woowacourse.movie.movie.MovieRepository
 import woowacourse.movie.ui.booking.BookingActivity
-import woowacourse.movie.ui.main.adapter.listview.MovieAdapter
+import woowacourse.movie.ui.main.adapter.recyclerview.MainAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,17 +22,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initAdapter() {
-        val movieAdapter = MovieAdapter(
+        /*val movieAdapter = MovieAdapter(
+            this,
+            { clickBook(it) },
+            { clickAdvertisement(it) },
+        )*/
+        val mainAdapter = MainAdapter(
             this,
             { clickBook(it) },
             { clickAdvertisement(it) },
         )
-        findViewById<ListView>(R.id.listMainMovie).adapter = movieAdapter
+        findViewById<RecyclerView>(R.id.listMainMovie).adapter = mainAdapter
 
-        val movies = MovieRepository.getMovies().toUiModel()
-        val advertisements = AdvertisementRepository.getAdvertisements().toUiModel()
+        val movies = MovieRepository.getMovies().toUiMovies()
+        val advertisements = AdvertisementRepository.getAdvertisements().toUiAdvertisements()
         val data = AdvertisementPolicy.mergeAdvertisement(movies, advertisements)
-        movieAdapter.initMovies(data)
+        mainAdapter.initMovies(data)
     }
 
     private fun clickBook(movieId: Long) {
---
+++ app/src/main/java/woowacourse/movie/ui/main/adapter/recyclerview/MainAdapter.kt
@@ -0,0 +1,55 @@
+package woowacourse.movie.ui.main.adapter.recyclerview
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.R
+import woowacourse.movie.model.main.MainData
+import woowacourse.movie.model.main.MainViewType
+
+class MainAdapter(
+    context: Context,
+    private val clickBook: (Long) -> Unit,
+    private val clickAd: (Intent) -> Unit,
+) : RecyclerView.Adapter<MainViewHolder>() {
+    private val movies = mutableListOf<MainData>()
+    private val layoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val mainViewType = MainViewType.getMainViewType(viewType)
+        val viewHolder: MainViewHolder = when (mainViewType) {
+            MainViewType.CONTENT -> {
+                MovieViewHolder(layoutInflater.inflate(R.layout.movie_list_item, parent, false))
+            }
+            MainViewType.ADVERTISEMENT -> {
+                AdvertisementViewHolder(
+                    layoutInflater.inflate(R.layout.advertisement_list_item, parent, false),
+                )
+            }
+        }
+        setViewHolderClick(viewHolder)
+        return viewHolder
+    }
+
+    private fun setViewHolderClick(viewHolder: MainViewHolder) {
+        when (viewHolder) {
+            is MovieViewHolder -> viewHolder.setBookingClick(clickBook)
+            is AdvertisementViewHolder -> viewHolder.setAdvertisementClick(clickAd)
+        }
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.onBind(movies[position])
+    }
+
+    override fun getItemCount(): Int = movies.size
+    override fun getItemViewType(position: Int): Int = movies[position].mainViewType.ordinal
+
+    fun initMovies(items: List<MainData>) {
+        movies.clear()
+        movies.addAll(items)
+        notifyDataSetChanged()
+    }
+}
