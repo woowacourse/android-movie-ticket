e4c24cf30a8054757aa51f6144ffb1f86e5d6242
ki960213 <ki960213@github.com>
2023-04-23 22:03:29 +0900

    feat(Movie): 영화의 상영과 좌석들의 위치를 받아서 예매 금액을 계산하는 기능 구현

--- domain/src/main/java/woowacourse/movie/domain/Movie.kt
+++ domain/src/main/java/woowacourse/movie/domain/Movie.kt
@@ -16,19 +16,26 @@ class Movie(
 
     fun reserve(screeningInfo: ScreeningInfoOfMovie, seatPoints: Set<Point>): ReservationResult {
         require(screeningInfo in _screeningInfos) { NOT_EXIST_SCREENING_INFO_ERROR }
-        require(screeningInfo.canBeReserved(seatPoints)) { CAN_NOT_BE_RESERVED_ERROR }
+        require(screeningInfo.canBeReserved(seatPoints)) { NOT_EXIST_SEAT_ERROR }
 
-        val fee = seatPoints.sumOf {
-            val initFee = screeningInfo.getInitFee(it)
-            DiscountApplicator.applyDiscount(screeningInfo.screeningDateTime, initFee)
-        }
+        val fee = calculateReservationFee(screeningInfo, seatPoints)
 
         return ReservationResult(id, screeningInfo, seatPoints, fee)
     }
 
+    fun calculateReservationFee(screeningInfo: ScreeningInfoOfMovie, seatPoints: Set<Point>): Money {
+        require(screeningInfo in _screeningInfos) { NOT_EXIST_SCREENING_INFO_ERROR }
+        require(screeningInfo.canBeReserved(seatPoints)) { NOT_EXIST_SEAT_ERROR }
+
+        return seatPoints.sumOf {
+            val initFee = screeningInfo.getInitFee(it)
+            DiscountApplicator.applyDiscount(screeningInfo.screeningDateTime, initFee)
+        }
+    }
+
     companion object {
-        private const val NOT_EXIST_SCREENING_INFO_ERROR = "예매하려는 상영이 존재하지 않습니다."
-        private const val CAN_NOT_BE_RESERVED_ERROR = "해당 좌석을 예매할 수 없습니다."
+        private const val NOT_EXIST_SCREENING_INFO_ERROR = "상영이 존재하지 않습니다."
+        private const val NOT_EXIST_SEAT_ERROR = "좌석이 존재하지 않습니다."
     }
 }
 
--- domain/src/test/java/woowacourse/movie/domain/MovieTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/MovieTest.kt
@@ -16,7 +16,7 @@ internal class MovieTest {
 
         assertThatIllegalArgumentException().isThrownBy {
             movie.reserve(ScreeningInfoOfMovie(LocalDateTime.MIN, movieHouse), setOf(Point(1, 1)))
-        }.withMessage("예매하려는 상영이 존재하지 않습니다.")
+        }.withMessage("상영이 존재하지 않습니다.")
     }
 
     @Test
@@ -27,7 +27,7 @@ internal class MovieTest {
 
         assertThatIllegalArgumentException().isThrownBy {
             movie.reserve(ScreeningInfoOfMovie(LocalDateTime.MAX, movieHouse), setOf(Point(1, 3)))
-        }.withMessage("해당 좌석을 예매할 수 없습니다.")
+        }.withMessage("좌석이 존재하지 않습니다.")
     }
 
     @Test
@@ -42,6 +42,28 @@ internal class MovieTest {
     }
 
     @Test
+    fun `영화가 가지고 있지 않는 상영 정보에 대해 예매 금액을 계산하면 에러가 발생한다`() {
+        val movie = Movie(1, "제목", Minute(152), "요약")
+        val movieHouse = MovieHouse(1, setOf(Seat(1, 1, SeatClass.S), Seat(1, 2, SeatClass.B)))
+        movie.addScreening(ScreeningInfoOfMovie(LocalDateTime.MAX, movieHouse))
+
+        assertThatIllegalArgumentException().isThrownBy {
+            movie.calculateReservationFee(ScreeningInfoOfMovie(LocalDateTime.MIN, movieHouse), setOf(Point(1, 1)))
+        }.withMessage("상영이 존재하지 않습니다.")
+    }
+
+    @Test
+    fun `예매 금액을 계산할 때 영화가 상영되는 영화관에 입력 받은 위치 중 좌석이 없는 위치가 있다면 에러가 발생한다`() {
+        val movie = Movie(1, "제목", Minute(152), "요약")
+        val movieHouse = MovieHouse(1, setOf(Seat(1, 1, SeatClass.S), Seat(1, 2, SeatClass.B)))
+        movie.addScreening(ScreeningInfoOfMovie(LocalDateTime.MAX, movieHouse))
+
+        assertThatIllegalArgumentException().isThrownBy {
+            movie.calculateReservationFee(ScreeningInfoOfMovie(LocalDateTime.MAX, movieHouse), setOf(Point(1, 3)))
+        }.withMessage("좌석이 존재하지 않습니다.")
+    }
+
+    @Test
     fun `예매할 수 있다면 예매 금액은 모든 좌석 위치에 할인이 적용된 금액의 합과 같다`() {
         val movie = Movie(1, "제목", Minute(152), "요약")
         val movieHouse = MovieHouse(1, setOf(Seat(1, 1, SeatClass.S), Seat(1, 2, SeatClass.B)))
@@ -49,9 +71,9 @@ internal class MovieTest {
             ScreeningInfoOfMovie(LocalDateTime.of(2024, 3, 10, 9, 0), movieHouse)
         movie.addScreening(screeningInfoOfMovie)
 
-        val actual = movie.reserve(
+        val actual = movie.calculateReservationFee(
             screeningInfoOfMovie, setOf(Point(1, 1), Point(1, 2))
-        ).fee
+        )
 
         assertThat(actual).isEqualTo(Money(18_500))
     }
