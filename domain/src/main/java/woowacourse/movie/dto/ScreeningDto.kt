016a6993cabb4c8642326791572280e47dd437d7
ki960213 <ki960213@github.com>
2023-04-23 22:31:43 +0900

    feat(ScreeningInfoDto): 예매할 수 있는 좌석 정보 추가

--- domain/src/main/java/woowacourse/movie/dto/ScreeningDto.kt
+++ domain/src/main/java/woowacourse/movie/dto/ScreeningDto.kt
@@ -3,11 +3,16 @@ package woowacourse.movie.dto
 import woowacourse.movie.domain.ScreeningInfoOfMovie
 import java.time.LocalDateTime
 
-data class ScreeningInfoDto(val screeningDateTime: LocalDateTime) {
+data class ScreeningInfoDto(
+    val screeningDateTime: LocalDateTime,
+    val seatPoints: List<Pair<Int, Int>>
+) {
 
     companion object {
         fun from(screeningInfo: ScreeningInfoOfMovie): ScreeningInfoDto {
-            return ScreeningInfoDto(screeningInfo.screeningDateTime)
+            return ScreeningInfoDto(
+                screeningInfo.screeningDateTime,
+                screeningInfo.movieHouse.seats.map { it.point.row to it.point.column })
         }
     }
 }
