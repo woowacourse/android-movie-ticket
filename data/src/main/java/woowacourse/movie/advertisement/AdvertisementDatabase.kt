e50b52680515cfff149e8e28010b2dd8cf40a74f
briandr97 <briandr@naver.com>
2023-04-24 05:22:05 +0900

    feat(AdvertisementDatabase): 광고 디비에서 select 로직 추가

--- data/src/main/java/woowacourse/movie/advertisement/AdvertisementDatabase.kt
+++ data/src/main/java/woowacourse/movie/advertisement/AdvertisementDatabase.kt
@@ -2,6 +2,11 @@ package woowacourse.movie.advertisement
 
 object AdvertisementDatabase {
     val advertisements = listOf<AdvertisementEntity>(
-        AdvertisementEntity("https://woowacourse.github.io/"),
+        AdvertisementEntity(0, "https://woowacourse.github.io/"),
     )
+
+    fun selectAdvertisement(id: Long): AdvertisementEntity {
+        return advertisements.find { it.id == id }
+            ?: throw NoSuchElementException()
+    }
 }
