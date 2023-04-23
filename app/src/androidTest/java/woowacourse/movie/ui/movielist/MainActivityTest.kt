484946520982b6d9281c82b6d46e320ed897e2d2
Sujin Kim <sujinkim919@gmail.com>
2023-04-23 22:57:51 +0900

    test: 무비 리스트에 대한 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/ui/movielist/MainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/ui/movielist/MainActivityTest.kt
@@ -1,6 +1,8 @@
 package woowacourse.movie.ui.movielist
 
 import android.content.Intent
+import android.net.Uri
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -10,16 +12,20 @@ import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.release
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.core.AllOf.allOf
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
 import woowacourse.movie.ui.moviedetail.MovieDetailActivity
+import woowacourse.movie.ui.movielist.adapter.MovieListViewType
 
 @RunWith(AndroidJUnit4::class)
 internal class MainActivityTest {
@@ -31,6 +37,11 @@ internal class MainActivityTest {
         init()
     }
 
+    @After
+    fun tearDown() {
+        release()
+    }
+
     @Test
     fun 첫번째_아이템을_선택하면_MovieDetailActivity로_이동한다() {
         onView(withId(R.id.main_movie_list))
@@ -40,15 +51,27 @@ internal class MainActivityTest {
     }
 
     @Test
-    fun 광고_아이템을_선택하면_ACTION_VIEW_intent가_실행된다() {
+    fun 광고_아이템을_선택하면_URI를_가진_ACTION_VIEW_intent가_실행된다() {
         onView(withId(R.id.main_movie_list))
             .perform(actionOnItemAtPosition<ViewHolder>(3, click()))
 
-        intended(hasAction(Intent.ACTION_VIEW))
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(Uri.parse("https://github.com/woowacourse"))
+            )
+        )
     }
 
-    @After
-    fun tearDown() {
-        release()
+    @Test
+    fun 영화가_세_번_노출될_때마다_광고가_한_번_노출된다() {
+        activityRule.scenario.onActivity { activity ->
+            val recyclerView = activity.findViewById<RecyclerView>(R.id.main_movie_list)
+
+            val actual = recyclerView.adapter?.getItemViewType(3)
+            val expected = MovieListViewType.AD.value
+
+            assertEquals(expected, actual)
+        }
     }
 }
