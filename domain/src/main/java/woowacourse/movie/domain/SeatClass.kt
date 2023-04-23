7fe4e0f7f83aeb1078b94fc035df27099d5d63d8
ki960213 <ki960213@github.com>
2023-04-23 20:49:50 +0900

    feat(MovieHouse): 영화관 내에 특정 좌표에 좌석이 존재하는지를 알려주는 기능과 특정 좌표의 좌석의 요금을 반환하는 기능 구현

--- domain/src/main/java/woowacourse/movie/domain/SeatClass.kt
+++ domain/src/main/java/woowacourse/movie/domain/SeatClass.kt
@@ -1,5 +1,5 @@
 package woowacourse.movie.domain
 
-enum class SeatClass(val seatFee: Int) {
-    S(15_000), A(12_000), B(10_000)
+enum class SeatClass(val seatFee: Money) {
+    S(Money(15_000)), A(Money(12_000)), B(Money(10_000))
 }
