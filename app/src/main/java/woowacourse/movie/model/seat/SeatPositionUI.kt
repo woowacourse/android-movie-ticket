53420c9bb0cc5429ddc6967bd3628444706e2110
inseonyun <yuninseon@naver.com>
2023-04-24 12:58:24 +0900

    refactor(SeatTable): 테이블 좌석 및 컬러 코드 상에서 적용하도록 수정

--- app/src/main/java/woowacourse/movie/model/seat/SeatPositionUI.kt
+++ app/src/main/java/woowacourse/movie/model/seat/SeatPositionUI.kt
@@ -9,9 +9,10 @@ data class SeatPositionUI(
     val col: ColUI
 ) : Parcelable {
     fun getSeatPositionUIFormat() =
-        POSITION_UI_FORMAT.format(('A' + row.x), (col.y + COLUMN_UI_CONDITION))
+        POSITION_UI_FORMAT.format((START_SEAT_POSITION + row.x), (col.y + COLUMN_UI_CONDITION))
 
     companion object {
+        private const val START_SEAT_POSITION = 'A'
         private const val POSITION_UI_FORMAT = "%c%d"
         private const val COLUMN_UI_CONDITION = 1
     }
--- app/src/main/java/woowacourse/movie/ui/seatselection/SeatTable.kt
+++ app/src/main/java/woowacourse/movie/ui/seatselection/SeatTable.kt
@@ -1,12 +1,16 @@
 package woowacourse.movie.ui.seatselection
 
+import android.content.Context
 import android.view.View
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.core.view.children
+import com.woowacourse.movie.domain.seat.Rank
+import woowacourse.movie.R
 import woowacourse.movie.model.ReservationUI
 import woowacourse.movie.model.TicketCountUI
+import woowacourse.movie.model.mapper.seat.toSeatPosition
 import woowacourse.movie.model.seat.ColUI
 import woowacourse.movie.model.seat.RowUI
 import woowacourse.movie.model.seat.SeatPositionUI
@@ -25,16 +29,36 @@ class SeatTable(
     private var selectedCount = TicketCountUI()
 
     init {
-        initTableClickListener()
+        initSeatTable()
     }
 
-    private fun initTableClickListener() {
+    private fun initSeatTable() {
         seatView.forEachIndexed { row, rowTexts ->
             rowTexts.forEachIndexed { col, textView ->
+                val position = SeatPositionUI(RowUI(row), ColUI(col))
+                setText(position, textView)
                 textView.setOnClickListener { view ->
-                    val position = SeatPositionUI(RowUI(row), ColUI(col))
                     view.setSeatOnClick(position)
                 }
+            }
+        }
+    }
+
+    private fun setText(seatPosition: SeatPositionUI, view: TextView) {
+        view.text = seatPosition.getSeatPositionUIFormat()
+        view.setTextColor(getTextColor(Rank.valueOf(seatPosition.toSeatPosition()), view.context))
+    }
+
+    private fun getTextColor(rank: Rank, context: Context): Int {
+        return when (rank) {
+            Rank.B -> {
+                context.getColor(R.color.seat_b_rank_color)
+            }
+            Rank.A -> {
+                context.getColor(R.color.seat_a_rank_color)
+            }
+            Rank.S -> {
+                context.getColor(R.color.seat_s_rank_color)
             }
         }
     }
