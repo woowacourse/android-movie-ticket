09f752b159d7434ace033ef7dca2bf4726eb188b
Doyeop Kim <dygames34@gmail.com>
2023-04-24 17:17:48 +0900

    refactor: AdvertisementViewHolder에서 불필요한 함수 제거

--- app/src/main/java/woowacourse/movie/view/data/AdvertisementViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/data/AdvertisementViewHolder.kt
@@ -3,12 +3,11 @@ package woowacourse.movie.view.data
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class AdvertisementViewHolder(private val view: View, onClickItem: (Int) -> Unit) : RecyclerView.ViewHolder(view) {
+class AdvertisementViewHolder(private val view: View, onClickItem: (Int) -> Unit) :
+    RecyclerView.ViewHolder(view) {
     init {
         view.setOnClickListener {
             onClickItem(adapterPosition)
         }
-    }
-    fun bind() {
     }
 }
