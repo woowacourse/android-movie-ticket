25bf48a0fd3ac46f6b02126408ae8c7f966a77ee
boogieWoogie <kimjinuk1999@gmail.com>
2023-04-24 16:31:44 +0900

    refactor(ScreeningSeatTest): Parameterized를 이용해 코드를 간단히하기

--- domain/src/test/kotlin/domain/seat/ScreeningSeatTest.kt
+++ domain/src/test/kotlin/domain/seat/ScreeningSeatTest.kt
@@ -1,73 +1,30 @@
 package domain.seat
 
-import domain.payment.PaymentAmount
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-class ScreeningSeatTest {
+@RunWith(Parameterized::class)
+class ScreeningSeatTest(
+    private val seat: ScreeningSeat,
+    private val seatRate: SeatRate
+) {
 
     @Test
-    fun `A열_위치를_가지는_좌석들은_B등급이다`() {
-        // given
-        val seat = ScreeningSeat.valueOf(SeatRow.A, SeatColumn.THIRD)
-
-        // when
-        val paymentAmount = seat.paymentAmount
-
-        // then
-        val expected = PaymentAmount(10000)
-        assertEquals(paymentAmount, expected)
+    fun `A_B열은_B등급_C_D열은_S등급_E_열은_A등급이다`() {
+        assertEquals(seat.rate, seatRate)
     }
 
-    @Test
-    fun `B열_위치를_가지는_좌석들은_B등급이다`() {
-        // given
-        val seat = ScreeningSeat.valueOf(SeatRow.B, SeatColumn.THIRD)
-
-        // when
-        val seatRate = seat.rate
-
-        // then
-        val expected = SeatRate.A
-        assertEquals(seatRate, expected)
-    }
-
-    @Test
-    fun `C열_위치를_가지는_좌석들은_S등급이다`() {
-        // given
-        val seat = ScreeningSeat.valueOf(SeatRow.C, SeatColumn.THIRD)
-
-        // when
-        val seatRate = seat.rate
-
-        // then
-        val expected = SeatRate.S
-        assertEquals(seatRate, expected)
-    }
-
-    @Test
-    fun `D열_위치를_가지는_좌석들은_S등급이다`() {
-        // given
-        val seat = ScreeningSeat.valueOf(SeatRow.D, SeatColumn.THIRD)
-
-        // when
-        val seatRate = seat.rate
-
-        // then
-        val expected = SeatRate.S
-        assertEquals(seatRate, expected)
-    }
-
-    @Test
-    fun `E열_위치를_가지는_좌석들은_A등급이다`() {
-        // given
-        val seat = ScreeningSeat.valueOf(SeatRow.E, SeatColumn.THIRD)
-
-        // when
-        val seatRate = seat.rate
-
-        // then
-        val expected = SeatRate.A
-        assertEquals(seatRate, expected)
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): List<Array<Any>> = listOf(
+            arrayOf(ScreeningSeat.valueOf(SeatRow.A, SeatColumn.FIRST), SeatRate.B),
+            arrayOf(ScreeningSeat.valueOf(SeatRow.B, SeatColumn.FIRST), SeatRate.B),
+            arrayOf(ScreeningSeat.valueOf(SeatRow.C, SeatColumn.FIRST), SeatRate.S),
+            arrayOf(ScreeningSeat.valueOf(SeatRow.D, SeatColumn.FIRST), SeatRate.S),
+            arrayOf(ScreeningSeat.valueOf(SeatRow.E, SeatColumn.FIRST), SeatRate.A),
+        )
     }
 }
