ed254a42519b1fc22cd03ef25fa20dae9077fbb0
SangHyun Moon <no1msh1217@gmail.com>
2023-04-24 12:43:10 +0900

    refactor: 영화목록 불필요한 테스트 삭제 및 중복 부분 제거

--- app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
@@ -2,6 +2,7 @@ package woowacourse.movie
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
@@ -20,71 +21,64 @@ class MainActivityTest {
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun 메인액티비티는_영화리사이클러뷰를_가지고_있다() {
-        onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
-    }
-
-    @Test
     fun 리사이클러뷰_첫번째_아이템에_영화데이터의_첫번째_데이터가_들어있다() {
         onView(withId(R.id.rv_movie_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
-            .check(
-                matches(
-                    hasDescendant(
-                        allOf(
-                            withText(MovieMockData.movies10000[0].title),
-                            isDisplayed(),
-                        ),
-                    ),
-                ),
-            )
+            .performScrollTo(0)
+            .checkMovieTitleIsDisplayed(0)
     }
 
     @Test
     fun 리사이클러뷰_두번째_아이템에_영화데이터의_두번째_데이터가_들어있다() {
         onView(withId(R.id.rv_movie_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(1))
-            .check(
-                matches(
-                    hasDescendant(
-                        allOf(
-                            withText(MovieMockData.movies10000[1].title),
-                            isDisplayed(),
-                        ),
-                    ),
-                ),
-            )
+            .performScrollTo(1)
+            .checkMovieTitleIsDisplayed(1)
     }
 
     @Test
     fun 리사이클러뷰_세번째_아이템에_영화데이터의_세번째_데이터가_들어있다() {
         onView(withId(R.id.rv_movie_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(2))
-            .check(
-                matches(
-                    hasDescendant(
-                        allOf(
-                            withText(MovieMockData.movies10000[2].title),
-                            isDisplayed(),
-                        ),
-                    ),
-                ),
-            )
+            .performScrollTo(2)
+            .checkMovieTitleIsDisplayed(2)
     }
 
     @Test
     fun 리사이클러뷰_네번째_아이템에_광고가_표시된다() {
         onView(withId(R.id.rv_movie_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(3))
-            .check(
-                matches(
-                    hasDescendant(
-                        allOf(
-                            withId(R.id.iv_ad),
-                            isDisplayed(),
-                        ),
+            .performScrollTo(3)
+            .checkImageIsDisplayed(R.id.iv_ad)
+    }
+
+    @Test
+    fun 리사이클러뷰_다섯번째_아이템에_영화데이터의_다섯번째_데이터가_들어있다() {
+        onView(withId(R.id.rv_movie_list))
+            .performScrollTo(4)
+            .checkMovieTitleIsDisplayed(4)
+    }
+
+    private fun ViewInteraction.performScrollTo(position: Int) =
+        perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(position))
+
+    private fun ViewInteraction.checkMovieTitleIsDisplayed(position: Int) =
+        check(
+            matches(
+                hasDescendant(
+                    allOf(
+                        withText(MovieMockData.movies10000[position].title),
+                        isDisplayed(),
                     ),
                 ),
-            )
-    }
+            ),
+        )
+
+    private fun ViewInteraction.checkImageIsDisplayed(id: Int) =
+        check(
+            matches(
+                hasDescendant(
+                    allOf(
+                        withId(id),
+                        isDisplayed(),
+                    ),
+                ),
+            ),
+        )
 }
