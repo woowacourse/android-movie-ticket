aa5050c19e762037dbc78db0cda05b1826e06fb4
hyemdooly <hyemdooly@gmail.com>
2023-04-24 14:08:21 +0900

    refactor: MovieDetailActivityTest 수정

--- app/src/androidTest/java/woowacourse/movie/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MovieDetailActivityTest.kt
@@ -1,14 +1,13 @@
 package woowacourse.movie
 
 import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Rule
 import org.junit.Test
 import woowacourse.movie.activity.moviedetail.MovieDetailActivity
 import woowacourse.movie.domain.movie.Movie
@@ -25,37 +24,32 @@ class MovieDetailActivityTest {
         "해리포터 첫 번째 시리즈"
     ).toPresentation(R.drawable.img)
 
-    private val intent =
-        Intent(ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java).apply {
-            putExtra(MovieDetailActivity.MOVIE_KEY, movieModel)
-        }
-
-    @get:Rule
-    val activityRule = ActivityScenarioRule<MovieDetailActivity>(intent)
+    lateinit var activityScenario: ActivityScenario<MovieDetailActivity>
 
     @Test
-    fun 제목을_띄운다() {
+    fun 영화_정보를_띄운다() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java)
+        intent.putExtra(MovieDetailActivity.MOVIE_KEY, movieModel)
+        activityScenario = ActivityScenario.launch(intent)
         onView(withId(R.id.text_title)).check(matches(withText("해리포터와 마법사의 돌 1")))
-    }
-
-    @Test
-    fun 상영일을_띄운다() {
         onView(withId(R.id.text_playing_date)).check(matches(withText("상영일: 2023.3.1 ~ 2023.3.31")))
-    }
-
-    @Test
-    fun 설명을_띄운다() {
         onView(withId(R.id.text_description)).check(matches(withText("해리포터 첫 번째 시리즈")))
     }
 
     @Test
     fun 플러스_버튼을_클릭하면_매수가_올라간다() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java)
+        intent.putExtra(MovieDetailActivity.MOVIE_KEY, movieModel)
+        activityScenario = ActivityScenario.launch(intent)
         onView(withId(R.id.btn_plus)).perform(click())
         onView(withId(R.id.text_count)).check(matches(withText("2")))
     }
 
     @Test
     fun 마이너스_버튼을_클릭하면_매수가_내려간다() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java)
+        intent.putExtra(MovieDetailActivity.MOVIE_KEY, movieModel)
+        activityScenario = ActivityScenario.launch(intent)
         onView(withId(R.id.btn_plus)).perform(click())
         onView(withId(R.id.btn_plus)).perform(click())
         onView(withId(R.id.btn_minus)).perform(click())
