18d0974df6fd3c2a2ff23feea75c567699033a6c
briandr97 <briandr@naver.com>
2023-04-25 21:54:17 +0900

    refactor(MovieUiModel): 사용하지 않는 함수 삭제

--- app/src/main/java/woowacourse/movie/model/main/MovieUiModel.kt
+++ app/src/main/java/woowacourse/movie/model/main/MovieUiModel.kt
@@ -1,12 +1,8 @@
 package woowacourse.movie.model.main
 
 import androidx.annotation.DrawableRes
-import woowacourse.movie.model.main.MovieMapper.toDomainModel
-import woowacourse.movie.ticket.Seat
-import woowacourse.movie.ticket.Ticket
 import woowacourse.movie.ui.main.adapter.MainViewType
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class MovieUiModel(
     override val id: Long,
@@ -20,8 +16,4 @@ data class MovieUiModel(
     @DrawableRes val poster: Int,
 ) : MainData() {
     override val mainViewType: MainViewType = MainViewType.CONTENT
-
-    fun reserve(dateTime: LocalDateTime, seat: Seat): Ticket {
-        return this.toDomainModel().reserve(dateTime, seat)
-    }
 }
