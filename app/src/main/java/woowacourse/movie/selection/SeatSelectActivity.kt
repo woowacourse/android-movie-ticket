44de50be36b0097218dbc0f1a0905c7dc1488c4e
hyunji1203 <hj27720@gmail.com>
2023-04-28 07:19:04 +0900

    refactor: 색생과 문자열 'colors.xml'과 'strings.xml'로 분리

--- app/src/main/java/woowacourse/movie/selection/SeatSelectActivity.kt
+++ app/src/main/java/woowacourse/movie/selection/SeatSelectActivity.kt
@@ -1,7 +1,7 @@
 package woowacourse.movie.selection
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -14,6 +14,7 @@ import woowacourse.movie.KEY_RESERVATION_DATE
 import woowacourse.movie.KEY_RESERVATION_MONEY
 import woowacourse.movie.KEY_RESERVATION_SEATS
 import woowacourse.movie.KEY_RESERVATION_TIME
+import woowacourse.movie.R
 import woowacourse.movie.confirm.ReservationConfirmActivity
 import woowacourse.movie.databinding.ActivitySeatSelectBinding
 import woowacourse.movie.domain.DiscountCalculator
@@ -61,10 +62,11 @@ class SeatSelectActivity : BackKeyActionBarActivity() {
         dateTime = LocalDateTime.of(date.value, time.value)
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun initBind() {
         binding.selectTitle.text = movie.title
         binding.selectConfrimBtn.isEnabled = false
-        binding.selectConfrimBtn.setBackgroundColor(Color.parseColor(INACTIVATE_BUTTON_COLOR))
+        binding.selectConfrimBtn.setBackgroundColor(R.color.inactivate_btn)
         binding.seatTable.children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
@@ -78,34 +80,36 @@ class SeatSelectActivity : BackKeyActionBarActivity() {
             }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun selectSeat(index: Int, view: View) {
         if (chosenSeats.contains(positionFind(index))) {
             chosenSeats.remove(positionFind(index))
-            view.setBackgroundColor(Color.parseColor(INACTIVATE_SEAT_COLOR))
+            view.setBackgroundColor(R.color.inactivate_seat)
         } else {
             chosenSeats.add(positionFind(index))
-            view.setBackgroundColor(Color.parseColor(ACTIVATE_SEAT_COLOR))
+            view.setBackgroundColor(R.color.activate_seat)
         }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun activeConfirmBtn() {
         if (chosenSeats.size == reservationCount) {
             binding.selectConfrimBtn.isEnabled = true
-            binding.selectConfrimBtn.setBackgroundColor(Color.parseColor(ACTIVATE_BUTTON_COLOR))
+            binding.selectConfrimBtn.setBackgroundColor(R.color.activate_btn)
         } else {
             binding.selectConfrimBtn.isEnabled = false
-            binding.selectConfrimBtn.setBackgroundColor(Color.parseColor(INACTIVATE_BUTTON_COLOR))
+            binding.selectConfrimBtn.setBackgroundColor(R.color.inactivate_btn)
         }
     }
 
     private fun positionFind(index: Int): Seat {
         val row = index % 4 + 1
         val rank = when (index / 4 + 1) {
-            COLUMN_ONE -> SeatRank.A
-            COLUMN_TWO -> SeatRank.B
-            COLUMN_THREE -> SeatRank.C
-            COLUMN_FOUR -> SeatRank.D
-            COLUMN_FIVE -> SeatRank.E
+            R.string.columnOne -> SeatRank.A
+            R.string.columnTwo -> SeatRank.B
+            R.string.columnThree -> SeatRank.C
+            R.string.columnFour -> SeatRank.D
+            R.string.columnFive -> SeatRank.E
             else -> throw IllegalArgumentException(ERROR_MESSAGE)
         }
         return Seat(rank, row)
@@ -122,10 +126,10 @@ class SeatSelectActivity : BackKeyActionBarActivity() {
     private fun retryConfirm() {
         binding.selectConfrimBtn.setOnClickListener {
             AlertDialog.Builder(this)
-                .setTitle(DIALOG_TITLE)
-                .setMessage(DIALOG_MESSAGE)
-                .setPositiveButton(DIALOG_POSITIVE_MESSAGE) { _, _ -> putIntent() }
-                .setNegativeButton(DIALOG_NEGATIVE_MESSAGE) { dialog, _ -> dialog.dismiss() }
+                .setTitle(R.string.dialogTitle)
+                .setMessage(R.string.dialogMessage)
+                .setPositiveButton(R.string.dialogPositive) { _, _ -> putIntent() }
+                .setNegativeButton(R.string.dialogNegative) { dialog, _ -> dialog.dismiss() }
                 .setCancelable(false)
                 .show()
         }
@@ -143,22 +147,6 @@ class SeatSelectActivity : BackKeyActionBarActivity() {
     }
 
     companion object {
-        private const val INACTIVATE_BUTTON_COLOR = "#B7B7B7"
-        private const val ACTIVATE_BUTTON_COLOR = "#6200EE"
-        private const val INACTIVATE_SEAT_COLOR = "#FFFFFF"
-        private const val ACTIVATE_SEAT_COLOR = "#FAFF00"
-
         private const val ERROR_MESSAGE = "[ERROR]"
-
-        private const val COLUMN_ONE = 1
-        private const val COLUMN_TWO = 2
-        private const val COLUMN_THREE = 3
-        private const val COLUMN_FOUR = 4
-        private const val COLUMN_FIVE = 5
-
-        private const val DIALOG_TITLE = "예매 확인"
-        private const val DIALOG_MESSAGE = "정말 예매하시겠습니까?"
-        private const val DIALOG_POSITIVE_MESSAGE = "예매 완료"
-        private const val DIALOG_NEGATIVE_MESSAGE = "취소"
     }
 }
