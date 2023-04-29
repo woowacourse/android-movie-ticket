b6229c9fb5aadde34141ee134d9d2b99b2d1cea3
s9hn <tnghqn@naver.com>
2023-04-29 20:31:46 +0900

    [REFACTOR(SelectedState)] class 분리

---
+++ app/src/main/java/woowacourse/movie/ui/seatreservation/uimodel/SelectState.kt
@@ -0,0 +1,8 @@
+package woowacourse.movie.ui.seatreservation.uimodel
+
+sealed class SelectState() {
+    object ABLE : SelectState()
+    object DISABLE : SelectState()
+    object REABLE : SelectState()
+    object MAX : SelectState()
+}
