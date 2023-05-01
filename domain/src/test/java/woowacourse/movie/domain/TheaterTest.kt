4a54ad592e2f60014c9a7b928bed1567a877bb12
ki960213 <ki960213@github.com>
2023-05-01 23:26:41 +0900

    feat(Theater): 극장 좌석 생성 기능 구현 및 극장 생성 시 제약 조건 추가
    - 좌석 행의 개수와 좌석 열의 개수가 양수여야 하는 제약 조건 추가

--- domain/src/test/java/woowacourse/movie/domain/TheaterTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/TheaterTest.kt
@@ -1,7 +1,9 @@
 package woowacourse.movie.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 internal class TheaterTest {
 
@@ -13,5 +15,27 @@ internal class TheaterTest {
         theater.id = 2L
 
         assertThat(theater.id).isEqualTo(1L)
+    }
+
+    @Test
+    fun `극장을 생성할 때 좌석의 행 수 또는 열 수가 양수가 아니면 에러가 발생한다`() {
+        val seatRows = 0
+        val seatColumns = 1
+
+        assertThatIllegalArgumentException().isThrownBy { Theater(seatRows, seatColumns) }
+            .withMessage("좌석들의 행과 열의 개수는 양수여야 합니다.")
+    }
+
+    @Test
+    fun `극장을 생성하면 1행 1열부터 입력된 행의 개수행 입력된 열의 개수열까지 좌석이 생성된다`() {
+        val seatRows = 5
+        val seatColumns = 4
+        val theater = Theater(seatRows, seatColumns)
+
+        assertAll(
+            { assertThat(theater.seats.keys).hasSize(20) },
+            { assertThat(theater.seats.keys).contains(Point(1, 1), Point(seatRows, seatColumns)) }
+        )
+
     }
 }
