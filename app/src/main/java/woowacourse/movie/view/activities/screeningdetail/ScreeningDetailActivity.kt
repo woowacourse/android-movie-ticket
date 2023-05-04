d7c40eee2231ff2fe6c77ed93cd34fe2a9be586d
ki960213 <ki960213@github.com>
2023-05-04 15:41:58 +0900

    feat(ScreeningDetailActivity): 화면 회전 시 타임 스피너의 포지션 유지 기능 구현

--- app/src/main/java/woowacourse/movie/view/activities/screeningdetail/ScreeningDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/view/activities/screeningdetail/ScreeningDetailActivity.kt
@@ -16,12 +16,13 @@ import java.time.format.DateTimeFormatter
 class ScreeningDetailActivity : AppCompatActivity(), ScreeningDetailContract.View {
 
     private val presenter: ScreeningDetailContract.Presenter = ScreeningDetailPresenter(this)
-    private var dateSpinnerPosition: Int = 0
     private var timeSpinnerPosition: Int = 0
+    private var savedInstanceState: Bundle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screening_detail)
+        this.savedInstanceState = savedInstanceState
 
         presenter.loadScreeningData(getScreeningIdFromIntent())
     }
@@ -92,11 +93,14 @@ class ScreeningDetailActivity : AppCompatActivity(), ScreeningDetailContract.Vie
                 position: Int,
                 id: Long
             ) {
-                dateSpinnerPosition = position
                 initTimeSpinner(
                     screeningDateTimes[screeningDates[position]]
                         ?: throw IllegalArgumentException("${screeningDates[position]}은 상영하지 않는 날이라서 타임 스피너를 초기화할 수 없습니다.")
                 )
+                savedInstanceState?.run {
+                    val timeSpinner = findViewById<Spinner>(R.id.time_spinner)
+                    timeSpinner.setSelection(this.getInt(TIME_SPINNER_POSITION))
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) = Unit
@@ -122,28 +126,13 @@ class ScreeningDetailActivity : AppCompatActivity(), ScreeningDetailContract.Vie
         super.onSaveInstanceState(outState)
 
         outState.apply {
-            putInt(DATE_SPINNER_POSITION, dateSpinnerPosition)
             putInt(TIME_SPINNER_POSITION, timeSpinnerPosition)
         }
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        dateSpinnerPosition = savedInstanceState.getInt(DATE_SPINNER_POSITION)
-        timeSpinnerPosition = savedInstanceState.getInt(TIME_SPINNER_POSITION)
-
-        val dateSpinner = findViewById<Spinner>(R.id.date_spinner)
-        val timeSpinner = findViewById<Spinner>(R.id.time_spinner)
-
-        dateSpinner.setSelection(dateSpinnerPosition)
-        timeSpinner.setSelection(timeSpinnerPosition)
     }
 
     companion object {
         const val SCREENING_ID = "SCREENING_ID"
         private val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        private const val DATE_SPINNER_POSITION = "DATE_SPINNER_POSITION"
         private const val TIME_SPINNER_POSITION = "TIME_SPINNER_POSITION"
     }
 }
