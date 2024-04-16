049769773d5eb11617e9cfcfacea35bf28182c81
songpink <jejeje04@naver.com>
2024-04-16 16:35:56 +0900

    feat: Movie 클래스 선언

---
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -0,0 +1,9 @@
+package woowacourse.movie
+
+data class Movie(
+    val title: String,
+    val poster: String,
+    val screeningDate: String,
+    val runningTime: Int,
+    val description: String,
+)
