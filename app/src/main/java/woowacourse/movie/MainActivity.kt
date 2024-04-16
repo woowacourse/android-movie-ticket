08af35d6f72ca41f250078c33f7af7ccba554103
murjune <sis9221302@gmail.com>
2024-04-16 18:07:57 +0900

    feat: movie ListView

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,19 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = listOf(MovieItem())
+        val adapter = MovieAdapter(this, items)
+        val listView = findViewById<ListView>(R.id.list_view)
+        listView.adapter = adapter
+    }
     }
 }
---
+++ app/src/main/java/woowacourse/movie/MovieAdapter.kt
@@ -0,0 +1,46 @@
+package woowacourse.movie
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MovieAdapter(context: Context, private val movies: List<MovieItem>) : BaseAdapter() {
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getCount(): Int = movies.size
+
+    override fun getItem(position: Int): MovieItem = movies[position]
+
+    override fun getItemId(position: Int): Long = movies[position].id
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup?,
+    ): View {
+        val view = convertView ?: inflater.inflate(R.layout.item_movie, null)
+
+        val movie = getItem(position)
+        val postImageView = view.findViewById<ImageView>(R.id.iv_movie_post)
+        val title = view.findViewById<TextView>(R.id.tv_movie_title)
+        val date = view.findViewById<TextView>(R.id.tv_movie_running_date)
+        val runningTime = view.findViewById<TextView>(R.id.tv_movie_running_time)
+
+        postImageView.setImageResource(movie.res)
+        title.text = movie.title
+        date.text = movie.runningDate
+        runningTime.text = movie.runningTime
+
+        view.findViewById<Button>(R.id.btn_movie_reservation).setOnClickListener {
+            view.context.startActivity(Intent(view.context, DetailMovieActivity::class.java))
+        }
+
+        return view
+    }
+}
