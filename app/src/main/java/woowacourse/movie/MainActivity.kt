7d5d52d225543510db02d7c00f6f63dbbe2344b8
jaeyeongjo <nos05075@naver.com>
2024-04-16 16:05:17 +0900

    feat: MainActivity에 movieListView 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,22 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movieListView = findViewById<ListView>(R.id.list_view)
+
+        val movies = mutableListOf<Movie>()
+        movies.add(
+            Movie(R.drawable.poster, "해리 포터와 마법사의 돌", "2024.3.1", 152),
+        )
+        val adapter = ListViewAdapter(movies)
+
+        movieListView.adapter = adapter
     }
 }
