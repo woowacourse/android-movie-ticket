ca8d5236856494add1e1d91576e7e3d53d255667
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-16 19:52:46 +0900

    style: ktlint 적용

--- app/src/test/java/woowacourse/movie/model/MovieTest.kt
+++ app/src/test/java/woowacourse/movie/model/MovieTest.kt
@@ -6,15 +6,16 @@ import org.junit.jupiter.api.Test
 class MovieTest {
     @Test
     fun `영화 정보를 생성한다`() {
-        val movie = Movie(
-            posterImage = "https://movie.com/poster.jpg",
-            title = "테넷",
-            screeningDate = "2020-08-26",
-            runningTime = 150,
-            summary = "시간을 건너뛰는 SF 영화",
-        )
-        
-        assertThat(movie.posterImage).isEqualTo("https://movie.com/poster.jpg")
+        val movie =
+            Movie(
+                posterImage = "posterImage",
+                title = "테넷",
+                screeningDate = "2020-08-26",
+                runningTime = 150,
+                summary = "시간을 건너뛰는 SF 영화",
+            )
+
+        assertThat(movie.posterImage).isEqualTo("posterImage")
         assertThat(movie.title).isEqualTo("테넷")
         assertThat(movie.screeningDate).isEqualTo("2020-08-26")
         assertThat(movie.runningTime).isEqualTo(150)
