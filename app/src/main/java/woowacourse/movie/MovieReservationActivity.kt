985a98a4ae4be0bbd56db3a6b9fa1c5d483804a3
songpink <jejeje04@naver.com>
2024-04-16 17:26:15 +0900

    feat: 영화 상세 정보를 보는 기능 구현

--- app/src/main/java/woowacourse/movie/MovieReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieReservationActivity.kt
@@ -3,9 +3,9 @@ package woowacourse.movie
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MovieReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_reservation)
     }
 }
