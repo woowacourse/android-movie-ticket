4e4856aa4185cba4e36cb9f23bb0860a17cdaf1a
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-16 22:24:24 +0900

    feat: 영화 상세 정보 화면 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/movie/ReservationDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/ReservationDetailActivity.kt
@@ -1,11 +1,25 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReservationDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation_detail)
+
+        val poster = findViewById<ImageView>(R.id.image_view_reservation_detail_poster)
+        val title = findViewById<TextView>(R.id.text_view_reservation_detail_title)
+        val screeningDate = findViewById<TextView>(R.id.text_view_reservation_screening_date)
+        val runningTime = findViewById<TextView>(R.id.text_view_reservation_running_time)
+        val summary = findViewById<TextView>(R.id.text_view_reservation_summary)
+
+        poster.setImageResource(intent.getIntExtra("poster", 0))
+        title.text = intent.getStringExtra("title")
+        screeningDate.text = intent.getStringExtra("screeningDate")
+        runningTime.text = intent.getStringExtra("runningTime")
+        summary.text = intent.getStringExtra("summary")
     }
 }
