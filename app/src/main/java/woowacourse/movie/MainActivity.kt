1e3cbe896e79b1cc322b998ace94f560f95feb73
Gimun Kim <kmkim7575@gmail.com>
2024-04-16 20:17:46 +0900

    refactor: 영화 데이터 리스트 전역 데이터 변경
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.adapter.MovieAdapter
-import woowacourse.movie.model.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,17 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val moviesView = findViewById<ListView>(R.id.lv_movies)
-        val movies =
-            listOf(
-                Movie(
-                    title = "해리 포터와 마법사의 돌",
-                    thumbnail = "https://github.com/kmkim2689/codetree-TILs/assets/101035437/c57f8b0c-5b15-418c-88db-15f2ada4cdcf",
-                    date = "2024.3.1",
-                    runningTime = 152,
-                    introduction = "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
-                ),
-            )
-
-        moviesView.adapter = MovieAdapter(movies)
+        moviesView.adapter = MovieAdapter(MOVIES)
     }
 }
