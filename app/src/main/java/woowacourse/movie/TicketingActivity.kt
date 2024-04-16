700a9955ce8e62cc03bc63f20516dac490054ef7
Gimun Kim <kmkim7575@gmail.com>
2024-04-16 21:55:21 +0900

    feat: 예매 화면에서 예매 완료 버튼을 통해 완료 화면으로 이동하는 기능 구현
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/main/java/woowacourse/movie/TicketingActivity.kt
+++ app/src/main/java/woowacourse/movie/TicketingActivity.kt
@@ -1,5 +1,6 @@
 package woowacourse.movie
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
@@ -20,7 +21,7 @@ class TicketingActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.iv_thumbnail).apply { }
             findViewById<TextView>(R.id.tv_title).apply { text = it.title }
             findViewById<TextView>(R.id.tv_date).apply { text = "$text ${it.date}" }
-            findViewById<TextView>(R.id.tv_running_time).apply { text = "${text}${it.runningTime}" }
+            findViewById<TextView>(R.id.tv_running_time).apply { text = "$text ${it.runningTime}" }
             findViewById<TextView>(R.id.tv_introduction).apply { text = it.introduction }
         }
 
@@ -28,6 +29,7 @@ class TicketingActivity : AppCompatActivity() {
         val minusButton = findViewById<Button>(R.id.btn_minus)
         val plusButton = findViewById<Button>(R.id.btn_plus)
         val countText = findViewById<TextView>(R.id.tv_count).apply { text = count.toString() }
+        val completeButton = findViewById<Button>(R.id.btn_complete)
 
         minusButton.setOnClickListener {
             if (count > 1) {
@@ -40,10 +42,26 @@ class TicketingActivity : AppCompatActivity() {
             count++
             countText.text = count.toString()
         }
+
+        completeButton.setOnClickListener {
+            Intent(this, TicketingResultActivity::class.java).apply {
+                putExtra(EXTRA_MOVIE_TITLE, movie?.title)
+                putExtra(EXTRA_MOVIE_DATE, movie?.date)
+                putExtra(EXTRA_NUMBER_OF_PEOPLE, count)
+                startActivity(this)
+                finish()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) finish()
         return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val EXTRA_MOVIE_TITLE = "movie_title"
+        const val EXTRA_MOVIE_DATE = "movie_date"
+        const val EXTRA_NUMBER_OF_PEOPLE = "number_of_people"
     }
 }
---
+++ app/src/main/java/woowacourse/movie/TicketingResultActivity.kt
@@ -0,0 +1,11 @@
+package woowacourse.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TicketingResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticketing_result)
+    }
+}
