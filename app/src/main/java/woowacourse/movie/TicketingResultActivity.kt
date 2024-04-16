adbdf1a096762e4d70a844262e634c588594ea2e
Gimun Kim <kmkim7575@gmail.com>
2024-04-16 21:56:13 +0900

    feat: 완료 화면에서 뒤로 가기 버튼을 통해 영화 목록 화면으로 돌아가는 기능 구현
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/main/java/woowacourse/movie/TicketingResultActivity.kt
+++ app/src/main/java/woowacourse/movie/TicketingResultActivity.kt
@@ -1,11 +1,18 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class TicketingResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticketing_result)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return super.onOptionsItemSelected(item)
     }
 }
