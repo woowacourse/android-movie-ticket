403b8629c0507a88e6e6702fb1e5772a3e32b798
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-16 22:47:51 +0900

    feat: 영화 예매 정보를 예매 완료 화면으로 전달하는 기능 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/movie/ReservationDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/ReservationDetailActivity.kt
@@ -1,9 +1,12 @@
 package woowacourse.movie
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import domain.Ticket
 
 class ReservationDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,16 +15,45 @@ class ReservationDetailActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val ticket = Ticket()
         val poster = findViewById<ImageView>(R.id.image_view_reservation_detail_poster)
         val title = findViewById<TextView>(R.id.text_view_reservation_detail_title)
         val screeningDate = findViewById<TextView>(R.id.text_view_reservation_screening_date)
         val runningTime = findViewById<TextView>(R.id.text_view_reservation_running_time)
         val summary = findViewById<TextView>(R.id.text_view_reservation_summary)
+        val reservationButton = findViewById<Button>(R.id.button_reservation_detail_finished)
+        val plusButton = findViewById<Button>(R.id.button_reservation_detail_plus)
+        val minusButton = findViewById<Button>(R.id.button_reservation_detail_minus)
+        val numberOfTickets = findViewById<TextView>(R.id.text_view_reservation_detail_number_of_tickets)
+
+        val titleDetail = intent.getStringExtra("title")
+        val screeningDateDetail = intent.getStringExtra("screeningDate")
+        val runningTimeDetail = intent.getStringExtra("runningTime")
+        val summaryDetail = intent.getStringExtra("summary")
 
         poster.setImageResource(intent.getIntExtra("poster", 0))
-        title.text = intent.getStringExtra("title")
-        screeningDate.text = intent.getStringExtra("screeningDate")
-        runningTime.text = intent.getStringExtra("runningTime")
-        summary.text = intent.getStringExtra("summary")
+        title.text = titleDetail
+        screeningDate.text = screeningDateDetail
+        runningTime.text = runningTimeDetail
+        summary.text = summaryDetail
+
+        plusButton.setOnClickListener {
+            ticket.increaseCount()
+            numberOfTickets.text = ticket.count.toString()
+        }
+
+        minusButton.setOnClickListener {
+            ticket.decreaseCount()
+            numberOfTickets.text = ticket.count.toString()
+        }
+
+        reservationButton.setOnClickListener {
+            val intent = Intent(this, ReservationFinishedActivity::class.java)
+            intent.putExtra("title", titleDetail)
+            intent.putExtra("screeningDate", screeningDateDetail)
+            intent.putExtra("runningTime", runningTimeDetail)
+            intent.putExtra("numberOfTickets", ticket.count)
+            startActivity(intent)
+        }
     }
 }
