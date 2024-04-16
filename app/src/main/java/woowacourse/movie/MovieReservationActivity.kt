09afc649725f67db69cc78a468cd51ea7365c14e
namyunsuk <yunsuk0328@naver.com>
2024-04-16 20:26:55 +0900

    feat: 영화 상세 정보 페이지 ui 구현

---
+++ app/src/main/java/woowacourse/movie/MovieReservationActivity.kt
@@ -0,0 +1,11 @@
+package woowacourse.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_reservation)
+    }
+}
