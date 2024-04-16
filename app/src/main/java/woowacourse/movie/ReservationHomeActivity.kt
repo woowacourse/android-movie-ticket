c2c43d8645e7ae3943296dae853d735a24795240
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-16 16:13:35 +0900

    design: 영화 예매 홈 UI 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/movie/ReservationHomeActivity.kt
+++ app/src/main/java/woowacourse/movie/ReservationHomeActivity.kt
@@ -3,9 +3,9 @@ package woowacourse.movie
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ReservationHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_reservation_home)
     }
 }
