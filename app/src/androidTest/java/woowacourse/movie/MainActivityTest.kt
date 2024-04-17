0253b60b4c9dbca8449d73a8c9a82f98398829e8
ii2001 <iivks2001@gmail.com>
2024-04-17 16:12:56 +0900

    :sparkles: 제목 타이틀 Test 제작

---
+++ app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
@@ -0,0 +1,32 @@
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.CoreMatchers.anything
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import woowacourse.movie.R
+import woowacourse.movie.activity.MainActivity
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listViewDisplaysMovieInfo() {
+        onView(withId(R.id.movies_list_item)).check(matches(isDisplayed()))
+
+        onData(anything())
+            .inAdapterView(withId(R.id.movies_list_item))
+            .atPosition(0)
+            .onChildView(withId(R.id.movie_title))
+            .check(matches(withText("차람과 하디의 진지한 여행기")))
+    }
+}
