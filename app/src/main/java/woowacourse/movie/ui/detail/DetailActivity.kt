0b615f95e1dc180f5bde56e06b1bbec7f3ab2680
sh1mj1 <7wlgns@gmail.com>
2024-04-17 14:18:48 +0900

    feat($detail): 영화 예매 상세 페이지 뷰 구현
    Co-authored-by: junjange <wnswkd486@gmail.com>

---
+++ app/src/main/java/woowacourse/movie/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
