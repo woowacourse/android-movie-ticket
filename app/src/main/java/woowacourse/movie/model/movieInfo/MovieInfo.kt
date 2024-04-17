93db71b554c0e3c2fea22848242392b65d41c4cb
ii2001 <iivks2001@gmail.com>
2024-04-17 15:04:30 +0900

    :recycle: MovieInfo 상영일 Date에서 LocalDate로 변경

--- app/src/main/java/woowacourse/movie/model/movieInfo/MovieInfo.kt
+++ app/src/main/java/woowacourse/movie/model/movieInfo/MovieInfo.kt
@@ -1,9 +1,9 @@
 package woowacourse.movie.model.movieInfo
 
 import java.io.Serializable
-import java.util.Date
+import java.time.LocalDate
 
-class MovieInfo(val title: Title, val releaseDate: Date, val runningTime: RunningTime, val synopsis: Synopsis): Serializable
+class MovieInfo(val title: Title, val releaseDate: LocalDate, val runningTime: RunningTime, val synopsis: Synopsis): Serializable
 
 
 
