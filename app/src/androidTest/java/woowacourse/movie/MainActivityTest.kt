44dae0707f3c7ae442a9998a1ac14cb094c96012
namyunsuk <yunsuk0328@naver.com>
2024-04-17 19:11:47 +0900

    feat: ui 최상위 레이아웃에 id 부여

--- app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
@@ -6,19 +6,22 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun 지금_예매_버튼을_누르면_영화_상세_정보_페이지로_이동한다() {
+    fun `지금_예매_버튼을_누르면_영화_상세_정보_페이지로_이동한다`() {
         onView(withId(R.id.reservation_button))
             .perform(click())
 
-        onView(withId(R.id.movie_reservation_main))
+        onView(withId(R.id.movie_reservation_root))
             .check(matches(isDisplayed()))
     }
 }
