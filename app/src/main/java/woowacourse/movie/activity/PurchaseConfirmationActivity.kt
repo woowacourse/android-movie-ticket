7e53a5d76ef769df4e71797dd726fc8b59925010
ii2001 <iivks2001@gmail.com>
2024-04-17 14:49:02 +0900

    :sparkles: charge Print

--- app/src/main/java/woowacourse/movie/activity/PurchaseConfirmationActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/PurchaseConfirmationActivity.kt
@@ -1,15 +1,21 @@
 package woowacourse.movie.activity
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
+import woowacourse.movie.model.theater.Theater
 
 class PurchaseConfirmationActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.purchase_confirmation)
-
+        val theater = intent.getSerializableExtra("theater", Theater::class.java)
+        val charge= if (theater!=null) theater.charge else 13000
         val ticketNum = intent.getIntExtra("ticketNum", 0)
-        // Here you might display the ticketNum or handle the purchase
+        findViewById<TextView>(R.id.ticket_charge).text = "price: ${ticketNum*charge}"
     }
 }
