a90e5716c15071dcf08c2018346b7916f312d4a1
kkosang <eiwoqp77@gmail.com>
2024-04-17 20:53:26 +0900

    feat: MainActivity MVP 패턴 적용 - MainContract interface 구현 - MainPresenter class 구현

---
+++ app/src/main/java/woowacourse/movie/MainContract.kt
@@ -0,0 +1,22 @@
+package woowacourse.movie
+
+import android.content.Intent
+
+interface MainContract {
+    interface View {
+        fun titleTextView(position: Int)
+
+        fun screenDateTextView(position: Int)
+
+        fun runningTimeTextView(position: Int)
+
+        fun imageView(position: Int)
+    }
+
+    interface Presenter {
+        fun putData(
+            intent: Intent,
+            position: Int,
+        )
+    }
+}
