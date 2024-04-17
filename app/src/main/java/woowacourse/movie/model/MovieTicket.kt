c0270a4ba349c3a3cb88d66149739ef152b5caff
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-17 14:12:39 +0900

    feat: 영화 예매 결과를 가지는 MovieTicket 구현

---
+++ app/src/main/java/woowacourse/movie/model/MovieTicket.kt
@@ -0,0 +1,30 @@
+package woowacourse.movie.model
+
+class MovieTicket(
+    val movieTitle: String,
+    val screeningDate: String,
+) {
+    private var _count = MIN_RESERVATION_COUNT
+    val count
+        get() = _count
+
+    fun plusCount() {
+        if (count < MAX_RESERVATION_COUNT) {
+            _count++
+        }
+    }
+
+    fun minusCount() {
+        if (count > MIN_RESERVATION_COUNT) {
+            _count--
+        }
+    }
+
+    fun totalPrice() = count * PRICE_PER_TICKET
+
+    companion object {
+        const val MIN_RESERVATION_COUNT = 1
+        const val MAX_RESERVATION_COUNT = 20
+        const val PRICE_PER_TICKET = 13000
+    }
+}
