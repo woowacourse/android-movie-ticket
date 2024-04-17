482fdda8a1c7031c8fb101abb147cf2f8d15928b
ii2001 <iivks2001@gmail.com>
2024-04-17 16:01:40 +0900

    :sparkles: Movie List 생성

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -3,40 +3,48 @@ package woowacourse.movie.activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
+import woowacourse.movie.adapter.MovieAdapter
+import woowacourse.movie.model.movieInfo.MovieDate
 import woowacourse.movie.model.movieInfo.MovieInfo
 import woowacourse.movie.model.movieInfo.RunningTime
 import woowacourse.movie.model.movieInfo.Synopsis
 import woowacourse.movie.model.movieInfo.Title
 import woowacourse.movie.model.theater.Theater
 import java.time.LocalDate
-import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var moviesListView: ListView
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var moviesList: List<MovieInfo>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.movie_list_item)
+        setContentView(R.layout.mainactivity)
+
 
         val movieInfo = MovieInfo(
-            Title("차람과 하디의 진지한 여행기"),
-            LocalDate.of(2024, 2, 25),
-            RunningTime(230),
-            Synopsis("wow!")
+        Title("차람과 하디의 진지한 여행기"),
+        MovieDate(LocalDate.of(2024, 2, 25)) ,
+        RunningTime(230),
+        Synopsis("wow!")
         )
-        val theater = Theater(movieInfo)
-        findViewById<TextView>(R.id.movie_title).text= "${movieInfo.title}"
-        findViewById<TextView>(R.id.movie_release_date).text= "상영일: ${movieInfo.releaseDate}"
-        findViewById<TextView>(R.id.movie_duration).text= "${movieInfo.runningTime}분"
 
-        val detailsButton = findViewById<Button>(R.id.movie_details_button)
-        val intent = Intent(this, MovieDetailActivity::class.java).apply {
-            putExtra("Theater", theater)
+        val theater = Theater(movieInfo)
+        val theaterList = listOf(
+            theater
+        )
+        val moviesList = theaterList.map{
+            it.movie
         }
-        detailsButton.setOnClickListener {
-            startActivity(intent)
-        }
+        moviesListView = findViewById(R.id.movies_list_item)
+        movieAdapter = MovieAdapter(this, theaterList)
+        moviesListView.adapter = movieAdapter
+
+
+
     }
 }
 
---
+++ app/src/main/java/woowacourse/movie/adapter/MovieAdapter.kt
@@ -0,0 +1,43 @@
+package woowacourse.movie.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import woowacourse.movie.R
+import woowacourse.movie.activity.MovieDetailActivity
+import woowacourse.movie.model.theater.Theater
+
+class MovieAdapter(context: Context, theaters: List<Theater>) :
+    ArrayAdapter<Theater>(context, 0, theaters) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var listItemView = convertView
+        if (listItemView == null) {
+            listItemView =
+                LayoutInflater.from(context).inflate(R.layout.movie_list_item, parent, false)
+        }
+
+        val theater: Theater? = getItem(position)
+        val movie = theater?.movie
+
+        listItemView?.findViewById<TextView>(R.id.movie_title)?.text = movie?.title.toString()
+        listItemView?.findViewById<TextView>(R.id.movie_release_date)?.text =
+            "상영일: ${movie?.releaseDate}"
+        listItemView?.findViewById<TextView>(R.id.movie_duration)?.text =
+            "러닝타임: ${movie?.runningTime}분"
+        val detailsButton = listItemView?.findViewById<Button>(R.id.movie_details_button)
+
+        val intent = Intent(this.context, MovieDetailActivity::class.java).apply {
+            putExtra("Theater", theater)
+        }
+
+        detailsButton?.setOnClickListener {
+            context.startActivity(intent)
+        }
+        return listItemView!!
+    }
+}
