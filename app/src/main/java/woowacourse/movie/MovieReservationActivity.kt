95dbe9866dabd08e13fac1b3c640966d06e788da
songpink <jejeje04@naver.com>
2024-04-17 14:58:44 +0900

    refactor: 함수 분리

--- app/src/main/java/woowacourse/movie/MovieReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieReservationActivity.kt
@@ -1,11 +1,105 @@
 package woowacourse.movie
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
 
 class MovieReservationActivity : AppCompatActivity() {
+    private lateinit var context: Context
+    private var ticketCount = 1
+    private lateinit var titleView: TextView
+    private lateinit var screeningDateView: TextView
+    private lateinit var runningDateView: TextView
+    private lateinit var ticketCountView: TextView
+    private lateinit var posterView: ImageView
+    private lateinit var descriptionView: TextView
+    private lateinit var minusNumberButton: Button
+    private lateinit var plusNumberButton: Button
+    private lateinit var ticketingButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_reservation)
+        context = this@MovieReservationActivity
+        val movie =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getSerializableExtra("movie", Movie::class.java)
+            } else {
+                intent.getSerializableExtra("movie") as? Movie
+            }
+        initView(movie)
+    }
+
+    private fun initView(movie: Movie?) {
+        titleView = findViewById(R.id.movie_detail_title)
+        screeningDateView = findViewById(R.id.movie_detail_screening_date)
+        runningDateView = findViewById(R.id.movie_detail_running_date)
+        descriptionView = findViewById(R.id.movie_detail_description)
+        ticketCountView = findViewById(R.id.ticket_count)
+        posterView = findViewById(R.id.movie_detail_poster)
+
+        minusNumberButton = findViewById(R.id.minus_button)
+        plusNumberButton = findViewById(R.id.plus_button)
+        ticketingButton = findViewById(R.id.ticketing_button)
+
+        movie?.let {
+            titleView.text = it.title
+            screeningDateView.text = "상영일: ${it.screeningDate}"
+            runningDateView.text = "러닝타임: ${it.runningTime}분"
+            descriptionView.text = it.description
+
+            val resourceId = context.resources.getIdentifier(movie.poster, "drawable", context.packageName)
+            posterView.setImageResource(resourceId)
+        }
+        setTicketCountView()
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        minusNumberButton.setOnClickListener {
+            minusNumber()
+            setTicketCountView()
+        }
+        plusNumberButton.setOnClickListener {
+            plusNumber()
+            setTicketCountView()
+        }
+        ticketingButton.setOnClickListener {
+            ticketing()
+        }
+    }
+
+    private fun setTicketCountView() {
+        ticketCountView.text = ticketCount.toString()
+    }
+
+    private fun minusNumber() {
+        if (ticketCount > 1) {
+            ticketCount--
+        }
+    }
+
+    private fun plusNumber() {
+        ticketCount++
+    }
+
+    private fun ticketing() {
+        val ticket =
+            Ticket(
+                title = titleView.text.toString(),
+                screeningDate = screeningDateView.text.toString(),
+                price = ticketCount * 13000,
+                numberOfPeople = ticketCount,
+            )
+
+        val intent = Intent(context, MovieReservationCompleteActivity::class.java)
+        intent.putExtra("ticket", ticket as Serializable)
+        context.startActivity(intent)
     }
 }
