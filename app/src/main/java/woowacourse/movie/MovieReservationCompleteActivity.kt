62b9dcbbbd4bf5ece870934ab0f47bd12355114d
namyunsuk <yunsuk0328@naver.com>
2024-04-17 13:37:20 +0900

    feat: 영화 예매 완료 페이지 ui 구현

---
+++ app/src/main/java/woowacourse/movie/MovieReservationCompleteActivity.kt
@@ -0,0 +1,11 @@
+package woowacourse.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieReservationCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_reservation_complete)
+    }
+}
