0c991189b8b2be8105e37a3bf36f54469e741ad2
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-17 14:13:16 +0900

    feat: 영화 예매 결과를 화면에 출력하는 기능 구현

--- app/src/main/java/woowacourse/movie/MovieDetailActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieDetailActivity.kt
@@ -2,24 +2,27 @@ package woowacourse.movie
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.model.MovieTicket
 
 class MovieDetailActivity : AppCompatActivity() {
-    var reservationCount = MIN_RESERVATION_COUNT
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         val posterImageId = intent.getIntExtra("posterImageId", 0)
-        val title = intent.getStringExtra("title")
-        val screeningDate = intent.getStringExtra("screeningDate")
+        val title = intent.getStringExtra("title") ?: ""
+        val screeningDate = intent.getStringExtra("screeningDate") ?: ""
         val runningTime = intent.getIntExtra("runningTime", 0)
-        val summary = intent.getStringExtra("summary")
+        val summary = intent.getStringExtra("summary") ?: ""
 
         val reservationCountTextView = findViewById<TextView>(R.id.reservationCount)
+
+        val movieTicket = MovieTicket(title, screeningDate)
 
         findViewById<ImageView>(R.id.posterImage).setImageResource(posterImageId)
         findViewById<TextView>(R.id.title).text = title
@@ -29,30 +32,32 @@ class MovieDetailActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.summary).text = summary
 
         findViewById<TextView>(R.id.minusButton).setOnClickListener {
-            if (reservationCount > MIN_RESERVATION_COUNT) {
-                reservationCount--
-                reservationCountTextView.text = reservationCount.toString()
-            }
+            movieTicket.minusCount()
+            reservationCountTextView.text = movieTicket.count.toString()
         }
 
         findViewById<TextView>(R.id.plusButton).setOnClickListener {
-            if (reservationCount < MAX_RESERVATION_COUNT) {
-                reservationCount++
-                reservationCountTextView.text = reservationCount.toString()
-            }
+            movieTicket.plusCount()
+            reservationCountTextView.text = movieTicket.count.toString()
         }
 
         findViewById<TextView>(R.id.reserveButton).setOnClickListener {
             val intent = Intent(this, ReservationResultActivity::class.java)
-            intent.putExtra("title", title)
-            intent.putExtra("screeningDate", screeningDate)
-            intent.putExtra("reservationCount", reservationCount)
+            intent.putExtra("title", movieTicket.movieTitle)
+            intent.putExtra("screeningDate", movieTicket.screeningDate)
+            intent.putExtra("reservationCount", movieTicket.count)
+            intent.putExtra("totalPrice", movieTicket.totalPrice())
             startActivity(intent)
         }
     }
-
-    companion object {
-        const val MIN_RESERVATION_COUNT = 1
-        const val MAX_RESERVATION_COUNT = 20
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/woowacourse/movie/ReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/ReservationResultActivity.kt
@@ -1,11 +1,36 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReservationResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation_result)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        
+        val title = intent.getStringExtra("title") ?: ""
+        val screeningDate = intent.getStringExtra("screeningDate") ?: ""
+        val reservationCount = intent.getIntExtra("reservationCount", 0)
+        val totalPrice = intent.getIntExtra("totalPrice", 0)
+
+        findViewById<TextView>(R.id.title).text = title
+        findViewById<TextView>(R.id.screeningDate).text = screeningDate
+        findViewById<TextView>(R.id.reservationCount).text =
+            this.getString(R.string.reservation_count_format, reservationCount)
+        findViewById<TextView>(R.id.totalPrice).text =
+            this.getString(R.string.reservation_total_price_format, totalPrice)
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
