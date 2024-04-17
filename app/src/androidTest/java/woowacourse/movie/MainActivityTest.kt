cd4b1a741ae006b87d7aa6f43c153cc9009ea92c
namyunsuk <yunsuk0328@naver.com>
2024-04-17 16:01:43 +0900

    test: MainActivity의 "지금 예매" 버튼 테스트

---
+++ app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun 지금_예매_버튼을_누르면_영화_상세_정보_페이지로_이동한다() {
+        onView(withId(R.id.reservation_button))
+            .perform(click())
+
+        onView(withId(R.id.movie_reservation_main))
+            .check(matches(isDisplayed()))
+    }
+}
