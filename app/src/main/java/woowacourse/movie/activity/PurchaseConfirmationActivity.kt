bb6b6c51aa5b66c00042bcfe2114a9ab5801e286
ii2001 <iivks2001@gmail.com>
2024-04-17 16:01:14 +0900

    :sparkles: 결제 후 영화정보 검사

--- app/src/main/java/woowacourse/movie/activity/PurchaseConfirmationActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/PurchaseConfirmationActivity.kt
@@ -13,9 +13,16 @@ class PurchaseConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.purchase_confirmation)
-        val theater = intent.getSerializableExtra("theater", Theater::class.java)
-        val charge= if (theater!=null) theater.charge else 13000
+        val theater = intent.getSerializableExtra("Theater", Theater::class.java)
         val ticketNum = intent.getIntExtra("ticketNum", 0)
+
+        if(theater != null) {
+            val movie = theater.movie
+            findViewById<TextView>(R.id.movie_title_confirmation).text = movie.title.toString()
+            findViewById<TextView>(R.id.movie_screening_day).text = movie.releaseDate.toString()
+        }
+
+        val charge = if(theater != null) theater.charge else 13000
         findViewById<TextView>(R.id.ticket_charge).text = "price: ${ticketNum*charge}"
     }
 }
