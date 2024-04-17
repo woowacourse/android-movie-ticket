edd15509e96455079d0cb4b9b2974826f6683035
ii2001 <iivks2001@gmail.com>
2024-04-17 15:05:20 +0900

    :recycle: 영화 정보들의 toString 추가

--- app/src/main/java/woowacourse/movie/model/movieInfo/RunningTime.kt
+++ app/src/main/java/woowacourse/movie/model/movieInfo/RunningTime.kt
@@ -2,8 +2,10 @@ package woowacourse.movie.model.movieInfo
 
 import java.io.Serializable
 
-class RunningTime(private val time: Int): Serializable {
+class RunningTime(val time: Int): Serializable {
     init {
         require(time>0){ "상영시간은 존재해야한다." }
     }
+
+    override fun toString() = time.toString()
 }
--- app/src/main/java/woowacourse/movie/model/movieInfo/Synopsis.kt
+++ app/src/main/java/woowacourse/movie/model/movieInfo/Synopsis.kt
@@ -2,8 +2,10 @@ package woowacourse.movie.model.movieInfo
 
 import java.io.Serializable
 
-class Synopsis(private val content: String): Serializable {
+class Synopsis(val content: String): Serializable {
     init {
         require(content.isNotEmpty()){ "시놉시스의 길이는 1이상이어야 합니다." }
     }
+    override fun toString() = content
+
 }
