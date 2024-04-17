46bd4c0bc8ea179a0bbb44053b18eee455b6888c
ii2001 <iivks2001@gmail.com>
2024-04-17 15:03:24 +0900

    :sparkles: MainActivity 영화 정보 반영

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
 import woowacourse.movie.model.movieInfo.MovieInfo
@@ -10,6 +11,7 @@ import woowacourse.movie.model.movieInfo.RunningTime
 import woowacourse.movie.model.movieInfo.Synopsis
 import woowacourse.movie.model.movieInfo.Title
 import woowacourse.movie.model.theater.Theater
+import java.time.LocalDate
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +19,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_list_item)
 
-        val movieInfo = MovieInfo(Title("MovieTitle"), Date(4024,2,29),RunningTime(230), Synopsis("wow!"))
+        val movieInfo = MovieInfo(
+            Title("차람과 하디의 진지한 여행기"),
+            LocalDate.of(2024, 2, 25),
+            RunningTime(230),
+            Synopsis("wow!")
+        )
         val theater = Theater(movieInfo)
+        findViewById<TextView>(R.id.movie_title).text= "${movieInfo.title}"
+        findViewById<TextView>(R.id.movie_release_date).text= "상영일: ${movieInfo.releaseDate}"
+        findViewById<TextView>(R.id.movie_duration).text= "${movieInfo.runningTime}분"
+
         val detailsButton = findViewById<Button>(R.id.movie_details_button)
         val intent = Intent(this, MovieDetailActivity::class.java).apply {
             putExtra("Theater", theater)
