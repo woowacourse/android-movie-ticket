a6060545bcf4c8fc62d15fdf2e3161a74522597b
Junyoung-WON <wjyo60@gmail.com>
2024-04-18 16:54:47 +0900

    test: TestFixture 수정 및 테스트 코드에 적용

--- app/src/test/java/woowacourse/movie/domain/model/MovieTest.kt
+++ app/src/test/java/woowacourse/movie/domain/model/MovieTest.kt
@@ -6,19 +6,12 @@ import org.junit.jupiter.api.Test
 class MovieTest {
     @Test
     fun `영화 정보를 생성한다`() {
-        val movie =
-            Movie(
-                posterImageId = 1,
-                title = "테넷",
-                screeningDate = "2020-08-26",
-                runningTime = 150,
-                summary = "시간을 건너뛰는 SF 영화",
-            )
+        val movie = createMovieInfo()
 
-        assertThat(movie.posterImageId).isEqualTo(1)
-        assertThat(movie.title).isEqualTo("테넷")
-        assertThat(movie.screeningDate).isEqualTo("2020-08-26")
-        assertThat(movie.runningTime).isEqualTo(150)
-        assertThat(movie.summary).isEqualTo("시간을 건너뛰는 SF 영화")
+        assertThat(movie.posterImageId).isEqualTo(POSTER_IMAGE_ID)
+        assertThat(movie.title).isEqualTo(TITLE)
+        assertThat(movie.screeningDate).isEqualTo(SCREENING_DATE)
+        assertThat(movie.runningTime).isEqualTo(RUNNING_TIME)
+        assertThat(movie.summary).isEqualTo(SUMMARY)
     }
 }
--- app/src/test/java/woowacourse/movie/domain/model/TestFixture.kt
+++ app/src/test/java/woowacourse/movie/domain/model/TestFixture.kt
@@ -1,4 +1,16 @@
 package woowacourse.movie.domain.model
 
+const val POSTER_IMAGE_ID = 1
 const val TITLE = "테넷"
 const val SCREENING_DATE = "2021-08-30"
+const val RUNNING_TIME = 150
+const val SUMMARY = "시간을 건너뛰는 SF 영화"
+
+fun createMovieInfo(): Movie =
+    Movie(
+        posterImageId = POSTER_IMAGE_ID,
+        title = TITLE,
+        screeningDate = SCREENING_DATE,
+        runningTime = RUNNING_TIME,
+        summary = SUMMARY,
+    )
