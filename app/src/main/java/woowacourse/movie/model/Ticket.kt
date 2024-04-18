2723b9b937ede7c437281705d488ead3430b5c3c
namyunsuk <yunsuk0328@naver.com>
2024-04-18 14:58:29 +0900

    style: 코드 줄바꿈

--- app/src/main/java/woowacourse/movie/model/Ticket.kt
+++ app/src/main/java/woowacourse/movie/model/Ticket.kt
@@ -1,6 +1,9 @@
 package woowacourse.movie.model
 
-class Ticket(val reservationCount: ReservationCount, private val price: Int = DEFAULT_PRICE) {
+class Ticket(
+    val reservationCount: ReservationCount,
+    private val price: Int = DEFAULT_PRICE,
+) {
     fun amount(): Int = price * reservationCount.count
 
     companion object {
