d657436a50102eb4d6e50abd1c01bee82ea8cf2b
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-18 13:37:13 +0900

    refactor: 중복된 테스트 코드를 Test Fixture 로 분리
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/androidTest/java/woowacourse/movie/activity/ReservationHomeActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/activity/ReservationHomeActivityTest.kt
@@ -20,42 +20,37 @@ class ReservationHomeActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(ReservationHomeActivity::class.java)
 
-    @Test
-    fun `영화_목록에서_첫번째_아이템의_타이틀을_보여준다`() {
+    private val moviesFirstItem =
         onData(
             anything(),
         ).inAdapterView(
             withId(R.id.list_view_reservation_home),
-        ).atPosition(0).onChildView(withId(R.id.item_movie_catalog_text_view_title)).check(
+        ).atPosition(0)
+
+    @Test
+    fun `영화_목록에서_첫번째_아이템의_타이틀을_보여준다`() {
+        moviesFirstItem.onChildView(withId(R.id.item_movie_catalog_text_view_title)).check(
             matches(withText("해리 포터와 마법사의 돌")),
         )
     }
 
     @Test
     fun `영화_목록에서_첫번째_아이템의_상영일을_보여준다`() {
-        onData(
-            anything(),
-        ).inAdapterView(
-            withId(R.id.list_view_reservation_home),
-        ).atPosition(0).onChildView(withId(R.id.item_movie_catalog_text_view_screening_date)).check(
+        moviesFirstItem.onChildView(withId(R.id.item_movie_catalog_text_view_screening_date)).check(
             matches(withText("2001.11.14")),
         )
     }
 
     @Test
     fun `영화_목록에서_첫번째_아이템의_상영시간을_보여준다`() {
-        onData(
-            anything(),
-        ).inAdapterView(
-            withId(R.id.list_view_reservation_home),
-        ).atPosition(0).onChildView(withId(R.id.item_movie_catalog_text_view_running_time)).check(
+        moviesFirstItem.onChildView(withId(R.id.item_movie_catalog_text_view_running_time)).check(
             matches(withText("152분")),
         )
     }
 
     @Test
     fun `영화_목록에서_첫번째_아이템의_버튼을_눌렀을_때_예약_상세_화면으로_이동`() {
-        onData(anything()).inAdapterView(withId(R.id.list_view_reservation_home)).atPosition(0).onChildView(
+        moviesFirstItem.onChildView(
             withId(R.id.item_movie_catalog_button_reservation),
         ).perform(click())
 
