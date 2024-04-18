4dc2efbff9b36c1f7f83f2721cc73acc24d5728a
murjune <sis9221302@gmail.com>
2024-04-18 11:36:42 +0900

    feat: MovieReservation 총 금액 계산 로직

--- domain/src/main/java/woowacourse/movie/MovieReservation.kt
+++ domain/src/main/java/woowacourse/movie/MovieReservation.kt
@@ -1,7 +1,13 @@
 package woowacourse.movie
 
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
 data class MovieReservation(
+    val id: Long,
     val movie: ScreeningMovie,
     val headCount: HeadCount,
-    val cancelDeadLine: Int
-)
+    val cancelDeadLine: Duration = 15.minutes
+) {
+    val totalPrice: Price get() = movie.price * headCount.count
+}
