cf37dcb437ce4dbbfe3d448373c0c3646e37f685
hevton <hevton22@gmail.com>
2024-04-18 14:46:00 +0900

    refactor: Serializable 제거

--- app/src/main/java/woowacourse/movie/model/MovieTicket.kt
+++ app/src/main/java/woowacourse/movie/model/MovieTicket.kt
@@ -1,12 +1,10 @@
 package woowacourse.movie.model
 
-import java.io.Serializable
-
 class MovieTicket(
     val title: String,
     val date: Long,
     val count: Int,
-) : Serializable {
+) {
     val price: Int = TICKET_PRICE * count
 
     companion object {
