418d886930e8140453aeb0ec60ec6a52ce3f1b32
murjune <sis9221302@gmail.com>
2024-04-18 15:16:29 +0900

    feat: Stub Movie Repository

---
+++ domain/src/main/java/woowacourse/movie/repository/MovieRepository.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.repository
+
+import woowacourse.movie.model.HeadCount
+import woowacourse.movie.model.MovieReservation
+import woowacourse.movie.model.ScreeningMovie
+import java.time.LocalDateTime
+
+interface MovieRepository {
+    fun screenMovies(): List<ScreeningMovie>
+
+    fun screenMovieById(id: Long): ScreeningMovie
+
+    fun reserveMovie(
+        id: Long,
+        dateTime: LocalDateTime,
+        count: HeadCount
+    ): Result<Unit>
+
+    fun movieReservationById(id: Long): MovieReservation
+}
