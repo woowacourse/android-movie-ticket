f13aab00dddcd7b017910c870cb562c6ecb434a5
murjune <sis9221302@gmail.com>
2024-04-18 11:33:44 +0900

    feat: Price VO

---
+++ domain/src/main/java/woowacourse/movie/Price.kt
@@ -0,0 +1,19 @@
+package woowacourse.movie
+
+@JvmInline
+value class Price(val price: Long) {
+
+    init {
+        require(price >= MIN_PRICE) {
+            "$price - 가격은 ${MIN_PRICE}원 이상 이어야 합니다."
+        }
+    }
+
+    operator fun times(count: Int): Price {
+        return Price(price * count)
+    }
+
+    companion object {
+        private const val MIN_PRICE = 0L
+    }
+}
