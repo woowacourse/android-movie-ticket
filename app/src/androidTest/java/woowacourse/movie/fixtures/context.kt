ff1de2ceebc16a652f8dc324e8cbbc1d1bc6eb6d
murjune <sis9221302@gmail.com>
2024-04-18 14:12:45 +0900

    test: context fixture

---
+++ app/src/androidTest/java/woowacourse/movie/fixtures/context.kt
@@ -0,0 +1,6 @@
+package woowacourse.movie.fixtures
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+
+val context: Context get() = ApplicationProvider.getApplicationContext<Context>()
