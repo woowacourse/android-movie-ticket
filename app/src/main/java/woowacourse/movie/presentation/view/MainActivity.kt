657208a87b9eb802c952a54312bf59d685087cc6
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-18 14:28:25 +0900

    style: import 순서를 알파벳 순서에 맞추어 수정

--- app/src/main/java/woowacourse/movie/presentation/view/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/MainActivity.kt
@@ -2,12 +2,12 @@ package woowacourse.movie.presentation.view
 
 import android.content.Intent
 import android.widget.ListView
-import woowacourse.movie.presentation.contract.MainContract
-import woowacourse.movie.presentation.presenter.MainPresenterImpl
-import woowacourse.movie.presentation.adapter.MovieListAdapter
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.presentation.adapter.MovieListAdapter
 import woowacourse.movie.presentation.base.BaseActivity
+import woowacourse.movie.presentation.contract.MainContract
+import woowacourse.movie.presentation.presenter.MainPresenterImpl
 
 class MainActivity : BaseActivity(), MainContract.View {
     private lateinit var adapter: MovieListAdapter
