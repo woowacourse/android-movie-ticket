7c80a8669865be6cc7bdaf4e169beca82b26b4ac
kkosang <eiwoqp77@gmail.com>
2024-04-18 10:47:09 +0900

    rename(Ticket): 클래스 네이밍 시그니처 변경 - Count -> Ticket - amount property -> count

--- app/src/main/java/woowacourse/movie/model/Ticket.kt
+++ app/src/main/java/woowacourse/movie/model/Ticket.kt
@@ -1,23 +1,23 @@
 package woowacourse.movie.model
 
-data class Count(var amount: Int = DEFAULT_AMOUNT) {
+data class Ticket(var count: Int = DEFAULT_AMOUNT) {
     init {
-        require(amount > 0) {
+        require(count > 0) {
             ERROR_AMOUNT_LESS_THAN_ONE
         }
     }
 
     fun sub() {
-        if (amount > 1) {
-            amount--
+        if (count > 1) {
+            count--
         }
     }
 
     fun add() {
-        amount++
+        count++
     }
 
-    fun price(): Int = amount * TICKET_PRICE
+    fun price(): Int = count * TICKET_PRICE
 
     companion object {
         const val DEFAULT_AMOUNT: Int = 1
--- app/src/test/java/woowacourse/movie/model/TicketTest.kt
+++ app/src/test/java/woowacourse/movie/model/TicketTest.kt
@@ -2,40 +2,40 @@ package woowacourse.movie.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import woowacourse.movie.model.Count.Companion.TICKET_PRICE
+import woowacourse.movie.model.Ticket.Companion.TICKET_PRICE
 
-class CountTest {
+class TicketTest {
     @Test
     fun `amount의 값이 2이상일 때, sub함수 호출하면 amount의 값이 1감소한다`() {
-        val count = Count(2)
+        val ticket = Ticket(2)
 
-        count.sub()
+        ticket.sub()
 
-        assertThat(count.amount).isEqualTo(1)
+        assertThat(ticket.count).isEqualTo(1)
     }
 
     @Test
     fun `amount의 값이 2미만일 때, sub함수 호출하면 amount의 값은 감소하지 않는다`() {
-        val count = Count(1)
+        val ticket = Ticket(1)
 
-        count.sub()
+        ticket.sub()
 
-        assertThat(count.amount).isEqualTo(1)
+        assertThat(ticket.count).isEqualTo(1)
     }
 
     @Test
     fun `add함수 호출하면 amount의 값이 1증가한다`() {
-        val count = Count(2)
+        val ticket = Ticket(2)
 
-        count.add()
+        ticket.add()
 
-        assertThat(count.amount).isEqualTo(3)
+        assertThat(ticket.count).isEqualTo(3)
     }
 
     @Test
     fun `amount값에 비례하여 price값을 구할 수 있다`() {
-        val count = Count(3)
+        val ticket = Ticket(3)
 
-        assertThat(count.price()).isEqualTo(3 * TICKET_PRICE)
+        assertThat(ticket.price()).isEqualTo(3 * TICKET_PRICE)
     }
 }
