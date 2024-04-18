996faad0b09cb00088a41f0d9af1028a9d769800
songpink <jejeje04@naver.com>
2024-04-18 13:57:27 +0900

    refactor: 뷰홀더 사용, 함수분리

--- app/src/main/java/woowacourse/movie/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/MovieListAdapter.kt
@@ -6,15 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import java.io.Serializable
 
 class MovieListAdapter(
     private val context: Context,
     private val movieList: ArrayList<Movie>,
 ) : BaseAdapter() {
+    private lateinit var viewHolder: ViewHolder
+
     override fun getCount(): Int {
         return movieList.size
     }
@@ -32,27 +31,44 @@ class MovieListAdapter(
         convertView: View?,
         parent: ViewGroup?,
     ): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.movie_item, null)
-
+        val view: View
         val movie = movieList[position]
-        val title = view.findViewById<TextView>(R.id.movie_title)
-        val poster = view.findViewById<ImageView>(R.id.movie_poster)
-        val screeningDate = view.findViewById<TextView>(R.id.movie_screening_date)
-        val runningTime = view.findViewById<TextView>(R.id.movie_running_time)
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.movie_item, parent)
+            makeViewHolder(view)
+        } else {
+            view = convertView
+            viewHolder = convertView.tag as ViewHolder
+        }
+        setViewHolder(movie)
+        setClickListener(movie)
+        return view
+    }
 
-        title.text = movie.title
-        poster.setImageResource(movie.posterResourceId)
-        screeningDate.text = movie.screeningDate
-        runningTime.text = movie.runningTime.toString()
+    private fun makeViewHolder(view: View) {
+        viewHolder =
+            ViewHolder(
+                view.findViewById(R.id.movie_title),
+                view.findViewById(R.id.movie_poster),
+                view.findViewById(R.id.movie_screening_date),
+                view.findViewById(R.id.movie_running_time),
+                view.findViewById(R.id.movie_reservation_button),
+            )
+        view.tag = viewHolder
+    }
 
-        val movieReservationButton = view.findViewById<Button>(R.id.movie_reservation_button)
+    private fun setViewHolder(movie: Movie) {
+        viewHolder.title.text = movie.title
+        viewHolder.poster.setImageResource(movie.posterResourceId)
+        viewHolder.screeningDate.text = movie.screeningDate
+        viewHolder.runningTime.text = movie.runningTime.toString()
+    }
 
-        movieReservationButton.setOnClickListener {
+    private fun setClickListener(movie: Movie) {
+        viewHolder.movieReservationButton.setOnClickListener {
             val intent = Intent(context, MovieReservationActivity::class.java)
-            intent.putExtra(Movie.KEY_NAME_MOVIE, getItem(position) as Serializable)
+            intent.putExtra(Movie.KEY_NAME_MOVIE, movie as Serializable)
             context.startActivity(intent)
         }
-
-        return view
     }
 }
