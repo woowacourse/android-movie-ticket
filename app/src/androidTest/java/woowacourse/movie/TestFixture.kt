94aeba91f444268b356c28de8ef6bc7f08fde815
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-18 14:22:59 +0900

    feat: TestFixture를 별도의 파일로 분리
    - ReservationHomeActivityTest -> TestFixture로 이동
    
    Co-authored-by: haeum808 <haeumueah@gmail.com>

---
+++ app/src/androidTest/java/woowacourse/movie/TestFixture.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.Matchers
+
+object TestFixture {
+    val moviesFirstItem =
+        Espresso.onData(
+            Matchers.anything(),
+        ).inAdapterView(
+            ViewMatchers.withId(R.id.list_view_reservation_home),
+        ).atPosition(0)
+}
