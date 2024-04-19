0990805d55935a4af222e3a0b0e2dceb89e0c59a
aprilgom <aprilgom@gmail.com>
2024-04-20 00:35:24 +0900

    feat(Theater): 에러 표기 대신 임시로 사용할 default 객체 구현

--- app/src/main/java/woowacourse/movie/model/theater/Theater.kt
+++ app/src/main/java/woowacourse/movie/model/theater/Theater.kt
@@ -1,6 +1,22 @@
 package woowacourse.movie.model.theater
 
+import woowacourse.movie.model.movieInfo.MovieDate
 import woowacourse.movie.model.movieInfo.MovieInfo
+import woowacourse.movie.model.movieInfo.RunningTime
+import woowacourse.movie.model.movieInfo.Synopsis
+import woowacourse.movie.model.movieInfo.Title
 import java.io.Serializable
+import java.time.LocalDate
 
-class Theater(val movie: MovieInfo, val charge: Int = 13000) : Serializable
+class Theater(val movie: MovieInfo, val charge: Int = 13000) : Serializable {
+    companion object {
+        val default = Theater(
+            MovieInfo(
+                Title("default"),
+                MovieDate(LocalDate.of(2222, 2, 2)),
+                RunningTime(222),
+                Synopsis("defaultSynopsis")
+            )
+        )
+    }
+}
