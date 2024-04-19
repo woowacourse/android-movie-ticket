425bbd1e5337e41bb932f7977bc326785db7f6a8
ii2001 <iivks2001@gmail.com>
2024-04-19 11:01:38 +0900

    :recycle: 테스트 코드 정렬

--- app/src/androidTest/java/woowacourse/movie/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MovieDetailActivityTest.kt
@@ -3,7 +3,6 @@ package woowacourse.movie
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
