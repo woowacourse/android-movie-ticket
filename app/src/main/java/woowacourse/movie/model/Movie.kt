d3a3fc3e5d3dd79b175d3f30fd89ab1af90531f5
hevton <hevton22@gmail.com>
2024-04-19 16:16:50 +0900

    refactor: tools 기능 활용

--- app/src/main/java/woowacourse/movie/model/Movie.kt
+++ app/src/main/java/woowacourse/movie/model/Movie.kt
@@ -1,7 +1,10 @@
 package woowacourse.movie.model
 
+import androidx.annotation.DrawableRes
+
 data class Movie(
     val id: Long,
+    @DrawableRes
     val thumbnail: Int,
     val title: String,
     val description: String,
