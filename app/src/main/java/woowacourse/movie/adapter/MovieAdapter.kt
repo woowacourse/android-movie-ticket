692e6e58354f17d2d8f0608d22e74e44237a5733
Gimun Kim <kmkim7575@gmail.com>
2024-04-19 15:46:40 +0900

    refactor: 단순 렌더링 로직만 존재하는 영화목록 어댑터를 위한 Presenter 제거

--- app/src/main/java/woowacourse/movie/adapter/MovieAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MovieAdapter.kt
@@ -1,6 +1,6 @@
 package woowacourse.movie.adapter
 
-import android.util.Log
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,15 +10,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import woowacourse.movie.R
 import woowacourse.movie.model.Movie
-import woowacourse.movie.presenter.MovieAdapterPresenter
-import woowacourse.movie.presenter.contract.MovieAdapterContract
 
 class MovieAdapter(
     private val movies: List<Movie>,
     private val onTicketingButtonClick: (Int) -> Unit,
-) : BaseAdapter(), MovieAdapterContract {
-    private val presenter = MovieAdapterPresenter(this)
-
+) : BaseAdapter() {
     override fun getCount(): Int = movies.size
 
     override fun getItem(position: Int): Any = movies[position]
@@ -30,34 +26,33 @@ class MovieAdapter(
         convertView: View?,
         parent: ViewGroup?,
     ): View {
+        val context = parent?.context ?: throw Exception() // TODO 예외 처리 방식 확정 필요
         val movieViewHolder: MovieViewHolder
         val view: View
         if (convertView == null) {
-            Log.d("adapter", "new")
-            view =
-                LayoutInflater.from(parent?.context)
-                    .inflate(R.layout.item_movie, parent, false)
+            view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
             movieViewHolder = MovieViewHolder(view)
             view.tag = movieViewHolder
         } else {
             view = convertView
             movieViewHolder = convertView.tag as MovieViewHolder
         }
-
-        presenter.assignInitialView(movies[position], movieViewHolder)
+        assignInitialView(context, movies[position], movieViewHolder)
         return view
     }
 
-    override fun assignInitialView(
+    private fun assignInitialView(
+        context: Context,
         movie: Movie,
-        itemView: MovieViewHolder,
+        viewHolder: MovieViewHolder,
     ) {
-        itemView.title.text = movie.title
-        itemView.date.text = itemView.date.context?.getString(R.string.title_date, movie.date)
-        itemView.runningTime.text =
-            itemView.runningTime.context?.getString(R.string.title_running_time, movie.runningTime)
-        itemView.ticketingButton.setOnClickListener { onTicketingButtonClick(movie.id) }
-        itemView.thumbnail.setImageResource(movie.thumbnail)
+        viewHolder.apply {
+            title.text = movie.title
+            date.text = context.getString(R.string.title_date, movie.date)
+            runningTime.text = context.getString(R.string.title_running_time, movie.runningTime)
+            ticketingButton.setOnClickListener { onTicketingButtonClick(movie.id) }
+            thumbnail.setImageResource(movie.thumbnail)
+        }
     }
 
     inner class MovieViewHolder(itemView: View) {
