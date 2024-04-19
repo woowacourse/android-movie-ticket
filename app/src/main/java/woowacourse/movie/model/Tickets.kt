03fe55394d4789de44b6ba4b6c3a0c56c491135d
JoYehyun <jo990714@naver.com>
2024-04-19 15:09:54 +0900

    refactor: 티켓 가격 설정 가능하도록 변경

--- app/src/main/java/woowacourse/movie/model/Tickets.kt
+++ app/src/main/java/woowacourse/movie/model/Tickets.kt
@@ -1,10 +1,10 @@
 package woowacourse.movie.model
 
-class Tickets(private val count: Count) {
+class Tickets(private val count: Count, private val price: Int = DEFAULT_TICKET_PRICE) {
     val totalPrice: Int
-        get() = count.value * TICKET_PRICE
+        get() = count.value * price
 
     companion object {
-        private const val TICKET_PRICE = 13000
+        private const val DEFAULT_TICKET_PRICE = 13000
     }
 }
--- app/src/test/java/woowacourse/movie/model/TicketsTest.kt
+++ app/src/test/java/woowacourse/movie/model/TicketsTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class TicketsTest {
     @Test
     fun `주어진_티켓_수에_맞는_총_금액을_계산하여_반환한다`() {
-        val tickets = Tickets(Count(3))
+        val tickets = Tickets(Count(3), 13000)
         assertThat(tickets.totalPrice).isEqualTo(39000)
     }
 }
