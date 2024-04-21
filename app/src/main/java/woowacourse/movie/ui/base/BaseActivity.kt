c64540a28f36e326d4bfbc4cf22b45118202ed37
Namyunsuk <yunsuk0328@naver.com>
2024-04-21 20:46:34 +0900

    refactor: 사용하지 않는 추상 메서드 제거

--- app/src/main/java/woowacourse/movie/ui/base/BaseActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/base/BaseActivity.kt
@@ -1,13 +1,7 @@
 package woowacourse.movie.ui.base
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity<P> : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initializePresenter()
-    }
-
     abstract fun initializePresenter(): P
 }
