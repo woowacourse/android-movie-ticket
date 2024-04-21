a0eec1e6f99cce2cf8e4f48b64a11fa1e92b86fc
김진우 <jinwoo3661@naver.com>
2024-04-21 17:17:59 +0900

    refactor: sample 데이터 분리

---
+++ app/src/main/java/woowacourse/movie/data/MockMovies.kt
@@ -0,0 +1,33 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.R
+import woowacourse.movie.domain.model.Movie
+
+object MockMovies {
+    private const val DEFAULT_MOVIE_TITLE = "영화가 존재하지 않습니다."
+
+    val sampleMovies =
+        listOf(
+            Movie(
+                movieId = 1,
+                title = "해리 포터와 마법사의 돌",
+                screeningDate = "2024.3.1",
+                runningTime = 152,
+                posterResourceId = R.drawable.harry_potter_poster,
+                description =
+                "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. " +
+                        "해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. " +
+                        "크리스 콜럼버스가 감독을 맡았다.",
+            ),
+        )
+
+    val defaultMovie =
+        Movie(
+            movieId = -1,
+            title = DEFAULT_MOVIE_TITLE,
+            screeningDate = "",
+            runningTime = 0,
+            posterResourceId = null,
+            description = "",
+        )
+}
--- app/src/main/java/woowacourse/movie/data/MovieRepositoryImpl.kt
+++ app/src/main/java/woowacourse/movie/data/MovieRepositoryImpl.kt
@@ -6,41 +6,12 @@ import woowacourse.movie.domain.repository.MovieRepository
 
 class MovieRepositoryImpl : MovieRepository {
     override fun loadMovies(): List<Movie> {
-        return sampleMovies
+        return MockMovies.sampleMovies
     }
 
     override fun getMovie(id: Int): Movie {
-        return sampleMovies.find { movie: Movie ->
+        return MockMovies.sampleMovies.find { movie: Movie ->
             movie.movieId == id
-        } ?: defaultMovie
-    }
-
-    companion object {
-        private const val DEFAULT_MOVIE_TITLE = "영화가 존재하지 않습니다."
-
-        val sampleMovies =
-            listOf(
-                Movie(
-                    movieId = 1,
-                    title = "해리 포터와 마법사의 돌",
-                    screeningDate = "2024.3.1",
-                    runningTime = 152,
-                    posterResourceId = R.drawable.harry_potter_poster,
-                    description =
-                        "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. " +
-                            "해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. " +
-                            "크리스 콜럼버스가 감독을 맡았다.",
-                ),
-            )
-
-        val defaultMovie =
-            Movie(
-                movieId = -1,
-                title = DEFAULT_MOVIE_TITLE,
-                screeningDate = "",
-                runningTime = 0,
-                posterResourceId = null,
-                description = "",
-            )
+        } ?: MockMovies.defaultMovie
     }
 }
