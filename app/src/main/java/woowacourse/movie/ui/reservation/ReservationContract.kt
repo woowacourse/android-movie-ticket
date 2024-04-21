19da12cb8f87b317d0843fd9fd96dc80a0a55495
sh1mj1 <7wlgns@gmail.com>
2024-04-21 21:21:19 +0900

    feat(ReservationPresenter2): 예약 정보를 로드
    - ReservationContract2 를 구현한다

--- app/src/main/java/woowacourse/movie/ui/reservation/ReservationContract.kt
+++ app/src/main/java/woowacourse/movie/ui/reservation/ReservationContract.kt
@@ -1,10 +1,29 @@
 package woowacourse.movie.ui.reservation
 
 import woowacourse.movie.domain.model.Reservation
+import woowacourse.movie.domain.model.Reservation2
 
 interface ReservationContract {
     interface View {
         fun showReservation(reservation: Reservation)
+
+        fun showToastMessage(message: String)
+
+        fun showSnackBar(message: String)
+
+        fun goToBack(message: String)
+
+        fun unexpectedFinish(message: String)
+    }
+
+    interface Presenter {
+        fun loadReservation(id: Int)
+    }
+}
+
+interface ReservationContract2 {
+    interface View {
+        fun showReservation(reservation: Reservation2)
 
         fun showToastMessage(message: String)
 
--- app/src/main/java/woowacourse/movie/ui/reservation/ReservationPresenter.kt
+++ app/src/main/java/woowacourse/movie/ui/reservation/ReservationPresenter.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.ui.reservation
 
 import woowacourse.movie.domain.repository.ReservationRepository
+import woowacourse.movie.domain.repository.ReservationRepository2
 
 class ReservationPresenter(
     private val view: ReservationContract.View,
@@ -9,6 +10,22 @@ class ReservationPresenter(
     override fun loadReservation(id: Int) {
         repository.findById(id).onSuccess { screen ->
             view.showReservation(screen)
+        }.onFailure { e ->
+            when (e) {
+                is NoSuchElementException -> view.goToBack("해당하는 상영 정보가 없습니다.")
+                else -> view.unexpectedFinish("예상치 못한 에러가 발생했습니다")
+            }
+        }
+    }
+}
+
+class ReservationPresenter2(
+    private val view: ReservationContract2.View,
+    private val repository: ReservationRepository2,
+) : ReservationContract2.Presenter {
+    override fun loadReservation(id: Int) {
+        repository.findById(id).onSuccess { reservation ->
+            view.showReservation(reservation)
         }.onFailure { e ->
             when (e) {
                 is NoSuchElementException -> view.goToBack("해당하는 상영 정보가 없습니다.")
