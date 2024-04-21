3fdd52161ff2764262d5d7bd8df3c688c42c3a5b
sh1mj1 <7wlgns@gmail.com>
2024-04-21 21:12:44 +0900

    feat(Reservation2): Screen2 를 사용

--- app/src/main/java/woowacourse/movie/domain/model/Reservation.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Reservation.kt
@@ -8,3 +8,12 @@ data class Reservation(
     val totalPrice: Int
         get() = ticket.count * screen.price
 }
+
+data class Reservation2(
+    val id: Int,
+    val screen: Screen2,
+    val ticket: Ticket,
+) {
+    val totalPrice: Int
+        get() = ticket.count * screen.price
+}
