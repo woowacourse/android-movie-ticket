ff9e1120c80b854bf1da38aff67683936617f6c3
sh1mj1 <7wlgns@gmail.com>
2024-04-21 18:09:00 +0900

    feat(ScreenPresenter2): ScreenContract2 를 구현하여 상영 정보를 보여준다
    - 상영 정보를 뷰에서 필요한 것들만 사용하도록 변환한다

--- app/src/main/java/woowacourse/movie/ui/screen/ScreenContract.kt
+++ app/src/main/java/woowacourse/movie/ui/screen/ScreenContract.kt
@@ -1,10 +1,21 @@
 package woowacourse.movie.ui.screen
 
 import woowacourse.movie.domain.model.Screen
+import woowacourse.movie.ui.ScreenPreviewUI
 
 interface ScreenContract {
     interface View {
         fun showScreens(screens: List<Screen>)
+    }
+
+    interface Presenter {
+        fun loadScreens()
+    }
+}
+
+interface ScreenContract2 {
+    interface View {
+        fun showScreens(screens: List<ScreenPreviewUI>)
     }
 
     interface Presenter {
--- app/src/main/java/woowacourse/movie/ui/screen/ScreenPresenter.kt
+++ app/src/main/java/woowacourse/movie/ui/screen/ScreenPresenter.kt
@@ -1,6 +1,12 @@
 package woowacourse.movie.ui.screen
 
+import woowacourse.movie.domain.model.Movie2
+import woowacourse.movie.domain.model.Screen2
+import woowacourse.movie.domain.repository.MovieRepository
 import woowacourse.movie.domain.repository.ScreenRepository
+import woowacourse.movie.domain.repository.ScreenRepository2
+import woowacourse.movie.ui.MoviePreviewUI
+import woowacourse.movie.ui.ScreenPreviewUI
 
 class ScreenPresenter(
     private val view: ScreenContract.View,
@@ -10,4 +16,29 @@ class ScreenPresenter(
         val screens = repository.load()
         view.showScreens(screens)
     }
+}
+
+class ScreenPresenter2(
+    private val view: ScreenContract2.View,
+    private val movieRepository: MovieRepository,
+    private val screenRepository: ScreenRepository2,
+) : ScreenContract2.Presenter {
+    override fun loadScreens() {
+        val screens = screenRepository.load()
+
+        val screenPreviewUIs = screens.map { screen -> screen.toPreviewUI() }
+        view.showScreens(screenPreviewUIs)
+    }
+
+    private fun Screen2.toPreviewUI() = ScreenPreviewUI(
+        id = id,
+        moviePreviewUI = movie.toPreviewUI(),
+        date = date,
+    )
+
+    private fun Movie2.toPreviewUI() = MoviePreviewUI(
+        title = title,
+        image = movieRepository.imageSrc(id),
+        runningTime = runningTime,
+    )
 }
