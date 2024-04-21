ff98717168af0072bd8f11bc0c301156b6f161ac
murjune <sis9221302@gmail.com>
2024-04-21 17:55:41 +0900

    feat(ReservationResultActivity): InValid 한 Id 가 들어올 때 Error View 띄우기

---
+++ app/src/androidTest/java/woowacourse/movie/presentation/reservation/result/ReservationResultActivityTest.kt
@@ -0,0 +1,56 @@
+package woowacourse.movie.presentation.reservation.result
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.CoreMatchers
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import woowacourse.movie.R
+import woowacourse.movie.data.FakeMovieRepository
+import woowacourse.movie.model.HeadCount
+import woowacourse.movie.utils.context
+import java.time.LocalDateTime
+
+class ReservationResultActivityTest {
+
+    @Test
+    @DisplayName("유효한 ID 가 전달되었을 때, 예약 결과가 보여지는지 테스트")
+    fun test() {
+        launchSuccessScenario()
+
+        Espresso.onView(ViewMatchers.withId(R.id.cl_reservation_result_error))
+            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.cl_reservation_result_success))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+
+    @Test
+    @DisplayName("유효하지 않은 ID 가 전달되었을 때, 에러 화면이 보여지는지 테스트")
+    fun test2() {
+        ActivityScenario.launch<ReservationResultActivity>(
+            ReservationResultActivity.newIntent(
+                context,
+                ReservationResultActivity.INVALID_RESERVATION_ID,
+            ),
+        )
+
+        Espresso.onView(ViewMatchers.withId(R.id.cl_reservation_result_error))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.cl_reservation_result_success))
+            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
+    }
+
+    private fun launchSuccessScenario(): ActivityScenario<ReservationResultActivity> {
+        // 이렇게 안하고 어떻게 하지...?
+        FakeMovieRepository.reserveMovie(1, LocalDateTime.now(), HeadCount(1))
+        return ActivityScenario.launch<ReservationResultActivity>(
+            ReservationResultActivity.newIntent(
+                context,
+                1L,
+            ),
+        )
+    }
+}
--- app/src/main/java/woowacourse/movie/presentation/reservation/result/ReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/reservation/result/ReservationResultActivity.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import woowacourse.movie.R
 import woowacourse.movie.data.FakeMovieRepository
 
@@ -15,14 +17,25 @@ class ReservationResultActivity : AppCompatActivity(), ReservationResultView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation_result)
-
+        val id = intent.getLongExtra(KEY_RESERVATION_ID, INVALID_RESERVATION_ID)
+        if (id == INVALID_RESERVATION_ID) {
+            initErrorView()
+            return
+        }
         presenter =
             ReservationResultPresenter(
-                id = intent.getLongExtra(KEY_RESERVATION_ID, INVALID_RESERVATION_ID),
+                id = id,
                 repository = FakeMovieRepository,
                 view = this,
             )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initErrorView() {
+        val errorLayout = findViewById<LinearLayout>(R.id.cl_reservation_result_error)
+        val successLayout = findViewById<ConstraintLayout>(R.id.cl_reservation_result_success)
+        errorLayout.visibility = ConstraintLayout.VISIBLE
+        successLayout.visibility = ConstraintLayout.GONE
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -45,8 +58,8 @@ class ReservationResultActivity : AppCompatActivity(), ReservationResultView {
     }
 
     companion object {
-        private val KEY_RESERVATION_ID: String? = this::class.java.canonicalName
-        private const val INVALID_RESERVATION_ID: Long = -1
+        val KEY_RESERVATION_ID: String? = this::class.java.canonicalName
+        const val INVALID_RESERVATION_ID: Long = -1
 
         @JvmStatic
         fun newIntent(
