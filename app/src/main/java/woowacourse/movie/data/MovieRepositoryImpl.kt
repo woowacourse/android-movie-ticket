66ac250ac43b31b72ea1808bfdbd8f296b558748
김진우 <jinwoo3661@naver.com>
2024-04-21 16:25:54 +0900

    refactor: 전달받은 id값으로 데이터 로드 기능 구현

--- app/src/main/java/woowacourse/movie/data/MovieRepositoryImpl.kt
+++ app/src/main/java/woowacourse/movie/data/MovieRepositoryImpl.kt
@@ -9,10 +9,17 @@ class MovieRepositoryImpl : MovieRepository {
         return sampleMovies
     }
 
+    override fun getMovie(id: Int): Movie {
+        return sampleMovies.find { movie: Movie ->
+            movie.movieId == id
+        } ?: defaultMovie
+    }
+
     companion object {
         val sampleMovies =
             listOf(
                 Movie(
+                    movieId = 1,
                     title = "해리 포터와 마법사의 돌",
                     screeningDate = "2024.3.1",
                     runningTime = 152,
@@ -20,5 +27,14 @@ class MovieRepositoryImpl : MovieRepository {
                     description = "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
                 ),
             )
+
+        val defaultMovie =  Movie(
+            movieId = -1,
+            title = "잘못 된 영화 정보",
+            screeningDate = "",
+            runningTime = 0,
+            posterResourceId = null,
+            description = ""
+        )
     }
 }
--- app/src/main/java/woowacourse/movie/domain/repository/MovieRepository.kt
+++ app/src/main/java/woowacourse/movie/domain/repository/MovieRepository.kt
@@ -4,4 +4,5 @@ import woowacourse.movie.domain.model.Movie
 
 interface MovieRepository {
     fun loadMovies(): List<Movie>
+    fun getMovie(id: Int): Movie
 }
