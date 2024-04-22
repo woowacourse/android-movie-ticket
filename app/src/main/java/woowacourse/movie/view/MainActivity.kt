4a1b2e85d9e12581193969e220e04f9f64a1bd87
Hogu59 <dbstjdguswkd@naver.com>
2024-04-22 14:50:23 +0900

    feat(MovieData): move data to repository

--- app/src/main/java/woowacourse/movie/view/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/view/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import woowacourse.movie.R
 import woowacourse.movie.adapter.MovieListAdapter
 import woowacourse.movie.presenter.MainPresenter
-import java.time.LocalDate
+import woowacourse.movie.repository.MovieData
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainPresenter: MainPresenter
@@ -16,15 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mainPresenter = MainPresenter()
 
-        mainPresenter.registerMovie(
-            "해리포터와 마법사의 돌",
-            listOf(LocalDate.of(2024, 3, 1)),
-            152,
-            "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
-            R.drawable.harry_image1,
-        )
-
-        val movieAdapter = MovieListAdapter(this, mainPresenter)
+        val movieAdapter = MovieListAdapter(this, MovieData.movies)
         val listView = findViewById<ListView>(R.id.movie_list)
         listView.adapter = movieAdapter
     }
