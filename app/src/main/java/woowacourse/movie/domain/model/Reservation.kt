feadceb8874c3fb396704420eee94e592b074fbe
sh1mj1 <7wlgns@gmail.com>
2024-04-22 15:42:00 +0900

    feat($domain/model): 인터페이스 클래스, Null 객체 생성

--- app/src/main/java/woowacourse/movie/domain/model/Reservation.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Reservation.kt
@@ -2,7 +2,7 @@ package woowacourse.movie.domain.model
 
 data class Reservation(
     val id: Int,
-    val screen: Screen,
+    val screen: IScreen,
     val ticket: Ticket,
 ) {
     val totalPrice: Int
--- app/src/main/java/woowacourse/movie/domain/model/Screen.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Screen.kt
@@ -1,8 +1,25 @@
 package woowacourse.movie.domain.model
 
+import java.lang.IllegalStateException
+
 data class Screen(
-    val id: Int,
-    val movie: Movie,
-    val date: String,
-    val price: Int,
-)
+    override val id: Int,
+    override val movie: Movie,
+    override val date: String,
+    override val price: Int,
+) : IScreen
+
+interface IScreen {
+    val id: Int
+    val movie: IMovie
+    val date: String
+    val price: Int
+}
+
+data class NullScreen(
+    override val id: Int = -1,
+    override val movie: IMovie = NullMovie(),
+    override val date: String = "",
+    override val price: Int = -1,
+    val throwable: Throwable = IllegalStateException("예기치 못한 오류"),
+) : IScreen
