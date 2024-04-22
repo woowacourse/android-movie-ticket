1a4108fe0f1d1ba2d1cdde1f3dfc7e5dbc16520e
gaeun <geun5744@gmail.com>
2024-04-22 19:49:26 +0900

    delete: 불필요한 default value 제거

--- app/src/main/java/woowacourse/movie/model/MovieReservationUiModel.kt
+++ app/src/main/java/woowacourse/movie/model/MovieReservationUiModel.kt
@@ -7,7 +7,7 @@ data class MovieReservationUiModel(
     val id: Long,
     val title: String,
     @DrawableRes val imageRes: Int = R.drawable.img_movie_poster,
-    val screenDate: String = "상영일: 2024.3.1",
-    val description: String = "",
-    val runningTime: String = "러닝타임: 152분",
+    val screenDate: String,
+    val description: String,
+    val runningTime: String,
 )
--- app/src/main/java/woowacourse/movie/model/ScreenMovieUiModel.kt
+++ app/src/main/java/woowacourse/movie/model/ScreenMovieUiModel.kt
@@ -7,6 +7,6 @@ data class ScreenMovieUiModel(
     val id: Long,
     val title: String,
     @DrawableRes val imageRes: Int = R.drawable.img_movie_poster,
-    val screenDate: String = "상영일: 2024.3.1",
-    val runningTime: String = "러닝타임: 152분",
+    val screenDate: String,
+    val runningTime: String,
 )
