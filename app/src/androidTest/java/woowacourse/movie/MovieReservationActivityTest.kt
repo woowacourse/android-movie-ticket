80f9177865130f35caa0c96bf4c6668ee9949a1c
gaeun <geun5744@gmail.com>
2024-04-22 14:30:49 +0900

    delete: 불필요한 import 제거

--- app/src/androidTest/java/woowacourse/movie/MovieReservationActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MovieReservationActivityTest.kt
@@ -2,10 +2,7 @@ package woowacourse.movie
 
 import android.content.Intent
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
