b19de4df8a750074343161057165db641c1aebda
ii2001 <iivks2001@gmail.com>
2024-04-23 10:40:52 +0900

    :recycle: Theater 내부 초기가격 상수화

--- app/src/main/java/woowacourse/movie/model/theater/Theater.kt
+++ app/src/main/java/woowacourse/movie/model/theater/Theater.kt
@@ -3,4 +3,8 @@ package woowacourse.movie.model.theater
 import woowacourse.movie.model.movieInfo.MovieInfo
 import java.io.Serializable
 
-class Theater(val movie: MovieInfo, val charge: Int = 13000) : Serializable
+class Theater(val movie: MovieInfo, val charge: Int = INITIAL_PRICE) : Serializable {
+    companion object {
+        const val INITIAL_PRICE = 13000
+    }
+}
