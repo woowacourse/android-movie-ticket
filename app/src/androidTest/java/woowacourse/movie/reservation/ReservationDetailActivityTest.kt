6bc617e5d67fb74b334552608477290cfe0f4c61
haeum808 <haeum808@gmail.com>
2024-04-23 15:40:49 +0900

    test: 처음 영화 티켓 수는 1이라는 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/reservation/ReservationDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/reservation/ReservationDetailActivityTest.kt
@@ -1,11 +1,8 @@
 package woowacourse.movie.reservation
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -14,8 +11,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
-import woowacourse.movie.TestFixture.moviesFirstItem
-import woowacourse.movie.home.HomeActivity
 import woowacourse.movie.reservation.detail.ReservationDetailActivity
 
 @RunWith(AndroidJUnit4::class)
@@ -24,20 +19,23 @@ class ReservationDetailActivityTest {
     var activityRule = ActivityScenarioRule(ReservationDetailActivity::class.java)
 
     @Test
-    fun `빼기_버튼을_누르면_값이_감소한다`() {
-        onView(withId(R.id.button_reservation_detail_minus))
-            .perform(click())
-
-        onView(withId(R.id.text_view_reservation_detail_number_of_tickets))
-            .check(matches(withText("1")))
+    fun `영화_티켓_디폴트_값은_1이다`() {
+        onView(withId(R.id.text_view_reservation_detail_number_of_tickets)).check(matches(withText("1")))
     }
 
     @Test
-    fun `더하기_버튼을_누르면_값이_증가한다`() {
-        onView(withId(R.id.button_reservation_detail_plus))
-            .perform(click())
+    fun `빼기_버튼을_누르면_영화_티켓_수가_감소한다`() {
+        onView(withId(R.id.button_reservation_detail_plus)).perform(click())
 
-        onView(withId(R.id.text_view_reservation_detail_number_of_tickets))
-            .check(matches(withText("2")))
+        onView(withId(R.id.button_reservation_detail_minus)).perform(click())
+
+        onView(withId(R.id.text_view_reservation_detail_number_of_tickets)).check(matches(withText("1")))
+    }
+
+    @Test
+    fun `더하기_버튼을_누르면_영화_티켓_수가_증가한다`() {
+        onView(withId(R.id.button_reservation_detail_plus)).perform(click())
+
+        onView(withId(R.id.text_view_reservation_detail_number_of_tickets)).check(matches(withText("2")))
     }
 }
