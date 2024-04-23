6dd2cfeaf59a7690652758ebb9b5f9603e0c26e2
ii2001 <iivks2001@gmail.com>
2024-04-23 11:40:39 +0900

    :recycle: getSerializable 분기처리

--- app/src/main/java/woowacourse/movie/purchaseConfirmation/PurchaseConfirmationActivityPresenter.kt
+++ app/src/main/java/woowacourse/movie/purchaseConfirmation/PurchaseConfirmationActivityPresenter.kt
@@ -2,12 +2,14 @@ package woowacourse.movie.purchaseConfirmation
 
 import android.content.Intent
 import android.os.Build
-import androidx.annotation.RequiresApi
 import woowacourse.movie.model.theater.Theater
 
-@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class PurchaseConfirmationActivityPresenter(intent: Intent) {
-    private val theater = intent.getSerializableExtra("Theater", Theater::class.java)
+    private val theater = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        intent.getSerializableExtra("Theater", Theater::class.java)
+    } else {
+        intent.getSerializableExtra("Theater") as? Theater
+    }
     private val numberOfTicket = intent.getIntExtra("ticketNum", 0)
     private val ticketOfCharge = theater?.charge ?: 13000
     val movie = theater?.movie
