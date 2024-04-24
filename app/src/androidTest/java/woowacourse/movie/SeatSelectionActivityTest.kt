3e5a925b3e056bf12dd2ba00cd612eeaf69586de
Gimun Kim <kmkim7575@gmail.com>
2024-04-24 16:43:34 +0900

    feat: 정해진 인원 수만큼까지 좌석을 선택할 수 있는 기능 추가

--- app/src/androidTest/java/woowacourse/movie/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/SeatSelectionActivityTest.kt
@@ -1,13 +1,20 @@
 package woowacourse.movie
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,7 +22,10 @@ class SeatSelectionActivityTest {
     @get:Rule
     val activityScenarioRule =
         ActivityScenarioRule<SeatSelectionActivity>(
-            Intent(ApplicationProvider.getApplicationContext(), SeatSelectionActivity::class.java).apply {
+            Intent(
+                ApplicationProvider.getApplicationContext(),
+                SeatSelectionActivity::class.java,
+            ).apply {
                 putExtra("movie_id", 0L)
             },
         )
@@ -23,15 +33,32 @@ class SeatSelectionActivityTest {
     @Test
     fun `영화_제목과_좌석_배치도가_올바르게_표출된다`() {
         onView(withId(R.id.tv_movie_title)).check(matches(withText("해리 포터와 마법사의 돌")))
-        onView(withId(R.id.tl_screens)).check(matches(isDisplayed()))
+        onView(withId(R.id.tl_screens)).check(matches(hasChildCount(5)))
     }
 
     @Test
     fun `특정_좌석을_선택하면_배경색이_선택_색상으로_변경된다`() {
+        activityScenarioRule.scenario.onActivity {
+            val item =
+                it.findViewById<TableLayout>(R.id.tl_screens).children.filterIsInstance<TableRow>()
+                    .first().children.filterIsInstance<TextView>().first()
+            item.performClick()
+            val color = (item.background as ColorDrawable).color
+            assertEquals(color, ContextCompat.getColor(it, R.color.yellow))
+        }
     }
 
     @Test
     fun `특정_좌석_선택을_취소하면_미선택_색상으로_변경된다`() {
+        activityScenarioRule.scenario.onActivity {
+            val item =
+                it.findViewById<TableLayout>(R.id.tl_screens).children.filterIsInstance<TableRow>()
+                    .first().children.filterIsInstance<TextView>().first()
+            item.performClick()
+            item.performClick()
+            val color = (item.background as ColorDrawable).color
+            assertEquals(color, ContextCompat.getColor(it, R.color.white))
+        }
     }
 
     @Test
--- app/src/main/java/woowacourse/movie/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/SeatSelectionActivity.kt
@@ -1,26 +1,33 @@
 package woowacourse.movie
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.TypedValue.COMPLEX_UNIT_SP
 import android.view.Gravity
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
 import woowacourse.movie.TicketingActivity.Companion.EXTRA_MOVIE_ID
 import woowacourse.movie.model.MovieData.findMovieById
 import woowacourse.movie.model.Result
+import java.lang.IllegalStateException
 
 class SeatSelectionActivity : AppCompatActivity() {
+    var positions = Positions.of(maxCount = 3)
+    val button: Button by lazy { findViewById(R.id.btn_complete_reservation) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val id = intent.getLongExtra(EXTRA_MOVIE_ID, -1)
         val movie = findMovieById(id)
+        button.isClickable = false
         when (movie) {
             is Result.Success -> {
                 findViewById<TextView>(R.id.tv_movie_title).text = movie.data.title
@@ -49,8 +56,56 @@ class SeatSelectionActivity : AppCompatActivity() {
                         text = "${rowIndex + 1}${columnIndex + 1}"
                         setTextSize(COMPLEX_UNIT_SP, 22f)
                         gravity = Gravity.CENTER
-                        setTextColor(Color.BLACK)
-                        setPadding(60)
+                        setTextColor(ContextCompat.getColor(this@SeatSelectionActivity, R.color.black))
+                        val position = Position(rowIndex, columnIndex)
+
+                        if (position in positions.value) {
+                            setBackgroundColor(
+                                ContextCompat.getColor(
+                                    this@SeatSelectionActivity,
+                                    R.color.yellow,
+                                ),
+                            )
+                        } else {
+                            setBackgroundColor(
+                                ContextCompat.getColor(
+                                    this@SeatSelectionActivity,
+                                    R.color.white,
+                                ),
+                            )
+                        }
+                        setPadding(40 * resources.displayMetrics.density.toInt())
+                        setOnClickListener {
+                            if (position !in positions.value) {
+                                runCatching {
+                                    positions =
+                                        Positions.of(
+                                            3,
+                                            positions.value + position,
+                                        )
+                                    setBackgroundColor(
+                                        ContextCompat.getColor(
+                                            this@SeatSelectionActivity,
+                                            R.color.yellow,
+                                        ),
+                                    )
+                                }.onFailure {
+                                    Toast.makeText(
+                                        this@SeatSelectionActivity,
+                                        "추가 불가능",
+                                        Toast.LENGTH_SHORT,
+                                    ).show()
+                                }
+                            } else {
+                                positions = Positions.of(3, positions.value - position)
+                                setBackgroundColor(
+                                    ContextCompat.getColor(
+                                        this@SeatSelectionActivity,
+                                        R.color.white,
+                                    ),
+                                )
+                            }
+                        }
                     }
                 tableRow.addView(nameOfSeat)
             }
@@ -63,3 +118,22 @@ data class SeatInfo(
     val numOfRows: Int,
     val numOfColumns: Int,
 )
+
+data class Position(
+    val row: Int,
+    val column: Int,
+)
+
+class Positions private constructor(
+    val value: List<Position>,
+) {
+    companion object {
+        fun of(
+            maxCount: Int,
+            values: List<Position> = emptyList(),
+        ): Positions {
+            if (values.size > maxCount) throw IllegalStateException()
+            return Positions(values)
+        }
+    }
+}
