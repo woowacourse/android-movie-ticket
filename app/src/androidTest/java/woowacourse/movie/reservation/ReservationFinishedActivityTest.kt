b36e44980a1de41c48866b3062a3526bac68794b
haeum808 <haeum808@gmail.com>
2024-04-24 18:01:30 +0900

    refactor: 중복코드 @Before을 이용해서 제거

--- app/src/androidTest/java/woowacourse/movie/reservation/ReservationFinishedActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/reservation/ReservationFinishedActivityTest.kt
@@ -7,64 +7,45 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.junit.Before
 import org.junit.Test
 import woowacourse.movie.R
 import woowacourse.movie.reservation.finished.ReservationFinishedActivity
 
 class ReservationFinishedActivityTest {
-    @Test
-    fun `예매한_영화_제목을_보여준다`() {
-        val intent =
+    private lateinit var intent: Intent
+
+    @Before
+    fun setUp() {
+        intent =
             Intent(ApplicationProvider.getApplicationContext(), ReservationFinishedActivity::class.java).apply {
                 putExtra("movieId", 0)
+                putExtra("ticketCount", 1)
             }
+        ActivityScenario.launch<ReservationFinishedActivity>(intent)
+    }
 
-        ActivityScenario.launch<ReservationFinishedActivity>(intent).onActivity {
-            it.intent = intent
-        }
-
+    @Test
+    fun `예매한_영화_제목을_보여준다`() {
         onView(withId(R.id.text_view_reservation_finished_title)).check(matches(withText("해리 포터와 마법사의 돌")))
     }
 
     @Test
     fun `예매한_영화_상영일을_보여준다`() {
-        val intent =
-            Intent(ApplicationProvider.getApplicationContext(), ReservationFinishedActivity::class.java).apply {
-                putExtra("movieId", 0)
-            }
-
-        ActivityScenario.launch<ReservationFinishedActivity>(intent).onActivity {
-            it.intent = intent
-        }
-
         onView(withId(R.id.text_view_reservation_finished_screening_date)).check(matches(withText("2001.11.14")))
     }
 
     @Test
     fun `예매한_영화_관람인원을_보여준다`() {
-        val intent =
-            Intent(ApplicationProvider.getApplicationContext(), ReservationFinishedActivity::class.java).apply {
-                putExtra("ticketCount", 1)
-            }
+        val ticketCount = 1
 
-        ActivityScenario.launch<ReservationFinishedActivity>(intent).onActivity {
-            it.intent = intent
-        }
-
-        onView(withId(R.id.text_view_reservation_finished_number_of_tickets)).check(matches(withText("1")))
+        onView(withId(R.id.text_view_reservation_finished_number_of_tickets)).check(matches(withText(ticketCount.toString())))
     }
 
     @Test
     fun `예매한_영화_총_결제금액을_보여준다`() {
-        val intent =
-            Intent(ApplicationProvider.getApplicationContext(), ReservationFinishedActivity::class.java).apply {
-                putExtra("ticketCount", 1)
-            }
+        val ticketPrice = "13,000"
 
-        ActivityScenario.launch<ReservationFinishedActivity>(intent).onActivity {
-            it.intent = intent
-        }
-
-        onView(withId(R.id.text_view_reservation_finished_ticket_price)).check(matches(withText("13,000")))
+        onView(withId(R.id.text_view_reservation_finished_ticket_price)).check(matches(withText(ticketPrice)))
     }
 }
