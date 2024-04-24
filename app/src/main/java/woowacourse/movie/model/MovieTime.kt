2970e3f24997881df6a1393b0972cce77cb979d1
dpcks0509 <dpcks0509@gmail.com>
2024-04-24 11:49:52 +0900

    refactor: MovieTime에 LocalTime 사용

--- app/src/main/java/woowacourse/movie/model/MovieTime.kt
+++ app/src/main/java/woowacourse/movie/model/MovieTime.kt
@@ -1,21 +1,30 @@
 package woowacourse.movie.model
 
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 class MovieTime(private val isWeekend: Boolean) {
     fun generateTimes(): List<String> {
         val times = mutableListOf<String>()
-        var currentTime = if (isWeekend) WEEKEND_MIN_HOUR else WEEKDAY_MIN_HOUR
-        while (currentTime <= MAX_HOUR) {
-            val formatTime = String.format("%02d:00", currentTime)
-            times.add(formatTime)
-            currentTime += OFFSET_HOUR
+        var currentTime = if (isWeekend) WEEKEND_BASE_LOCAL_TIME else WEEKDAY_BASE_LOCAL_TIME
+        repeat(TIMES_SIZE) {
+            times.add(formatLocalTime(currentTime))
+            currentTime = currentTime.plusHours(OFFSET_HOUR)
         }
         return times
     }
 
+    private fun formatLocalTime(currentTime: LocalTime): String {
+        val formattedTime = currentTime.format(DateTimeFormatter.ofPattern("HH:mm"))
+        val hour = formattedTime.substring(0, 2).toInt() + 1
+        val minute = formattedTime.substring(3).toInt()
+        return String.format("%02d:%02d", hour, minute)
+    }
+
     companion object {
-        private const val WEEKDAY_MIN_HOUR = 9
-        private const val WEEKEND_MIN_HOUR = 10
-        private const val MAX_HOUR = 24
-        private const val OFFSET_HOUR = 2
+        private val WEEKDAY_BASE_LOCAL_TIME = LocalTime.of(8, 0)
+        private val WEEKEND_BASE_LOCAL_TIME = LocalTime.of(9, 0)
+        private const val TIMES_SIZE = 8
+        private const val OFFSET_HOUR = 2L
     }
 }
