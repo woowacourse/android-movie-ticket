d33f132d1bd31cd011007c74f292ff6a674259d8
Hogu59 <dbstjdguswkd@naver.com>
2024-04-24 15:44:23 +0900

    refactor(Movie): change property
    - id : Int -> Long
    - naming signature : imgSrc -> imgResId

--- app/src/main/java/woowacourse/movie/domain/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/movie/Movie.kt
@@ -4,12 +4,12 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 data class Movie(
-    val id: Int,
+    val id: Long,
     val title: String,
     val runningTime: Int,
     val screenPeriod: List<LocalDate>,
     val description: String,
-    val imgSrc: Int,
+    val imgResId: Int,
 ) {
     fun screenPeriodToString(): String {
         if (screenPeriod.size == 1) return screenPeriod[0].format(DateTimeFormatter.ofPattern("yyyy.M.d"))
