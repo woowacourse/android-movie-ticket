9f56aa406c2bd0ec469b767e5d20f53d62af9373
Junyoung-WON <wjyo60@gmail.com>
2024-04-24 19:10:43 +0900

    feat: 상영 날짜와 시간에 대한 클래스 구현
    - 상영 정보와 상영 날짜 및 시간을 가지고 있다.
    
    - 변경할 상영 날짜가 상영 날짜 범위에 속한다면 변경이 가능하다.
    
    - 변경할 상영 시간이 상영 시간 범위에 속한다면 변경이 가능하다

---
+++ app/src/main/java/woowacourse/movie/domain/model/ScreeningDateTime.kt
@@ -0,0 +1,25 @@
+package woowacourse.movie.domain.model
+
+import java.time.LocalDate
+
+class ScreeningDateTime (val screeningInfo: ScreeningInfo) {
+    val screeningDate = ScreeningDate(screeningInfo.startDate, screeningInfo.runningTime)
+
+    fun changeScreeningDate(
+        year: Int,
+        month: Int,
+        day: Int,
+    ) {
+        val changingDate = LocalDate.of(year, month, day)
+        if (screeningInfo.isInScreeningDateRange(changingDate)) {
+            screeningDate.changeDate(year, month, day)
+        }
+    }
+
+    fun changeScreeningTime(
+        hour: Int,
+        minute: Int,
+    ) {
+        screeningDate.changeTime(hour, minute)
+    }
+}
