12584ac485bcbb44f6dfb5e9343a83be1a5b5b6e
dpcks0509 <dpcks0509@gmail.com>
2024-04-25 01:42:05 +0900

    feat: 좌석선택 페이지 view 추가

---
+++ app/src/main/java/woowacourse/movie/seat/view/SeatSelectionActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.seat.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.movie.R
+
+class SeatSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_selection)
+    }
+}
