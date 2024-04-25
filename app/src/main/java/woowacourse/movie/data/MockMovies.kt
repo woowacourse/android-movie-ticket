457f164fa99c62cfec348f38b5745b7d1aa96f52
김진우 <jinwoo3661@naver.com>
2024-04-25 22:27:45 +0900

    refactor: MovieData 추가

--- app/src/main/java/woowacourse/movie/data/MockMovies.kt
+++ app/src/main/java/woowacourse/movie/data/MockMovies.kt
@@ -11,13 +11,23 @@ object MockMovies {
             Movie(
                 movieId = 1,
                 title = "해리 포터와 마법사의 돌",
-                screeningDate = LocalDate.of(2024, 3, 1),
+                screeningStartDate = LocalDate.of(2024, 3, 1),
+                screeningEndDate = LocalDate.of(2024, 3, 31),
                 runningTime = 152,
-                imageName = "harry_potter_poster",
+                imageName = "harry_potter_poster_1",
                 description =
                     "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. " +
                         "해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. " +
                         "크리스 콜럼버스가 감독을 맡았다.",
+            ),
+            Movie(
+                movieId = 2,
+                title = "해리 포터와 비밀의 방",
+                screeningStartDate = LocalDate.of(2024, 4, 1),
+                screeningEndDate = LocalDate.of(2024, 4, 28),
+                runningTime = 162,
+                imageName = "harry_potter_poster_2",
+                description = "해리 포터와 비밀의 방",
             ),
         )
 
@@ -25,7 +35,8 @@ object MockMovies {
         Movie(
             movieId = -1,
             title = DEFAULT_MOVIE_TITLE,
-            screeningDate = LocalDate.now(),
+            screeningStartDate =  LocalDate.now(),
+            screeningEndDate =  LocalDate.now(),
             runningTime = 0,
             imageName = null,
             description = "",
