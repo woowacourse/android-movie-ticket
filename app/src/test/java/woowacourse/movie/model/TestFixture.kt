2997897a435743101e2b3e042bcfee4a0fd41b6e
Namyunsuk <yunsuk0328@naver.com>
2024-04-25 20:39:40 +0900

    test: 리팩토링에 따른 픽스터수정

--- app/src/test/java/woowacourse/movie/model/TestFixture.kt
+++ app/src/test/java/woowacourse/movie/model/TestFixture.kt
@@ -1,11 +1,14 @@
 package woowacourse.movie.model
 
+import woowacourse.movie.model.movie.MovieContent
+import java.time.LocalDate
+
 val movieContent =
     MovieContent(
         "movie_poster",
         "해리 포터와 마법사의 돌",
-        MovieDate(2024, 3, 1),
-        MovieDate(2024, 3, 28),
+        LocalDate.of(2024, 3, 1),
+        LocalDate.of(2024, 3, 28),
         152,
         "해리",
     )
