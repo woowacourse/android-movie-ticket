9da6b886e9e80552d7b35f17aea1b482683e0a87
Gimun Kim <kmkim7575@gmail.com>
2024-04-25 13:53:58 +0900

    test: 화면의 좌상, 좌하, 우상, 우하단 클릭 시에도 다이얼로그가 사라지지 않는지의 여부 테스트

--- app/src/androidTest/java/woowacourse/movie/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/SeatSelectionActivityTest.kt
@@ -21,6 +21,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -39,6 +40,23 @@ class SeatSelectionActivityTest {
             },
         )
 
+    private lateinit var device: UiDevice
+    private lateinit var seatItems: Sequence<TextView>
+
+    @Before
+    fun setUp() {
+        device = UiDevice.getInstance(getInstrumentation())
+        activityScenarioRule.scenario.onActivity {
+            seatItems =
+                it.findViewById<TableLayout>(R.id.tl_screens)
+                    .children
+                    .filterIsInstance<TableRow>()
+                    .first()
+                    .children
+                    .filterIsInstance<TextView>()
+        }
+    }
+
     @Test
     fun `영화_제목과_좌석_배치도가_올바르게_표출된다`() {
         onView(withId(R.id.tv_movie_title)).check(matches(withText("해리 포터와 마법사의 돌")))
@@ -48,10 +66,9 @@ class SeatSelectionActivityTest {
     @Test
     fun `특정_좌석을_선택하면_배경색이_선택_색상으로_변경된다`() {
         activityScenarioRule.scenario.onActivity {
-            val item =
-                it.findViewById<TableLayout>(R.id.tl_screens).children.filterIsInstance<TableRow>()
-                    .first().children.filterIsInstance<TextView>().first()
+            val item = seatItems.first()
             item.performClick()
+
             val color = (item.background as ColorDrawable).color
             assertEquals(color, ContextCompat.getColor(it, R.color.yellow))
         }
@@ -60,11 +77,9 @@ class SeatSelectionActivityTest {
     @Test
     fun `특정_좌석_선택을_취소하면_미선택_색상으로_변경된다`() {
         activityScenarioRule.scenario.onActivity {
-            val item =
-                it.findViewById<TableLayout>(R.id.tl_screens).children.filterIsInstance<TableRow>()
-                    .first().children.filterIsInstance<TextView>().first()
-            item.performClick()
-            item.performClick()
+            val item = seatItems.first()
+            repeat(2) { item.performClick() }
+
             val color = (item.background as ColorDrawable).color
             assertEquals(color, ContextCompat.getColor(it, R.color.white))
         }
@@ -72,20 +87,13 @@ class SeatSelectionActivityTest {
 
     @Test
     fun `화면이_회전되어도_좌석_선택_정보가_유지된다`() {
+        // TODO
     }
 
     @Test
     fun `인원수에_맞는_좌석_수만큼_선택되면_더_이상_다른_좌석이_선택되지_않는다`() {
         activityScenarioRule.scenario.onActivity {
-            val firstRowItems =
-                it.findViewById<TableLayout>(R.id.tl_screens)
-                    .children
-                    .filterIsInstance<TableRow>()
-                    .first()
-                    .children
-                    .filterIsInstance<TextView>()
-
-            firstRowItems.forEach { textView ->
+            seatItems.forEach { textView ->
                 textView.performClick()
             }
             val lastItem =
@@ -116,10 +124,7 @@ class SeatSelectionActivityTest {
     fun `확인_버튼_클릭_시_예약_확정_다이얼로그가_표출된다`() {
         performMultipleSeatsSelection()
         onView(withId(R.id.btn_complete_reservation)).perform(click())
-        onView(withText("예매 확인")).check(matches(isDisplayed()))
-        onView(withText("정말 예매하시겠습니까?")).check(matches(isDisplayed()))
-        onView(withId(android.R.id.button1)).check(matches(isDisplayed()))
-        onView(withId(android.R.id.button2)).check(matches(isDisplayed()))
+        checkDialogExists()
     }
 
     @Test
@@ -127,34 +132,57 @@ class SeatSelectionActivityTest {
         performMultipleSeatsSelection()
         onView(withId(R.id.btn_complete_reservation)).perform(click())
         onView(withId(android.R.id.button2)).perform(click())
-        onView(withText("예매 확인")).check(doesNotExist())
-        onView(withText("정말 예매하시겠습니까?")).check(doesNotExist())
-        onView(withId(android.R.id.button1)).check(doesNotExist())
-        onView(withId(android.R.id.button2)).check(doesNotExist())
+        checkDialogDoesNotExist()
     }
 
     @Test
-    fun `예약_확정_다이얼로그_이외의_영역_클릭_시에도_다이얼로그가_사라지지_않는다`() {
+    fun `예약_확정_다이얼로그_이외의_영역_중_좌상단_클릭_시_다이얼로그가_사라지지_않는다`() {
         performMultipleSeatsSelection()
         onView(withId(R.id.btn_complete_reservation)).perform(click())
-        val device = UiDevice.getInstance(getInstrumentation())
         device.click(0, 0)
+        checkDialogExists()
+    }
+
+    @Test
+    fun `예약_확정_다이얼로그_이외의_영역_중_우상단_클릭_시_다이얼로그가_사라지지_않는다`() {
+        performMultipleSeatsSelection()
+        onView(withId(R.id.btn_complete_reservation)).perform(click())
+        device.click(device.displayWidth, 0)
+        checkDialogExists()
+    }
+
+    @Test
+    fun `예약_확정_다이얼로그_이외의_영역_중_좌하단_클릭_시_다이얼로그가_사라지지_않는다`() {
+        performMultipleSeatsSelection()
+        onView(withId(R.id.btn_complete_reservation)).perform(click())
+        device.click(0, device.displayHeight)
+        checkDialogExists()
+    }
+
+    @Test
+    fun `예약_확정_다이얼로그_이외의_영역_중_우하단_클릭_시_다이얼로그가_사라지지_않는다`() {
+        performMultipleSeatsSelection()
+        onView(withId(R.id.btn_complete_reservation)).perform(click())
+        device.click(device.displayWidth, device.displayHeight)
+        checkDialogExists()
+    }
+
+    private fun checkDialogExists() {
         onView(withText("예매 확인")).check(matches(isDisplayed()))
         onView(withText("정말 예매하시겠습니까?")).check(matches(isDisplayed()))
         onView(withId(android.R.id.button1)).check(matches(isDisplayed()))
         onView(withId(android.R.id.button2)).check(matches(isDisplayed()))
     }
 
+    private fun checkDialogDoesNotExist() {
+        onView(withText("예매 확인")).check(doesNotExist())
+        onView(withText("정말 예매하시겠습니까?")).check(doesNotExist())
+        onView(withId(android.R.id.button1)).check(doesNotExist())
+        onView(withId(android.R.id.button2)).check(doesNotExist())
+    }
+
     private fun performMultipleSeatsSelection() {
         activityScenarioRule.scenario.onActivity {
-            val firstRowItems =
-                it.findViewById<TableLayout>(R.id.tl_screens)
-                    .children
-                    .filterIsInstance<TableRow>()
-                    .first()
-                    .children
-                    .filterIsInstance<TextView>()
-
             val lastRowItems =
                 it.findViewById<TableLayout>(R.id.tl_screens)
                     .children
@@ -163,8 +191,8 @@ class SeatSelectionActivityTest {
                     .children
                     .filterIsInstance<TextView>()
 
-            firstRowItems.first().performClick()
-            firstRowItems.last().performClick()
+            seatItems.first().performClick()
+            seatItems.last().performClick()
             lastRowItems.first().performClick()
             lastRowItems.last().performClick()
         }
