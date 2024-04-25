8aa935f15d9b7dcbfc2c7f2f384fb4a1277f14f0
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-26 00:51:23 +0900

    test(ReservationDetail): 상영일, 상영시간 UI 테스트 구현

--- app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationDetailActivityTest.kt
@@ -2,25 +2,45 @@ package woowacourse.movie.view.reservation
 
 import android.content.pm.ActivityInfo
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withSpinnerText
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
 import woowacourse.movie.TestFixture.moviesFirstItem
 import woowacourse.movie.view.home.ReservationHomeActivity
+import java.time.LocalDate
+import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 class ReservationDetailActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(ReservationDetailActivity::class.java)
+
+    @Before
+    fun setUp() {
+        activityRule.scenario.recreate()
+    }
+
+    @After
+    fun tearDown() {
+        activityRule.scenario.close()
+    }
 
     @Test
     fun `티켓의_기본_수량은_1장이다`() {
@@ -69,5 +89,43 @@ class ReservationDetailActivityTest {
 
         // then
         onView(withId(R.id.text_view_reservation_detail_number_of_tickets)).check(matches(withText("2")))
+    }
+
+    @Test
+    fun `2024년_3월_4일의_상영일을_스피너로_선택하면_상영일_스피너에_2024-03-04_보여진다`() {
+        // given
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 4)))).perform(click())
+        // then
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).check(matches(withSpinnerText(containsString("2024-03-04"))))
+    }
+
+    @Test
+    fun `12시의_상영시간을_스피너로_선택하면_상영시간_스피너에_선택한_상영_시간이_보여진다`() {
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(12, 0)))).perform(click())
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("12:00"))))
+    }
+
+    @Test
+    fun `상영일을_평일로_선택하면_상영시간_스피너의_기본값은_10시이다`() {
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 1)))).perform(click())
+
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(10, 0)))).perform(click())
+
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("10:00"))))
+    }
+
+    @Test
+    fun `상영일을_주말로_선택하면_상영시간_스피너의_기본값은_9시이다`() {
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 2)))).perform(click())
+
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(9, 0)))).perform(click())
+
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("09:00"))))
     }
 }
