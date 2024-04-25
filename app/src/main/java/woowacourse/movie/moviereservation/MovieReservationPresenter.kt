3e67cd62031232be717e44441f6a36b18b607561
gaeun <geun5744@gmail.com>
2024-04-25 17:55:02 +0900

    refactor: runCatching 위치 이동

--- app/src/main/java/woowacourse/movie/moviereservation/MovieReservationPresenter.kt
+++ app/src/main/java/woowacourse/movie/moviereservation/MovieReservationPresenter.kt
@@ -1,47 +1,56 @@
 package woowacourse.movie.moviereservation
 
+import android.util.Log
 import woowacourse.movie.model.HeadCount
 import woowacourse.movie.repository.MovieRepository
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 class MovieReservationPresenter(
-    private val view: MovieReservationView,
+    private val view: MovieReservationContract.View,
     private val repository: MovieRepository,
-) {
-    fun loadMovieDetail(screenMovieId: Long) {
+) : MovieReservationContract.Presenter {
+    override fun loadMovieDetail(screenMovieId: Long) {
         runCatching {
             val screeningMovie = repository.screenMovieById(screenMovieId)
-            view.showMovieReservation(screeningMovie.toMovieReservationUiModel())
+            view.showMovieInfo(screeningMovie.toMovieReservationUiModel())
+            view.showScreeningDateTime(screeningMovie.toScreeningDateTimeUiModel())
         }.onFailure {
+            Log.d("테스트", "${it.message}")
             view.showScreeningMovieError()
         }
     }
 
-    fun plusCount(currentCount: HeadCountUiModel) {
+    override fun plusCount(currentCount: HeadCountUiModel) {
         val count = currentCount.toHeadCount()
         view.updateHeadCount(count.increase().toHeadCountUiModel())
     }
 
-    fun minusCount(currentCount: HeadCountUiModel) {
+    override fun minusCount(currentCount: HeadCountUiModel) {
         val count = currentCount.toHeadCount()
         runCatching {
-            view.updateHeadCount(count.decrease().toHeadCountUiModel())
+            count.decrease().toHeadCountUiModel()
+        }.onSuccess { updatedCount ->
+            view.updateHeadCount(updatedCount)
         }.onFailure {
             view.showCantDecreaseError(HeadCount.MIN_COUNT)
         }
     }
 
-    fun completeReservation(
+    override fun completeReservation(
         screenMovieId: Long,
         currentCount: HeadCountUiModel,
     ) {
-        repository.reserveMovie(
-            screenMovieId,
-            dateTime = repository.screenMovieById(screenMovieId).screenDateTimes.first().date.toDefaultLocalDateTime(),
-            count = currentCount.toHeadCount(),
-        ).onSuccess {
+        runCatching {
+            repository.reserveMovie(
+                screenMovieId,
+                dateTime = repository.screenMovieById(screenMovieId).screenDateTimes.first().date.toDefaultLocalDateTime(),
+                count = currentCount.toHeadCount(),
+            )
+        }.onSuccess {
             view.navigateToReservationResultView(it)
+        }.onFailure {
+            view.showMovieReservationError()
         }
     }
 
