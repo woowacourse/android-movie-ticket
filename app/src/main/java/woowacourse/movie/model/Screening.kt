f044fc8f83cadbbee0a863e861a328b288909c9e
Gimun Kim <kmkim7575@gmail.com>
2024-04-25 21:15:38 +0900

    feat: 상영 정보에 대한 객체 추가

---
+++ app/src/main/java/woowacourse/movie/model/Screening.kt
@@ -0,0 +1,37 @@
+package woowacourse.movie.model
+
+import java.time.LocalDate
+
+class Screening private constructor(
+    val screeningId: Long,
+    val movieId: Long,
+    val theater: Theater,
+    val datePeriod: DatePeriod,
+) {
+    val dates: List<String>
+        get() {
+            var currentDate: LocalDate = datePeriod.startDate
+            return mutableListOf<String>().apply {
+                do {
+                    add(currentDate.toString())
+                    currentDate = currentDate.plusDays(datePeriod.dateSpan)
+                } while (currentDate <= datePeriod.endDate)
+            }
+        }
+
+    companion object {
+        private val DEFAULT_THEATER = Theater.ofDefaultSize()
+
+        fun of(
+            screeningId: Long,
+            movieId: Long,
+            datePeriod: DatePeriod,
+        ): Screening =
+            Screening(
+                screeningId,
+                movieId,
+                DEFAULT_THEATER,
+                datePeriod,
+            )
+    }
+}
---
+++ app/src/test/java/woowacourse/movie/model/ScreeningTest.kt
@@ -0,0 +1,44 @@
+package woowacourse.movie.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class ScreeningTest {
+    @Test
+    fun `상영_정보에_따른_올바른_기간들을_반환한다`() {
+        // given
+        val screening =
+            Screening.of(
+                0,
+                1,
+                DatePeriod(
+                    startDate = LocalDate.of(2024, 3, 1),
+                    endDate = LocalDate.of(2024, 3, 15),
+                    dateSpan = 1,
+                ),
+            )
+
+        // then
+        assertEquals(
+            listOf(
+                "2024-03-01",
+                "2024-03-02",
+                "2024-03-03",
+                "2024-03-04",
+                "2024-03-05",
+                "2024-03-06",
+                "2024-03-07",
+                "2024-03-08",
+                "2024-03-09",
+                "2024-03-10",
+                "2024-03-11",
+                "2024-03-12",
+                "2024-03-13",
+                "2024-03-14",
+                "2024-03-15",
+            ),
+            screening.dates,
+        )
+    }
+}
