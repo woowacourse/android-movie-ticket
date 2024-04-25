00e32eee68b5938717d192f7ceab6f5c6b1b9811
songpink <jejeje04@naver.com>
2024-04-25 19:56:53 +0900

    feat: 버튼 색 변경

--- app/src/main/java/woowacourse/movie/seats/view/SeatsActivity.kt
+++ app/src/main/java/woowacourse/movie/seats/view/SeatsActivity.kt
@@ -1,21 +1,12 @@
 package woowacourse.movie.seats.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.R
 
 class SeatsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_seats)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
