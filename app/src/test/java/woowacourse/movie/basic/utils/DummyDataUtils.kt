78201e3f061a62a4a50c872e016f03f779afe231
JunJangE <wnswkd486@gmail.com>
2024-04-25 09:48:40 +0900

    test: 데이터 구조 변경으로 인한 더미 데이터 수정

--- app/src/test/java/woowacourse/movie/basic/utils/DummyDataUtils.kt
+++ app/src/test/java/woowacourse/movie/basic/utils/DummyDataUtils.kt
@@ -4,11 +4,12 @@ import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.domain.model.Reservation
 import woowacourse.movie.domain.model.Screen
+import woowacourse.movie.domain.model.ScreenDate
 import woowacourse.movie.domain.model.Seat
 import woowacourse.movie.domain.model.SeatBoard
 import woowacourse.movie.domain.model.SeatRank
 import woowacourse.movie.presentation.model.ReservationInfo
-import woowacourse.movie.presentation.model.Ticket
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 fun getDummyMovie(): Movie {
@@ -26,29 +27,29 @@ fun getDummyMovie(): Movie {
 fun getDummyScreen(): Screen {
     return Screen(
         id = 1,
-        movie = getDummyMovie(),
+        movie =
+            Movie(
+                title = "해리 포터와 마법사의 돌",
+                runningTime = 152,
+                imageSrc = R.drawable.img_poster,
+                description =
+                    "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. " +
+                        "해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
+                13_000,
+            ),
         startDate = "2024-03-01",
         endDate = "2024-03-07",
         selectableDates =
             listOf(
-                "2024-03-01",
-                "2024-03-02",
-                "2024-03-03",
-                "2024-03-04",
-                "2024-03-05",
-                "2024-03-06",
-                "2024-03-07",
-            ),
-        selectableTimes =
-            listOf(
-                "09:00",
-                "11:00",
-                "13:00",
-                "15:00",
-                "17:00",
-                "19:00",
-                "21:00",
-                "23:00",
+                ScreenDate(LocalDate.of(2024, 4, 1)),
+                ScreenDate(LocalDate.of(2024, 4, 2)),
+                ScreenDate(LocalDate.of(2024, 4, 3)),
+                ScreenDate(LocalDate.of(2024, 4, 4)),
+                ScreenDate(LocalDate.of(2024, 4, 5)),
+                ScreenDate(LocalDate.of(2024, 4, 6)),
+                ScreenDate(LocalDate.of(2024, 4, 7)),
+                ScreenDate(LocalDate.of(2024, 4, 8)),
+                ScreenDate(LocalDate.of(2024, 4, 9)),
             ),
     )
 }
@@ -65,7 +66,7 @@ fun getDummyReservation(): Reservation {
     return Reservation(
         id = 1,
         getDummyMovie(),
-        Ticket(3),
+        3,
         seats = getDummySeats(),
         dateTime = LocalDateTime.now(),
     )
