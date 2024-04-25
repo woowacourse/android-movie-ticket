f801fa695afd5efe733c4031b21a147c0559f0a5
김진우 <jinwoo3661@naver.com>
2024-04-25 17:37:52 +0900

    feat: MockSeat data 생성

---
+++ app/src/main/java/woowacourse/movie/data/MockSeats.kt
@@ -0,0 +1,26 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.domain.model.MovieSeat
+import woowacourse.movie.domain.model.SeatType
+
+object MockSeats {
+    private val rowList = listOf("A", "B", "C", "D", "E")
+    private val columnList = listOf(1, 2, 3, 4)
+
+    val sampleSeats = rowList.map { row ->
+        columnList.map { column ->
+            val seatType = when (row) {
+                "A", "B" -> SeatType.B
+                "C", "D" -> SeatType.S
+                else -> SeatType.A
+            }
+            MovieSeat(
+                seatName = "$row$column",
+                seatType = seatType
+            )
+        }
+    }
+
+    val defaultSeat = MovieSeat(seatName = "", seatType = SeatType.S)
+
+}
---
+++ app/src/main/java/woowacourse/movie/domain/model/MovieSeat.kt
@@ -0,0 +1,6 @@
+package woowacourse.movie.domain.model
+
+data class MovieSeat(
+    val seatName: String,
+    val seatType: SeatType,
+)
