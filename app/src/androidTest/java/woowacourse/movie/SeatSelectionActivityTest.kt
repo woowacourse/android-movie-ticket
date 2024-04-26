38858c080fa801cc84a4b7271060ad5e372c7bb2
JoYehyun <jo990714@naver.com>
2024-04-26 14:47:59 +0900

    test: 좌석 선택 UI 테스트

---
+++ app/src/androidTest/java/woowacourse/movie/SeatSelectionActivityTest.kt
@@ -0,0 +1,94 @@
+package woowacourse.movie
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.movie.presentation.seatSelection.SeatSelectionActivity
+import woowacourse.movie.presentation.ticketing.TicketingActivity
+
+class SeatSelectionActivityTest {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<SeatSelectionActivity> =
+        ActivityScenarioRule<SeatSelectionActivity>(
+            Intent(
+                ApplicationProvider.getApplicationContext(),
+                SeatSelectionActivity::class.java,
+            ).apply {
+                putExtra(TicketingActivity.EXTRA_MOVIE_ID, 0)
+                putExtra(TicketingActivity.EXTRA_COUNT, 2) // 선택 가능한 좌석 개수
+                putExtra(TicketingActivity.EXTRA_SCREENING_DATE_TIME, "")
+            },
+        )
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private val unClickedColor = ContextCompat.getColor(context, R.color.unClickedSeat_bgr)
+    private val clickedColor = ContextCompat.getColor(context, R.color.clickedSeat_bgr)
+
+    @Test
+    fun `좌석을_선택하면_선택된_좌석의_배경색이_변한다`() {
+        onView(withId(R.id.tv_seat_item_0))
+            .perform(click())
+            .check(matches(withBackgroundColor(clickedColor)))
+    }
+
+    @Test
+    fun `선택된_좌석을_재선택하면_해당_좌석의_선택이_해제되며_배경색이_원래대로_변한다`() {
+        onView(withId(R.id.tv_seat_item_0))
+            .perform(click())
+            .perform(click())
+            .check(matches(withBackgroundColor(unClickedColor)))
+    }
+
+    @Test
+    fun `선택_가능한_좌석_개수만큼_좌석이_선택되었을_경우에는_하단_버튼이_활성화된다`() {
+        onView(withId(R.id.tv_seat_item_0)).perform(click())
+        onView(withId(R.id.tv_seat_item_1)).perform(click())
+
+        onView(withId(R.id.btn_complete)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun `선택_가능한_좌석_개수만큼_좌석이_선택되지_않았을_경우에는_하단_버튼이_비활성화된다`() {
+        onView(withId(R.id.tv_seat_item_0)).perform(click())
+
+        onView(withId(R.id.btn_complete)).check(matches(not(isEnabled())))
+    }
+
+    @Test
+    fun `활성화된_하단_버튼을_클릭하면_예매_확인_다이얼로그_창이_뜬다`() {
+        onView(withId(R.id.tv_seat_item_0)).perform(click())
+        onView(withId(R.id.tv_seat_item_1)).perform(click())
+        onView(withId(R.id.btn_complete)).perform(click())
+
+        onView(withText("예매 확인")).check(matches(isDisplayed()))
+    }
+
+    private fun withBackgroundColor(expectedColor: Int): Matcher<View> {
+        return object : BoundedMatcher<View, View>(View::class.java) {
+            override fun describeTo(description: Description?) {
+                description?.appendText("with background color: $expectedColor")
+            }
+
+            override fun matchesSafely(item: View?): Boolean {
+                val backgroundColor = (item?.background as? ColorDrawable)?.color ?: 0
+                return backgroundColor == expectedColor
+            }
+        }
+    }
+}
