117b5107db9b33dee56b1c92ab4dfd1af9cb9888
김진우 <jinwoo3661@naver.com>
2024-04-26 15:22:07 +0900

    refactor: 티켓 모델 재생성

---
+++ app/src/main/java/woowacourse/movie/presentation/model/TicketModel.kt
@@ -0,0 +1,39 @@
+package woowacourse.movie.presentation.model
+
+import woowacourse.movie.domain.model.MovieSeat
+import woowacourse.movie.domain.model.Ticket
+import java.io.Serializable
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class TicketModel(
+    val title: String,
+    val screeningDate: LocalDate,
+    val screeningTime: LocalDateTime,
+    val seats : List<String>,
+    val count: Int,
+    val price: Int,
+) : Serializable {
+    companion object {
+        private const val DEFAULT_TICKET_TITLE = "티켓정보가 존재하지 않습니다."
+        val defaultTicket = TicketModel(
+            price = 0,
+            screeningDate = LocalDate.now(),
+            screeningTime = LocalDateTime.now(),
+            title = DEFAULT_TICKET_TITLE,
+            seats = listOf(),
+            count = 0,
+        )
+    }
+}
+
+fun Ticket.toTicketModel(): TicketModel {
+    return TicketModel(
+        title = title,
+        screeningDate = movieDate.currentDate,
+        screeningTime = movieDate.currentTime,
+        count = count,
+        price = price,
+        seats = seats.map { it.seatName }
+    )
+}
