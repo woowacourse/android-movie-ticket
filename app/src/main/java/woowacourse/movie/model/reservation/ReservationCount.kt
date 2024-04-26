7fa08ee544e5b35511c52d262185cb4834d48003
kimhm0728 <kimhm0728@naver.com>
2024-04-27 01:22:04 +0900

    fix: 예매 인원의 최대값 수정

--- app/src/main/java/woowacourse/movie/model/reservation/ReservationCount.kt
+++ app/src/main/java/woowacourse/movie/model/reservation/ReservationCount.kt
@@ -20,7 +20,7 @@ class ReservationCount(val count: Int = DEFAULT_VALUE) : Serializable {
     companion object {
         private const val DEFAULT_VALUE = 1
         private const val MIN_VALUE = 1
-        private const val MAX_VALUE = 50
+        private const val MAX_VALUE = 20
         private const val OFFSET_VALUE = 1
         private const val INVALID_COUNT_MESSAGE = "예매 인원은 ${MIN_VALUE}에서 $MAX_VALUE 사이여야 합니다."
     }
