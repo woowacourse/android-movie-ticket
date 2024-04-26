dbcd9b34bf77d7c1ddaae4bd7cc1645e6c8dc48d
김진우 <jinwoo3661@naver.com>
2024-04-26 15:27:36 +0900

    refactor: Date 모델 수정

--- app/src/main/java/woowacourse/movie/presentation/utils/DateConverter.kt
+++ app/src/main/java/woowacourse/movie/presentation/utils/DateConverter.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.presentation.utils
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun LocalDate.toCustomString(): String {
@@ -8,7 +9,7 @@ fun LocalDate.toCustomString(): String {
     return this.format(formatter)
 }
 
-fun String.toLocalDate(): LocalDate {
-    val formatter = DateTimeFormatter.ofPattern("yyyy.M.d")
-    return LocalDate.parse(this, formatter)
+fun LocalDateTime.toCustomString(): String {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return this.format(formatter)
 }
