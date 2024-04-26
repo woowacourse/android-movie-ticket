e937e489d75cadfefba231ba4b43eff2d7b52f07
김진우 <jinwoo3661@naver.com>
2024-04-26 15:21:55 +0900

    feat: Movie Date,Time 정보를 담는 클래스 생성

---
+++ app/src/main/java/woowacourse/movie/domain/model/MovieDate.kt
@@ -0,0 +1,21 @@
+package woowacourse.movie.domain.model
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class MovieDate(
+    movieDate: LocalDate = LocalDate.now(),
+    movieTime : LocalDateTime = LocalDateTime.now(),
+) {
+    var currentDate: LocalDate = movieDate
+        private set
+    var currentTime: LocalDateTime = movieTime
+        private set
+
+    fun setCurrentDate(newDate: LocalDate){
+        currentDate = newDate
+    }
+    fun setCurrentTime(newTime: LocalDateTime){
+        currentTime = newTime
+    }
+}
