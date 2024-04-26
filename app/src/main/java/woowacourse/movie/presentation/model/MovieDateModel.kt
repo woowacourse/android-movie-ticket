d945ca6dc45d505839dfc2a6ea3d9e36c4b90e77
김진우 <jinwoo3661@naver.com>
2024-04-26 20:47:20 +0900

    feat: 뷰에서 사용하는 모델 선언

---
+++ app/src/main/java/woowacourse/movie/presentation/model/MovieDateModel.kt
@@ -0,0 +1,18 @@
+package woowacourse.movie.presentation.model
+
+import woowacourse.movie.domain.model.MovieDate
+import java.io.Serializable
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class MovieDateModel(
+    val screeningDate: LocalDate,
+    val screeningTime: LocalDateTime,
+): Serializable
+
+fun MovieDate.toMovieDateModel(): MovieDateModel {
+    return MovieDateModel(
+        screeningDate = currentDate,
+        screeningTime = currentTime,
+    )
+}
---
+++ app/src/main/java/woowacourse/movie/presentation/seat/model/MovieSeatModel.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie.presentation.seat.model
+
+import woowacourse.movie.domain.model.MovieSeat
+import woowacourse.movie.domain.model.SeatType
+import java.io.Serializable
+
+class MovieSeatModel(
+    val seatName: String,
+    val seatType: SeatType,
+): Serializable
+
+fun MovieSeat.toMovieSeatModel(): MovieSeatModel{
+    return MovieSeatModel(
+        seatName = seatName,
+        seatType = seatType,
+    )
+}
+
+fun MovieSeatModel.toMovieSeat() : MovieSeat {
+    return MovieSeat(
+        seatName = seatName,
+        seatType = seatType,
+    )
+}
