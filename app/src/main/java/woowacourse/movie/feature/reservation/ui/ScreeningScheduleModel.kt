1e2de5830d5083bef6832b85f95ae8ca652f37c1
somin <salth0601@gmail.com>
2024-04-26 14:47:38 +0900

    feat: DailySchedule의 일급 컬렉션 ScreeningSchedule과 UiModel 추가

---
+++ app/src/main/java/woowacourse/movie/feature/reservation/ui/ScreeningScheduleModel.kt
@@ -0,0 +1,13 @@
+package woowacourse.movie.feature.reservation.ui
+
+import woowacourse.movie.domain.screening.ScreeningSchedule
+
+data class ScreeningScheduleModel(val dailySchedules: List<DailyScheduleModel>){
+    fun getScheduleDates() = dailySchedules.map { it.date }
+}
+
+fun ScreeningSchedule.toUiModel(): ScreeningScheduleModel {
+    return ScreeningScheduleModel(
+        dailySchedules.map { it.toUiModel() }
+    )
+}
