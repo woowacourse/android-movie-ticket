28716cae69381fc6398db4511119f8c6d50e815d
kimhm0728 <kimhm0728@naver.com>
2024-04-27 17:30:24 +0900

    test: 영화 목록 Presenter 테스트 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,12 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.mockk:mockk:1.13.8")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:runner:1.4.0")
---
+++ app/src/test/java/woowacourse/movie/feature/home/MovieHomePresenterTest.kt
@@ -0,0 +1,35 @@
+package woowacourse.movie.feature.home
+
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.model.data.MovieRepository
+import woowacourse.movie.model.data.MovieRepositoryImpl
+
+class MovieHomePresenterTest {
+    private lateinit var view: MovieHomeContract.View
+    private val repository: MovieRepository = MovieRepositoryImpl
+    private lateinit var presenter: MovieHomePresenter
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk<MovieHomeContract.View>()
+        presenter = MovieHomePresenter(view, repository)
+    }
+
+    @Test
+    fun `영화 데이터를 불러오면 영화 목록 뷰가 초기화된다`() {
+        // given
+        every { view.initializeMovieList(any()) } just runs
+
+        // when
+        presenter.loadMovieData()
+
+        // then
+        verify { view.initializeMovieList(repository.findAll()) }
+    }
+}
