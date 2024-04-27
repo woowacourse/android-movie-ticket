c6d53bbfb5340592854590da0cd8788aab361188
somin <salth0601@gmail.com>
2024-04-28 01:59:38 +0900

    feat: 상영 정보를 보여주는 ScreeningViewHolder 생성

---
+++ app/src/main/java/woowacourse/movie/feature/main/ui/ScreeningViewHolder.kt
@@ -0,0 +1,33 @@
+package woowacourse.movie.feature.main.ui
+
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.R
+
+class ScreeningViewHolder(
+    val view: View,
+    private val itemClickListener: ScreeningAdapter.OnItemClickListener,
+) : RecyclerView.ViewHolder(view) {
+    private val poster: ImageView = view.findViewById(R.id.list_img_poster)
+    private val title: TextView = view.findViewById(R.id.list_movie_title)
+    private val screeningPeriod: TextView = view.findViewById(R.id.screening_period)
+    private val runningTime: TextView = view.findViewById(R.id.running_time)
+    private val reservationButton: Button = view.findViewById(R.id.btn_reservation)
+
+    fun bind(screening: ScreeningModel) {
+        val screeningPeriodText = screening.getFormattedScreeningPeriod(view.context)
+        val runningTimeText = screening.getFormattedRunningTime(view.context)
+        with(this) {
+            poster.setImageResource(screening.poster)
+            title.text = screening.title
+            screeningPeriod.text = screeningPeriodText
+            runningTime.text = runningTimeText
+            reservationButton.setOnClickListener {
+                itemClickListener.onItemClick(screening.id)
+            }
+        }
+    }
+}
