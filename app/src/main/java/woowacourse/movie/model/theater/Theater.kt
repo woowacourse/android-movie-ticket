25be3197836de5609b44b34839dd654d1b71ff82
Gimun Kim <kmkim7575@gmail.com>
2024-04-27 19:33:15 +0900

    test: 상영관에 대한 테스트 케이스 추가

--- app/src/main/java/woowacourse/movie/model/theater/Theater.kt
+++ app/src/main/java/woowacourse/movie/model/theater/Theater.kt
@@ -5,6 +5,8 @@ class Theater private constructor(
     val rowClassInfo: Map<Int, SeatClass>,
 ) {
     companion object {
+        private const val ERROR_DUPLICATED_ROW_NUMBERS = "중복된 행 번호를 가질 수 없습니다."
+        private const val ERROR_ROW_NUMBERS_NOT_IN_A_ROW = "연속된 행 번호를 가져야 합니다."
         private const val DEFAULT_COLUMN_SIZE = 4
         private val DEFAULT_ROW_CLASS_INFO =
             mapOf(
@@ -18,8 +20,10 @@ class Theater private constructor(
         fun of(
             rowClassInfo: Map<Int, SeatClass> = DEFAULT_ROW_CLASS_INFO,
             columnSize: Int = DEFAULT_COLUMN_SIZE,
-        ): Theater =
-            Theater(
+        ): Theater {
+            require(rowClassInfo.keys.distinct().size == rowClassInfo.keys.size) { ERROR_DUPLICATED_ROW_NUMBERS }
+            require(rowClassInfo.keys.maxOf { it } == rowClassInfo.keys.size) { ERROR_ROW_NUMBERS_NOT_IN_A_ROW }
+            return Theater(
                 theaterSize =
                     TheaterSize(
                         rowClassInfo.size,
@@ -27,5 +31,6 @@ class Theater private constructor(
                     ),
                 rowClassInfo = rowClassInfo,
             )
+        }
     }
 }
--- app/src/test/java/woowacourse/movie/model/theater/TheaterTest.kt
+++ app/src/test/java/woowacourse/movie/model/theater/TheaterTest.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.model.theater
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 
 class TheaterTest {
     @Test
@@ -26,5 +27,37 @@ class TheaterTest {
             { assertEquals(SeatClass.S, theater.rowClassInfo[3]) },
             { assertEquals(SeatClass.A, theater.rowClassInfo[5]) },
         )
+    }
+
+    @Test
+    fun `상영관_정보_생성_시_중복된_행_번호_제공_시_예외를_발생시킨다`() {
+        assertThrows<IllegalArgumentException>("중복된 행 번호를 가질 수 없습니다.") {
+            Theater.of(
+                mapOf(
+                    1 to SeatClass.B,
+                    1 to SeatClass.B,
+                    3 to SeatClass.S,
+                    4 to SeatClass.S,
+                    5 to SeatClass.A,
+                ),
+                4,
+            )
+        }
+    }
+
+    @Test
+    fun `상영관_정보_생성_시_첫_행_번호부터_연속되지_않은_행_번호_제공_시_예외를_발생시킨다`() {
+        assertThrows<IllegalArgumentException>("중복된 행 번호를 가질 수 없습니다.") {
+            Theater.of(
+                mapOf(
+                    2 to SeatClass.B,
+                    3 to SeatClass.B,
+                    4 to SeatClass.S,
+                    5 to SeatClass.S,
+                    6 to SeatClass.A,
+                ),
+                4,
+            )
+        }
     }
 }
