12e9cb134b848c92b13567ad043d06ce973ad656
gaeun <geun5744@gmail.com>
2024-04-29 00:56:28 +0900

    fix: 좌석 생성 row 수정

--- domain/src/main/java/woowacourse/movie/model/MovieTheater.kt
+++ domain/src/main/java/woowacourse/movie/model/MovieTheater.kt
@@ -3,13 +3,13 @@ package woowacourse.movie.model
 class MovieTheater(
     private val rowRate: Map<SeatRate, List<Int>>,
     private val colLength: Int,
-    val defaultPrice: Int = 0
+    val defaultPrice: Int = 0,
 ) {
     fun seats(): List<Seat> =
         rowRate.flatMap { (seatRate, rateRows) ->
             rateRows.flatMap { row ->
                 (0 until colLength).map { col ->
-                    Seat(seatRate, row, col)
+                    Seat(seatRate, row - 1, col)
                 }
             }
         }
