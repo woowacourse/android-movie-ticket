d144d989e5c47dc26643ca80d30a43684ec3f4e0
aprilgom <aprilgom@gmail.com>
2024-04-28 20:50:19 +0900

    fix(PseudoMovieRepository): 시놉시스의 줄바꿈 제거, 가짜 레포지토리의 데이터 수정

--- app/src/main/java/woowacourse/movie/repository/PseudoMovieRepository.kt
+++ app/src/main/java/woowacourse/movie/repository/PseudoMovieRepository.kt
@@ -5,6 +5,8 @@ import woowacourse.movie.model.movie.MovieDetail
 import woowacourse.movie.model.movie.RunningTime
 import woowacourse.movie.model.movie.Synopsis
 import woowacourse.movie.model.movie.Title
+import woowacourse.movie.model.schedule.ScreeningDate
+import woowacourse.movie.model.schedule.ScreeningPeriod
 
 class PseudoMovieRepository : MovieRepository {
     override fun getMovies(): List<Movie> = movies
@@ -27,10 +29,16 @@ class PseudoMovieRepository : MovieRepository {
                     synopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsys
                     synopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsys
                     synopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsyssynopsys
-                    """.trimIndent().trimEnd(),
+                    """.trimIndent().replace("\n", "")
                 ),
             )
-        private val pseudoMovie = Movie.default
+        private val pseudoMovie = Movie(
+            pseudoMovieDetail,
+            ScreeningPeriod(
+                ScreeningDate.of(2024, 4, 26),
+                ScreeningDate.of(2024, 4, 28),
+            )
+        )
 
         private val movies = List(11) { pseudoMovie }
     }
