a2f167b2ef0df2052dc6c1c9f1b2749530d38588
haeum808 <haeum808@gmail.com>
2024-04-28 13:59:36 +0900

    feat: 고른 좌석의 총 티켓 값을 계산하는 기능 구현

---
+++ app/src/main/java/woowacourse/movie/model/Rank.kt
@@ -0,0 +1,11 @@
+package woowacourse.movie.model
+
+enum class Rank(private val price: Int) {
+    S(15_000),
+    A(12_000),
+    B(10_000);
+
+    companion object {
+        fun calculateTotalPrice(ranks: List<Rank>): Int = ranks.sumOf { it.price }
+    }
+}
