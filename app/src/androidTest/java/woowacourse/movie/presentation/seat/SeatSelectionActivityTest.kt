d1726409bead8c8a84c086429c3aab96e5ced01f
김진우 <jinwoo3661@naver.com>
2024-04-28 23:58:51 +0900

    refactor: SeatSelectionActivityTest 테스트코드 수정

--- app/src/androidTest/java/woowacourse/movie/presentation/seat/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/presentation/seat/SeatSelectionActivityTest.kt
@@ -3,9 +3,7 @@ package woowacourse.movie.presentation.seat
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.widget.TableLayout
-import android.widget.TableRow
 import android.widget.TextView
-import androidx.core.view.children
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -19,9 +17,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.MovieDate
-import woowacourse.movie.domain.model.Ticket
-import woowacourse.movie.presentation.model.toTicketModel
+import woowacourse.movie.presentation.model.PendingMovieReservationModel
+import woowacourse.movie.presentation.model.toMovieDateModel
 import woowacourse.movie.presentation.reservation.MovieReservationPresenter
+import woowacourse.movie.utils.findTextViewByTextInTableLayout
 
 @RunWith(AndroidJUnit4::class)
 class SeatSelectionActivityTest {
@@ -32,15 +31,16 @@ class SeatSelectionActivityTest {
                 ApplicationProvider.getApplicationContext(),
                 SeatSelectionActivity::class.java,
             ).apply {
-                val ticketModel =
-                    Ticket(
+                val pendingMovieReservationModel =
+                    PendingMovieReservationModel(
                         title = "해리 포터와 마법사의 돌",
-                        movieDate = MovieDate(),
+                        movieDate = MovieDate().toMovieDateModel(),
                         count = 2,
-                        price = 0,
-                        seats = listOf(),
-                    ).toTicketModel()
-                putExtra(MovieReservationPresenter.KEY_NAME_TICKET, ticketModel)
+                    )
+                putExtra(
+                    MovieReservationPresenter.KEY_NAME_PENDING_RESERVATION,
+                    pendingMovieReservationModel
+                )
             },
         )
 
@@ -53,13 +53,11 @@ class SeatSelectionActivityTest {
     @Test
     fun `좌석을_모두_선택_후_확인_가능_버튼이_활성화되어야_한다`() {
         activityRule.scenario.onActivity { context ->
-            val view =
-                context.findViewById<TableLayout>(R.id.seat_table).children.filterIsInstance<TableRow>()
-                    .flatMap { row -> row.children }.filterIsInstance<TextView>().toList()
+            val tableView = context.findViewById<TableLayout>(R.id.seat_table)
             val confirmButton = context.findViewById<TextView>(R.id.confirm_button)
             val expected = (confirmButton.background as ColorDrawable).color
-            view[0].performClick()
-            view[1].performClick()
+            tableView.findTextViewByTextInTableLayout("A1")?.performClick()
+            tableView.findTextViewByTextInTableLayout("A2")?.performClick()
             val actual = (confirmButton.background as ColorDrawable).color
             assertNotEquals(expected, actual)
         }
@@ -68,12 +66,12 @@ class SeatSelectionActivityTest {
     @Test
     fun `선택되지_않은_좌석을_선택했을_때_선택한_좌석_자리의_색상이_초기_색상과_달라진다`() {
         activityRule.scenario.onActivity { context ->
-            val view =
-                context.findViewById<TableLayout>(R.id.seat_table).children.filterIsInstance<TableRow>()
-                    .flatMap { row -> row.children }.filterIsInstance<TextView>().first()
-            val expected = view.background
-            view.performClick()
-            val actual = view.background
+            val tableView =
+                context.findViewById<TableLayout>(R.id.seat_table)
+            val textView = tableView.findTextViewByTextInTableLayout("A1")
+            val expected = textView?.background
+            textView?.performClick()
+            val actual = textView?.background
 
             assertNotEquals(expected, actual)
         }
@@ -82,13 +80,13 @@ class SeatSelectionActivityTest {
     @Test
     fun `선택된_좌석을_선택했을_때_선택한_좌석_자리의_색상이_초기_색상으로_달라진다`() {
         activityRule.scenario.onActivity { context ->
-            val view =
-                context.findViewById<TableLayout>(R.id.seat_table).children.filterIsInstance<TableRow>()
-                    .flatMap { row -> row.children }.filterIsInstance<TextView>().last()
-            view.performClick()
-            val expected = (view.background as ColorDrawable).color
-            view.performClick()
-            val actual = (view.background as ColorDrawable).color
+            val tableView =
+                context.findViewById<TableLayout>(R.id.seat_table)
+            val textView = tableView.findTextViewByTextInTableLayout("A2")
+            textView?.performClick()
+            val expected = (textView?.background as ColorDrawable).color
+            textView.performClick()
+            val actual = (textView.background as ColorDrawable).color
 
             assertNotEquals(expected, actual)
         }
