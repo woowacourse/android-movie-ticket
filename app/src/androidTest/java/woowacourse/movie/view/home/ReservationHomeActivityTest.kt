90e4035105a64c8f424c3e9980b544ce066e5ede
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-28 16:20:37 +0900

    test(ReservationHomeActivity): 리싸이클러뷰 item 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/view/home/ReservationHomeActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/home/ReservationHomeActivityTest.kt
@@ -3,9 +3,12 @@ package woowacourse.movie.view.home
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Description
@@ -15,6 +18,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
+import woowacourse.movie.TestFixture.FIRST_ITEM_POSITION
+import woowacourse.movie.TestFixture.movies
 import woowacourse.movie.view.home.adapter.viewholder.AdvertisementViewHolder
 import woowacourse.movie.view.home.adapter.viewholder.MovieViewHolder
 
@@ -26,6 +31,18 @@ class ReservationHomeActivityTest {
     @Test
     fun `영화_목록을_보여준다`() {
         onView(withId(R.id.recycler_view_reservation_home)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `영화_제목을_보여준다`() {
+        onView(withId(R.id.recycler_view_reservation_home)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                FIRST_ITEM_POSITION,
+                click(),
+            ),
+        )
+
+        onView(withText(movies[FIRST_ITEM_POSITION].title)).check(matches(isDisplayed()))
     }
 
     @Test
