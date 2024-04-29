1f55fda407ce9414fc726d154b7a622f958ac17b
gaeun <geun5744@gmail.com>
2024-04-29 20:37:17 +0900

    test: 테스트 메서드명 수정

--- app/src/androidTest/java/woowacourse/movie/study/ArrayAdapterTest.kt
+++ app/src/androidTest/java/woowacourse/movie/study/ArrayAdapterTest.kt
@@ -1,11 +1,14 @@
-package study
+package woowacourse.movie.study
 
 import android.content.Context
 import android.widget.ArrayAdapter
 import androidx.test.core.app.ApplicationProvider
+import org.junit.Before
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 private const val TEST_RESOURCE_ID: Int = 1
 
@@ -19,66 +22,74 @@ class ArrayAdapterTest {
 
     @BeforeEach
     fun setUp() {
-        context = ApplicationProvider.getApplicationContext()
+        context = context
     }
 
     @Test
-    fun test1() {
+    fun `List에_add와_clear를_하면_에러가_발생한다`() {
         val list = listOf(1)
         val adapter = ArrayAdapter(context, TEST_RESOURCE_ID, list)
-        adapter.add(2)
-        adapter.clear()
+        assertThrows<UnsupportedOperationException> {
+            adapter.add(2)
+            adapter.clear()
+        }
     }
 
     @Test
-    fun test2() {
+    fun `List에_add와_clear를_하면_에러가_발생한다2`() {
         val list = listOf(1, 2)
         val adapter = ArrayAdapter(context, TEST_RESOURCE_ID, list)
-        adapter.add(3)
-        adapter.clear()
+        assertThrows<UnsupportedOperationException> {
+            adapter.add(3)
+            adapter.clear()
+        }
     }
 
     @Test
-    fun test3() {
+    fun `코틀린의_list는_자바의_singletonList의_instance다`() {
         val list = listOf(1)
         val clazz = java.util.Collections.singletonList(1)::class.java
         Assertions.assertTrue(clazz.isInstance(list))
     }
 
     @Test
-    fun test4() {
+    fun `코틀린의_list는_자바의_asList의_instance다`() {
         val list = listOf(1, 2)
         val clazz = java.util.Arrays.asList(1)::class.java
         Assertions.assertTrue(clazz.isInstance(list))
     }
 
     @Test
-    fun test5() {
+    fun `toList를_통해_생성된_객체는_자바의_asList_instace에_속한다`() {
         val list = listOf(1).toList()
         val clazz = java.util.Collections.singletonList(1)::class.java
         Assertions.assertTrue(clazz.isInstance(list))
     }
 
     @Test
-    fun test6() {
+    fun `toList를_통해_생성된_객체는_자바의_ArrayList_instace에_속한다`() {
         val list = listOf(1, 2).toList()
         val clazz = java.util.ArrayList<Int>()::class.java
         Assertions.assertTrue(clazz.isInstance(list))
     }
 
     @Test
-    fun test7() {
+    fun `toMutableList를_통해_생성된_객체를_넣어주면_add를_할_때_에러가_발생하지_않는다`() {
         val list = listOf(1).toMutableList()
         val adapter = ArrayAdapter(context, TEST_RESOURCE_ID, list)
-        adapter.add(2)
-        adapter.clear()
+        assertDoesNotThrow {
+            adapter.add(2)
+            adapter.clear()
+        }
     }
 
     @Test
-    fun test8() {
+    fun `toMutableList를_통해_생성된_객체를_넣어주면_add를_할_때_에러가_발생하지_않는다2`() {
         val list = listOf(1, 2).toMutableList()
         val adapter = ArrayAdapter(context, TEST_RESOURCE_ID, list)
-        adapter.add(3)
-        adapter.clear()
+        assertDoesNotThrow {
+            adapter.add(3)
+            adapter.clear()
+        }
     }
 }
