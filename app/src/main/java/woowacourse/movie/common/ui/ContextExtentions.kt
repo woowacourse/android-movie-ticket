543bdb3a4f4caebb6c806f0e56b13aedaf3990ab
murjune <sis9221302@gmail.com>
2024-04-30 02:41:34 +0900

    feat: StartActivity util 함수

--- app/src/main/java/woowacourse/movie/common/ui/ContextExtentions.kt
+++ app/src/main/java/woowacourse/movie/common/ui/ContextExtentions.kt
@@ -1,8 +1,14 @@
 package woowacourse.movie.common.ui
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+inline fun <reified T : Activity> Context.startActivity(block: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, T::class.java).apply(block))
 }
