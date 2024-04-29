121cffa19d258bfc0290612632e3c5120d974ac6
sh1mj1 <7wlgns@gmail.com>
2024-04-29 17:35:38 +0900

    test(ReservationCompleteActivityTest): show Time and Seats of Reservation

--- app/src/androidTest/java/woowacourse/movie/ui/reservation/ReservationCompleteActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/ui/reservation/ReservationCompleteActivityTest.kt
@@ -12,6 +12,16 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
+import woowacourse.movie.domain.model.DateTime
+import woowacourse.movie.domain.model.Grade
+import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.domain.model.Position
+import woowacourse.movie.domain.model.Screen
+import woowacourse.movie.domain.model.Seat
+import woowacourse.movie.domain.model.Seats
+import woowacourse.movie.domain.repository.DummyReservation
+import java.time.LocalDate
+import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 class ReservationCompleteActivityTest {
@@ -25,7 +35,7 @@ class ReservationCompleteActivityTest {
 
     @Test
     fun 예약한_영화의_제목을_표시한다() {
-        onView(withId(R.id.tv_reservation_title)).check(matches(withText("해리 포터와 마법사의 돌")))
+        onView(withId(R.id.tv_reservation_title)).check(matches(withText("")))
     }
 
     @Test
@@ -35,13 +45,32 @@ class ReservationCompleteActivityTest {
 
     @Test
     fun 상영_영화_예매_수를_표시한다() {
-        onView(withId(R.id.tv_reservation_count)).check(matches(withText("일반 1명")))
+        onView(withId(R.id.tv_reservation_count)).check(matches(withText("일반 2명 |")))
     }
 
     @Test
     fun 상영_영화_예매_가격을_표시한다() {
-        onView(withId(R.id.tv_reservation_amount)).check(matches(withText("13,000원 (현장 결제)")))
+        onView(withId(R.id.tv_reservation_amount)).check(matches(withText("27,000원 (현장 결제)")))
     }
 
-    private fun testFixtureReservationId(): Result<Int> = throw NotImplementedError() // TODO: save new Reservation
+    @Test
+    fun showTheReservedSeats() {
+        onView(withId(R.id.tv_reserved_seats)).check(matches(withText("A1,B2")))
+    }
+
+    @Test
+    fun showTimeOfReservation() {
+        onView(withId(R.id.tv_reservation_time)).check(matches(withText("10:00")))
+    }
+
+    private fun testFixtureReservationId(): Int {
+        return DummyReservation.save(
+            Screen.NULL,
+            Seats(
+                Seat(Position(0, 0), Grade.S),
+                Seat(Position(1, 1), Grade.A),
+            ),
+            DateTime(LocalDate.of(2024, 3, 1), LocalTime.of(10, 0)),
+        ).getOrThrow()
+    }
 }
