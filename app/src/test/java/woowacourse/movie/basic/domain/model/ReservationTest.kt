8360fb7cf8590806d3b0fce985775c6682596e40
JunJangE <wnswkd486@gmail.com>
2024-04-29 10:49:28 +0900

    test: ReservationTest 각 랭크에 따른 계산 테스트 진행

--- app/src/test/java/woowacourse/movie/basic/domain/model/ReservationTest.kt
+++ app/src/test/java/woowacourse/movie/basic/domain/model/ReservationTest.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import woowacourse.movie.basic.utils.getDummyMovie
 import woowacourse.movie.basic.utils.getDummySeats
 import woowacourse.movie.domain.model.Reservation
+import woowacourse.movie.domain.model.Seat
+import woowacourse.movie.domain.model.SeatRank
 import java.time.LocalDateTime
 
 class ReservationTest {
@@ -22,5 +24,47 @@ class ReservationTest {
 
         // then
         assertThat(reservation.totalPrice).isEqualTo(36_000)
+    }
+
+    @Test
+    fun `예매할 좌석 자리가 S랭크 3자리일 때, 총 가격은 45,000 이다`() {
+        // given & when
+        val reservation =
+            Reservation(
+                id = 1,
+                getDummyMovie(),
+                3,
+                seats =
+                    listOf(
+                        Seat("A", 1, SeatRank.S),
+                        Seat("A", 2, SeatRank.S),
+                        Seat("A", 3, SeatRank.S),
+                    ),
+                dateTime = LocalDateTime.now(),
+            )
+
+        // then
+        assertThat(reservation.totalPrice).isEqualTo(45_000)
+    }
+
+    @Test
+    fun `예매할 좌석 자리가 B랭크 3자리일 때, 총 가격은 30,000 이다`() {
+        // given & when
+        val reservation =
+            Reservation(
+                id = 1,
+                getDummyMovie(),
+                3,
+                seats =
+                    listOf(
+                        Seat("A", 1, SeatRank.B),
+                        Seat("A", 2, SeatRank.B),
+                        Seat("A", 3, SeatRank.B),
+                    ),
+                dateTime = LocalDateTime.now(),
+            )
+
+        // then
+        assertThat(reservation.totalPrice).isEqualTo(30_000)
     }
 }
