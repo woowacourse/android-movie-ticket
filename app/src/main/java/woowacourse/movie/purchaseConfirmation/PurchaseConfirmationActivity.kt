433273388aa417d48cd60c4bea252d5a8fe80944
ii2001 <iivks2001@gmail.com>
2024-04-30 11:59:02 +0900

    :sparkles: 최종 구매 화면에서 좌석 표기

--- app/src/main/java/woowacourse/movie/purchaseConfirmation/PurchaseConfirmationActivity.kt
+++ app/src/main/java/woowacourse/movie/purchaseConfirmation/PurchaseConfirmationActivity.kt
@@ -11,13 +11,14 @@ class PurchaseConfirmationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.purchase_confirmation)
         val ticketPrice = intent.getStringExtra("ticketPrice")
+        val seatNumber = intent.getStringExtra("seatNumber")
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         val presenter = PurchaseConfirmationActivityPresenter(intent)
         val movie = presenter.movie
         findViewById<TextView>(R.id.movie_title_confirmation).text = movie?.title.toString()
         findViewById<TextView>(R.id.purchase_movie_running_time).text =
             movie?.runningTime.toString()
+        findViewById<TextView>(R.id.reserved_seat).text = seatNumber
         findViewById<TextView>(R.id.ticket_charge).text =
             getString(R.string.price_format, ticketPrice)
     }
