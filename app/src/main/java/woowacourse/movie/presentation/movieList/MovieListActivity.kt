621fa469e8454155f011c517e3cbb20af2d728be
JoYehyun <jo990714@naver.com>
2024-04-30 14:14:52 +0900

    refactor: 버튼 리스너 위치 수정

--- app/src/main/java/woowacourse/movie/presentation/movieList/MovieListActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/movieList/MovieListActivity.kt
@@ -8,7 +8,7 @@ import woowacourse.movie.model.Movie
 import woowacourse.movie.presentation.movieList.adapter.MovieAdapter
 import woowacourse.movie.presentation.ticketing.TicketingActivity
 
-class MovieListActivity : AppCompatActivity(), MovieListContract.View {
+class MovieListActivity : AppCompatActivity(), MovieListContract.View, MovieListClickListener {
     private val presenter = MovieListPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +19,14 @@ class MovieListActivity : AppCompatActivity(), MovieListContract.View {
 
     override fun displayMovies(movies: List<Movie>) {
         val movieList: RecyclerView = findViewById(R.id.rv_movies)
-        movieList.adapter = MovieAdapter(movies, presenter)
+        movieList.adapter = MovieAdapter(movies, this)
     }
 
     override fun navigate(movieId: Int) {
         startActivity(TicketingActivity.createIntent(this, movieId))
+    }
+
+    override fun ticketingButtonClick(movieId: Int) {
+        navigate(movieId)
     }
 }
--- app/src/main/java/woowacourse/movie/presentation/movieList/MovieListPresenter.kt
+++ app/src/main/java/woowacourse/movie/presentation/movieList/MovieListPresenter.kt
@@ -2,15 +2,11 @@ package woowacourse.movie.presentation.movieList
 
 import woowacourse.movie.data.MovieRepository
 
-class MovieListPresenter(private val movieListContractView: MovieListContract.View) : MovieListContract.Presenter, MovieListClickListener {
+class MovieListPresenter(private val movieListContractView: MovieListContract.View) : MovieListContract.Presenter {
     private val movieRepository = MovieRepository()
 
     override fun loadMovies() {
         val movies = movieRepository.getAllMovies()
         movieListContractView.displayMovies(movies)
-    }
-
-    override fun ticketingButtonClick(movieId: Int) {
-        movieListContractView.navigate(movieId)
     }
 }
