887dc5400ef03b574fadee70350d19afb2451ba8
gaeun <geun5744@gmail.com>
2024-04-30 21:42:21 +0900

    test: recyclerview 광고 테스트 구현

--- app/src/androidTest/java/woowacourse/movie/ScreeningMovieActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/ScreeningMovieActivityTest.kt
@@ -1,5 +1,9 @@
 package woowacourse.movie
 
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -9,12 +13,16 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf.instanceOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
 import woowacourse.movie.model.ScreeningMovie
 import woowacourse.movie.model.ScreeningMovies
+import woowacourse.movie.screeningmovie.AdvertiseViewHolder
 import woowacourse.movie.screeningmovie.MovieAdapter
 import woowacourse.movie.screeningmovie.ScreenMovieUiModel
 import woowacourse.movie.screeningmovie.ScreeningMovieActivity
@@ -32,11 +40,6 @@ class ScreeningMovieActivityTest {
             val items =
                 ScreeningMovies(
                     listOf(
-                        ScreeningMovie.STUB,
-                        ScreeningMovie.STUB,
-                        ScreeningMovie.STUB,
-                        ScreeningMovie.STUB,
-                        ScreeningMovie.STUB,
                         ScreeningMovie.STUB,
                         ScreeningMovie.STUB,
                         ScreeningMovie.STUB,
@@ -66,6 +69,31 @@ class ScreeningMovieActivityTest {
             ).atPosition(0),
         )
         onView(withText(screenMovieUiModel3.runningTime)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    @DisplayName("리스트의 길이가 3 이상이면 광고가 나타난다.")
+    fun advertisement_display_When_list_size_is_more_3() {
+        onView(withId(R.id.rcv_screening_movie)).perform(
+            RecyclerViewActions.scrollToHolder(
+                instanceOf(AdvertiseViewHolder::class.java),
+            ).atPosition(0),
+        )
+        onView(withDrawable(R.drawable.img_advertisement)).check(matches(isDisplayed()))
+    }
+
+    private fun withDrawable(
+        @DrawableRes id: Int,
+    ) = object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("ImageView with drawable same as drawable with id $id")
+        }
+
+        override fun matchesSafely(view: View): Boolean {
+            val context = view.context
+            val expectedBitmap = context.getDrawable(id)?.toBitmap()
+            return view is ImageView && view.drawable.toBitmap().sameAs(expectedBitmap)
+        }
     }
 
     companion object {
