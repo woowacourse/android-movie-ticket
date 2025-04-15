9d2569b9c897e7dd0b2e6afac7d7a81dc7ed44d6
cucumber99 <dlsguq1@gmail.com>
2025-04-15 16:37:57 +0900

    feat: 영화 목록 기능 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,10 +1,14 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.model.Movie
+import woowacourse.movie.ui.adapter.MovieAdapter
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val movies = listOf(
+            Movie(
+                "승부",
+                LocalDate.of(2025,3,26),
+                115,
+                R.drawable.match
+            )
+        )
+
+        val adapter = MovieAdapter(this, movies)
+        val listView = findViewById<ListView>(R.id.movies)
+        listView.adapter = adapter
     }
 }
