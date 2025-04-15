7869441fb33de7cf107af54b38cd7000742d31ea
cucumber99 <dlsguq1@gmail.com>
2025-04-15 18:44:38 +0900

    feat: 예매 정보 페이지 구현

--- app/src/main/java/woowacourse/movie/ui/view/ReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/view/ReservationActivity.kt
@@ -1,11 +1,16 @@
 package woowacourse.movie.ui.view
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.R
+import woowacourse.movie.model.ReservedMovie
+import java.io.Serializable
 
 class ReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,21 @@ class ReservationActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val reservedMovie = intent.intentSerializable("Reservation", ReservedMovie::class.java)
+        val title = findViewById<TextView>(R.id.title)
+        val screeningDate = findViewById<TextView>(R.id.screeningDate)
+        title.text = reservedMovie.title
+        screeningDate.text = reservedMovie.screeningDate.toString()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T: Serializable> Intent.intentSerializable(key: String, customClass: Class<T>): T {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            this.getSerializableExtra(key, customClass)!!
+        } else {
+            this.getSerializableExtra(key) as T
         }
     }
 }
