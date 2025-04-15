65de6113c73d7b02fc07ea16fa7d8d835c6847e9
박지민 <pjm5489@naver.com>
2025-04-15 15:08:38 +0900

    test: 영화정보 테스트

---
+++ app/src/test/java/woowacourse/movie/MovieTest.kt
@@ -0,0 +1,27 @@
+package woowacourse.movie
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import woowacourse.movie.model.Movie
+import java.time.LocalDate
+
+class MovieTest {
+    @Test
+    fun `제목, 상영일, 러닝타임, 포스터 이미지 경로를 가진다`() {
+        val movie =
+            Movie(
+                "승부",
+                LocalDate.of(2025, 3, 26),
+                115,
+                "res/drawable/images/match.jpeg",
+            )
+
+        assertSoftly(movie) {
+            title shouldBe "승부"
+            screeningDate shouldBe LocalDate.of(2025, 3, 26)
+            runningTime shouldBe 115
+            posterPath shouldBe "res/drawable/images/match.jpeg"
+        }
+    }
+}
