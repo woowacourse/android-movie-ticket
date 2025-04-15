a14f1daa6e40a19d3e7a257668ad2b7621702ee5
이지윤 <02leejiyun@naver.com>
2025-04-15 16:12:17 +0900

    feat: 예매 버튼 클릭 시 화면 전환 구현

--- app/src/main/java/woowacourse/movie/MovieAdapter.kt
+++ app/src/main/java/woowacourse/movie/MovieAdapter.kt
@@ -1,17 +1,19 @@
 package woowacourse.movie
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import java.time.format.DateTimeFormatter
 
 class MovieAdapter(
     private val context: Context,
-    private val movies: List<Movie>
+    private val movies: List<Movie>,
 ) : BaseAdapter() {
     override fun getCount(): Int = movies.size
 
@@ -19,21 +21,32 @@ class MovieAdapter(
 
     override fun getItemId(p0: Int): Long = p0.toLong()
 
-    override fun getView(position: Int, convertView: View?, container: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(context)
-            .inflate(R.layout.item_movie, container, false)
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        container: ViewGroup?,
+    ): View {
+        val view =
+            convertView ?: LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_movie, container, false)
 
         val poster = view.findViewById<ImageView>(R.id.poster)
         val title = view.findViewById<TextView>(R.id.movie_title)
         val screeningDate = view.findViewById<TextView>(R.id.screening_date)
         val runningTime = view.findViewById<TextView>(R.id.running_time)
+        val reserveButton = view.findViewById<Button>(R.id.reserve_button)
 
         val movie = getItem(position)
-
         poster.setImageResource(movie.poster)
         title.text = movie.title
         screeningDate.text = movie.screeningDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
         runningTime.text = "${movie.runningTime}분"
+
+        reserveButton.setOnClickListener {
+            val intent = Intent(context, MovieReservationCompletionActivity::class.java)
+            context.startActivity(intent)
+        }
 
         return view
     }
--- app/src/main/java/woowacourse/movie/MovieReservationCompletionActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieReservationCompletionActivity.kt
@@ -1,36 +1,20 @@
 package woowacourse.movie
 
 import android.os.Bundle
-import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.time.LocalDate
 
-class MainActivity : AppCompatActivity() {
+class MovieReservationCompletionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_reservation_completion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val movies: List<Movie> =
-            listOf(
-                Movie(
-                    title = "해리 포터와 마법사의 돌",
-                    screeningDate = LocalDate.of(2025, 4, 1),
-                    runningTime = 152,
-                    poster = R.drawable.harry_potter_poster,
-                ),
-            )
-
-        val movieListView = findViewById<ListView>(R.id.movie_list)
-        val movieAdapter = MovieAdapter(this, movies)
-        movieListView.adapter = movieAdapter
     }
 }
