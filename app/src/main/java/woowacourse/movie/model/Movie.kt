bb06b367aca35409c499e77783e1439ad94e7ebb
devfeijoa <devfeijoa@gmail.com>
2025-04-15 15:07:09 +0900

    feat: 영화는 기본 정보를 가진다

---
+++ app/src/main/java/woowacourse/movie/model/Movie.kt
@@ -0,0 +1,8 @@
+package woowacourse.movie.model
+
+data class Movie(
+    val posterResId: Int,
+    val title: String,
+    val date: String,
+    val runningTime: String
+)
--- app/src/main/java/woowacourse/movie/view/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package woowacourse.movie
+package woowacourse.movie.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
