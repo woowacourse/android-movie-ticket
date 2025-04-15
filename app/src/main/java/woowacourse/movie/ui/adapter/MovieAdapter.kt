a56cb7b7f9a71b4eb8522e2a0a0f9020e8088c50
cucumber99 <dlsguq1@gmail.com>
2025-04-15 16:35:40 +0900

    feat: ListView 어댑터 구현

---
+++ app/src/main/java/woowacourse/movie/ui/adapter/MovieAdapter.kt
@@ -0,0 +1,35 @@
+package woowacourse.movie.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import woowacourse.movie.R
+import woowacourse.movie.model.Movie
+
+class MovieAdapter(
+    context: Context,
+    private val movies: List<Movie>
+) : ArrayAdapter<Movie>(context, 0, movies) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.item_movie, parent, false)
+
+        val movie = movies[position]
+
+        val imagePoster = view.findViewById<ImageView>(R.id.poster)
+        val title = view.findViewById<TextView>(R.id.title)
+        val screeningDate = view.findViewById<TextView>(R.id.screeningDate)
+        val runningTime = view.findViewById<TextView>(R.id.runningTime)
+
+        title.text = movie.title
+        screeningDate.text = context.getString(R.string.date_text, movie.screeningDate)
+        runningTime.text = context.getString(R.string.runningTime_text, movie.runningTime.toString())
+        imagePoster.setImageResource(movie.posterRes)
+
+        return view
+    }
+}
