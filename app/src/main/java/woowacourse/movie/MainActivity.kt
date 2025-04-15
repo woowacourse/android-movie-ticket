8a42d9164e2aa7d418a191bb54afd685fa1ec19c
HamBeomJoon <hbj0802@naver.com>
2025-04-15 16:45:29 +0900

    feat: 리스트 뷰랑 아이템 뷰 연결

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,10 +1,12 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.model.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val movieListView = findViewById<ListView>(R.id.lv_main_movies)
+        movieListView.adapter = MovieAdapter(this, Movie.values)
     }
 }
