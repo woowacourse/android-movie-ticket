daca129a46e03cb8a5200edff064e298341c44f0
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-15 16:15:57 +0900

    feat: 영화 예매 기능 구현

--- app/src/main/java/woowacourse/movie/domain/BookingStatus.kt
+++ app/src/main/java/woowacourse/movie/domain/BookingStatus.kt
@@ -1,8 +1,13 @@
 package woowacourse.movie.domain
 
-class BookingStatus(
+data class BookingStatus(
     val movie: Movie,
     val isBooked: Boolean,
 ) {
-
+    fun book(): BookingStatus {
+        if (isBooked == false) {
+            return BookingStatus(this.movie, !isBooked)
+        }
+        throw IllegalStateException("이미 예매된 상태입니다.")
+    }
 }
---
+++ app/src/test/java/woowacourse/movie/BookingStatusTest.kt
@@ -0,0 +1,48 @@
+package woowacourse.movie
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import woowacourse.movie.domain.BookingStatus
+import woowacourse.movie.domain.Movie
+import java.time.LocalDateTime
+import kotlin.time.Duration.Companion.minutes
+
+class BookingStatusTest {
+
+    @Test
+    fun `영화가 예매가 안된 상태이면 예매할 수 있다`() {
+        val movie = Movie(
+            "해리포터와 마법사의 돌",
+            "해리포터 이미지",
+            LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+            LocalDateTime.of(2025, 4, 25, 0, 0, 0),
+            152.minutes
+        )
+
+        val bookingStatus = BookingStatus(movie, false)
+
+        val actual = bookingStatus.book()
+
+        val expected = BookingStatus(movie, true)
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `영화가 예매가 된 상태이면 예매할 수 없다`() {
+        val movie = Movie(
+            "해리포터와 마법사의 돌",
+            "해리포터 이미지",
+            LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+            LocalDateTime.of(2025, 4, 25, 0, 0, 0),
+            152.minutes
+        )
+
+        val bookingStatus = BookingStatus(movie, true)
+
+        assertThrows<IllegalStateException> {
+            bookingStatus.book()
+        }
+    }
+}
