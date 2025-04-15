af8db878f3c18a696f6032f2dc36e91bc473884e
devfeijoa <devfeijoa@gmail.com>
2025-04-15 16:58:48 +0900

    feat: 영화 목록 화면 구현

--- app/src/main/java/woowacourse/movie/view/MoviesActivity.kt
+++ app/src/main/java/woowacourse/movie/view/MoviesActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.R
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
