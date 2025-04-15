fbdd821d4d524831a14b1882ca3b78fe61f2fd44
leeyerin0210 <lyl8661@gmail.com>
2025-04-15 17:07:30 +0900

    feat : 다음 화면으로 넘어가는 기능 구현

--- app/src/main/java/woowacourse/movie/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/MovieListAdapter.kt
@@ -1,10 +1,12 @@
 package woowacourse.movie
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -25,6 +27,16 @@ class MovieListAdapter(context: Context, items: MutableList<MovieItem>): ArrayAd
             runningTime.text = it.runningTime
         }
 
+        val button = view.findViewById<Button>(R.id.reservation_button)
+        button.setOnClickListener {
+            val intent = Intent(context,ReservationActivity::class.java).apply {
+                putExtra("TITLE",title.text)
+                putExtra("DATE",date.text)
+            }
+            context.startActivity(intent)
+        }
+
         return view
+
     }
 }
