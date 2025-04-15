31721ee73e4b294e2c2d1ac90e597e87a2042ae3
leeyerin0210 <lyl8661@gmail.com>
2025-04-15 17:08:14 +0900

    feat(ReservationActivity.kt) : 예매 정보 화면 구현

---
+++ app/src/main/java/woowacourse/movie/ReservationActivity.kt
@@ -0,0 +1,28 @@
+package woowacourse.movie
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reservation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.reservation)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val title = findViewById<TextView>(R.id.title)
+        val date = findViewById<TextView>(R.id.date)
+
+        title.text = intent.getStringExtra("TITLE")
+        date.text = intent.getStringExtra("DATE")
+
+    }
+}
