013e215083c8da6dce209b10b75b28ea2fa9efc3
leeyerin0210 <lyl8661@gmail.com>
2025-04-15 15:37:59 +0900

    feat(MainActivity.kt): 영화 목록을 ListView에 출력하는 MainActivity 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,20 +1,32 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var allItems: MutableList<MovieItem>
+    private lateinit var adapter: MovieListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        allItems = mutableListOf(
+            MovieItem(R.drawable.harry_potter_poster, "해리 포터와 마법사의 돌", "상영일: 2025.4.1", "러닝타임: 152분")
+        )
+
+        val listView = findViewById<ListView>(R.id.movie_list)
+        adapter = MovieListAdapter(this, allItems)
+        listView.adapter = adapter
     }
 }
