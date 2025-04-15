d09581279968b8f68db37b7b67fbe692094837fe
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-15 15:05:26 +0900

    feat: posterId 파라미터에 @DrawableRes 어노테이션 추가

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -1,10 +1,12 @@
 package woowacourse.movie
 
+import androidx.annotation.DrawableRes
 import java.time.LocalDate
 
 data class Movie(
     val title: String,
     val screeningDate: LocalDate,
     val runningTime: Int,
+    @DrawableRes
     val posterId: Int,
 )
