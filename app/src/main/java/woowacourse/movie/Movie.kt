1d7d08ea2bedcccadf5122c9f363cd4b0a0bf232
junseo511 <prime54@g.skku.edu>
2025-04-15 16:48:11 +0900

    feat: 영화 포스터 이미지 적용

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -11,5 +11,5 @@ data class Movie(
 
 val movies: List<Movie> =
     listOf(
-        Movie("해리 포터와 마법사의 돌", LocalDate.of(2025, 4, 1), 152, 0),
+        Movie("해리 포터와 마법사의 돌", LocalDate.of(2025, 4, 1), 152, R.drawable.img_poster_harry_potter_and_the_philosophers_stone),
     )
