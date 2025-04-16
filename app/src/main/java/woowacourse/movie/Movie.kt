5558247b0ad54d8c278bbc8eead1545a6e61804d
ijh1298 <imjh0417@gmail.com>
2025-04-16 21:44:33 +0900

    refactor : Movie 상영 일자 프로퍼티 객체로 수정

--- app/src/main/java/woowacourse/movie/Movie.kt
+++ app/src/main/java/woowacourse/movie/Movie.kt
@@ -1,10 +1,11 @@
 package woowacourse.movie
 
 import androidx.annotation.DrawableRes
+import woowacourse.movie.domain.model.ScreeningDate
 
 class Movie(
     val title: String,
     @DrawableRes val poster: Int,
-    val releaseDate: String,
+    val releaseDate: ScreeningDate,
     val runningTime: String,
 )
