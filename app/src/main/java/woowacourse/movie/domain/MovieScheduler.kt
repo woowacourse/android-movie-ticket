e93daa264dc3cb2aeb2b84790ddd4c510ea888ee
hwannow <vnfdlvgid061@naver.com>
2025-04-16 16:29:17 +0900

    feat: 상영 가능 시간 반환 기능 추가

--- app/src/main/java/woowacourse/movie/domain/MovieScheduler.kt
+++ app/src/main/java/woowacourse/movie/domain/MovieScheduler.kt
@@ -1,6 +1,9 @@
 package woowacourse.movie.domain
 
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 class MovieScheduler {
     fun reservableDates(
@@ -35,5 +38,39 @@ class MovieScheduler {
         }
 
         return dates
+    }
+
+    fun reservableTimes(
+        selectedDate: LocalDate,
+        currentDate: LocalDateTime,
+    ): List<LocalTime> {
+        val isToday = selectedDate == currentDate.toLocalDate()
+        val screeningDayType = ScreeningDayType.of(selectedDate)
+        val startTime =
+            if (!isToday) {
+                screeningDayType.startTime
+            } else {
+                currentDate.toLocalTime().coerceAtLeast(screeningDayType.startTime)
+            }
+        return getTimesBetween(startTime, screeningDayType)
+    }
+
+    private fun getTimesBetween(
+        startTime: LocalTime,
+        screeningDayType: ScreeningDayType,
+        endTime: LocalTime = END_TIME,
+    ): List<LocalTime> {
+        val hourDiff = ChronoUnit.HOURS.between(startTime, endTime)
+
+        val hours = (0..hourDiff).map { startTime.plusHours(it) }
+
+        return when (screeningDayType) {
+            ScreeningDayType.WEEKDAY -> hours.filter { it.hour % 2 == 0 }
+            ScreeningDayType.WEEKEND -> hours.filter { it.hour % 2 == 1 }
+        }
+    }
+
+    companion object {
+        private val END_TIME = LocalTime.of(23, 59)
     }
 }
--- app/src/test/java/woowacourse/movie/domain/MovieSchedulerTest.kt
+++ app/src/test/java/woowacourse/movie/domain/MovieSchedulerTest.kt
@@ -3,6 +3,8 @@ package woowacourse.movie.domain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 class MovieSchedulerTest {
     private val movieScheduler = MovieScheduler()
@@ -30,6 +32,89 @@ class MovieSchedulerTest {
 
         // when
         val actual = movieScheduler.reservableDates(screeningDate, currentDate)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `현재 날짜와 선택된 날짜가 같고 평일인데, 현재 시간이 빠른 경우 현재 시간 기준으로 짝수 시간을 반환한다`() {
+        // given
+        val selectedDate = APRIL_THIRTIETH
+        val currentTime = LocalDateTime.of(APRIL_THIRTIETH, LocalTime.of(20, 0))
+        val expected: List<LocalTime> =
+            listOf(
+                LocalTime.of(20, 0),
+                LocalTime.of(22, 0),
+            )
+
+        // when
+        val actual = movieScheduler.reservableTimes(selectedDate, currentTime)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `현재 날짜와 선택된 날짜가 같고 주말인데, 현재 시간이 빠른 경우 현재 시간 기준으로 홀수 시간을 반환한다`() {
+        // given
+        val selectedDate = MAY_THIRD
+        val currentTime = LocalDateTime.of(MAY_THIRD, LocalTime.of(20, 0))
+        val expected: List<LocalTime> =
+            listOf(
+                LocalTime.of(21, 0),
+                LocalTime.of(23, 0),
+            )
+
+        // when
+        val actual = movieScheduler.reservableTimes(selectedDate, currentTime)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `현재 날짜와 선택된 날짜가 다르고 평일이면 상영 시간 기준으로 짝수 시간을 반환한다`() {
+        // given
+        val selectedDate = MAY_FIRST
+        val currentTime = LocalDateTime.of(APRIL_THIRTIETH, LocalTime.of(20, 0))
+        val expected: List<LocalTime> =
+            listOf(
+                LocalTime.of(10, 0),
+                LocalTime.of(12, 0),
+                LocalTime.of(14, 0),
+                LocalTime.of(16, 0),
+                LocalTime.of(18, 0),
+                LocalTime.of(20, 0),
+                LocalTime.of(22, 0),
+            )
+
+        // when
+        val actual = movieScheduler.reservableTimes(selectedDate, currentTime)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `현재 날짜와 선택된 날짜가 다르고 주말이면 상영 시간 기준으로 홀수 시간을 반환한다`() {
+        // given
+        val selectedDate = MAY_THIRD
+        val currentTime = LocalDateTime.of(APRIL_THIRTIETH, LocalTime.of(20, 0))
+        val expected: List<LocalTime> =
+            listOf(
+                LocalTime.of(9, 0),
+                LocalTime.of(11, 0),
+                LocalTime.of(13, 0),
+                LocalTime.of(15, 0),
+                LocalTime.of(17, 0),
+                LocalTime.of(19, 0),
+                LocalTime.of(21, 0),
+                LocalTime.of(23, 0),
+            )
+
+        // when
+        val actual = movieScheduler.reservableTimes(selectedDate, currentTime)
 
         // then
         assertThat(actual).isEqualTo(expected)
