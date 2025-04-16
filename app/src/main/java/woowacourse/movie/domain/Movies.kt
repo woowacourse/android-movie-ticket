a5a80aea5e62622123294f315952cfdd93e9f54b
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-16 12:57:25 +0900

    feat: movies 하드 코딩 값 설정

--- app/src/main/java/woowacourse/movie/domain/Movies.kt
+++ app/src/main/java/woowacourse/movie/domain/Movies.kt
@@ -1,5 +1,9 @@
 package woowacourse.movie.domain
 
+import woowacourse.movie.R
+import java.time.LocalDateTime
+import kotlin.time.Duration.Companion.minutes
+
 data class Movies(
     val movies: Map<String, Movie>,
 ) {
@@ -11,5 +15,8 @@ data class Movies(
 
     companion object {
         private const val ERR_MOVIE_NOT_FOUND = "영화가 존재하지 않습니다."
+        val movies = listOf<Movie>(Movie("해리포터와 마법사의 돌", R.drawable.movie_poster,  LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+            LocalDateTime.of(2025, 4, 25, 23, 59, 59),
+            125.minutes))
     }
 }
