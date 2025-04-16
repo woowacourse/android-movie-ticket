78741f74ee74a120ff3d9f9dde23615ce7859de3
cucumber99 <dlsguq1@gmail.com>
2025-04-16 18:05:09 +0900

    refactor: 메소드 분리

--- app/src/main/java/woowacourse/movie/ui/view/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package woowacourse.movie
+package woowacourse.movie.ui.view
 
 import android.os.Bundle
 import android.widget.ListView
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.R
 import woowacourse.movie.model.Movie
 import woowacourse.movie.ui.adapter.MovieAdapter
 import java.time.LocalDate
@@ -13,6 +14,13 @@ import java.time.LocalDate
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupScreen()
+        val adapter = MovieAdapter(this, movies)
+        val listView = findViewById<ListView>(R.id.movies)
+        listView.adapter = adapter
+    }
+
+    private fun setupScreen() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -20,10 +28,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val adapter = MovieAdapter(this, movies)
-        val listView = findViewById<ListView>(R.id.movies)
-        listView.adapter = adapter
     }
 
     companion object {
@@ -31,12 +35,14 @@ class MainActivity : AppCompatActivity() {
             Movie(
                 "승부",
                 LocalDate.of(2025,3,26),
+                LocalDate.of(2025, 4, 26),
                 115,
                 R.drawable.match
             ),
             Movie(
                 "미키 17",
                 LocalDate.of(2025, 2, 28),
+                LocalDate.of(2025, 3, 27),
                 137,
                 R.drawable.mickey
             )
