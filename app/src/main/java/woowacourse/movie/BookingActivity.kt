5b30d318931106ee636becdc3ee43cceeb838ea2
leeyerin0210 <lyl8661@gmail.com>
2025-04-16 13:13:48 +0900

    feat : 예매 상세정보 넘겨받는 기능 구현

--- app/src/main/java/woowacourse/movie/BookingActivity.kt
+++ app/src/main/java/woowacourse/movie/BookingActivity.kt
@@ -1,6 +1,8 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,22 @@ class BookingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_booking)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.booking)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val title = findViewById<TextView>(R.id.title)
+        val startDate = findViewById<TextView>(R.id.start_date)
+        val endDate = findViewById<TextView>(R.id.end_date)
+        val runningTime = findViewById<TextView>(R.id.running_time)
+        val poster = findViewById<ImageView>(R.id.movie_poster)
+
+        poster.setImageResource(intent.getIntExtra("POSTER",0))
+        title.text = intent.getStringExtra("TITLE")
+        startDate.text = intent.getStringExtra("START_DATE")
+        endDate.text = intent.getStringExtra("END_DATE")
+        runningTime.text = intent.getStringExtra("RUNNING_TIME")
     }
 }
