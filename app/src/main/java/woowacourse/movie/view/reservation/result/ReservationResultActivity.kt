ca3924cfead81b51aeffd04872f55dd99eefb040
moondev03 <moondev03@gmail.com>
2025-04-16 13:07:18 +0900

    feat: 영화 예매 완료 화면 구현

--- app/src/main/java/woowacourse/movie/view/reservation/result/ReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/result/ReservationResultActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.view.reservation.result
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,17 +13,35 @@ class ReservationResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_reservation_result)
-        setWindowInsets()
 
+        setWindowInsets()
+        displayReservationResult()
+    }
+
+    private fun displayReservationResult() {
         val title = intent.getStringExtra(getString(R.string.bundle_key_movie_title))
         val date = intent.getStringExtra(getString(R.string.bundle_key_movie_date))
+
+        val tvCancelDescription = findViewById<TextView>(R.id.tv_cancel_description)
+        tvCancelDescription.text =
+            getString(R.string.reservation_result_cancel_time_description, CANCELLATION_TIME)
+
+        val tvMovieTitle = findViewById<TextView>(R.id.tv_movie_title)
+        tvMovieTitle.text = title
+
+        val tvMovieDate = findViewById<TextView>(R.id.tv_movie_date)
+        tvMovieDate.text = date
     }
 
     private fun setWindowInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    companion object {
+        private const val CANCELLATION_TIME = 15
     }
 }
