2841c41baa4bf25425eb9790e872e0d8884265db
cucumber99 <dlsguq1@gmail.com>
2025-04-16 18:01:45 +0900

    refactor: 확장 함수 분리

---
+++ app/src/main/java/woowacourse/movie/ui/view/IntentExtension.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie.ui.view
+
+import android.content.Intent
+import android.os.Build
+import java.io.Serializable
+
+@Suppress("UNCHECKED_CAST")
+fun <T: Serializable> Intent.intentSerializable(key: String, customClass: Class<T>): T {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getSerializableExtra(key, customClass)!!
+    } else {
+        this.getSerializableExtra(key) as T
+    }
+}
