60a1c65d3e2b1aa11d835522bbd83074d9426c27
jerry8282 <ka06196@naver.com>
2025-04-16 13:08:24 +0900

    feat: 지금 예매 버튼을 누르면 예매 완료 화면으로 넘어간다

--- app/src/main/java/woowacourse/movie/CompleteActivity.kt
+++ app/src/main/java/woowacourse/movie/CompleteActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Complete : AppCompatActivity() {
+class CompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/woowacourse/movie/domain/MyAdapter.kt
+++ app/src/main/java/woowacourse/movie/domain/MyAdapter.kt
@@ -1,11 +1,14 @@
 package woowacourse.movie.domain
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import woowacourse.movie.CompleteActivity
 import woowacourse.movie.R
 
 class MyAdapter(private val items: List<Movie>) : BaseAdapter() {
@@ -35,11 +38,18 @@ class MyAdapter(private val items: List<Movie>) : BaseAdapter() {
         val titleTextView: TextView = view.findViewById(R.id.movie_title)
         val dateTextView: TextView = view.findViewById(R.id.movie_date)
         val timeTextView: TextView = view.findViewById(R.id.movie_time)
+        val reserveButton: Button = view.findViewById(R.id.reserve_button)
 
         imageView.setImageResource(items[position].image)
         titleTextView.text = items[position].title
         dateTextView.text = parent?.context?.getString(R.string.movieDate, items[position].date)
         timeTextView.text = parent?.context?.getString(R.string.movieTime, items[position].time)
+        reserveButton.setOnClickListener {
+            val intent = Intent(parent?.context, CompleteActivity::class.java)
+            intent.putExtra("movieTitle", items[position].title)
+            intent.putExtra("movieDate", items[position].date)
+            parent?.context?.startActivity(intent)
+        }
 
         return view
     }
