47cb9362d7103b4d01e33f7df0df13a3232d0892
m6z1 <sonmyungj1zz@gmail.com>
2025-04-16 18:55:42 +0900

    refactor: 영화 목록 뷰 네이밍 변경

--- app/src/main/java/woowacourse/movie/MoviesActivity.kt
+++ app/src/main/java/woowacourse/movie/MoviesActivity.kt
@@ -10,11 +10,11 @@ import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.ReservationActivity.Companion.MOVIE_DATA_KEY
 import woowacourse.movie.model.Movie
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
