6f40c776be86e10975d8225ab825dc51cab664b0
moondev03 <moondev03@gmail.com>
2025-04-16 19:18:26 +0900

    feat: 영화 예매 화면 뷰 구현

---
+++ app/src/main/java/woowacourse/movie/view/reservation/ReservationActivity.kt
@@ -0,0 +1,9 @@
+package woowacourse.movie.view.reservation
+
+import woowacourse.movie.R
+import woowacourse.movie.view.base.BaseActivity
+
+class ReservationActivity : BaseActivity(R.layout.activity_reservation) {
+    override fun setupViews() {
+    }
+}
