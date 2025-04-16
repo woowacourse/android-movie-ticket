b65b4e51a0473a8970a0c912a03b18abce37f171
DoableTuple <doabletuple@gmail.com>
2025-04-16 14:56:41 +0900

    feat: 선택 가능한 상영 날짜와 시간을 반환하는 기능 구현

---
+++ app/src/main/java/woowacourse/movie/domain/Scheduler.kt
@@ -0,0 +1,51 @@
+package woowacourse.movie.domain
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class Scheduler {
+    fun getScreeningDates(
+        startDate: LocalDate,
+        endDate: LocalDate,
+        today: LocalDate,
+    ): List<LocalDate> {
+        val earliestDate = if (startDate.isBefore(today)) today else startDate
+        var currentDate = earliestDate
+        val dates = mutableListOf<LocalDate>()
+        while (!currentDate.isAfter(endDate)) {
+            dates.add(currentDate)
+            currentDate = currentDate.plusDays(1)
+        }
+        return dates
+    }
+
+    fun getShowTimes(
+        selectedDate: LocalDate,
+        now: LocalDateTime,
+    ): List<Int> {
+        val isToday = selectedDate == now.toLocalDate()
+        val showTimes = if (selectedDate.isWeekend()) WEEKEND_SHOW_TIMES else WEEKDAY_SHOW_TIMES
+        return if (isToday) {
+            showTimes.filter { time -> time > now.hour }
+        } else {
+            showTimes
+        }
+    }
+
+    private fun LocalDate.isWeekend(): Boolean {
+        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
+    }
+
+    companion object {
+        private const val WEEKEND_OPENING_HOUR = 9
+        private const val WEEKDAY_OPENING_HOUR = 10
+        private const val CLOSING_HOUR = 24
+        private const val SCREENING_TIME_INTERVAL = 2
+
+        private val WEEKEND_SHOW_TIMES =
+            (WEEKEND_OPENING_HOUR until CLOSING_HOUR step SCREENING_TIME_INTERVAL).toList()
+        private val WEEKDAY_SHOW_TIMES =
+            (WEEKDAY_OPENING_HOUR until CLOSING_HOUR step SCREENING_TIME_INTERVAL).toList()
+    }
+}
---
+++ app/src/test/java/woowacourse/movie/domain/SchedulerTest.kt
@@ -0,0 +1,114 @@
+package woowacourse.movie.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class SchedulerTest {
+    private val scheduler = Scheduler()
+
+    @Test
+    fun `상영 시작일이 오늘보다 빠르면 오늘부터 종료일까지 반환한다`() {
+        // given
+        val startDate = LocalDate.of(2025, 4, 1)
+        val endDate = LocalDate.of(2025, 4, 10)
+        val today = LocalDate.of(2025, 4, 5)
+
+        // when
+        val actual = scheduler.getScreeningDates(startDate, endDate, today)
+        val expected =
+            listOf(
+                LocalDate.of(2025, 4, 5),
+                LocalDate.of(2025, 4, 6),
+                LocalDate.of(2025, 4, 7),
+                LocalDate.of(2025, 4, 8),
+                LocalDate.of(2025, 4, 9),
+                LocalDate.of(2025, 4, 10),
+            )
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `상영 시작일이 오늘 이후면 상영 기간 전체를 반환한다`() {
+        // given
+        val startDate = LocalDate.of(2025, 4, 2)
+        val endDate = LocalDate.of(2025, 4, 10)
+        val today = LocalDate.of(2025, 4, 1)
+
+        // when
+        val actual = scheduler.getScreeningDates(startDate, endDate, today)
+        val expected =
+            listOf(
+                LocalDate.of(2025, 4, 2),
+                LocalDate.of(2025, 4, 3),
+                LocalDate.of(2025, 4, 4),
+                LocalDate.of(2025, 4, 5),
+                LocalDate.of(2025, 4, 6),
+                LocalDate.of(2025, 4, 7),
+                LocalDate.of(2025, 4, 8),
+                LocalDate.of(2025, 4, 9),
+                LocalDate.of(2025, 4, 10),
+            )
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `선택한 상영일이 오늘이 아니고 주중이면 10시부터 24시까지 2시간 간격으로 상영 시간을 반환한다`() {
+        // given
+        val selectedDate = LocalDate.of(2025, 4, 14)
+        val now = LocalDateTime.of(2025, 4, 12, 0, 0, 0)
+
+        // when
+        val actual = scheduler.getShowTimes(selectedDate, now)
+        val expected = (10 until 24 step 2).toList()
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `선택한 상영일이 오늘이 아니고 주말이면 9시부터 24시까지 2시간 간격으로 상영 시간을 반환한다`() {
+        // given
+        val selectedDate = LocalDate.of(2025, 4, 13)
+        val now = LocalDateTime.of(2025, 4, 12, 0, 0, 0)
+
+        // when
+        val actual = scheduler.getShowTimes(selectedDate, now)
+        val expected = (9 until 24 step 2).toList()
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `선택한 상영일이 오늘이고 주중이면 현재 시간과 10시 중 더 늦은 시간부터 24시까지 2시간 간격으로 상영 시간을 반환한다`() {
+        // given
+        val selectedDate = LocalDate.of(2025, 4, 14)
+        val now = LocalDateTime.of(2025, 4, 14, 13, 30, 0)
+
+        // when
+        val actual = scheduler.getShowTimes(selectedDate, now)
+        val expected = (14 until 24 step 2).toList()
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `선택한 상영일이 오늘이고 주말이면 현재 시간과 9시 중 더 늦은 시간부터 24시까지 2시간 간격으로 상영 시간을 반환한다`() {
+        // given
+        val selectedDate = LocalDate.of(2025, 4, 13)
+        val now = LocalDateTime.of(2025, 4, 13, 13, 30, 0)
+
+        // when
+        val actual = scheduler.getShowTimes(selectedDate, now)
+        val expected = (15 until 24 step 2).toList()
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+}
