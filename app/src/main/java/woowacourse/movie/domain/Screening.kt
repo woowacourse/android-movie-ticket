5d30e24759b791e7cfb65c6eabe7184460290736
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-16 17:51:20 +0900

    feat: Screening에 상영 시간표 추가

--- app/src/main/java/woowacourse/movie/domain/Screening.kt
+++ app/src/main/java/woowacourse/movie/domain/Screening.kt
@@ -7,8 +7,12 @@ data class Screening(
     private val movie: Movie,
     val period: ClosedRange<LocalDate>,
 ) {
+    private val showTimePolicy = ShowtimePolicy()
+
     @DrawableRes
     val posterId: Int = movie.posterId
     val title: String = movie.title
     val runningTime: Int = movie.runningTime
+
+    fun showtimes(date: LocalDate) = showTimePolicy.showtimes(date)
 }
