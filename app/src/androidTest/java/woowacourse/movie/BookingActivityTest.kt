07ec4aac47d6a885dede217e804bb89bb5ae813a
gahyunkim <mapledt001@naver.com>
2025-04-17 15:36:15 +0900

    test: UiTest 구현
    - BookingActivityTest, BookingCompleteActivityTest 구현

--- app/src/androidTest/java/woowacourse/movie/BookingActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/BookingActivityTest.kt
@@ -3,14 +3,24 @@ package woowacourse.movie
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
+import androidx.test.espresso.matcher.RootMatchers.isPlatformPopup
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withSpinnerText
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -29,7 +39,6 @@ class BookingActivityTest {
         Intents.init()
 
         val movie = mockMovie()
-
         val intent =
             Intent(ApplicationProvider.getApplicationContext(), BookingActivity::class.java).apply {
                 putExtra("movieData", movie)
@@ -67,7 +76,7 @@ class BookingActivityTest {
         onView(withId(R.id.tv_booking_screening_date)).check(
             matches(
                 allOf(
-                    withText("2024.4.1 ~ 2024.4.25"),
+                    withText("2025.4.1 ~ 2025.4.25"),
                     isDisplayed(),
                 ),
             ),
@@ -98,37 +107,190 @@ class BookingActivityTest {
         )
     }
 
-//    @Test
-//    fun `minus_버튼을_누르면_인원수가_줄어든다`() {
-//        onView(withId(R.id.btn_minus))
-//            .perform(click())
-//            .check(
-//            matches(
-//                allOf(
-//                    withText(""),
-//                    isDisplayed(),
-//                ),
-//            ),
-//        )
-//    }
-//
-//    @Test
-//    fun `plus_버튼을_누르면_인원수가_증가한다`() {
-//        TODO("Not yet implemented")
-//    }
-//
-//    @Test
-//    fun `인원수가_0_이면_-버튼을_눌러도_다이알로그가_뜨지_않는다`() {
-//        TODO("Not yet implemented")
-//    }
+    @Test
+    fun `minus_버튼을_눌렀을때_인원수가_0_이하면_변하지_않는다`() {
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("0")))
+
+        onView(withId(R.id.btn_minus))
+            .perform(click())
+
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("0")))
+    }
+
+    @Test
+    fun `plus_버튼을_누르면_인원수가_증가한다`() {
+        onView(withId(R.id.btn_plus)).perform(click())
+
+        onView(withId(R.id.tv_people_count)).check(
+            matches(
+                allOf(
+                    withText("1"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `minus_버튼을_눌렀을때_인원수가_1_이상이면_줄어든다`() {
+        onView(withId(R.id.btn_plus)).perform(click())
+
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("1")))
+
+        onView(withId(R.id.btn_minus))
+            .perform(click())
+
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("0")))
+    }
+
+    @Test
+    fun `확인_버튼을_누르면_다이알로그가_뜬다`() {
+        onView(withId(R.id.btn_plus)).perform(click())
+
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("1")))
+
+        onView(withId(R.id.btn_reserve_confirm)).perform(click())
+
+        onView(withText("예매 확인"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("정말 예매하시겠습니까?"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("예매 완료"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `다이알로그에서_취소를_누르면_화면이_닫힌다`() {
+        onView(withId(R.id.btn_plus)).perform(click())
+
+        onView(withId(R.id.tv_people_count))
+            .check(matches(withText("1")))
+
+        onView(withId(R.id.btn_reserve_confirm)).perform(click())
+
+        onView(withText("예매 확인"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("정말 예매하시겠습니까?"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("취소"))
+            .perform(click())
+    }
+
+    @Test
+    fun `특정_날짜를_선택했을_때_주말인_경우_시간이_정상적으로_표시되고_선택된다`() {
+        onView(withId(R.id.spinner_screening_date)).perform(click())
+
+        val targetDate = "2025-04-20"
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`(targetDate)))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.spinner_screening_date))
+            .check(matches(withSpinnerText(containsString(targetDate))))
+
+        onView(withId(R.id.spinner_screening_time)).perform(click())
+
+        // 주말 로직에 따라 10:00, 12:00이 떠야함
+        onData(`is`("12:00"))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.spinner_screening_time))
+            .check(matches(withSpinnerText(containsString("12:00"))))
+    }
+
+    @Test
+    fun `특정_날짜를_선택했을_때_평일인_경우_시간이_정상적으로_표시되고_선택된다`() {
+        onView(withId(R.id.spinner_screening_date)).perform(click())
+
+        // 2025-04-18은 평일
+        val targetDate = "2025-04-18"
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`(targetDate)))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.spinner_screening_date))
+            .check(matches(withSpinnerText(containsString(targetDate))))
+
+        onView(withId(R.id.spinner_screening_time)).perform(click())
+
+        // 평일 로직에 따라 9:00, 12:00이 떠야함
+        onData(`is`("09:00"))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.spinner_screening_time))
+            .check(matches(withSpinnerText(containsString("09:00"))))
+    }
+
+    @Test
+    fun `예매_완료_버튼을_누르면_다음_화면으로_데이터를_가지고_넘어간다`() {
+        onView(withId(R.id.spinner_screening_date)).perform(click())
+
+        // 2025-04-18은 평일
+        val targetDate = "2025-04-18"
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`(targetDate)))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.spinner_screening_time)).perform(click())
+        val targetTime = "11:00"
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`(targetTime)))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+
+        onView(withId(R.id.btn_plus)).perform(click())
+        onView(withId(R.id.btn_reserve_confirm)).perform(click())
+
+        onView(withText("예매 완료"))
+            .perform(click())
+
+        intended(
+            allOf(
+                hasComponent(BookingCompleteActivity::class.java.name),
+                hasExtraWithKey("bookingResult"),
+            ),
+        )
+
+        onView(withId(R.id.tv_complete_title))
+            .check(
+                matches(
+                    allOf(
+                        withText("해리 포터와 마법사의 돌"),
+                        isDisplayed(),
+                    ),
+                ),
+            )
+
+        onView(withId(R.id.tv_complete_screening_date))
+            .check(matches(allOf(withText("2025.04.18"), isDisplayed())))
+
+        onView(withId(R.id.tv_complete_screening_time))
+            .check(matches(allOf(withText("11:00"), isDisplayed())))
+
+        onView(withId(R.id.tv_head_count))
+            .check(matches(allOf(withText("일반 1명"), isDisplayed())))
+
+        onView(withId(R.id.tv_booking_amount))
+            .check(matches(allOf(withText("13,000원 (현장 결제)"), isDisplayed())))
+    }
 
     private fun mockMovie(): Movie {
         return Movie(
             imageSource = R.drawable.harry_potter,
             title = "해리 포터와 마법사의 돌",
             runningTime = 152,
-            screeningStartDate = LocalDate.of(2024, 4, 1),
-            screeningEndDate = LocalDate.of(2024, 4, 25),
+            screeningStartDate = LocalDate.of(2025, 4, 1),
+            screeningEndDate = LocalDate.of(2025, 4, 25),
         )
     }
 }
---
+++ app/src/androidTest/java/woowacourse/movie/BookingCompleteActivityTest.kt
@@ -0,0 +1,113 @@
+package woowacourse.movie
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.movie.model.BookingResult
+
+class BookingCompleteActivityTest {
+    private lateinit var scenario: ActivityScenario<BookingCompleteActivity>
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(BookingCompleteActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+
+        val bookingResult = mockBookingResult()
+        val intent =
+            Intent(ApplicationProvider.getApplicationContext(), BookingCompleteActivity::class.java).apply {
+                putExtra("bookingResult", bookingResult)
+            }
+
+        scenario = ActivityScenario.launch(intent)
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+        scenario.close()
+    }
+
+    @Test
+    fun `화면에_영화_제목이_표시된다`() {
+        onView(withId(R.id.tv_complete_title)).check(
+            matches(
+                allOf(
+                    withText("해리 포터와 마법사의 돌"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `화면에_선택한_영화_상영일이_표시된다`() {
+        onView(withId(R.id.tv_complete_screening_date)).check(
+            matches(
+                allOf(
+                    withText("2025.04.20"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `화면에_선택한_영화_상영시간이_표시된다`() {
+        onView(withId(R.id.tv_complete_screening_time)).check(
+            matches(
+                allOf(
+                    withText("12:00"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `화면에_선택한_영화_예매인원이_표시된다`() {
+        onView(withId(R.id.tv_head_count)).check(
+            matches(
+                allOf(
+                    withText("일반 2명"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `화면에_선택한_영화_결제금액이_표시된다`() {
+        onView(withId(R.id.tv_booking_amount)).check(
+            matches(
+                allOf(
+                    withText("26,000원 (현장 결제)"),
+                    isDisplayed(),
+                ),
+            ),
+        )
+    }
+
+    private fun mockBookingResult(): BookingResult {
+        return BookingResult(
+            title = "해리 포터와 마법사의 돌",
+            _headCount = 2,
+            _selectedDate = "2025.04.20",
+            _selectedTime = "12:00",
+        )
+    }
+}
