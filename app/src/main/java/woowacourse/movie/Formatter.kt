92baf76918154a60bc5d9a9108db68a2d49eec80
m6z1 <sonmyungj1zz@gmail.com>
2025-04-17 14:11:47 +0900

    chore: 불필요한 함수 제거

--- app/src/main/java/woowacourse/movie/Formatter.kt
+++ app/src/main/java/woowacourse/movie/Formatter.kt
@@ -11,19 +11,9 @@ class Formatter {
 
     fun priceToUI(price: Int): String = PRICE_UI_FORMATTER.format(price)
 
-    fun uiToLocalDate(time: String): LocalDate {
-        val year = time.slice(YEAR_RANGE).toInt()
-        val month = time.slice(MONTH_RANGE).toInt()
-        val date = time.slice(DAY_RANGE).toInt()
-        return LocalDate.of(year, month, date)
-    }
-
     companion object {
         private val DATE_UI_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd")
         private const val TIME_UI_SUFFIX = "%d:00"
         private val PRICE_UI_FORMATTER = DecimalFormat("#,###")
-        private val YEAR_RANGE = 0..3
-        private val MONTH_RANGE = 5..6
-        private val DAY_RANGE = 8..9
     }
 }
