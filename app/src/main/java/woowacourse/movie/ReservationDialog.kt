bb54bdb21c8d8691832b567605d3ec004b4f2590
jerry8282 <ka06196@naver.com>
2025-04-17 15:33:59 +0900

    refactor: Dialog객체 분리

---
+++ app/src/main/java/woowacourse/movie/ReservationDialog.kt
@@ -0,0 +1,25 @@
+package woowacourse.movie
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AlertDialog
+import woowacourse.movie.domain.Ticket
+
+class ReservationDialog(
+    private val context: Context,
+    private val ticket: Ticket,
+) {
+    fun popUp() {
+        AlertDialog.Builder(context)
+            .setTitle("예매 확인")
+            .setMessage("정말 예매하시겠습니까?")
+            .setPositiveButton("예매 완료") { _, _ ->
+                val intent = Intent(context, CompleteActivity::class.java)
+                intent.putExtra("ticket", ticket)
+                context.startActivity(intent)
+            }
+            .setNegativeButton("취소") { dialog, _ -> dialog.dismiss() }
+            .setCancelable(false)
+            .show()
+    }
+}
