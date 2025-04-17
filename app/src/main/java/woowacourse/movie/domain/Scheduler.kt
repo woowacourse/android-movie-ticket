15ffdb02428de473b0887676b7d0270cba0f025a
이지윤 <02leejiyun@naver.com>
2025-04-17 11:10:18 +0900

    refactor: 평일과 주말을 enum으로 정의

--- app/src/main/java/woowacourse/movie/domain/Scheduler.kt
+++ app/src/main/java/woowacourse/movie/domain/Scheduler.kt
@@ -1,6 +1,5 @@
 package woowacourse.movie.domain
 
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -26,31 +25,15 @@ class Scheduler {
         now: LocalDateTime,
     ): List<LocalTime> {
         val isToday = selectedDate == now.toLocalDate()
-        val showTimes = if (selectedDate.isWeekend()) WEEKEND_SHOW_TIMES else WEEKDAY_SHOW_TIMES
+        val dayType = DayType.of(selectedDate)
+        val showTimes =
+            (dayType.openingHour until dayType.closingHour step dayType.interval).map { hour ->
+                LocalTime.of(hour, 0)
+            }
         return if (isToday) {
             showTimes.filter { time -> time.hour > now.hour }
         } else {
             showTimes
         }
-    }
-
-    private fun LocalDate.isWeekend(): Boolean {
-        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
-    }
-
-    companion object {
-        private const val WEEKEND_OPENING_HOUR = 9
-        private const val WEEKDAY_OPENING_HOUR = 10
-        private const val CLOSING_HOUR = 24
-        private const val SCREENING_TIME_INTERVAL = 2
-
-        private val WEEKEND_SHOW_TIMES =
-            (WEEKEND_OPENING_HOUR until CLOSING_HOUR step SCREENING_TIME_INTERVAL).map { hour ->
-                LocalTime.of(hour, 0)
-            }
-        private val WEEKDAY_SHOW_TIMES =
-            (WEEKDAY_OPENING_HOUR until CLOSING_HOUR step SCREENING_TIME_INTERVAL).map { hour ->
-                LocalTime.of(hour, 0)
-            }
     }
 }
