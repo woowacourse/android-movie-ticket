a67a222fecafa762efa679181bfd7cf35f45db78
medAndro <med4ndro@gmail.com>
2025-04-17 13:53:30 +0900

    test: 예약 액티비티의 스피너를 제외한 테스트 코드 추가

--- app/src/androidTest/java/woowacourse/movie/ReservationActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/ReservationActivityTest.kt
@@ -3,9 +3,12 @@ package woowacourse.movie
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
@@ -44,8 +47,51 @@ class ReservationActivityTest {
         )
 
     @Test
+    fun `영화_제목이_표시된다`() {
+        onView(withId(R.id.tv_reservation_movie_title))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `상영일이_표시된다`() {
+        onView(withId(R.id.tv_reservation_movie_period))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
     fun `포스터가_표시된다`() {
         onView(withId(R.id.iv_reservation_poster))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `러닝타임이_표시된다`() {
+        onView(withId(R.id.tv_reservation_movie_running_time))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `티켓_인원수를_버튼으로_증감시킬_수_있다`() {
+        onView(withId(R.id.tv_reservation_audience_count))
+            .check(matches(withText("1")))
+
+        onView(withId(R.id.btn_reservation_plus))
+            .perform(click())
+        onView(withId(R.id.tv_reservation_audience_count))
+            .check(matches(withText("2")))
+
+        onView(withId(R.id.btn_reservation_minus))
+            .perform(click())
+        onView(withId(R.id.tv_reservation_audience_count))
+            .check(matches(withText("1")))
+    }
+
+    @Test
+    fun `선택_완료를_클릭하면_다이얼로그가_표시된다`() {
+        onView(withId(R.id.btn_reservation_select_complete))
+            .perform(click())
+        onView(withText(R.string.ticket_dialog_title))
+            .inRoot(RootMatchers.isDialog())
             .check(matches(isDisplayed()))
     }
 }
