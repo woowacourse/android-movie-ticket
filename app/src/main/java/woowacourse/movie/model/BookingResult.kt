d84fa25f9ce43b199101b19c1b6de3f94599c621
yrsel <xxknu88@gmail.com>
2025-04-17 15:10:38 +0900

    feat: 예약현황 직접 처리하도록 구현

--- app/src/main/java/woowacourse/movie/model/BookingResult.kt
+++ app/src/main/java/woowacourse/movie/model/BookingResult.kt
@@ -5,12 +5,37 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class BookingResult(
-    val headCount: Int,
-    val selectedDate: String,
-    val selectedTime: String,
+    val title: String,
+    private var _headCount: Int,
+    private var _selectedDate: String,
+    private var _selectedTime: String,
 ) : Parcelable {
+    val headCount get() = _headCount
+    val selectedDate get() = _selectedDate
+    val selectedTime get() = _selectedTime
+
     fun calculateAmount(): Int {
         return TICKET_PRICE * headCount
+    }
+
+    fun plusHeadCount() {
+        _headCount++
+    }
+
+    fun minusHeadCount() {
+        _headCount--
+    }
+
+    fun updateDate(date: String) {
+        _selectedDate = date
+    }
+
+    fun updateTime(time: String) {
+        _selectedTime = time
+    }
+
+    fun isHeadCountValid(): Boolean {
+        return headCount > 0
     }
 
     companion object {
