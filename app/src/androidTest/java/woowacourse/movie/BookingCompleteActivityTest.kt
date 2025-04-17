5e900df16bcdf04b10a722e63ab56d6d0f499d87
chanho0908 <uni070@naver.com>
2025-04-17 13:58:35 +0900

    feat : 예매 완료 후 예매 완료 화면에 예매 정보 전달 기능 추가

--- app/src/androidTest/java/woowacourse/movie/BookingCompleteActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/BookingCompleteActivityTest.kt
@@ -9,6 +9,10 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
+import woowacourse.movie.BookingActivity.Companion.KEY_BOOKING_DATE_TIME
+import woowacourse.movie.BookingActivity.Companion.KEY_BOOKING_MOVIE_TITLE
+import woowacourse.movie.BookingActivity.Companion.KEY_BOOKING_PEOPLE_COUNT
+import woowacourse.movie.BookingActivity.Companion.KEY_BOOKING_TICKET_PRICE
 import woowacourse.movie.fixture.fakeContext
 
 class BookingCompleteActivityTest {
@@ -21,8 +25,10 @@ class BookingCompleteActivityTest {
                 fakeContext,
                 BookingCompleteActivity::class.java,
             ).apply {
-                putExtra("title", "해리 포터와 마법사의 돌")
-                putExtra("bookingTime", "2025.4.1")
+                putExtra(KEY_BOOKING_MOVIE_TITLE, "해리 포터와 마법사의 돌")
+                putExtra(KEY_BOOKING_DATE_TIME, "2025.4.1 12:00")
+                putExtra(KEY_BOOKING_PEOPLE_COUNT, 2)
+                putExtra(KEY_BOOKING_TICKET_PRICE, 26000)
             }
         ActivityScenario.launch<BookingCompleteActivity>(intent)
     }
@@ -36,6 +42,18 @@ class BookingCompleteActivityTest {
     @DisplayName("전달 받은 상영 시간을 출력한다")
     @Test
     fun movieReleaseTimeDisplayTest() {
-        onView(withId(R.id.tv_release_date)).check(matches(withText("2025.4.1")))
+        onView(withId(R.id.tv_release_date)).check(matches(withText("2025.4.1 12:00")))
+    }
+
+    @DisplayName("전달 받은 예매 인원을 출력한다")
+    @Test
+    fun moviePeopleCountDisplayTest() {
+        onView(withId(R.id.tv_people_count)).check(matches(withText("일반 2명")))
+    }
+
+    @DisplayName("전달 받은 예매 가격을 출력한다")
+    @Test
+    fun moviePriceDisplayTest() {
+        onView(withId(R.id.tv_price)).check(matches(withText("26,000원 (현장 결제)")))
     }
 }
--- app/src/main/java/woowacourse/movie/BookingActivity.kt
+++ app/src/main/java/woowacourse/movie/BookingActivity.kt
@@ -1,6 +1,9 @@
 package woowacourse.movie
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -21,8 +24,11 @@ import woowacourse.movie.MainActivity.Companion.KEY_MOVIE_START_DAY
 import woowacourse.movie.MainActivity.Companion.KEY_MOVIE_START_MONTH
 import woowacourse.movie.MainActivity.Companion.KEY_MOVIE_START_YEAR
 import woowacourse.movie.MainActivity.Companion.KEY_MOVIE_TITLE
+import woowacourse.movie.StringFormatter.dotDateFormat
+import woowacourse.movie.domain.model.PeopleCount
 import woowacourse.movie.domain.model.ScreeningDate
 import woowacourse.movie.domain.model.ScreeningTime
+import woowacourse.movie.domain.model.TicketType
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -58,6 +64,8 @@ class BookingActivity : AppCompatActivity() {
             insets
         }
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val movieTitleView = findViewById<TextView>(R.id.tv_title)
         movieTitleView.text = intent.getStringExtra(KEY_MOVIE_TITLE)
 
@@ -67,8 +75,8 @@ class BookingActivity : AppCompatActivity() {
         val movieReleaseDateView = findViewById<TextView>(R.id.tv_screening_period)
         movieReleaseDateView.text =
             getString(R.string.text_date_period).format(
-                startDate.toDotFormat(),
-                endDate.toDotFormat(),
+                dotDateFormat(startDate),
+                dotDateFormat(endDate),
             )
 
         val movieRunningTimeView = findViewById<TextView>(R.id.tv_running_time)
@@ -126,6 +134,7 @@ class BookingActivity : AppCompatActivity() {
     private fun setButtonListener() {
         val increaseBtn = findViewById<Button>(R.id.btn_increase)
         val decreaseBtn = findViewById<Button>(R.id.btn_decrease)
+        val bookingBtn = findViewById<Button>(R.id.btn_booking_complete)
 
         val peopleCount = findViewById<TextView>(R.id.tv_people_count)
 
@@ -155,22 +164,50 @@ class BookingActivity : AppCompatActivity() {
             .setTitle(title)
             .setMessage(description)
             .setPositiveButton(getString(R.string.text_booking_dialog_positive_button)) { _, _ ->
+                moveToBookingCompleteActivity()
             }
             .setNegativeButton(getString(R.string.text_booking_dialog_negative_button)) { dialog, _ ->
                 dialog.dismiss()
             }
             .show()
     }
+
+    private fun moveToBookingCompleteActivity() {
+        val title = findViewById<TextView>(R.id.tv_title).text
+        val date = findViewById<Spinner>(R.id.sp_date).selectedItem
+        val time = findViewById<Spinner>(R.id.sp_time).selectedItem
+        val count = findViewById<TextView>(R.id.tv_people_count).text.toString().toInt()
+        val price = PeopleCount(count).ticketPrice(TicketType.GENERAL)
+
+        val intent =
+            Intent(this, BookingCompleteActivity::class.java).apply {
+                putExtra(KEY_BOOKING_MOVIE_TITLE, title)
+                putExtra(KEY_BOOKING_DATE_TIME, "$date $time")
+                putExtra(KEY_BOOKING_PEOPLE_COUNT, count)
+                putExtra(KEY_BOOKING_TICKET_PRICE, price)
+            }
+
+        startActivity(intent)
     }
 
-    private fun LocalDate.toDotFormat(): String {
-        val formatter = DateTimeFormatter.ofPattern("yyyy.M.d")
-        return format(formatter)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
-        const val DEFAULT_DATE_YEAR = 1
-        const val DEFAULT_DATE_MONTH = 1
-        const val DEFAULT_DATE_DAY = 1
+        private const val DEFAULT_DATE_YEAR = 1
+        private const val DEFAULT_DATE_MONTH = 1
+        private const val DEFAULT_DATE_DAY = 1
+        const val KEY_BOOKING_MOVIE_TITLE = "BOOKING_MOVIE_TITLE"
+        const val KEY_BOOKING_DATE_TIME = "BOOKING_DATE_TIME"
+        const val KEY_BOOKING_PEOPLE_COUNT = "BOOKING_PEOPLE_COUNT"
+        const val KEY_BOOKING_TICKET_PRICE = "BOOKING_TICKET_PRICE"
     }
 }
