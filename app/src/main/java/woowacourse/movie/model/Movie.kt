1b65285b4cbbc66439ff87f7756b1ac373312459
gahyunkim <mapledt001@naver.com>
2025-04-17 16:27:47 +0900

    feat: Movie 예외 처리 구현

--- app/src/main/java/woowacourse/movie/model/Movie.kt
+++ app/src/main/java/woowacourse/movie/model/Movie.kt
@@ -12,4 +12,17 @@ data class Movie(
     val screeningStartDate: LocalDate,
     val screeningEndDate: LocalDate,
     val runningTime: Int,
-) : Parcelable
+) : Parcelable {
+    init {
+        require(title.isNotBlank()) { "영화 제목은 비어있을 수 없다" }
+        require(runningTime > 0) { "영화 상영시간은 0보다 커야한다" }
+        require(isDateValid()) { "시작 날짜는 종료 날짜보다 앞서야합니다 " }
+    }
+
+    private fun isDateValid(): Boolean {
+        return screeningStartDate.isBefore(screeningEndDate) ||
+            screeningStartDate.isEqual(
+                screeningEndDate,
+            )
+    }
+}
