69496a78611349a2365597c4d4de5ac1a624c4e2
chanho0908 <uni070@naver.com>
2025-04-17 13:47:58 +0900

    feat : 문자열 포맷팅 클래스 추가

---
+++ app/src/main/java/woowacourse/movie/StringFormatter.kt
@@ -0,0 +1,19 @@
+package woowacourse.movie
+
+import android.icu.text.DecimalFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+object StringFormatter {
+    fun dotDateFormat(time: LocalDate): String {
+        val formatter = DateTimeFormatter.ofPattern(DATE_TIME_DOT_FORMAT)
+        return time.format(formatter)
+    }
+
+    fun thousandFormat(price: Int): String {
+        return DecimalFormat(THOUSAND_UNIT_FORMAT).format(price)
+    }
+
+    private const val DATE_TIME_DOT_FORMAT = "yyyy.M.d"
+    private const val THOUSAND_UNIT_FORMAT = "#,###"
+}
