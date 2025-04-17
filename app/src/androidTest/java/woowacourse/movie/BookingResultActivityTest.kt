b1c15f75a855548e88fc15505f422a7a209d924b
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-04-17 13:29:59 +0900

    test : BookingResultActivity 테스트 작성

---
+++ app/src/androidTest/java/woowacourse/movie/BookingResultActivityTest.kt
@@ -0,0 +1,65 @@
+package woowacourse.movie
+
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.junit.Before
+import org.junit.Test
+
+class BookingResultActivityTest {
+    private lateinit var intent: Intent
+
+    @Before
+    fun setupIntent() {
+        intent = Intent(ApplicationProvider.getApplicationContext(), BookingResultActivity::class.java).apply {
+            putExtra("TITLE", "해리 포터와 마법사의 돌")
+            putExtra("DATE", "2025.4.17")
+            putExtra("TIME", "10:00")
+            putExtra("COUNT", "2")
+        }
+    }
+
+    @Test
+    fun 지정된_영화_제목을_표시한다() {
+        ActivityScenario.launch<BookingActivity>(intent).use {
+            onView(withId(R.id.title))
+                .check(matches(withText("해리 포터와 마법사의 돌")))
+        }
+    }
+
+    @Test
+    fun 지정된_날짜를_표시한다() {
+        ActivityScenario.launch<BookingActivity>(intent).use {
+            onView(withId(R.id.date))
+                .check(matches(withText("2025.4.17")))
+        }
+    }
+
+    @Test
+    fun 지정된_시간을_표시한다() {
+        ActivityScenario.launch<BookingActivity>(intent).use {
+            onView(withId(R.id.time))
+                .check(matches(withText("10:00")))
+        }
+    }
+
+    @Test
+    fun 발권된_티켓수에_따른_인원수를_표시한다() {
+        ActivityScenario.launch<BookingActivity>(intent).use {
+            onView(withId(R.id.count))
+                .check(matches(withText("일반 2명")))
+        }
+    }
+
+    @Test
+    fun 발권된_티켓수에_따른_금액을_표시한다() {
+        ActivityScenario.launch<BookingActivity>(intent).use {
+            onView(withId(R.id.money))
+                .check(matches(withText("26000원 (현장 결제)")))
+        }
+    }
+}
