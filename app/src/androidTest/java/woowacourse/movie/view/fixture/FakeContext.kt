693d97f964b025f67020f2b78c736ed1afb8e36a
moondev03 <moondev03@gmail.com>
2025-04-18 14:49:07 +0900

    refactor: unchecked nullability issues fix

--- app/src/androidTest/java/woowacourse/movie/view/fixture/FakeContext.kt
+++ app/src/androidTest/java/woowacourse/movie/view/fixture/FakeContext.kt
@@ -3,4 +3,4 @@ package woowacourse.movie.view.fixture
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 
-val fakeContext = ApplicationProvider.getApplicationContext<Context>()
+val fakeContext: Context = ApplicationProvider.getApplicationContext()
