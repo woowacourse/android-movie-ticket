c698c47130d17369e20888ad3733c4d30c3c2a3b
oungsi2000 <oungsi1000@gmail.com>
2025-04-18 16:25:57 +0900

    refactor(ReservationActivity): onCreate 메서드 분리

--- app/src/main/java/woowacourse/movie/activity/ReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/ReservationActivity.kt
@@ -28,10 +28,10 @@ class ReservationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReservationBinding
     private lateinit var reservationDay: LocalDate
     private lateinit var runningDateTime: LocalTime
-    private var runningTimePosition: Int = 0
-    private var datePosition: Int = 0
-    private var isSpinnerInitialized = false
+    private var runningTimePosition: Int = DEFAULT_POSITION
+    private var datePosition: Int = DEFAULT_POSITION
     private var memberCount = MEMBER_COUNT_DEFAULT
+    private var isSpinnerInitialized = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +45,80 @@ class ReservationActivity : AppCompatActivity() {
         }
 
         val movie = intent.getObjectFromIntent<Movie>(MainActivity.MOVIE_KEY)
+        setBindingText(movie)
+        setScreeningTimes(movie)
+        setEventListener(movie)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(MEMBER_COUNT_KEY, memberCount)
+        outState.putInt(RUNNING_TIME_KEY, runningTimePosition)
+        outState.putInt(RESERVATION_DAY_KEY, datePosition)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        memberCount = savedInstanceState.getInt(MEMBER_COUNT_KEY)
         binding.count.text = memberCount.toString()
+        binding.timePickerActions.setSelection(savedInstanceState.getInt(RUNNING_TIME_KEY))
+        binding.datePickerActions.setSelection(savedInstanceState.getInt(RESERVATION_DAY_KEY))
+    }
+
+    private fun setBindingText(movie: Movie) {
+        binding.count.text = memberCount.toString()
+        binding.bookedMovieRunningDayText.text =
+            binding.bookedMovieRunningDayText.context.getString(
+                R.string.movie_screening_date,
+                movie.startDateTime,
+                movie.endDateTime,
+            )
+        binding.bookedMovieTitleText.text = movie.title
+        binding.bookedMovieRunningTimeText.text =
+            binding.bookedMovieRunningTimeText.context.getString(
+                R.string.movie_running_time,
+                movie.runningTime.inWholeMinutes,
+            )
+    }
+
+    private fun setEventListener(movie: Movie) {
+        binding.plusButton.setOnClickListener {
+            memberCount++
+            binding.count.text = memberCount.toString()
+        }
+
+        binding.minusButton.setOnClickListener {
+            if (memberCount.toString().toInt() <= MINIMUM_MEMBER_COUNT) {
+                Toast.makeText(this, getString(R.string.request_least_one_person), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            memberCount--
+            binding.count.text = memberCount.toString()
+        }
+
+        binding.commonButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.complete_dialog_title))
+                .setMessage(getString(R.string.complete_dialog_message))
+                .setPositiveButton(getString(R.string.complete_dialog_positive_button)) { _, _ ->
+                    navigateToReservationComplete(
+                        BookingStatus(
+                            movie = movie,
+                            isBooked = true,
+                            memberCount = MemberCount(memberCount),
+                            bookedTime = LocalDateTime.of(reservationDay, runningDateTime),
+                        ),
+                    )
+                }
+                .setNegativeButton(R.string.complete_dialog_negative_button) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+                .setCancelable(false)
+        }
+    }
+
+    private fun setScreeningTimes(movie: Movie) {
         binding.datePickerActions.adapter =
             ReservationDaySpinnerAdapter(
                 MovieDateTime(movie.startDateTime, movie.endDateTime).betweenDates(),
@@ -96,74 +169,11 @@ class ReservationActivity : AppCompatActivity() {
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                 }
             }
-
-        binding.plusButton.setOnClickListener {
-            memberCount++
-            binding.count.text = memberCount.toString()
-        }
-
-        binding.minusButton.setOnClickListener {
-            if (memberCount.toString().toInt() <= MINIMUM_MEMBER_COUNT) {
-                Toast.makeText(this, getString(R.string.request_least_one_person), Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            memberCount--
-            binding.count.text = memberCount.toString()
-        }
-
-        binding.commonButton.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.complete_dialog_title))
-                .setMessage(getString(R.string.complete_dialog_message))
-                .setPositiveButton(getString(R.string.complete_dialog_positive_button)) { _, _ ->
-                    navigateToReservationComplete(
-                        BookingStatus(
-                            movie = movie,
-                            isBooked = true,
-                            memberCount = MemberCount(memberCount),
-                            bookedTime = LocalDateTime.of(reservationDay, runningDateTime),
-                        ),
-                    )
-                }
-                .setNegativeButton(R.string.complete_dialog_negative_button) { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .show()
-                .setCancelable(false)
-        }
-
-        binding.bookedMovieRunningDayText.text =
-            binding.bookedMovieRunningDayText.context.getString(
-                R.string.movie_screening_date,
-                movie.startDateTime,
-                movie.endDateTime,
-            )
-        binding.bookedMovieTitleText.text = movie.title
-        binding.bookedMovieRunningTimeText.text =
-            binding.bookedMovieRunningTimeText.context.getString(
-                R.string.movie_running_time,
-                movie.runningTime.inWholeMinutes,
-            )
     }
 
     private fun navigateToReservationComplete(bookingStatus: BookingStatus) {
         val intent = newIntent<ReservationCompleteActivity>(listOf(BOOKING_STATUS_KEY to bookingStatus))
         startActivity(intent)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(MEMBER_COUNT_KEY, memberCount)
-        outState.putInt(RUNNING_TIME_KEY, runningTimePosition)
-        outState.putInt(RESERVATION_DAY_KEY, datePosition)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        memberCount = savedInstanceState.getInt(MEMBER_COUNT_KEY)
-        binding.count.text = memberCount.toString()
-        binding.timePickerActions.setSelection(savedInstanceState.getInt(RUNNING_TIME_KEY))
-        binding.datePickerActions.setSelection(savedInstanceState.getInt(RESERVATION_DAY_KEY))
     }
 
     companion object {
@@ -173,5 +183,6 @@ class ReservationActivity : AppCompatActivity() {
         const val BOOKING_STATUS_KEY = "bookingStatus"
         const val MEMBER_COUNT_DEFAULT = 1
         const val MINIMUM_MEMBER_COUNT = 1
+        const val DEFAULT_POSITION = 0
     }
 }
