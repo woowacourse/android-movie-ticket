0914dc6808b010705a2864374a66c7e6f686ac5a
박지민 <pjm5489@naver.com>
2025-04-19 18:22:34 +0900

    refactor: 티켓 가격을 생성자 주입 방식으로 변경

--- app/src/main/java/woowacourse/movie/model/policy/DefaultPricingPolicy.kt
+++ app/src/main/java/woowacourse/movie/model/policy/DefaultPricingPolicy.kt
@@ -1,9 +1,7 @@
 package woowacourse.movie.model.policy
 
-class DefaultPricingPolicy : PricingPolicy {
-    override fun calculatePrice(headCount: Int): Int = headCount * DEFAULT_PRICE
-
-    companion object {
-        private const val DEFAULT_PRICE = 13000
-    }
+class DefaultPricingPolicy(
+    private val price: Int = 13000,
+) : PricingPolicy {
+    override fun calculatePrice(headCount: Int): Int = headCount * price
 }
