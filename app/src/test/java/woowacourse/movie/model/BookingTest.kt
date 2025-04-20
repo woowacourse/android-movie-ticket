00069aefb06e790d59e58f52cd68ba0247e05946
gahyunkim <mapledt001@naver.com>
2025-04-20 18:09:00 +0900

    test: BookingResult 코드 변경으로 인한 테스트 코드 수정

--- app/src/test/java/woowacourse/movie/model/BookingTest.kt
+++ app/src/test/java/woowacourse/movie/model/BookingTest.kt
@@ -25,7 +25,7 @@ class BookingTest {
     fun `상영일자에 맞는 상영일들을 가져온다`() {
         val booking = Booking(movie, LocalDate.of(2025, 4, 20), LocalTime.of(8, 0))
         val expected =
-            listOf(
+            localDates(
                 "2025-04-20",
                 "2025-04-21",
                 "2025-04-22",
@@ -43,9 +43,9 @@ class BookingTest {
     fun `현재 시간을 기준으로 예매 가능한 시간들을 가져온다`() {
         val booking = Booking(movie, LocalDate.of(2025, 4, 10), LocalTime.of(8, 0))
         val expected =
-            listOf("09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00")
+            localTimes("09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00")
 
-        val actual = booking.screeningTimes("2025-04-10")
+        val actual = booking.screeningTimes(LocalDate.of(2025, 4, 10))
 
         Assertions.assertThat(actual).isEqualTo(expected)
     }
@@ -54,9 +54,9 @@ class BookingTest {
     fun `주말인 경우에 해당하는 시간들을 가져온다`() {
         val booking = Booking(movie, LocalDate.of(2025, 4, 20), LocalTime.of(8, 0))
         val expected =
-            listOf("10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00", "24:00")
+            localTimes("10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00", "24:00")
 
-        val actual = booking.screeningTimes("2025-04-20")
+        val actual = booking.screeningTimes(LocalDate.of(2025, 4, 20))
 
         Assertions.assertThat(actual).isEqualTo(expected)
     }
@@ -65,10 +65,29 @@ class BookingTest {
     fun `평일인 경우에 해당하는 시간들을 가져온다`() {
         val booking = Booking(movie, LocalDate.of(2025, 4, 18), LocalTime.of(8, 0))
         val expected =
-            listOf("09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00")
+            localTimes("09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00")
 
-        val actual = booking.screeningTimes("2025-04-18")
+        val actual = booking.screeningTimes(LocalDate.of(2025, 4, 18))
 
         Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    private fun localDates(vararg dates: String): List<LocalDate> {
+        return dates.map { date ->
+            val (year, month, day) = date.split("-").map { it.toInt() }
+            LocalDate.of(year, month, day)
+        }
+    }
+
+    private fun localTimes(vararg times: String): List<LocalTime> {
+        return times.map { time ->
+            val (hour, min) = time.split(":").map { it.toInt() }
+
+            if (hour == 24 && min == 0) {
+                LocalTime.MIDNIGHT
+            } else {
+                LocalTime.of(hour, min)
+            }
+        }
     }
 }
