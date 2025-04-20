0269ed883c6047cb9f136a91f711caff27746894
gahyunkim <mapledt001@naver.com>
2025-04-20 20:51:25 +0900

    feat: spinner에서 필요한 listener 및 adapter 구현

---
+++ app/src/main/java/woowacourse/movie/ui/adapter/ScreeningTimeSpinnerAdapter.kt
@@ -0,0 +1,41 @@
+package woowacourse.movie.ui.adapter
+
+import android.R
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import woowacourse.movie.util.Formatter
+import java.time.LocalTime
+
+class ScreeningTimeSpinnerAdapter(
+    context: Context,
+    items: List<LocalTime>,
+) : ArrayAdapter<LocalTime>(context, R.layout.simple_spinner_item, items) {
+    init {
+        setDropDownViewResource(R.layout.simple_spinner_item)
+    }
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup,
+    ): View {
+        val view = super.getView(position, convertView, parent) as TextView
+        val time = getItem(position)
+        view.text = Formatter.simpleTimeFormat(time!!)
+        return view
+    }
+
+    override fun getDropDownView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup,
+    ): View {
+        val view = super.getDropDownView(position, convertView, parent) as TextView
+        val time = getItem(position)
+        view.text = Formatter.simpleTimeFormat(time!!)
+        return view
+    }
+}
