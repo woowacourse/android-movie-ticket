4f5d40053d0b32940dfc6399b1bfdb476a073ed6
rosemin928 <rosemin928@naver.com>
2025-04-20 16:05:20 +0900

    refactor: 뷰 재활용 할 수 있도록 수정

--- app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/adapter/MovieListAdapter.kt
@@ -5,50 +5,57 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.BaseAdapter
 import woowacourse.movie.BookingActivity
 import woowacourse.movie.R
 import woowacourse.movie.domain.MovieInfo
 
-class MovieListAdapter(context: Context, items: MutableList<MovieInfo>) :
-    ArrayAdapter<MovieInfo>(context, 0, items) {
-    override fun getView(
-        position: Int,
-        convertView: View?,
-        parent: ViewGroup,
-    ): View {
-        val view =
-            convertView ?: LayoutInflater.from(context)
+class MovieListAdapter(private val context: Context, private val items: List<MovieInfo>) :
+    BaseAdapter() {
+    override fun getCount(): Int = items.size
+
+    override fun getItem(position: Int): MovieInfo = items[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val viewHolder: MovieViewHolder
+
+        if (convertView == null) {
+            view = LayoutInflater.from(context)
                 .inflate(R.layout.movie_list_item, parent, false)
-
-        val item = getItem(position)
-        val image = view.findViewById<ImageView>(R.id.movie_image)
-        val title = view.findViewById<TextView>(R.id.title)
-        val startDate = view.findViewById<TextView>(R.id.start_date)
-        val endDate = view.findViewById<TextView>(R.id.end_date)
-        val runningTime = view.findViewById<TextView>(R.id.running_time)
-
-        item?.let {
-            image.setImageResource(it.poster)
-            title.text = it.title
-            startDate.text = it.startDate
-            endDate.text = it.endDate
-            runningTime.text = it.runningTime
+            viewHolder = MovieViewHolder(
+                view.findViewById(R.id.movie_image),
+                view.findViewById(R.id.title),
+                view.findViewById(R.id.start_date),
+                view.findViewById(R.id.end_date),
+                view.findViewById(R.id.running_time),
+                view.findViewById(R.id.reservation_button)
+            )
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as MovieViewHolder
         }
 
-        val button = view.findViewById<Button>(R.id.reservation_button)
-        button.setOnClickListener {
-            if (item != null) {
-                val intent =
-                    Intent(context, BookingActivity::class.java).apply {
-                        putExtra("MOVIE_INFO", item)
-                    }
+        val item = getItem(position)
+
+        viewHolder.apply {
+            image.setImageResource(item.poster)
+            title.text = item.title
+            startDate.text = item.startDate
+            endDate.text = item.endDate
+            runningTime.text = item.runningTime
+
+            button.setOnClickListener {
+                val intent = Intent(context, BookingActivity::class.java).apply {
+                    putExtra("MOVIE_INFO", item)
+                }
                 context.startActivity(intent)
             }
         }
+
         return view
     }
 }
---
+++ app/src/main/java/woowacourse/movie/adapter/MovieViewHolder.kt
@@ -0,0 +1,14 @@
+package woowacourse.movie.adapter
+
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MovieViewHolder(
+    val image: ImageView,
+    val title: TextView,
+    val startDate: TextView,
+    val endDate: TextView,
+    val runningTime: TextView,
+    val button: Button
+)
