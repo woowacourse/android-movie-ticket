b7bf52015072caf791f99bc38c7b8ecb257d2095
DoableTuple <doabletuple@gmail.com>
2025-04-21 01:19:09 +0900

    refactor: 변수 타입 명시

--- app/src/androidTest/java/woowacourse/movie/fixtures/Fixtures.kt
+++ app/src/androidTest/java/woowacourse/movie/fixtures/Fixtures.kt
@@ -8,7 +8,7 @@ import woowacourse.movie.domain.Ticket
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-val fakeContext = ApplicationProvider.getApplicationContext<Context>()
+val fakeContext: Context = ApplicationProvider.getApplicationContext()
 
 val movie =
     Movie(
