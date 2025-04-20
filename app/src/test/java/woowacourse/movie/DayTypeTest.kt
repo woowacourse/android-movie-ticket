6fa56ee18676a96b9bed6db86cbbacd280bf2775
ijh1298 <imjh0417@gmail.com>
2025-04-20 15:35:03 +0900

    test : DayTypeTest 테스트 코드 수정

--- app/src/test/java/woowacourse/movie/DayTypeTest.kt
+++ app/src/test/java/woowacourse/movie/DayTypeTest.kt
@@ -7,14 +7,20 @@ import java.time.LocalDate
 
 class DayTypeTest {
     @Test
-    fun `전달받은 날짜가 평일이면 WEEKDAY를 반한한다`() {
-        val type = DayType.of(LocalDate.of(2025, 4, 16))
-        assertEquals(type, DayType.WEEKDAY)
+    fun `전달받은 날짜가 평일이면 WEEKDAY이다`() {
+        val actual = DayType.of(LocalDate.of(2025, 4, 16))
+
+        val expected = DayType.WEEKDAY
+
+        assertEquals(actual, expected)
     }
 
     @Test
-    fun `전달받은 날짜가 주말이면 WEEKEND를 반한한다`() {
-        val type = DayType.of(LocalDate.of(2025, 4, 19))
-        assertEquals(type, DayType.WEEKEND)
+    fun `전달받은 날짜가 주말이면 WEEKEND이다`() {
+        val actual = DayType.of(LocalDate.of(2025, 4, 19))
+
+        val expected = DayType.WEEKEND
+
+        assertEquals(actual, expected)
     }
 }
