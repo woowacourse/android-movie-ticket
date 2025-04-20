874501adee395c62d7f0b592d872ae0d2a2ee00d
devfeijoa <devfeijoa@gmail.com>
2025-04-20 15:31:57 +0900

    refactor: view holder 구현

--- app/src/main/java/woowacourse/movie/view/movies/MovieListAdapter.kt
+++ app/src/main/java/woowacourse/movie/view/movies/MovieListAdapter.kt
@@ -4,12 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
-import woowacourse.movie.view.extension.toDateTimeFormatter
 
 class MovieListAdapter(
     private val movies: List<Movie>,
@@ -26,64 +22,20 @@ class MovieListAdapter(
         convertView: View?,
         parent: ViewGroup?,
     ): View {
-        val view =
-            convertView ?: LayoutInflater
-                .from(parent?.context)
-                .inflate(R.layout.item_movie, parent, false)
+        val view: View
+        val viewHolder: MovieViewHolder
 
-        val movie = getItem(position)
-
-        setMoviePoster(view, movie)
-        setMovieInfo(view, movie)
-        setReservationButton(view, movie)
-        return view
-    }
-
-    private fun setMoviePoster(
-        view: View,
-        movie: Movie,
-    ) {
-        val ivPoster = view.findViewById<ImageView>(R.id.iv_poster)
-        ivPoster.setImageResource(movie.posterResId)
-    }
-
-    private fun setMovieInfo(
-        view: View,
-        movie: Movie,
-    ) {
-        val tvTitle = view.findViewById<TextView>(R.id.tv_title)
-        tvTitle.text = movie.title
-
-        val tvDate = view.findViewById<TextView>(R.id.tv_date)
-        tvDate.text =
-            movie.screeningPeriod.run {
-                view.context.getString(
-                    R.string.movie_date,
-                    MOVIE_SCREENING_PERIOD_FORMAT.toDateTimeFormatter()?.let { formatter ->
-                        movie.screeningPeriod.startDate.format(formatter)
-                    },
-                    MOVIE_SCREENING_PERIOD_FORMAT.toDateTimeFormatter()?.let { formatter ->
-                        movie.screeningPeriod.endDate.format(formatter)
-                    },
-                )
-            }
-
-        val tvRunningTime = view.findViewById<TextView>(R.id.tv_running_time)
-        tvRunningTime.text =
-            view.context.getString(R.string.running_time, movie.runningTime.toString())
-    }
-
-    private fun setReservationButton(
-        view: View,
-        movie: Movie,
-    ) {
-        val btnReservation = view.findViewById<Button>(R.id.btn_reservation)
-        btnReservation.setOnClickListener {
-            eventListener.onClick(movie)
+        if (convertView == null) {
+            view =
+                LayoutInflater
+                    .from(parent?.context)
+                    .inflate(R.layout.item_movie, parent, false)
+            viewHolder = MovieViewHolder(view).apply { bind(getItem(position), eventListener) }
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            (convertView.tag as MovieViewHolder).bind(getItem(position), eventListener)
         }
-    }
-
-    companion object {
-        private const val MOVIE_SCREENING_PERIOD_FORMAT = "yyyy.M.d"
+        return view
     }
 }
---
+++ app/src/main/java/woowacourse/movie/view/movies/MovieViewHolder.kt
@@ -0,0 +1,47 @@
+package woowacourse.movie.view.movies
+
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import woowacourse.movie.R
+import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.view.extension.toDateTimeFormatter
+
+class MovieViewHolder(
+    val view: View,
+) {
+    private val ivPoster = view.findViewById<ImageView>(R.id.iv_poster)
+    private val tvTitle = view.findViewById<TextView>(R.id.tv_title)
+    private val tvDate = view.findViewById<TextView>(R.id.tv_date)
+    private val tvRunningTime = view.findViewById<TextView>(R.id.tv_running_time)
+    private val btnReservation = view.findViewById<Button>(R.id.btn_reservation)
+
+    fun bind(
+        movie: Movie,
+        eventListener: OnMovieEventListener,
+    ) {
+        ivPoster.setImageResource(movie.posterResId)
+        tvTitle.text = movie.title
+        tvDate.text =
+            movie.screeningPeriod.run {
+                view.context.getString(
+                    R.string.movie_date,
+                    MOVIE_SCREENING_PERIOD_FORMAT.toDateTimeFormatter()?.let { formatter ->
+                        movie.screeningPeriod.startDate.format(formatter)
+                    },
+                    MOVIE_SCREENING_PERIOD_FORMAT.toDateTimeFormatter()?.let { formatter ->
+                        movie.screeningPeriod.endDate.format(formatter)
+                    },
+                )
+            }
+        tvRunningTime.text = view.context.getString(R.string.running_time, movie.runningTime.toString())
+        btnReservation.setOnClickListener {
+            eventListener.onClick(movie)
+        }
+    }
+
+    companion object {
+        private const val MOVIE_SCREENING_PERIOD_FORMAT = "yyyy.M.d"
+    }
+}
