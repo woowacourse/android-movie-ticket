70919f71ccdef9e5015e8693628649d7cd75619b
ijh1298 <imjh0417@gmail.com>
2025-04-20 15:50:13 +0900

    test : HeadcountTest 테스트 코드 추가

--- app/src/test/java/woowacourse/movie/domain/model/HeadcountTest.kt
+++ app/src/test/java/woowacourse/movie/domain/model/HeadcountTest.kt
@@ -1,17 +1,54 @@
 package woowacourse.movie.domain.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class HeadcountTest {
     @Test
-    fun `인원 수와 티켓 타입에 맞는 `() {
+    fun `인원 수는 증가할 수 있다`() {
+        val headcount = Headcount(1)
+        headcount.increase()
+
+        val actual = headcount.count
+
+        val expected = 2
+
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun increase() {
+    fun `인원 수는 최대 인원보다 증가하지 않는다`() {
+        val headcount = Headcount(100)
+        headcount.increase()
+
+        val actual = headcount.count
+
+        val expected = 100
+
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun decrease() {
+    fun `인원 수는 감소할 수 있다`() {
+        val headcount = Headcount(2)
+        headcount.decrease()
+
+        val actual = headcount.count
+
+        val expected = 1
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `인원 수는 최소 인원보다 감소하지 않는다`() {
+        val headcount = Headcount(1)
+        headcount.decrease()
+
+        val actual = headcount.count
+
+        val expected = 1
+
+        assertThat(actual).isEqualTo(expected)
     }
 }
