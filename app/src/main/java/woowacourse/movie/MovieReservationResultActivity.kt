2411cc22d331404db48a0cc95f0a3c43e4e68865
DoableTuple <doabletuple@gmail.com>
2025-04-21 01:06:55 +0900

    refactor: 뷰 초기화 로직 분리

--- app/src/main/java/woowacourse/movie/MovieReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieReservationResultActivity.kt
@@ -14,18 +14,7 @@ class MovieReservationResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeView()
-
-        val ticket: Ticket = intent.extras.getParcelableCompat<Ticket>(MovieReservationActivity.KEY_TICKET)
-
-        val title = findViewById<TextView>(R.id.movie_title)
-        val showtime = findViewById<TextView>(R.id.showtime)
-        val ticketCount = findViewById<TextView>(R.id.ticket_count)
-        val totalPrice = findViewById<TextView>(R.id.total_price)
-
-        title.text = ticket.movie.title
-        showtime.text = ticket.showtime.format(DateTimeFormatter.ofPattern(getString(R.string.date_format_pattern)))
-        ticketCount.text = getString(R.string.ticket_count_template).format(ticket.count)
-        totalPrice.text = getString(R.string.ticket_price_template).format(ticket.totalPrice())
+        initializeReservationInfo()
     }
 
     private fun initializeView() {
@@ -36,5 +25,18 @@ class MovieReservationResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initializeReservationInfo() {
+        val title = findViewById<TextView>(R.id.movie_title)
+        val showtime = findViewById<TextView>(R.id.showtime)
+        val ticketCount = findViewById<TextView>(R.id.ticket_count)
+        val totalPrice = findViewById<TextView>(R.id.total_price)
+
+        val ticket: Ticket = intent.extras.getParcelableCompat<Ticket>(MovieReservationActivity.KEY_TICKET)
+        title.text = ticket.movie.title
+        showtime.text = ticket.showtime.format(DateTimeFormatter.ofPattern(getString(R.string.date_format_pattern)))
+        ticketCount.text = getString(R.string.ticket_count_template).format(ticket.count)
+        totalPrice.text = getString(R.string.ticket_price_template).format(ticket.totalPrice())
     }
 }
