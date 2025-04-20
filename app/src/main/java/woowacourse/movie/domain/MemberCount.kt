248015151a53c21141bb114fb821ad5bbc8395f8
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-04-20 21:07:38 +0900

    feat(MemberCount): 총 티켓 가격 계산하는 기능 구현

--- app/src/main/java/woowacourse/movie/domain/MemberCount.kt
+++ app/src/main/java/woowacourse/movie/domain/MemberCount.kt
@@ -12,7 +12,12 @@ value class MemberCount(
         require(value >= MINIMUM_NUMBER_OF_PEOPLE) {"영화 예매 수는 1명이상이어야합니다."}
     }
 
+    fun calculateTicketPrices(): Int {
+        return value * TICKET_PRICE
+    }
+
     companion object {
         const val MINIMUM_NUMBER_OF_PEOPLE = 1
+        private const val TICKET_PRICE = 13_000
     }
 }
