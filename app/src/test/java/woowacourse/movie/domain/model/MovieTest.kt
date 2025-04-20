f3d71775c1ae0a6558fd2ef4bc71fa982840b832
moondev03 <moondev03@gmail.com>
2025-04-20 18:48:29 +0900

    test: Movie 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/movie/domain/model/MovieTest.kt
@@ -0,0 +1,30 @@
+package woowacourse.movie.domain.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class MovieTest {
+    @Test
+    fun `Movie 객체 생성 테스트`() {
+        val screeningPeriod =
+            ScreeningPeriod(
+                LocalDate.of(2025, 4, 1),
+                LocalDate.of(2025, 4, 25),
+            )
+        val runningTime = RunningTime(125)
+
+        val movie =
+            Movie(
+                title = "해리 포터와 마법사의 돌",
+                poster = "https://example.com/poster.jpg",
+                screeningPeriod = screeningPeriod,
+                runningTime = runningTime,
+            )
+
+        assertThat(movie.title).isEqualTo("해리 포터와 마법사의 돌")
+        assertThat(movie.poster).isEqualTo("https://example.com/poster.jpg")
+        assertThat(movie.screeningPeriod).isEqualTo(screeningPeriod)
+        assertThat(movie.runningTime).isEqualTo(runningTime)
+    }
+}
