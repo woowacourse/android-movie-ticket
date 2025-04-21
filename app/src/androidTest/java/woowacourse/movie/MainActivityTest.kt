2094f1820684a252ba1b13772fd41d7cf8657bbb
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-21 19:28:46 +0900

    test: listview에 여러 값일 경우 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MainActivityTest.kt
@@ -1,10 +1,13 @@
 package woowacourse.movie
 
-import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.CoreMatchers.anything
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,33 +15,38 @@ class MainActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    private lateinit var item: DataInteraction
+
+    @Before
+    fun setUp() {
+        item =
+            onData(anything())
+                .inAdapterView(withId(R.id.lv_movies))
+                .atPosition(20)
+    }
+
     @Test
     fun shouldDisplayTitleText() {
-        onView(withId(R.id.tv_title))
-            .check(matches(isDisplayed()))
+        item.onChildView(withId(R.id.tv_title)).check(matches(isDisplayed()))
     }
 
     @Test
     fun shouldDisplayScreeningDateText() {
-        onView(withId(R.id.tv_screening_date))
-            .check(matches(isDisplayed()))
+        item.onChildView(withId(R.id.tv_title)).check(matches(isDisplayed()))
     }
 
     @Test
     fun shouldDisplayRunningTimeText() {
-        onView(withId(R.id.tv_running_time))
-            .check(matches(isDisplayed()))
+        item.onChildView(withId(R.id.tv_running_time)).check(matches(isDisplayed()))
     }
 
     @Test
     fun shouldDisplayPosterImage() {
-        onView(withId(R.id.iv_poster))
-            .check(matches(isDisplayed()))
+        item.onChildView(withId(R.id.iv_poster)).check(matches(isDisplayed()))
     }
 
     @Test
     fun shouldDisplayReserveButton() {
-        onView(withId(R.id.btn_reserve))
-            .check(matches(isDisplayed()))
+        item.onChildView(withId(R.id.btn_reserve)).check(matches(isDisplayed()))
     }
 }
