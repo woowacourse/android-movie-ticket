c78ca3cfef6dd6627793a0d29d071745acc09ead
oungsi2000 <oungsi1000@gmail.com>
2025-04-21 17:23:32 +0900

    refactor(MovieDateTime): BuildList 사용

--- app/src/main/java/woowacourse/movie/domain/MovieDateTime.kt
+++ app/src/main/java/woowacourse/movie/domain/MovieDateTime.kt
@@ -10,18 +10,17 @@ class MovieDateTime(
     private val startDateTime: LocalDateTime,
     private val endDateTime: LocalDateTime,
 ) : Parcelable {
-    fun betweenDates(targetDate: LocalDate = LocalDate.now()): List<LocalDate> {
-        val dates = mutableListOf<LocalDate>()
-        val startDate = startDateTime.toLocalDate()
-        val endDate = endDateTime.toLocalDate()
+    fun betweenDates(targetDate: LocalDate = LocalDate.now()): List<LocalDate> =
+        buildList {
+            val startDate = startDateTime.toLocalDate()
+            val endDate = endDateTime.toLocalDate()
 
-        var standardDate = targetDate
-        if (standardDate.isBefore(startDate)) standardDate = startDate
-        if (standardDate.isAfter(endDate)) throw IllegalStateException("이미 상영이 종료된 영화입니다.")
-        while (!standardDate.isAfter(endDate)) {
-            dates.add(standardDate)
-            standardDate = standardDate.plusDays(1)
+            var standardDate = targetDate
+            if (standardDate.isBefore(startDate)) standardDate = startDate
+            if (standardDate.isAfter(endDate)) throw IllegalStateException("이미 상영이 종료된 영화입니다.")
+            while (!standardDate.isAfter(endDate)) {
+                add(standardDate)
+                standardDate = standardDate.plusDays(1)
+            }
         }
-        return dates.toList()
-    }
 }
