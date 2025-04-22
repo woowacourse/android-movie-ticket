f2223108db47cb9639804685901371a4085e00ef
지원 <cheerfulboys@naver.com>
2025-04-22 14:47:06 +0900

    refactor: Movie의 image 타입 Int를 리소스 ID로 표현하기

--- README.md
+++ README.md
@@ -74,6 +74,6 @@
 - [ ] createTicket() 분리
 - [ ] onClick 확장성 고려하기
 - [ ] DetailBookingActivity 다이어트 (익명 객체를 외부로 분리, 파일 분리)
-- [ ] Movie의 image 타입 Int를 리소스 ID로 표현하기
+- [x] Movie의 image 타입 Int를 리소스 ID로 표현하기
 - [ ] 스피너 테스트 코드 추가
 - [ ] 화면 회전 테스트 코드 추가
--- app/src/main/java/woowacourse/movie/domain/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/Movie.kt
@@ -1,11 +1,12 @@
 package woowacourse.movie.domain
 
 import android.os.Parcelable
+import androidx.annotation.DrawableRes
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Movie(
-    val image: Int,
+    @DrawableRes val image: Int,
     val title: String,
     val date: Date,
     val time: Int,
