e2db4f4b1419384434795908bf6a76f660ec589b
m6z1 <sonmyungj1zz@gmail.com>
2025-04-22 16:24:22 +0900

    test: MovieTicket 수정에 따른 테스트 수정

--- app/src/test/java/woowacourse/movie/MovieTicketTest.kt
+++ app/src/test/java/woowacourse/movie/MovieTicketTest.kt
@@ -2,13 +2,22 @@ package woowacourse.movie
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import woowacourse.movie.model.MovieDate
 import woowacourse.movie.model.MovieTicket
+import woowacourse.movie.model.MovieTime
+import java.time.LocalDate
 
 class MovieTicketTest {
     @Test
     fun `티켓 개수를 받으면 총 결제 금액을 반환한다`() {
         // given
-        val ticket = MovieTicket("라라랜드", "2025.04.17 12:53", 4)
+        val ticket =
+            MovieTicket(
+                title = "라라랜드",
+                movieDate = MovieDate(LocalDate.of(2025, 4, 22), LocalDate.of(2025, 4, 26)),
+                movieTime = MovieTime(),
+                count = 4,
+            )
 
         // when
         val price = ticket.price()
