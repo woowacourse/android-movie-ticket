08259a94862add13c877a9a75452c273468e9895
박지민 <pjm5489@naver.com>
2025-04-22 17:37:10 +0900

    style: 주석 추가

--- app/src/main/java/woowacourse/movie/MovieApplication.kt
+++ app/src/main/java/woowacourse/movie/MovieApplication.kt
@@ -6,6 +6,7 @@ import java.util.TimeZone
 class MovieApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // 대한민국 시간으로 조정
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     }
 }
