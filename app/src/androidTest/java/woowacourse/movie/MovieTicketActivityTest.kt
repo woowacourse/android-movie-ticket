e22b9632b669beebbc59ffb390608f525bce9216
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-04-22 15:06:54 +0900

    test: MovieTicketActivity 화면 테스트

--- app/src/androidTest/java/woowacourse/movie/MovieTicketActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/MovieTicketActivityTest.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -10,46 +11,52 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import woowacourse.movie.MovieFixture.HARRY_POTTER_DATE
+import woowacourse.movie.MovieFixture.HARRY_POTTER_RUNNING_TIME
+import woowacourse.movie.MovieFixture.HARRY_POTTER_TITLE
 
 @RunWith(AndroidJUnit4::class)
 class MovieTicketActivityTest {
     @get:Rule
-    val activityRule = ActivityScenarioRule(MovieTicketActivity::class.java)
+    val movieTicketRule = ActivityScenarioRule(MovieTicketActivity::class.java)
 
     @Test
-    fun 영화_목록이_표시된다() {
-        onView(withId(R.id.movies))
-            .check(matches(isDisplayed()))
-        onView(withId(R.id.movie_item_root))
-            .check(matches(isDisplayed()))
+    fun `초기화면에는_영화_목록이_있다`() {
+        onView(withId(R.id.movies)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun 초기_화면에_리스트_아이템의_요소들이_로드된다() {
-        onView(withId(R.id.movie_title))
-            .check(
-                matches(
-                    withText(
-                        MovieFixture.HARRY_POTTER_TITLE
-                    )
-                )
-            )
-        onView(withId(R.id.movie_date))
-            .check(
-                matches(
-                    withText(
-                        MovieFixture.HARRY_POTTER_DATE
-                    )
-                )
-            )
+    fun 영화_목록_아이템에는_영화_타이틀이_해리포터와_마법사의_돌이다() {
+        onView(withId(R.id.movie_title)).check(matches(withText(HARRY_POTTER_TITLE)))
+    }
 
-        onView(withId(R.id.movie_running_time))
-            .check(
-                matches(
-                    withText(
-                        MovieFixture.HARRY_POTTER_RUNNING_TIME
-                    )
-                )
-            )
+    @Test
+    fun 영화_목록_아이템에는_영화_상영일이_있다() {
+        onView(withId(R.id.movie_date)).check(matches(withText(HARRY_POTTER_DATE)))
+    }
+
+    @Test
+    fun 영화_목록_아이템에는_영화_러닝타임이_있다() {
+        onView(withId(R.id.movie_running_time)).check(matches(withText(HARRY_POTTER_RUNNING_TIME)))
+    }
+
+    @Test
+    fun 영화_목록_아이템에는_영화_포스터가_있다() {
+        onView(withId(R.id.movie_poster)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 영화_목록_아이템에는_지금예매_버튼이_있다() {
+        onView(withId(R.id.btn_book)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 영화_목록_아이템에_버튼이름이_지금예매이다() {
+        onView(withId(R.id.btn_book)).check(matches(withText("지금 예매")))
+    }
+
+    @Test
+    fun 화면을_스크롤할_수_있다() {
+        onView(withId(R.id.movies)).perform(swipeUp())
     }
 }
