3db7cbcf3f4a454e55aa4eefcff771ab5ece6a48
이지윤 <02leejiyun@naver.com>
2025-04-22 22:20:01 +0900

    refactor: 영화 포스터 resource id에 DrawableRes 어노테이션 적용

--- app/src/main/java/woowacourse/movie/domain/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/Movie.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.domain
 
 import android.os.Parcelable
+import androidx.annotation.DrawableRes
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
@@ -10,7 +11,7 @@ data class Movie(
     val startDate: LocalDate,
     val endDate: LocalDate,
     val runningTime: Int,
-    val poster: Int,
+    @DrawableRes val poster: Int,
 ) : Parcelable {
     init {
         require(startDate <= endDate)
