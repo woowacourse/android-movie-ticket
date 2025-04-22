e340349e9633cdd59cb1d84d62ec13616c4f41f1
leeyerin0210 <lyl8661@gmail.com>
2025-04-22 14:57:36 +0900

    refactor : 블랙박스 테스트로 변경

--- app/src/androidTest/java/woowacourse/movie/BookingResultActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/BookingResultActivityTest.kt
@@ -5,7 +5,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
 import org.junit.Test
@@ -29,40 +29,40 @@ class BookingResultActivityTest {
     @Test
     fun `지정된_영화_제목을_표시한다`() {
         ActivityScenario.launch<BookingActivity>(intent).use {
-            onView(withId(R.id.title))
-                .check(matches(withText("해리 포터와 마법사의 돌")))
+            onView(withText("해리 포터와 마법사의 돌"))
+                .check(matches(isDisplayed()))
         }
     }
 
     @Test
     fun `지정된_날짜를_표시한다`() {
         ActivityScenario.launch<BookingActivity>(intent).use {
-            onView(withId(R.id.date))
-                .check(matches(withText("2025.4.17")))
+            onView(withText("2025.4.17"))
+                .check(matches(isDisplayed()))
         }
     }
 
     @Test
     fun `지정된_시간을_표시한다`() {
         ActivityScenario.launch<BookingActivity>(intent).use {
-            onView(withId(R.id.time))
-                .check(matches(withText("10:00")))
+            onView(withText("10:00"))
+                .check(matches(isDisplayed()))
         }
     }
 
     @Test
     fun `발권된_티켓수에_따른_인원수를_표시한다`() {
         ActivityScenario.launch<BookingActivity>(intent).use {
-            onView(withId(R.id.count))
-                .check(matches(withText("일반 2명")))
+            onView(withText("일반 2명"))
+                .check(matches(isDisplayed()))
         }
     }
 
     @Test
     fun `발권된_티켓수에_따른_금액을_표시한다`() {
         ActivityScenario.launch<BookingActivity>(intent).use {
-            onView(withId(R.id.money))
-                .check(matches(withText("26000원 (현장 결제)")))
+            onView(withText("26000원 (현장 결제)"))
+                .check(matches(isDisplayed()))
         }
     }
 }
