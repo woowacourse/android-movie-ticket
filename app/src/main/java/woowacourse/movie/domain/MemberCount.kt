964c5c1df60dfd665eab0dafc3c80bd50c9deb38
oungsi2000 <oungsi1000@gmail.com>
2025-04-22 17:32:00 +0900

    refactor: domain 안드로이드 의존성 분리2

--- app/src/main/java/woowacourse/movie/domain/MemberCount.kt
+++ app/src/main/java/woowacourse/movie/domain/MemberCount.kt
@@ -1,13 +1,9 @@
 package woowacourse.movie.domain
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 @JvmInline
 value class MemberCount(
     val value: Int,
-) : Parcelable {
+) {
     init {
         require(value >= 1) { "영화 예매 수는 1명이상이어야합니다." }
     }
--- app/src/main/java/woowacourse/movie/dto/ReservationDto.kt
+++ app/src/main/java/woowacourse/movie/dto/ReservationDto.kt
@@ -3,14 +3,13 @@ package woowacourse.movie.dto
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import woowacourse.movie.domain.BookingStatus
-import woowacourse.movie.domain.MemberCount
 import java.time.LocalDateTime
 
 @Parcelize
 data class ReservationDto(
     val movie: MovieDto,
     val isBooked: Boolean,
-    val memberCount: MemberCount,
+    val memberCount: Int,
     val bookedTime: LocalDateTime,
     val totalPrice: Int,
 ) : Parcelable {
@@ -19,7 +18,7 @@ data class ReservationDto(
             return ReservationDto(
                 movie = MovieDto.fromMovie(bookingStatus.movie),
                 isBooked = bookingStatus.isBooked,
-                memberCount = bookingStatus.memberCount,
+                memberCount = bookingStatus.memberCount.value,
                 bookedTime = bookingStatus.bookedTime,
                 totalPrice = bookingStatus.calculateTicketPrices(),
             )
