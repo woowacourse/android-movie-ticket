52401cd754afa92caf3bffc6e0be6a0cab3208a4
m6z1 <sonmyungj1zz@gmail.com>
2025-04-24 01:06:07 +0900

    test: 변경된 모델에 따른 테스트 수정

--- app/src/test/java/woowacourse/movie/MovieTicketTest.kt
+++ app/src/test/java/woowacourse/movie/MovieTicketTest.kt
@@ -2,7 +2,6 @@ package woowacourse.movie
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import woowacourse.movie.model.MovieDate
 import woowacourse.movie.model.MovieTicket
 import woowacourse.movie.model.MovieTime
 import java.time.LocalDate
@@ -14,7 +13,7 @@ class MovieTicketTest {
         val ticket =
             MovieTicket(
                 title = "라라랜드",
-                movieDate = MovieDate(LocalDate.of(2025, 4, 22), LocalDate.of(2025, 4, 26)),
+                movieDate = LocalDate.of(2025, 4, 22),
                 movieTime = MovieTime(),
                 count = 4,
             )
