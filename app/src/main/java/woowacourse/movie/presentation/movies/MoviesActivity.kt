82548fcb6ee93d5275485f7efcc535ce544962a0
cucumber99 <dlsguq1@gmail.com>
2025-04-24 17:27:57 +0900

    feat: MoviesActivity MVP 패턴 적용

--- app/src/main/java/woowacourse/movie/presentation/movies/MoviesActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/movies/MoviesActivity.kt
@@ -4,27 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import woowacourse.movie.R
+import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.domain.model.ScreeningMovies
+import woowacourse.movie.presentation.booking.BookingActivity
 import woowacourse.movie.ui.BaseActivity
 import woowacourse.movie.ui.adapter.MovieAdapter
-import woowacourse.movie.presentation.booking.BookingActivity
 import woowacourse.movie.ui.constant.IntentKeys
 
-class MoviesActivity : BaseActivity() {
+class MoviesActivity : BaseActivity(), MoviesContract.View {
     override val layoutRes: Int
         get() = R.layout.activity_movies
-    private val movieAdapter: MovieAdapter by lazy {
-        MovieAdapter(this, movies) { movie ->
-            val intent = Intent(this, BookingActivity::class.java).apply {
-                putExtra(IntentKeys.MOVIE, movie)
-            }
-            startActivity(intent)
-        }
-    }
+
+    private lateinit var moviesPresenter: MoviesPresenter
+    private val moviesView: ListView by lazy { findViewById(R.id.listview_movies) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupScreen(layoutRes)
-        val listView = findViewById<ListView>(R.id.listview_movies)
-        listView.adapter = movieAdapter
+        moviesPresenter = MoviesPresenter(this, ScreeningMovies())
+        moviesPresenter.onViewCreated()
+    }
+
+    override fun showMovies(movies: List<Movie>) {
+        moviesView.adapter = MovieAdapter(this, movies) { movie ->
+            moviesPresenter.onMovieClicked(movie)
+        }
+    }
+
+    override fun navigateToBooking(movie: Movie) {
+        val intent = Intent(this, BookingActivity::class.java).apply {
+            putExtra(IntentKeys.MOVIE, movie)
+        }
+        startActivity(intent)
     }
 }
