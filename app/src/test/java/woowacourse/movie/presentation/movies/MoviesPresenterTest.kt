1c93f705f51f759e04dd78219f703dca98b3fe1a
cucumber99 <dlsguq1@gmail.com>
2025-04-24 17:28:47 +0900

    test: MoviesPresenter 테스트
    - 영화 목록 출력을 테스트합니다
    - 버튼 클릭을 테스트합니다

---
+++ app/src/test/java/woowacourse/movie/presentation/movies/MoviesPresenterTest.kt
@@ -0,0 +1,57 @@
+package woowacourse.movie.presentation.movies
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.domain.model.ScreeningMovies
+import java.time.LocalDate
+
+class MoviesPresenterTest {
+    private lateinit var view: MoviesContract.View
+    private lateinit var screeningMovies: ScreeningMovies
+    private lateinit var presenter: MoviesContract.Presenter
+
+    private val testMovies = listOf(
+        Movie(
+            "test",
+            LocalDate.of(2025, 4, 24),
+            LocalDate.of(2025, 4, 30),
+            100
+        )
+    )
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk(relaxed = true)
+        screeningMovies = mockk()
+        presenter = MoviesPresenter(view, screeningMovies)
+    }
+
+    @Test
+    fun `영화 목록을 가져와서 화면에 출력한다`() {
+        // Given
+        every { screeningMovies.getData() } returns testMovies
+
+        // When
+        presenter.onViewCreated()
+
+        // Then
+        verify { screeningMovies.getData() }
+        verify { view.showMovies(testMovies) }
+    }
+
+    @Test
+    fun `버튼을 누르면 예매 화면으로 이동한다`() {
+        // Given
+        val movie = testMovies[0]
+
+        // When
+        presenter.onMovieClicked(movie)
+
+        // Then
+        verify { view.navigateToBooking(movie) }
+    }
+}
