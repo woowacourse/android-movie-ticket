e127a5f47fa205b9a14e8bb81934fdc108764671
Ham BeomJoon <hbj0802@naver.com>
2025-04-24 22:08:04 +0900

    test: MoviePresenter test

---
+++ app/src/test/java/woowacourse/movie/presenter/MoviePresenterTest.kt
@@ -0,0 +1,34 @@
+package woowacourse.movie.presenter
+
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.presenter.movie.MoviePresenter
+import woowacourse.movie.view.movie.MovieContract
+
+class MoviePresenterTest {
+    private lateinit var presenter: MovieContract.Presenter
+    private lateinit var view: MovieContract.View
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk()
+        presenter = MoviePresenter(view)
+    }
+
+    @Test
+    fun `영화 목록을 불러온다`() {
+        // given
+        every { view.updateView(any()) } just Runs
+
+        // when: 영화 목록을 조회하면
+        presenter.fetchMovies()
+
+        // then: 뷰에 반영된다.
+        verify { view.updateView(any()) }
+    }
+}
