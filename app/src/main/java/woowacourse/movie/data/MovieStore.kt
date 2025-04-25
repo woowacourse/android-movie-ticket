b8404c5ef41dc39d46f2d02625bdbe87689897eb
chanho0908 <uni070@naver.com>
2025-04-25 13:11:48 +0900

    feat : 영화 정보를 가져오는 데이터 소스 추가

---
+++ app/src/main/java/woowacourse/movie/data/MovieStore.kt
@@ -0,0 +1,43 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.domain.model.booking.ScreeningDate
+import woowacourse.movie.domain.model.movies.Movie
+import java.time.LocalDate
+
+class MovieStore {
+    private val movieTitles =
+        listOf(
+            "해리 포터와 마법사의 돌",
+            "해리 포터와 비밀의 방",
+            "해리 포터와 아즈카반의 죄수",
+            "해리 포터와 불의 잔",
+            "해리 포터와 불사조 기사단",
+            "해리 포터와 혼혈 왕자",
+            "해리 포터와 죽음의 성물 1부",
+            "해리 포터와 죽음의 성물 2부",
+        )
+
+    private val movies: List<Movie> by lazy {
+        val screeningDate =
+            ScreeningDate(
+                LocalDate.of(2025, 5, 1),
+                LocalDate.of(2025, 5, 25),
+            )
+
+        List(10_000) { index ->
+            val titleIndex = index % movieTitles.size
+            Movie(
+                title = movieTitles[titleIndex],
+                posterResource = "harry_potter_${titleIndex + 1}",
+                releaseDate = screeningDate,
+                runningTime = 152,
+            )
+        }
+    }
+
+    fun get(index: Int): Movie = movies[index]
+
+    fun getAll(): List<Movie> = movies
+
+    fun size(): Int = movies.size
+}
