7448e4eb7a412ba373a90fee0b207924ff39cd42
gahyunkim <mapledt001@naver.com>
2025-04-25 14:14:09 +0900

    refactor: drawable 추가 및 코드 변경

--- app/src/main/java/woowacourse/movie/mapper/MovieUiMapper.kt
+++ app/src/main/java/woowacourse/movie/mapper/MovieUiMapper.kt
@@ -30,5 +30,8 @@ private fun getImageResIdFromUrl(imageUrl: String): Int {
 private val imageNameToResIdMap =
     mapOf(
         "harry_potter" to R.drawable.harry_potter,
+        "harry_potter2" to R.drawable.harry_potter2,
+        "harry_potter3" to R.drawable.harry_potter3,
+        "harry_potter4" to R.drawable.harry_potter4,
         "star_is_born" to R.drawable.star_is_born,
     )
