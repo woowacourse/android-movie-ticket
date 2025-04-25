1f71e6a48d89aa5d5ff1abfa37db740e0ff1a252
rosemin928 <rosemin928@naver.com>
2025-04-25 14:31:02 +0900

    refactor: 새로운 영화 아이템 추가

--- app/src/main/java/woowacourse/movie/activity/movielist/MovieListPresenter.kt
+++ app/src/main/java/woowacourse/movie/activity/movielist/MovieListPresenter.kt
@@ -14,6 +14,24 @@ class MovieListPresenter : MovieListContract.Presenter {
                 LocalDate.of(2025, 4, 25),
                 Duration.ofMinutes(152),
             ),
+            Movie(
+                "해리 포터와 비밀의 방",
+                LocalDate.of(2025, 4, 1),
+                LocalDate.of(2025, 4, 28),
+                Duration.ofMinutes(162),
+            ),
+            Movie(
+                "해리 포터와 아즈카반의 죄수",
+                LocalDate.of(2025, 5, 1),
+                LocalDate.of(2025, 5, 31),
+                Duration.ofMinutes(141),
+            ),
+            Movie(
+                "해리 포터와 불의 잔",
+                LocalDate.of(2025, 6, 1),
+                LocalDate.of(2025, 6, 30),
+                Duration.ofMinutes(157),
+            ),
         )
 
     override fun attachView(view: MovieListContract.View) {
