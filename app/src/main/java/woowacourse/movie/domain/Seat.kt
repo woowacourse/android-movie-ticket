09a7b1abb6b67d42c639daee538be60a9b94f8fd
DongjooSEO <readyjoo24@gmail.com>
2025-04-26 01:22:50 +0900

    feat: Seat 도메인 구현

---
+++ app/src/main/java/woowacourse/movie/domain/Seat.kt
@@ -0,0 +1,7 @@
+package woowacourse.movie.domain
+
+class Seat(
+    val row: Int,
+    val col: Int,
+    val rank: SeatType,
+)
---
+++ app/src/main/java/woowacourse/movie/domain/SeatType.kt
@@ -0,0 +1,9 @@
+package woowacourse.movie.domain
+
+enum class SeatType(
+    val price: Int,
+) {
+    B(10000),
+    A(12000),
+    S(15000),
+}
