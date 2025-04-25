93b55b336b7f5ac9355e290329a74cceba9f3cac
Ham BeomJoon <hbj0802@naver.com>
2025-04-25 20:14:12 +0900

    refactor: 좌석 선택 화면 가로모드 대응

--- app/src/main/java/woowacourse/movie/view/reservation/ReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/ReservationActivity.kt
@@ -36,7 +36,7 @@ class ReservationActivity :
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_reservation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sv_reservation)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/woowacourse/movie/view/reservation/seat/SeatSelectActivity.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/seat/SeatSelectActivity.kt
@@ -31,7 +31,7 @@ class SeatSelectActivity :
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_seat_select)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sv_seat_select)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
