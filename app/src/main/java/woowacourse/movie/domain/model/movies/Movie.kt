abe488b3761f21fe3142a01ad8ece806cbe9dab1
chanho0908 <uni070@naver.com>
2025-04-25 13:12:48 +0900

    feat : 영화 객체 Id 값 추가

--- app/src/main/java/woowacourse/movie/domain/model/movies/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/model/movies/Movie.kt
@@ -1,10 +1,12 @@
 package woowacourse.movie.domain.model.movies
 
 import woowacourse.movie.domain.model.booking.ScreeningDate
+import java.util.UUID
 
 class Movie(
+    val id: Long = UUID.randomUUID().leastSignificantBits,
     val title: String,
-    val posterResource: Poster,
+    val posterResource: String,
     val releaseDate: ScreeningDate,
     val runningTime: Int,
 )
