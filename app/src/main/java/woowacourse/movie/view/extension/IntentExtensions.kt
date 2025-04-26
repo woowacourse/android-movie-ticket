3a6ebf54500e68336c468ac3e126346c1beb71d4
m6z1 <sonmyungj1zz@gmail.com>
2025-04-27 03:57:00 +0900

    refactor: serializable 확장함수 null일 경우 에러메시지 추가

--- app/src/main/java/woowacourse/movie/view/extension/IntentExtensions.kt
+++ app/src/main/java/woowacourse/movie/view/extension/IntentExtensions.kt
@@ -4,9 +4,20 @@ import android.content.Intent
 import android.os.Build
 import java.io.Serializable
 
+const val SERIALIZABLE_EXTRA_ERROR_MESSAGE = "Serializable extra '%s'를 찾을 수 없습니다."
+
 inline fun <reified T : Serializable> Intent.getSerializableExtraData(key: String): T =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        requireNotNull(getSerializableExtra(key, T::class.java))
+        requireNotNull(
+            getSerializableExtra(
+                key,
+                T::class.java,
+            ),
+        ) { SERIALIZABLE_EXTRA_ERROR_MESSAGE.format(key) }
     } else {
-        requireNotNull(getSerializableExtra(key) as? T)
+        requireNotNull(getSerializableExtra(key) as? T) {
+            SERIALIZABLE_EXTRA_ERROR_MESSAGE.format(
+                key,
+            )
+        }
     }
