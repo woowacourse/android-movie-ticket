2ad23e17691571fb5443f589fc3bdb26420e78be
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-26 22:21:22 +0900

    feat: 좌석 화면 추가

---
+++ app/src/main/java/woowacourse/movie/ui/seat/SeatActivity.kt
@@ -0,0 +1,21 @@
+package woowacourse.movie.ui.seat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.R
+
+class SeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.seat)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
