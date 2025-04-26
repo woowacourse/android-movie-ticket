f271a9d2c7f9e0bb5c14c06e3e5233d849562503
hwannow <vnfdlvgid061@naver.com>
2025-04-27 00:00:09 +0900

    refactor: 영화 목록 요소 추가

--- app/src/main/java/woowacourse/movie/DefaultDatas.kt
+++ app/src/main/java/woowacourse/movie/DefaultDatas.kt
@@ -10,54 +10,56 @@ object DefaultDatas {
     private val endDate: LocalDate = LocalDate.of(2025, 4, 28)
 
     val movies =
-        listOf(
-            Movie(
-                "해리 포터와 마법사의 돌",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_01,
-            ),
-            Movie(
-                "해리 포터와 비밀의 방",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_02,
-            ),
-            Movie(
-                "해리 포터와 아즈카반의 죄수",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_03,
-            ),
-            Movie(
-                "해리 포터와 불의 잔",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_04,
-            ),
-            Movie(
-                "해리 포터와 불사조 기사단",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_05,
-            ),
-            Movie(
-                "해리 포터와 혼혈 왕자",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_06,
-            ),
-            Movie(
-                "해리 포터와 죽음의 성물 1부",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_07,
-            ),
-            Movie(
-                "해리 포터와 죽음의 성물 2부",
-                ScreeningDate(startDate, endDate),
-                RunningTime(152),
-                R.drawable.harry_potter_08,
-            ),
-        )
+        List(1250) {
+            listOf(
+                Movie(
+                    "해리 포터와 마법사의 돌",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_01,
+                ),
+                Movie(
+                    "해리 포터와 비밀의 방",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_02,
+                ),
+                Movie(
+                    "해리 포터와 아즈카반의 죄수",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_03,
+                ),
+                Movie(
+                    "해리 포터와 불의 잔",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_04,
+                ),
+                Movie(
+                    "해리 포터와 불사조 기사단",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_05,
+                ),
+                Movie(
+                    "해리 포터와 혼혈 왕자",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_06,
+                ),
+                Movie(
+                    "해리 포터와 죽음의 성물 1부",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_07,
+                ),
+                Movie(
+                    "해리 포터와 죽음의 성물 2부",
+                    ScreeningDate(startDate, endDate),
+                    RunningTime(152),
+                    R.drawable.harry_potter_08,
+                ),
+            )
+        }.flatten()
 }
