c15cb3aa14d7dc31c4f33c3c791e69a65a06e5ea
moondev03 <moondev03@gmail.com>
2025-04-27 03:22:43 +0900

    test: 티켓 발행기 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/movie/domain/model/cinema/ticket/TicketMachineTest.kt
@@ -0,0 +1,61 @@
+package woowacourse.movie.domain.model.cinema.ticket
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import woowacourse.movie.domain.model.cinema.DiceCinemaPricePolicy
+import woowacourse.movie.domain.model.cinema.screen.Seat
+import woowacourse.movie.domain.model.cinema.screen.SeatType
+import woowacourse.movie.domain.model.reservation.ReservationCount
+import woowacourse.movie.domain.model.reservation.ReservationInfo
+import java.time.LocalDateTime
+
+class TicketMachineTest {
+    private val policy = DiceCinemaPricePolicy()
+    private val ticketMachine = TicketMachine(policy)
+    private val seats =
+        listOf(
+            Seat(1, 1, SeatType.S_CLASS),
+            Seat(1, 2, SeatType.S_CLASS),
+        )
+    private lateinit var fakeReservationInfo: ReservationInfo
+
+    @BeforeEach
+    fun setUp() {
+        fakeReservationInfo =
+            ReservationInfo(
+                "해리 포터",
+                LocalDateTime.of(2025, 4, 27, 20, 0),
+                ReservationCount(2),
+            )
+        seats.forEach { fakeReservationInfo.updateSeats(it) }
+    }
+
+    @Test
+    fun `publishTickets 호출 시 TicketBundle 이 올바르게 생성된다`() {
+        val ticketBundle = ticketMachine.publishTickets(fakeReservationInfo)
+
+        assertAll(
+            { assertThat(ticketBundle.title).isEqualTo(fakeReservationInfo.title) },
+            { assertThat(ticketBundle.size).isEqualTo(seats.size) },
+            { assertThat(ticketBundle.dateTime).isEqualTo(fakeReservationInfo.reservationDateTime) },
+            {
+                val expectedTotalPrice = seats.sumOf { policy.calculatePrice(it.type) }
+                assertThat(ticketBundle.totalPrice).isEqualTo(expectedTotalPrice)
+            },
+            { assertThat(ticketBundle.labels).containsExactlyElementsOf(seats) },
+        )
+    }
+
+    @Test
+    fun `canPublish 호출 시 좌석 수와 예약 인원 수가 일치하면 true 를 반환한다`() {
+        assertThat(ticketMachine.canPublish(fakeReservationInfo)).isTrue()
+    }
+
+    @Test
+    fun `canPublish 호출 시 좌석 수와 예약 인원 수가 다르면 false 를 반환한다`() {
+        fakeReservationInfo.updateSeats(seats.first())
+        assertThat(ticketMachine.canPublish(fakeReservationInfo)).isFalse()
+    }
+}
