9e973b4424091463167e401c09ac0c23bd0dc406
junseo511 <prime54@g.skku.edu>
2025-04-26 15:47:35 +0900

    refactor: 생성자 변수명 명시 제거

--- app/src/main/java/woowacourse/movie/domain/model/MovieDates.kt
+++ app/src/main/java/woowacourse/movie/domain/model/MovieDates.kt
@@ -5,13 +5,12 @@ value class MovieDates(
     val value: List<MovieDate>,
 ) {
     constructor(startDate: MovieDate, endDate: MovieDate) : this(
-        value =
-            generateSequence(startDate) { date ->
-                if (date.value.isBefore(endDate.value)) {
-                    MovieDate(date.value.plusDays(1))
-                } else {
-                    null
-                }
-            }.toList(),
+        generateSequence(startDate) { date ->
+            if (date.value.isBefore(endDate.value)) {
+                MovieDate(date.value.plusDays(1))
+            } else {
+                null
+            }
+        }.toList(),
     )
 }
