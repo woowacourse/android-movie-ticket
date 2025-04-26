4e88848a2adfb9d638e16dc3a2af6e5503b13fe4
지원 <cheerfulboys@naver.com>
2025-04-27 03:55:49 +0900

    feat: 좌석 선택 화면 activity_seating.xml 추가

---
+++ app/src/main/java/woowacourse/movie/seating/SeatingActivity.kt
@@ -0,0 +1,21 @@
+package woowacourse.movie.seating
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.R
+
+class SeatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seating)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.seating_root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
