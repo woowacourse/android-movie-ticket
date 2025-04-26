9b1710f8676ea32e60215e7a1e54de4e26024c91
junseo511 <prime54@g.skku.edu>
2025-04-26 17:07:55 +0900

    test: BookingSeatActivity 기타 UI 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/feature/BookingSeatActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/feature/BookingSeatActivityTest.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
@@ -15,7 +16,6 @@ import woowacourse.movie.R
 import woowacourse.movie.domain.model.BookingInfo
 import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.domain.model.MovieDate
-import woowacourse.movie.domain.model.MovieSeat
 import woowacourse.movie.domain.model.MovieSeats
 import woowacourse.movie.domain.model.MovieTime
 import woowacourse.movie.domain.model.TicketCount
@@ -43,7 +43,7 @@ class BookingSeatActivityTest {
                             ),
                         date = MovieDate(2025, 4, 1),
                         time = MovieTime(9, 0),
-                        seats = MovieSeats(setOf(MovieSeat(3, 1))),
+                        seats = MovieSeats(),
                         ticketCount = TicketCount(),
                     ).toUi(),
             )
@@ -52,7 +52,25 @@ class BookingSeatActivityTest {
     }
 
     @Test
+    fun 좌석을_선택하면_총_금액이_바뀐다() {
+        onView(withText("A1"))
+            .perform(click())
+
+        onView(withId(R.id.tv_booking_seat_movie_price))
+            .check(matches(withText("10000원")))
+    }
+
+    @Test
+    fun 좌석_없이_선택_완료_버튼을_누르면_완료_버튼이_눌리지_않는다() {
+        onView(withId(R.id.tv_booking_seat_select_complete))
+            .check(matches(isNotEnabled()))
+    }
+
+    @Test
     fun 선택_완료_버튼을_누르면_예매_확인_다이얼로그가_노출된다() {
+        onView(withText("A1"))
+            .perform(click())
+
         onView(withId(R.id.tv_booking_seat_select_complete))
             .perform(click())
 
@@ -62,6 +80,9 @@ class BookingSeatActivityTest {
 
     @Test
     fun 다이얼로그_외_영역을_터치해도_닫히지_않는다() {
+        onView(withText("A1"))
+            .perform(click())
+
         onView(withId(R.id.tv_booking_seat_select_complete))
             .perform(click())
 
