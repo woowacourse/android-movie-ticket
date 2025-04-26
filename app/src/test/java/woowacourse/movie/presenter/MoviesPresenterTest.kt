d8a4390c612904348121f95a8e009f18a230f708
junseo511 <prime54@g.skku.edu>
2025-04-27 00:12:05 +0900

    test: MoviesPresenter 테스트 구현

---
+++ app/src/test/java/woowacourse/movie/presenter/MoviesPresenterTest.kt
@@ -0,0 +1,46 @@
+package woowacourse.movie.presenter
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.domain.model.Movie.Companion.movies
+import woowacourse.movie.feature.mapper.toUi
+import woowacourse.movie.feature.movies.contract.MoviesContract
+import woowacourse.movie.feature.movies.presenter.MoviesPresenter
+
+class MoviesPresenterTest {
+    private lateinit var presenter: MoviesPresenter
+    private lateinit var view: MoviesContract.View
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk(relaxed = true)
+        presenter = MoviesPresenter(view)
+    }
+
+    @Test
+    fun `onCreateView 호출 시 영화 목록을 보여준다`() {
+        // given & when
+        presenter.onCreateView()
+
+        // then
+        verify {
+            view.showMovies(movies.map { it.toUi() })
+        }
+    }
+
+    @Test
+    fun `onMovieBookingClicked 호출 시 예약 상세 화면으로 이동한다`() {
+        // given
+        val movieUiModel = movies.first().toUi()
+
+        // when
+        presenter.onMovieBookingClicked(movieUiModel)
+
+        // then
+        verify {
+            view.navigateToBookingDetail(movieUiModel)
+        }
+    }
+}
