042c16e41e08423c9f6d1b01f054734a4d6c7e0b
moondev03 <moondev03@gmail.com>
2025-04-27 00:38:08 +0900

    test: 영화 예매 좌석 선택 화면 Presenter 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/movie/presentation/reservation/seat/ReservationSeatPresenterTest.kt
@@ -0,0 +1,110 @@
+package woowacourse.movie.presentation.reservation.seat
+
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.presentation.model.ReservationInfoUiModel
+import woowacourse.movie.presentation.model.SeatTypeUiModel
+import woowacourse.movie.presentation.model.SeatUiModel
+import woowacourse.movie.presentation.view.reservation.seat.ReservationSeatContract
+import woowacourse.movie.presentation.view.reservation.seat.ReservationSeatPresenter
+import java.time.LocalDateTime
+
+class ReservationSeatPresenterTest {
+    private lateinit var view: ReservationSeatContract.View
+    private lateinit var presenter: ReservationSeatContract.Presenter
+    private val fakeReservationInfo =
+        ReservationInfoUiModel(
+            "해리포터",
+            LocalDateTime.of(2025, 4, 1, 11, 0),
+            2,
+            listOf(),
+        )
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk()
+        presenter = ReservationSeatPresenter(view)
+    }
+
+    @Test
+    fun `예매 정보를 불러온다`() {
+        // Given: view의 setScreen 동작을 설정한다
+        every { view.setScreen(any(), any(), any()) } just Runs
+
+        // When: presenter가 데이터를 불러온다
+        presenter.fetchData(fakeReservationInfo)
+
+        // Then: view에 setScreen이 호출되어야 한다
+        verify { view.setScreen(fakeReservationInfo, any(), 0) }
+    }
+
+    @Test
+    fun `예매 정보를 불러오지 못하면 메시지를 노출한다`() {
+        // Given: view의 showMessage 동작을 설정한다
+        every { view.showMessage(any()) } just Runs
+
+        // When: presenter가 데이터를 불러온다
+        presenter.fetchData(null)
+
+        // Then: view에 showMessage가 호출되어야 한다
+        verify { view.showMessage(any()) }
+    }
+
+    @Test
+    fun `선택한 좌석 정보를 갱신한다`() {
+        val seat = SeatUiModel(0, 1, SeatTypeUiModel.B_CLASS)
+
+        // Given: 초기 데이터 로딩과 updateSeatStatus 동작을 설정한다
+        every { view.setScreen(any(), any(), any()) } just Runs
+        every { view.updateSeatStatus(any(), any(), any()) } just Runs
+        presenter.fetchData(fakeReservationInfo)
+
+        // When: 좌석을 선택하여 업데이트한다
+        presenter.updateSeat(seat)
+
+        // Then: 선택한 좌석 정보로 view의 updateSeatStatus가 호출되어야 한다
+        verify { view.updateSeatStatus(listOf(seat), any(), any()) }
+    }
+
+    @Test
+    fun `좌석 상태를 갱신하지 못한 경우 메시지를 노출한다`() {
+        val seat = SeatUiModel(0, 1, SeatTypeUiModel.B_CLASS)
+
+        // Given: view의 showMessage 동작을 설정한다
+        every { view.setScreen(any(), any(), any()) } just Runs
+        every { view.updateSeatStatus(any(), any(), any()) } just Runs
+        every { view.showMessage(any()) } just Runs
+        presenter.fetchData(fakeReservationInfo)
+        presenter.updateSeat(seat)
+        presenter.updateSeat(seat.copy(col = 2))
+
+        // When: 좌석을 선택하여 업데이트한다
+        presenter.updateSeat(seat.copy(col = 3))
+
+        // Then: view에 showMessage가 호출되어야 한다
+        verify { view.showMessage(any()) }
+    }
+
+    @Test
+    fun `티켓을 발행한다`() {
+        val seat = SeatUiModel(0, 1, SeatTypeUiModel.B_CLASS)
+
+        // Given: 초기 데이터 로딩, 좌석 업데이트, 티켓 발행 알림 동작을 설정한다
+        every { view.setScreen(any(), any(), any()) } just Runs
+        every { view.updateSeatStatus(any(), any(), any()) } just Runs
+        every { view.notifyPublishedTickets(any()) } just Runs
+        presenter.fetchData(fakeReservationInfo)
+        presenter.updateSeat(seat)
+
+        // When: 티켓을 발행한다
+        presenter.publishTickets()
+
+        // Then: view에 notifyPublishedTickets가 호출되어야 한다
+        verify { view.notifyPublishedTickets(any()) }
+    }
+}
