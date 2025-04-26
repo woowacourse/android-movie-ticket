0ca5734ff4338f39335b41f767867dd41bbd353a
ijh1298 <imjh0417@gmail.com>
2025-04-26 16:27:35 +0900

    refactor : MovieAdapter 영화 아이템, 광고 아이템 ViewType 구분 적용

--- app/src/main/java/woowacourse/movie/view/movie/MovieAdapter.kt
+++ app/src/main/java/woowacourse/movie/view/movie/MovieAdapter.kt
@@ -1,42 +1,70 @@
 package woowacourse.movie.view.movie
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.domain.model.MovieListItem
+import woowacourse.movie.domain.model.MovieListItem.AdItem
+import woowacourse.movie.domain.model.MovieListItem.MovieItem
 
 class MovieAdapter(
     val onClickBooking: (Movie) -> Unit,
-) : ListAdapter<Movie, MovieViewHolder>(
-        object : DiffUtil.ItemCallback<Movie>() {
+) : ListAdapter<MovieListItem, RecyclerView.ViewHolder>(
+        object : DiffUtil.ItemCallback<MovieListItem>() {
             override fun areItemsTheSame(
-                oldItem: Movie,
-                newItem: Movie,
-            ): Boolean = oldItem.title == newItem.title
+                oldItem: MovieListItem,
+                newItem: MovieListItem,
+            ): Boolean =
+                when {
+                    oldItem is MovieItem && newItem is MovieItem -> oldItem.movie.title == newItem.movie.title
+                    oldItem is AdItem && newItem is AdItem -> oldItem.advertisement.image == newItem.advertisement.image
+                    else -> false
+                }
 
             override fun areContentsTheSame(
-                oldItem: Movie,
-                newItem: Movie,
+                oldItem: MovieListItem,
+                newItem: MovieListItem,
             ): Boolean = oldItem == newItem
         },
     ) {
-    override fun getItem(position: Int): Movie? = currentList[position]
+    override fun getItemViewType(position: Int): Int =
+        when (getItem(position)) {
+            is MovieItem -> VIEW_TYPE_MOVIE
+            is AdItem -> VIEW_TYPE_ADVERTISEMENT
+        }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): MovieViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-        return MovieViewHolder(itemView, onClickBooking)
+    ): RecyclerView.ViewHolder {
+        val view: View
+
+        if (viewType == VIEW_TYPE_MOVIE) {
+            view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+            return MovieViewHolder(view, onClickBooking)
+        }
+        view =
+            LayoutInflater.from(parent.context).inflate(R.layout.advertisement_item, parent, false)
+        return AdvertisementViewHolder(view)
     }
 
     override fun onBindViewHolder(
-        holder: MovieViewHolder,
+        holder: RecyclerView.ViewHolder,
         position: Int,
     ) {
-        holder.bind(currentList[position])
+        when (val item = getItem(position)) {
+            is MovieItem -> (holder as MovieViewHolder).bind(item.movie)
+            is AdItem -> (holder as AdvertisementViewHolder).bind(item.advertisement)
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_MOVIE = 1
+        private const val VIEW_TYPE_ADVERTISEMENT = 2
     }
 }
