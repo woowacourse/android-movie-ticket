b9bb9be91702cccc00dcff838d1dd028624da722
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-04-26 16:58:40 +0900

    refactor: 사용하지 않는 import 삭제

--- app/src/androidTest/java/woowacourse/movie/view/MovieBookingActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/MovieBookingActivityTest.kt
@@ -12,7 +12,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.MovieFixture
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
