ca6c4d8279dd959ab5d54f22bd515657ab8c4131
junseo511 <prime54@g.skku.edu>
2025-04-27 00:15:57 +0900

    test: BookingComplete 테스트 구현

---
+++ app/src/test/java/woowacourse/movie/presenter/BookingCompletePresenterTest.kt
@@ -0,0 +1,41 @@
+package woowacourse.movie.presenter
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.feature.bookingcomplete.contract.BookingCompleteContract
+import woowacourse.movie.feature.bookingcomplete.presenter.BookingCompletePresenter
+import woowacourse.movie.feature.model.BookingInfoUiModel
+
+class BookingCompletePresenterTest {
+    private lateinit var presenter: BookingCompletePresenter
+    private lateinit var view: BookingCompleteContract.View
+
+    private lateinit var bookingInfoUiModel: BookingInfoUiModel
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk(relaxed = true)
+        presenter = BookingCompletePresenter(view)
+        bookingInfoUiModel = BookingInfoUiModel()
+    }
+
+    @Test
+    fun `onCreateView 호출 시 티켓 예약 결과를 보여준다`() {
+        // given & when
+        presenter.onCreateView(bookingInfoUiModel)
+
+        // then
+        verify { view.showBookingResult(bookingInfoUiModel) }
+    }
+
+    @Test
+    fun `onBackButtonClicked 호출 시 뒤로 이동한다`() {
+        // given & when
+        presenter.onBackButtonClicked()
+
+        // then
+        verify { view.navigateToBack() }
+    }
+}
