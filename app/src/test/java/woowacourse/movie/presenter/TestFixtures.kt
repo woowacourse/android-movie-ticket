365448edd8394b34dd5a36dc17083526797b8193
m6z1 <sonmyungj1zz@gmail.com>
2025-04-27 03:50:48 +0900

    test: SeatSelectionPresenter 테스트 추가

--- app/src/test/java/woowacourse/movie/presenter/TestFixtures.kt
+++ app/src/test/java/woowacourse/movie/presenter/TestFixtures.kt
@@ -32,6 +32,8 @@ val MOVIE_TO_RESERVE: MovieToReserve =
         TicketCount(2),
     )
 
+val SEAT_2_3 = Seat(2, 3)
+
 val MOVIE_TICKET_B1_C3: MovieTicket =
     MovieTicket(
         "라라랜드",
@@ -39,28 +41,3 @@ val MOVIE_TICKET_B1_C3: MovieTicket =
         MovieTime(),
         listOf(Seat(1, 0), Seat(2, 2)),
     )
-
-private val POSTER_IMAGES =
-    listOf(
-        R.drawable.harry_potter_azkaban,
-        R.drawable.harry_potter_final_1,
-        R.drawable.harry_potter_final_2,
-        R.drawable.harry_potter_glass,
-        R.drawable.harry_potter_knight,
-        R.drawable.harry_potter_prince,
-        R.drawable.harry_potter_rock,
-        R.drawable.harry_potter_room,
-    )
-
-val MOVIES: List<Movie> =
-    (1..1000).map { id ->
-        val poster = POSTER_IMAGES[(id - 1) % POSTER_IMAGES.size]
-        Movie(
-            id = id.toLong(),
-            title = "해리포터 $id",
-            poster = poster,
-            startDate = LocalDate.of(2025, 4, (id % 28) + 1),
-            endDate = LocalDate.of(2025, 5, (id % 28) + 1),
-            runningTime = 100 + (id % 60),
-        )
-    }
---
+++ app/src/test/java/woowacourse/movie/presenter/seatSelection/SeatSelectionPresenterTest.kt
@@ -0,0 +1,83 @@
+package woowacourse.movie.presenter.seatSelection
+
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.presenter.MOVIE_TO_RESERVE
+import woowacourse.movie.presenter.SEAT_2_3
+
+class SeatSelectionPresenterTest {
+    private lateinit var presenter: SeatSelectionPresenter
+    private lateinit var view: SeatSelectionContracts.View
+
+    @BeforeEach
+    fun setup() {
+        view = mockk()
+        presenter = SeatSelectionPresenter(view)
+    }
+
+    @Test
+    fun `좌석들을 가져오면 좌석들이 보인다`() {
+        // given:
+        every { view.showSeats(any()) } just Runs
+
+        // when:
+        presenter.loadSeats(5, 4)
+
+        // then:
+        verify { view.showSeats(any()) }
+    }
+
+    @Test
+    fun `예매할 영화 정보를 업데이트하면 영화 제목, 가격이 보이고 확인 버튼이 비활성화 된다`() {
+        // given:
+        every { view.showMovieTitle(any()) } just Runs
+        every { view.showPrice(any()) } just Runs
+        every { view.showButtonEnabled(false) } just Runs
+
+        // when:
+        presenter.updateMovieToReserve(MOVIE_TO_RESERVE)
+
+        // then:
+        every { view.showMovieTitle(any()) } just Runs
+        every { view.showPrice(any()) } just Runs
+        every { view.showButtonEnabled(false) } just Runs
+    }
+
+    @Test
+    fun `선택한 좌석을 업데이트 하면 좌석과 버튼의 활성화 여부가 업데이트되고 총 가격이 보인다`() {
+        // given:
+        every { view.updateSeatsEnabled(any()) } just Runs
+        every { view.showButtonEnabled(any()) } just Runs
+        every { view.showPrice(any()) } just Runs
+
+        // when:
+        presenter.updateSelectedSeat(SEAT_2_3)
+
+        // then:
+        verify { view.updateSeatsEnabled(any()) }
+        verify { view.showButtonEnabled(any()) }
+        verify { view.showPrice(any()) }
+    }
+
+    @Test
+    fun `예매 완료 요청이 오면 예매 완료 뷰가 보인다`() {
+        // given:
+        every { view.showMovieTitle(any()) } just Runs
+        every { view.showPrice(any()) } just Runs
+        every { view.showButtonEnabled(false) } just Runs
+        every { view.showReservationCompleteView(any()) } just Runs
+
+        presenter.updateMovieToReserve(MOVIE_TO_RESERVE)
+
+        // when:
+        presenter.onReservationCompleteRequest()
+
+        // then:
+        verify { view.showReservationCompleteView(any()) }
+    }
+}
