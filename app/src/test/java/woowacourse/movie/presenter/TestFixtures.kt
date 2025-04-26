89c3d13f5fd20d51b02241116d2bc299cbcc1867
m6z1 <sonmyungj1zz@gmail.com>
2025-04-27 03:40:10 +0900

    test: ReservationPresenter 테스트 추가

--- app/src/test/java/woowacourse/movie/presenter/TestFixtures.kt
+++ app/src/test/java/woowacourse/movie/presenter/TestFixtures.kt
@@ -10,6 +10,16 @@ import woowacourse.movie.model.ticket.MovieTicket
 import woowacourse.movie.model.ticket.TicketCount
 import java.time.LocalDate
 
+val MOVIE: Movie =
+    Movie(
+        1,
+        "라라랜드",
+        R.drawable.lalaland,
+        LocalDate.of(2025, 4, 1),
+        LocalDate.of(2025, 5, 30),
+        120,
+    )
+
 val MOVIE_TO_RESERVE: MovieToReserve =
     MovieToReserve(
         1,
