43ab9c0291625cf7984b25678570f7e0e8629370
moondev03 <moondev03@gmail.com>
2025-04-26 22:35:18 +0900

    test: 영화 예매 결과 - UI 테스트 코드 수정

--- app/src/androidTest/java/woowacourse/movie/presentation/view/reservation/result/ReservationResultActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/presentation/view/reservation/result/ReservationResultActivityTest.kt
@@ -8,23 +8,36 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
 import org.junit.Test
 import woowacourse.movie.R
-import woowacourse.movie.domain.model.reservation.ReservationCount
-import woowacourse.movie.domain.model.reservation.ReservationInfo
+import woowacourse.movie.domain.model.cinema.seat.Seat
+import woowacourse.movie.domain.model.cinema.seat.SeatType
+import woowacourse.movie.domain.model.cinema.ticket.Ticket
+import woowacourse.movie.domain.model.cinema.ticket.TicketBundle
 import woowacourse.movie.presentation.fixture.fakeContext
 import woowacourse.movie.presentation.model.toUiModel
 import java.time.LocalDateTime
 
 class ReservationResultActivityTest {
-    private val reservationInfo =
-        ReservationInfo(
-            title = "해리 포터와 마법사의 돌",
-            reservationDateTime = LocalDateTime.of(2025, 4, 15, 11, 0),
-            reservationCount = ReservationCount(2),
-        )
+    private val ticketBundle =
+        TicketBundle(
+            listOf(
+                Ticket(
+                    "해리 포터와 마법사의 돌",
+                    LocalDateTime.of(2025, 4, 15, 11, 0),
+                    Seat(0, 1, SeatType.B_CLASS),
+                    10_000,
+                ),
+                Ticket(
+                    "해리 포터와 마법사의 돌",
+                    LocalDateTime.of(2025, 4, 15, 11, 0),
+                    Seat(0, 2, SeatType.B_CLASS),
+                    10_000,
+                ),
+            ),
+        ).toUiModel()
 
     @Before
     fun setUp() {
-        val intent = ReservationResultActivity.newIntent(fakeContext, reservationInfo.toUiModel())
+        val intent = ReservationResultActivity.newIntent(fakeContext, ticketBundle)
         ActivityScenario.launch<ReservationResultActivity>(intent)
     }
 
@@ -47,14 +60,14 @@ class ReservationResultActivityTest {
     }
 
     @Test
-    fun `예매한_영화의_예매_인원_수를_보여준다`() {
+    fun `예매한_영화_예매_인원_수와_좌석_번호를_보여준다`() {
         onView(withId(R.id.tv_reservation_count_info))
-            .check(matches(withText("일반 2명")))
+            .check(matches(withText("일반 2명 | A1, A2")))
     }
 
     @Test
     fun `예매한_영화의_인원수에_맞는_총_티켓_가격을_보여준다`() {
         onView(withId(R.id.tv_reservation_total_price))
-            .check(matches(withText("26,000원 (현장 결제)")))
+            .check(matches(withText("20,000원 (현장 결제)")))
     }
 }
