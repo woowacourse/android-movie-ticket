da65fa0b8cee46cf18e84a2e3c9de8abdd411e18
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-27 22:53:41 +0900

    feat: 티켓 관리 기능 추가

--- app/src/main/java/woowacourse/movie/domain/model/Tickets.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Tickets.kt
@@ -5,14 +5,11 @@ import java.io.Serializable
 class Tickets(private val tickets: List<Ticket>) : Serializable {
     val count get() = tickets.size
 
-    init {
-        require(tickets.size >= MINIMUM_COUNT) { INVALID_COUNT }
-    }
+    fun isEmpty() = tickets.isEmpty()
+
+    fun addTicket(ticket: Ticket): Tickets = Tickets(tickets + ticket)
+
+    fun removeTicket(ticket: Ticket): Tickets = Tickets(tickets.filterNot { it.seat == ticket.seat })
 
     fun totalPrice() = tickets.sumOf { it.price }
-
-    companion object {
-        private const val INVALID_COUNT = "예약 개수는 1보다 같거나 커야 합니다."
-        private const val MINIMUM_COUNT = 1
-    }
 }
