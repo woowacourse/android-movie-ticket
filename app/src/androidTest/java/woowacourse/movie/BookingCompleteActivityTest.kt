4f24f346ba52497cf0b29f4aa517b98ffd4b6a16
DongjooSEO <readyjoo24@gmail.com>
2025-04-28 02:00:20 +0900

    test: BookingCompleteActivityTest 코드 수정

--- app/src/androidTest/java/woowacourse/movie/BookingCompleteActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/BookingCompleteActivityTest.kt
@@ -10,10 +10,7 @@ import org.junit.Before
 import org.junit.Test
 import woowacourse.movie.booking.complete.BookingCompleteActivity
 import woowacourse.movie.booking.complete.BookingCompleteActivity.Companion.newIntent
-import woowacourse.movie.domain.Movie
-import woowacourse.movie.domain.TicketInfo
-import woowacourse.movie.domain.TicketQuantity
-import java.time.LocalDate
+import woowacourse.movie.booking.complete.BookingCompleteUiModel
 
 @Suppress("ktlint:standard:function-naming")
 class BookingCompleteActivityTest {
@@ -21,34 +18,40 @@ class BookingCompleteActivityTest {
 
     @Before
     fun setup() {
-        val movie =
-            Movie(
+        val uiModel =
+            BookingCompleteUiModel(
                 title = "해리 포터와 마법사의 돌",
-                startDate = LocalDate.of(2025, 4, 1),
-                endDate = LocalDate.of(2025, 4, 25),
-                runningTime = 152,
-                poster = R.drawable.img_poster_harry_potter_and_the_philosophers_stone,
+                date = "2025-04-29",
+                time = "19:30",
+                seats = listOf("A1", "A2"),
+                ticketQuantity = 2,
+                ticketTotalPrice = 20000,
             )
 
-        val ticketInfo =
-            TicketInfo(
-                movie = movie,
-                date = "2025-04-01",
-                time = "09:00",
-                quantity = TicketQuantity(1),
-            )
         val intent =
             newIntent(
                 context = getApplicationContext(),
-                ticketInfo = ticketInfo,
+                uiModel = uiModel,
             )
 
         activityScenario = ActivityScenario.launch(intent)
     }
 
     @Test
-    fun 티켓_장수가_1이면_13000원을_출력한다() {
+    fun 티켓_정보를_출력한다() {
+        onView(withId(R.id.tv_booking_complete_movie_title))
+            .check(matches(withText("해리 포터와 마법사의 돌")))
+
+        onView(withId(R.id.tv_booking_complete_movie_date_time))
+            .check(matches(withText("2025-04-29 19:30")))
+
+        onView(withId(R.id.tv_booking_complete_ticket_count))
+            .check(matches(withText("일반 2명")))
+
+        onView(withId(R.id.booking_complete_seat_textview))
+            .check(matches(withText("A1, A2")))
+
         onView(withId(R.id.tv_booking_complete_ticket_total_price))
-            .check(matches(withText("13,000원 (현장 결제)")))
+            .check(matches(withText("20,000원 (현장 결제)")))
     }
 }
