1f31f58426cc82595715c81d5a7e5367296bb819
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-27 10:30:51 +0900

    feat: 관람 의자 추가

---
+++ app/src/main/java/woowacourse/movie/domain/model/Row.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.domain.model
+
+@JvmInline
+value class Row(val value: Int) {
+    init {
+        require(value > 0) { INVALID_VALUE_RANGE }
+    }
+
+    companion object {
+        const val INVALID_VALUE_RANGE = "열의 숫자는 0과 같거나 커야 합니다"
+    }
+}
---
+++ app/src/main/java/woowacourse/movie/domain/model/Seat.kt
@@ -0,0 +1,3 @@
+package woowacourse.movie.domain.model
+
+data class Seat(val row: Row, val column: Column, val seatRate: SeatRate)
