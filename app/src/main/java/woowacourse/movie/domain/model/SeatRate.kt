e067152922abd371498dd2f041f4f0738b335ff6
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-27 19:29:44 +0900

    feat: rate정보를 통해 좌석 등급 생성 기능 추가

--- app/src/main/java/woowacourse/movie/domain/model/SeatRate.kt
+++ app/src/main/java/woowacourse/movie/domain/model/SeatRate.kt
@@ -4,4 +4,16 @@ enum class SeatRate {
     S,
     A,
     B,
+    NONE,
+    ;
+
+    companion object {
+        fun of(rate: String): SeatRate =
+            when (rate) {
+                "s" -> S
+                "a" -> A
+                "b" -> B
+                else -> NONE
+            }
+    }
 }
