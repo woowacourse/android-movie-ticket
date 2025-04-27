c6a1b441bd8531da19fe4ee24eae4ee89d00098c
jerry8282 <ka06196@naver.com>
2025-04-28 04:30:59 +0900

    feat: 좌석을 추가 빼기의 기능 추가

--- app/src/main/java/woowacourse/movie/domain/Seats.kt
+++ app/src/main/java/woowacourse/movie/domain/Seats.kt
@@ -1,5 +1,11 @@
 package woowacourse.movie.domain
 
 class Seats(private val seats: MutableList<Seat>) {
+    val all get() = seats.toList()
+
+    fun addSeat(seat: Seat) = seats.add(seat)
+
+    fun removeSeat(seat: Seat) = seats.remove(seat)
+
     fun reservationPrice() = seats.sumOf { it.seatPrice() }
 }
