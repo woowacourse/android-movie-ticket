5b14cb3a7d99ddf50aca46230041ac0105bfe958
leeyerin0210 <lyl8661@gmail.com>
2025-04-27 21:56:59 +0900

    feat : 중간 좌석 선택 기능 추가

---
+++ app/src/main/java/woowacourse/movie/selectSeat/SelectSeatContract.kt
@@ -0,0 +1,38 @@
+package woowacourse.movie.selectSeat
+
+import woowacourse.movie.dto.Ticket
+
+interface SelectSeatContract {
+    interface View {
+        fun setSeatClicker()
+
+        fun setButton()
+
+        fun askToConfirmBook()
+
+        fun onSeatSelected(tag: String)
+
+        fun onSeatUnSelected(tag: String)
+
+        fun setTitle(ticket: Ticket)
+
+        fun setMoney(money: Int)
+
+        fun showFullSeat()
+
+        fun changeView(ticket: Ticket)
+    }
+
+    interface Presenter {
+        fun onViewCreated(ticket: Ticket)
+
+        fun onSeatClicked(
+            tag: String,
+            fullCount: Int,
+        )
+
+        fun onBookButtonClicked()
+
+        fun onYesClick()
+    }
+}
---
+++ app/src/main/java/woowacourse/movie/selectSeat/SelectSeats.kt
@@ -0,0 +1,56 @@
+package woowacourse.movie.selectSeat
+
+import woowacourse.movie.domain.Ticket
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+class SelectSeats {
+    lateinit var ticket: Ticket
+
+    fun setTicket(ticketDTO: woowacourse.movie.dto.Ticket) {
+        ticket =
+            Ticket(
+                ticketDTO.title,
+                parserDate(ticketDTO.date),
+                parserTime(ticketDTO.time),
+                mutableListOf(),
+                0,
+            )
+    }
+
+    fun isSeatSelected(seatTag: String): Boolean {
+        if (seatTag in ticket.seats) return true
+        return false
+    }
+
+    fun unSelectSeat(seatTag: String) {
+        minusMoney(seatTag)
+        ticket.seats.remove(seatTag)
+    }
+
+    fun selectSeat(seatTag: String) {
+        plusMoney(seatTag)
+        ticket.seats.add(seatTag)
+    }
+
+    fun minusMoney(seatTag: String) {
+        ticket.money -= SeatPrice.getPrice(seatTag)
+    }
+
+    fun plusMoney(seatTag: String) {
+        ticket.money += SeatPrice.getPrice(seatTag)
+    }
+
+    fun parserTime(time: String): LocalTime {
+        val formater = DateTimeFormatter.ofPattern("HH:mm")
+        return LocalTime.parse(time, formater)
+    }
+
+    fun parserDate(date: String): LocalDate {
+        val formater = DateTimeFormatter.ofPattern("yyyy.M.d")
+        return LocalDate.parse(date, formater)
+    }
+
+    fun isFullSeat(count: Int) = (ticket.seats.size == count)
+}
