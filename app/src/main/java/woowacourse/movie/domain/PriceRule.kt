5b75f1027f87562fcee2e9a1573b6119095e2a3b
oungsi2000 <oungsi1000@gmail.com>
2025-04-27 20:17:35 +0900

    refactor: 사용하지 않는 메서드 수정

--- app/src/main/java/woowacourse/movie/domain/PriceRule.kt
+++ app/src/main/java/woowacourse/movie/domain/PriceRule.kt
@@ -4,9 +4,4 @@ enum class PriceRule(val price: Int) {
     RANK_B(10_000),
     RANK_S(15_000),
     RANK_A(12_000),
-    ;
-
-    fun price(memberCount: MemberCount): Int {
-        return this.price * memberCount.value
-    }
 }
--- app/src/main/java/woowacourse/movie/dto/ReservationDto.kt
+++ app/src/main/java/woowacourse/movie/dto/ReservationDto.kt
@@ -2,7 +2,6 @@ package woowacourse.movie.dto
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import woowacourse.movie.domain.BookingStatus
 import woowacourse.movie.dto.MovieListData.MovieDto
 import java.time.LocalDateTime
 
@@ -11,14 +10,4 @@ data class ReservationDto(
     val movie: MovieDto,
     val memberCount: Int,
     val bookedTime: LocalDateTime,
-) : Parcelable {
-    companion object {
-        fun fromBookingStatus(bookingStatus: BookingStatus): ReservationDto {
-            return ReservationDto(
-                movie = MovieDto.fromMovie(bookingStatus.movie),
-                memberCount = bookingStatus.memberCount.value,
-                bookedTime = bookingStatus.bookedTime,
-            )
-        }
-    }
-}
+) : Parcelable
