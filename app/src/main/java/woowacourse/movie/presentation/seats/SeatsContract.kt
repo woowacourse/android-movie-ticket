92c2002562afb6d453500bd329654430dadfb43b
cucumber99 <dlsguq1@gmail.com>
2025-04-28 03:52:52 +0900

    feat: SeatContract 작성

---
+++ app/src/main/java/woowacourse/movie/presentation/seats/SeatsContract.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie.presentation.seats
+
+import woowacourse.movie.domain.model.movie.MovieTicket
+import woowacourse.movie.domain.model.seat.Seat
+
+interface SeatsContract {
+    interface View {
+        fun initSeats()
+        fun showMovieTitle(title: String)
+        fun showConfirmDialog()
+        fun showToast(message: String)
+        fun updateAmount(amount: Int)
+        fun updateConfirmButtonEnabled(canConfirm: Boolean)
+        fun navigateToSummary(ticket: MovieTicket)
+    }
+
+    interface Presenter {
+        fun onViewCreated()
+        fun getSeat(x: Int, y: Int): Seat
+        fun isSelectedSeat(seat: Seat): Boolean
+        fun onSeatClicked(seat: Seat)
+        fun onConfirmClicked()
+    }
+}
