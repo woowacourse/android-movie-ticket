87212f3b4c40393a9f2d34b51d9d0a8a0482ce05
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-27 19:33:01 +0900

    fix: 행 예외처리 수정

--- app/src/main/java/woowacourse/movie/domain/model/Row.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Row.kt
@@ -3,10 +3,10 @@ package woowacourse.movie.domain.model
 @JvmInline
 value class Row(val value: Int) {
     init {
-        require(value > 0) { INVALID_VALUE_RANGE }
+        require(value >= 0) { INVALID_VALUE_RANGE }
     }
 
     companion object {
-        const val INVALID_VALUE_RANGE = "열의 숫자는 0과 같거나 커야 합니다"
+        const val INVALID_VALUE_RANGE = "행의 숫자는 0과 같거나 커야 합니다"
     }
 }
