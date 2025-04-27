8c0822461911f1c19d7a6b93af68ba05a0f8a819
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-27 22:50:12 +0900

    feat: 티켓의 가격 계산 로직 변경

--- app/src/main/java/woowacourse/movie/domain/model/Ticket.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Ticket.kt
@@ -2,6 +2,20 @@ package woowacourse.movie.domain.model
 
 import java.io.Serializable
 
-data class Ticket(private val _price: Price) : Serializable {
-    val price = _price.value
+class Ticket(val seat: Seat) : Serializable {
+    val price = seatRatePrice(seat.seatRate).value
+
+    private fun seatRatePrice(seatRate: SeatRate): Price {
+        return when (seatRate) {
+            SeatRate.S -> Price(S_CLASS_PRICE)
+            SeatRate.A -> Price(A_CLASS_PRICE)
+            SeatRate.B -> Price(B_CLASS_PRICE)
+        }
+    }
+
+    companion object {
+        private const val S_CLASS_PRICE = 1_5000
+        private const val A_CLASS_PRICE = 1_2000
+        private const val B_CLASS_PRICE = 1_0000
+    }
 }
