7984968240cc4d1d39aff9d28f4af8e263490646
hwannow <vnfdlvgid061@naver.com>
2025-04-27 23:53:44 +0900

    refactor: 매직 넘버 상수화

--- app/src/main/java/woowacourse/movie/domain/RunningTime.kt
+++ app/src/main/java/woowacourse/movie/domain/RunningTime.kt
@@ -3,10 +3,11 @@ package woowacourse.movie.domain
 @JvmInline
 value class RunningTime(val time: Int) {
     init {
-        require(time > 0) { INVALID_TIME }
+        require(time > MINIMUM_TIME) { INVALID_TIME }
     }
 
     companion object {
+        private const val MINIMUM_TIME: Int = 0
         private const val INVALID_TIME = "상영시간은 양수여야 합니다"
     }
 }
--- app/src/main/java/woowacourse/movie/domain/Seat.kt
+++ app/src/main/java/woowacourse/movie/domain/Seat.kt
@@ -8,9 +8,18 @@ data class Seat(
 ) : Serializable {
     fun price(): Int {
         return when (x) {
-            0, 1 -> 10000
-            2, 3 -> 12000
-            else -> 15000
+            in B_CLASS_ROW -> B_CLASS_PRICE
+            in S_CLASS_ROW -> S_CLASS_PRICE
+            else -> A_CLASS_PRICE
         }
+    }
+
+    companion object {
+        private val B_CLASS_ROW = setOf(0, 1)
+        private val S_CLASS_ROW = setOf(2, 3)
+
+        private const val B_CLASS_PRICE = 10_000
+        private const val A_CLASS_PRICE = 12_000
+        private const val S_CLASS_PRICE = 15_000
     }
 }
