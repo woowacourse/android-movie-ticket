54e7f1119b8ed1ae5a6dc651b0177ac3e9f5ac15
jerry8282 <ka06196@naver.com>
2025-04-28 04:30:40 +0900

    test: 좌석을 추가 빼기의 테스트 추가

--- app/src/test/java/woowacourse/movie/domain/SeatsTest.kt
+++ app/src/test/java/woowacourse/movie/domain/SeatsTest.kt
@@ -10,7 +10,32 @@ class SeatsTest {
         val seats = Seats(mutableListOf(Seat(Position(0, 0)), Seat(Position(1, 1))))
         // when
         val actual = seats.reservationPrice()
-        val expected = 25000
+        val expected = 27000
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `좌석을 추가할 수 있다`() {
+        // given
+        val seats = Seats(mutableListOf())
+        seats.addSeat(Seat(Position(0, 0)))
+        seats.addSeat(Seat(Position(1, 1)))
+        // when
+        val actual = seats.all
+        val expected = listOf(Seat(Position(0, 0)), Seat(Position(1, 1)))
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `좌석을 빼낼 수 있다`() {
+        // given
+        val seats = Seats(mutableListOf(Seat(Position(0, 0)), Seat(Position(1, 1))))
+        seats.removeSeat(Seat(Position(0, 0)))
+        // when
+        val actual = seats.all
+        val expected = listOf(Seat(Position(1, 1)))
 
         assertThat(actual).isEqualTo(expected)
     }
