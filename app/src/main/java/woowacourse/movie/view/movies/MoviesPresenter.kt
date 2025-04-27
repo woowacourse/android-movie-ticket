074b8b2a210bad9003b0f18399b086e23747e2ac
devfeijoa <devfeijoa@gmail.com>
2025-04-27 22:00:37 +0900

    feat: 영화 목록(dummyMovie) 추가

--- app/src/main/java/woowacourse/movie/view/movies/MoviesPresenter.kt
+++ app/src/main/java/woowacourse/movie/view/movies/MoviesPresenter.kt
@@ -10,20 +10,48 @@ class MoviesPresenter(
     private val view: MoviesContract.View,
 ) : MoviesContract.Presenter {
     override fun loadData() {
-        val movies = listOf(dummyMovie)
-        view.showMovies(movies)
+        view.showMovies(dummyMovie)
     }
 
     companion object {
         val dummyMovie =
-            Movie(
-                R.drawable.harrypotter.toString(),
-                "해리 포터와 마법사의 돌",
-                ScreeningPeriod(
-                    LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 4, 25),
+            listOf(
+                Movie(
+                    R.drawable.harrypotter.toString(),
+                    "해리 포터와 마법사의 돌",
+                    ScreeningPeriod(
+                        LocalDate.of(2025, 4, 1),
+                        LocalDate.of(2025, 4, 25),
+                    ),
+                    RunningTime(152),
                 ),
-                RunningTime(152),
+                Movie(
+                    R.drawable.harrypotter_2.toString(),
+                    "해리 포터와 비밀의 방",
+                    ScreeningPeriod(
+                        LocalDate.of(2025, 4, 1),
+                        LocalDate.of(2025, 4, 28),
+                    ),
+                    RunningTime(162),
+                ),
+                Movie(
+                    R.drawable.harrypotter_3.toString(),
+                    "해리 포터와 아즈카반의 죄수",
+                    ScreeningPeriod(
+                        LocalDate.of(2025, 5, 1),
+                        LocalDate.of(2025, 5, 31),
+                    ),
+                    RunningTime(141),
+                ),
+                Movie(
+                    R.drawable.harrypotter_4.toString(),
+                    "해리 포터와 불의 잔",
+                    ScreeningPeriod(
+                        LocalDate.of(2025, 6, 1),
+                        LocalDate.of(2025, 6, 30),
+                    ),
+                    RunningTime(157),
+                ),
             )
     }
 }
