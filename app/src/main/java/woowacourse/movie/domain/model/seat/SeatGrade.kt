ac1dd8281d96cd1bfd6fcae2b94f1688f0ca9023
cucumber99 <dlsguq1@gmail.com>
2025-04-28 00:41:59 +0900

    feat: 좌석 등급 열거형 클래스 구현

---
+++ app/src/main/java/woowacourse/movie/domain/model/seat/SeatGrade.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.domain.model.seat
+
+enum class SeatGrade(val price: Int) {
+    B(10_000),
+    A(12_000),
+    S(15_000);
+
+    companion object {
+        fun from(colIndex: Int): SeatGrade {
+            return when(colIndex) {
+                0, 1 -> SeatGrade.B
+                2, 3 -> SeatGrade.S
+                4 -> SeatGrade.A
+                else -> throw IllegalArgumentException(GRADE_ERROR)
+            }
+        }
+
+        private const val GRADE_ERROR = "[ERROR] 해당하는 좌석 등급이 존재하지 않습니다."
+    }
+}
