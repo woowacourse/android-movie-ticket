ff2913e7cdd2025ec8533995764297dcfade7726
gahyunkim <mapledt001@naver.com>
2025-04-27 16:00:19 +0900

    test: SeatSelectionPresenterTest 테스트코드 구현

---
+++ app/src/test/java/woowacourse/movie/seat/SeatSelectionPresenterTest.kt
@@ -0,0 +1,85 @@
+package woowacourse.movie.seat
+
+import android.widget.TextView
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.booking.detail.TicketUiModel
+import woowacourse.movie.mapper.toUiModel
+import woowacourse.movie.model.HeadCount
+import woowacourse.movie.model.Seats
+import woowacourse.movie.model.Ticket
+import java.time.LocalDate
+import java.time.LocalTime
+
+class SeatSelectionPresenterTest {
+    private lateinit var presenter: SeatSelectionPresenter
+    private lateinit var mockView: SeatSelectionContract.View
+    private lateinit var mockTicket: Ticket
+    private lateinit var mockTicketUiData: TicketUiModel
+
+    @BeforeEach
+    fun setUp() {
+        mockView = mockk(relaxed = true)
+
+        mockTicket =
+            Ticket(
+                title = "해리 포터와 마법사의 돌",
+                headCount = HeadCount(3),
+                selectedDate = LocalDate.of(2028, 10, 13),
+                selectedTime = LocalTime.of(11, 0),
+                seats = Seats(emptyList()),
+            )
+
+        mockTicketUiData = mockTicket.toUiModel()
+
+        presenter = SeatSelectionPresenter(view = mockView, ticket = mockTicketUiData)
+    }
+
+    @Test
+    fun `티켓데이터가 null이면 오류 메시지를 띄우고 종료한다`() {
+        presenter = SeatSelectionPresenter(view = mockView, ticket = null)
+
+        presenter.initializeData(null)
+
+        verify { mockView.showToastErrorAndFinish("영화 정보를 불러올 수 없습니다.") }
+        confirmVerified(mockView)
+    }
+
+    @Test
+    fun `티켓을 바탕으로 View에 초기 데이터를 보여준다`() {
+        presenter = SeatSelectionPresenter(view = mockView, ticket = mockTicketUiData)
+
+        presenter.initializeData(null)
+
+        verify { mockView.showTicket(mockTicketUiData) }
+    }
+
+    @Test
+    fun `좌석을_클릭하면_좌석_상태와_버튼_활성화_상태를_갱신한다`() {
+        // given
+        val seatTextView = mockk<TextView>(relaxed = true)
+        every { seatTextView.text.toString() } returns "A1"
+
+        // when
+        presenter.initializeData(null) // 도메인 ticket 초기화
+        presenter.onSeatClicked(seatTextView)
+
+        // then
+        verify { mockView.showSeatState(seatTextView, isSelected = true) }
+        verify { mockView.setButtonEnabled(any()) }
+        verify { mockView.showTicket(match { it.seats == "A1" && it.totalPrice == "10,000" }) }
+    }
+
+    @Test
+    fun `예약버튼_클릭시_예약_다이얼로그를_표시한다`() {
+        presenter.initializeData(null)
+
+        presenter.onButtonClicked()
+
+        verify { mockView.showBookingAlertDialog(any()) }
+    }
+}
