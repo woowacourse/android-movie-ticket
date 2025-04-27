3b57aa0e9a7332c8e44944da527f94b6950c511f
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-28 03:09:53 +0900

    test: 영화_제목이_표시된다

---
+++ app/src/androidTest/java/woowacourse/movie/reservation/SeatSelectionActivityTest.kt
@@ -0,0 +1,32 @@
+package woowacourse.movie.reservation
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.movie.R
+import woowacourse.movie.view.reservation.SeatSelectionActivity
+import java.time.LocalDateTime
+
+class SeatSelectionActivityTest {
+    @get:Rule
+    val activityRule =
+        ActivityScenarioRule<SeatSelectionActivity>(
+            SeatSelectionActivity.newIntent(
+                ApplicationProvider.getApplicationContext(),
+                "해리 포터와 마법사의 돌",
+                3,
+                LocalDateTime.of(2025, 4, 15, 11, 0),
+            ),
+        )
+
+    @Test
+    fun `영화_제목이_표시된다`() {
+        onView(withId(R.id.tv_seat_selection_title))
+            .check(matches(withText("해리 포터와 마법사의 돌")))
+    }
+}
