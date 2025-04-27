af7a977b78dc11bc4b336b37b01ee8945ed5b918
leeyerin0210 <lyl8661@gmail.com>
2025-04-27 22:03:47 +0900

    feat : 결과 화면에 좌석이 나오도록 수정

--- app/src/main/java/woowacourse/movie/bookingResult/BookingResultActivity.kt
+++ app/src/main/java/woowacourse/movie/bookingResult/BookingResultActivity.kt
@@ -8,24 +8,33 @@ import woowacourse.movie.dto.Ticket
 import woowacourse.movie.util.DataUtils
 
 class BookingResultActivity : AppCompatActivity() {
+    lateinit var ticket: Ticket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_result)
 
-        val ticketDTO = DataUtils.getExtraOrFinish<Ticket>(intent, this, "TICKET") ?: return
+        ticket = DataUtils.getExtraOrFinish<Ticket>(intent, this, "TICKET") ?: return
 
-        findViewById<TextView>(R.id.title).text = ticketDTO.title
-        findViewById<TextView>(R.id.date).text = ticketDTO.date
-        findViewById<TextView>(R.id.time).text = ticketDTO.time
+        findViewById<TextView>(R.id.title).text = ticket.title
+        findViewById<TextView>(R.id.date).text = ticket.date
+        findViewById<TextView>(R.id.time).text = ticket.time
         findViewById<TextView>(R.id.count).text =
             String.format(
                 this.getString(R.string.people_count),
-                ticketDTO.count.toString(),
+                ticket.count.toString(),
             )
         findViewById<TextView>(R.id.money).text =
             String.format(
                 this.getString(R.string.payment),
-                ticketDTO.money,
+                ticket.money,
             )
+
+        setSeatTextView()
+    }
+
+    fun setSeatTextView() {
+        val seatView = findViewById<TextView>(R.id.seats)
+        seatView.text = ticket.seats.joinToString(", ")
     }
 }
