dd2ff3cd6a77790a603ddac65508b070c9be758f
cucumber99 <dlsguq1@gmail.com>
2025-04-28 14:25:22 +0900

    feat: 상영 영화 정보 및 포스터 추가

--- app/src/main/java/woowacourse/movie/domain/model/movie/ScreeningMovies.kt
+++ app/src/main/java/woowacourse/movie/domain/model/movie/ScreeningMovies.kt
@@ -7,14 +7,44 @@ class ScreeningMovies {
         Movie(
             "승부",
             LocalDate.of(2025, 3, 26),
-            LocalDate.of(2025, 4, 26),
+            LocalDate.of(2025, 5, 26),
             115,
         ),
         Movie(
             "미키 17",
             LocalDate.of(2025, 4, 1),
-            LocalDate.of(2025, 4, 29),
+            LocalDate.of(2025, 5, 13),
             137,
         ),
+        Movie(
+            "야당",
+            LocalDate.of(2025, 4, 1),
+            LocalDate.of(2025, 6, 1),
+            123,
+        ),
+        Movie(
+            "위플래쉬",
+            LocalDate.of(2025, 3, 12),
+            LocalDate.of(2025, 5, 31),
+            106,
+        ),
+        Movie(
+            "고독한 미식가 더 무비",
+            LocalDate.of(2025, 3, 19),
+            LocalDate.of(2025, 5, 8),
+            110,
+        ),
+        Movie(
+            "너의 췌장을 먹고 싶어",
+            LocalDate.of(2025, 4, 9),
+            LocalDate.of(2025, 6, 1),
+            115,
+        ),
+        Movie(
+            "로비",
+            LocalDate.of(2025, 4, 2),
+            LocalDate.of(2025, 5, 30),
+            106,
+        )
     )
 }
--- app/src/main/java/woowacourse/movie/ui/util/PosterMapper.kt
+++ app/src/main/java/woowacourse/movie/ui/util/PosterMapper.kt
@@ -6,7 +6,12 @@ import woowacourse.movie.R
 object PosterMapper {
     private val posters = mapOf(
         "승부" to R.drawable.match,
-        "미키 17" to R.drawable.mickey
+        "미키 17" to R.drawable.mickey,
+        "야당" to R.drawable.yadang,
+        "위플래쉬" to R.drawable.whiplash,
+        "고독한 미식가 더 무비" to R.drawable.godok,
+        "너의 췌장을 먹고 싶어" to R.drawable.pancreas,
+        "로비" to R.drawable.lobby
     )
 
     @DrawableRes
