47f24e6b6593523b3568bc677adc4141391acf20
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-28 14:34:40 +0900

    refactor: ScreeningViewHolder implement ScreeningContentViewHolder

--- app/src/main/java/woowacourse/movie/view/reservation/adapter/ScreeningViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/adapter/ScreeningViewHolder.kt
@@ -6,15 +6,35 @@ import android.widget.ImageView
 import android.widget.TextView
 import woowacourse.movie.R
 import woowacourse.movie.domain.reservation.Screening
+import woowacourse.movie.domain.reservation.ScreeningContent
 import woowacourse.movie.view.reservation.Poster.posterId
 
-class ScreeningItemViewHolder(
+class ScreeningViewHolder(
     private val view: View,
-) {
-    fun bind(
-        screening: Screening,
-        onClickReserveButton: (Screening) -> Unit,
-    ) {
+    private val onClickReserveButton: (Screening) -> Unit,
+) : ScreeningContentViewHolder(view) {
+    private val titleView =
+        view.findViewById<TextView>(R.id.tv_item_screening_title)
+            ?: error(ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_title"))
+    private val screeningDateView =
+        view.findViewById<TextView>(R.id.tv_item_screening_date) ?: error(
+            ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_date"),
+        )
+    private val runningTimeView =
+        view.findViewById<TextView>(R.id.tv_item_screening_running_time) ?: error(
+            ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_running_time"),
+        )
+    private val posterView =
+        view.findViewById<ImageView>(R.id.iv_item_screening_poster) ?: error(
+            ERROR_MESSAGE_NO_VIEW_ID.format("iv_item_screening_poster"),
+        )
+    private val reserveButton =
+        view.findViewById<Button>(R.id.btn_item_screening_reserve) ?: error(
+            ERROR_MESSAGE_NO_VIEW_ID.format("btn_item_screening_reserve"),
+        )
+
+    override fun bind(item: ScreeningContent) {
+        val screening = item as? Screening ?: error("")
         screening.run {
             titleView.text = screening.title
             screeningDateView.text =
@@ -38,26 +58,6 @@ class ScreeningItemViewHolder(
             }
         }
     }
-
-    private val titleView =
-        view.findViewById<TextView>(R.id.tv_item_screening_title)
-            ?: error(ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_title"))
-    private val screeningDateView =
-        view.findViewById<TextView>(R.id.tv_item_screening_date) ?: error(
-            ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_date"),
-        )
-    private val runningTimeView =
-        view.findViewById<TextView>(R.id.tv_item_screening_running_time) ?: error(
-            ERROR_MESSAGE_NO_VIEW_ID.format("tv_item_screening_running_time"),
-        )
-    private val posterView =
-        view.findViewById<ImageView>(R.id.iv_item_screening_poster) ?: error(
-            ERROR_MESSAGE_NO_VIEW_ID.format("iv_item_screening_poster"),
-        )
-    private val reserveButton =
-        view.findViewById<Button>(R.id.btn_item_screening_reserve) ?: error(
-            ERROR_MESSAGE_NO_VIEW_ID.format("btn_item_screening_reserve"),
-        )
 
     companion object {
         private const val ERROR_MESSAGE_NO_VIEW_ID = "Can't find view id of %s"
