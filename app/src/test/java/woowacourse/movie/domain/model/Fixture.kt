b7e5d250c31640a4dfc963c32fdf87f1a047feb4
Ham BeomJoon <hbj0802@naver.com>
2025-04-28 13:36:51 +0900

    test: 예매 완료 화면 presenter 테스트

--- app/src/test/java/woowacourse/movie/domain/model/Fixture.kt
+++ app/src/test/java/woowacourse/movie/domain/model/Fixture.kt
@@ -3,6 +3,8 @@ package woowacourse.movie.domain.model
 import woowacourse.movie.R
 import woowacourse.movie.model.Movie
 import woowacourse.movie.model.MovieTicket
+import woowacourse.movie.model.ReservationInfo
+import woowacourse.movie.model.Seats
 import java.time.LocalDate
 
 object Fixture {
@@ -21,5 +23,14 @@ object Fixture {
             LocalDate.of(2025, 4, 1),
             "14:00",
             3,
+        )
+
+    val dummyReservationInfo =
+        ReservationInfo(
+            "라라랜드",
+            LocalDate.of(2025, 4, 1),
+            "14:00",
+            Seats.create(),
+            20000,
         )
 }
---
+++ app/src/test/java/woowacourse/movie/presenter/ReservationCompletePresenterTest.kt
@@ -0,0 +1,38 @@
+package woowacourse.movie.presenter
+
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.domain.model.Fixture
+import woowacourse.movie.model.ReservationInfo
+import woowacourse.movie.view.reservation.complete.ReservationCompleteContract
+import woowacourse.movie.view.reservation.complete.ReservationCompletePresenter
+
+class ReservationCompletePresenterTest {
+    private lateinit var presenter: ReservationCompleteContract.Presenter
+    private lateinit var view: ReservationCompleteContract.View
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk()
+        presenter = ReservationCompletePresenter(view)
+    }
+
+    @Test
+    fun `데이터를 가져오면 예매 정보를 화면에 표시한다`() {
+        // given
+        val slot1 = slot<ReservationInfo>()
+        every { view.updateReservationInfo(capture(slot1)) } just Runs
+
+        // when
+        presenter.fetchData { Fixture.dummyReservationInfo }
+
+        // then
+        verify { view.updateReservationInfo(any()) }
+    }
+}
