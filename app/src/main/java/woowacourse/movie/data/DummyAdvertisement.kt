5535fcbcde4f9b9d4de04e5e3cfb673e789f3464
devfeijoa <devfeijoa@gmail.com>
2025-04-29 00:01:20 +0900

    feat: 광고 화면 구현

---
+++ app/src/main/java/woowacourse/movie/data/DummyAdvertisement.kt
@@ -0,0 +1,8 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.R
+import woowacourse.movie.domain.model.Advertisement
+
+object DummyAdvertisement {
+    val advertisement = Advertisement(R.drawable.ad)
+}
