e780901b042cf993d54300544234b9085d328453
leeyerin0210 <lyl8661@gmail.com>
2025-04-28 16:03:35 +0900

    test : SelectSeatsTest 테스트 코드 추가

--- app/src/test/java/woowacourse/movie/MovieScheduleGeneratorTest.kt
+++ app/src/test/java/woowacourse/movie/MovieScheduleGeneratorTest.kt
@@ -3,27 +3,47 @@ package woowacourse.movie
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import woowacourse.movie.model.MovieScheduleGenerator
+import java.time.LocalDate
+import java.time.LocalTime
 
 class MovieScheduleGeneratorTest {
     @Test
     fun `시작날짜와_끝_날짜를_알면_중간_날짜들을_알_수_있다`() {
         assertThat(
             MovieScheduleGenerator.generateScreeningDates(
-                "2024.4.1",
-                "2024.4.3",
+                LocalDate.of(2024, 4, 1),
+                LocalDate.of(2024, 4, 3),
             ),
         ).isEqualTo(
             listOf(
-                "2024.4.1",
-                "2024.4.2",
-                "2024.4.3",
+                LocalDate.of(2024, 4, 1),
+                LocalDate.of(2024, 4, 2),
+                LocalDate.of(2024, 4, 3),
             ),
         )
     }
 
     @Test
     fun `날짜의_평일_주말_여부에_따라_리턴되는_시작시간이_달라진다`() {
-        assertThat(MovieScheduleGenerator.generateScreeningTimesFor("2024.4.20")[0]).isEqualTo("09:00")
-        assertThat(MovieScheduleGenerator.generateScreeningTimesFor("2024.4.22")[0]).isEqualTo("10:00")
+        assertThat(
+            MovieScheduleGenerator.generateScreeningTimesFor(
+                LocalDate.of(
+                    2024,
+                    4,
+                    20,
+                ),
+            )[0],
+        ).isEqualTo(
+            LocalTime.of(9, 0),
+        )
+        assertThat(
+            MovieScheduleGenerator.generateScreeningTimesFor(
+                LocalDate.of(
+                    2024,
+                    4,
+                    22,
+                ),
+            )[0],
+        ).isEqualTo(LocalTime.of(10, 0))
     }
 }
