8a90afac7b55af8e2463c08d23a97609a85add4b
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-28 14:37:31 +0900

    refactor: Seats 제거

--- app/src/main/java/woowacourse/movie/domain/model/Seats.kt
+++
@@ -1,7 +0,0 @@
-package woowacourse.movie.domain.model
-
-class Seats(val seats: List<Seat>) {
-    fun register(seat: Seat): Seats = Seats(seats + seat)
-
-    fun cancel(seat: Seat): Seats = Seats(seats - seat)
-}
