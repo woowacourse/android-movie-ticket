7235e124a7e5ba6dc03b35bbdf4bcb8422bad8df
devfeijoa <devfeijoa@gmail.com>
2025-04-28 21:19:08 +0900

    refactor: nullable 하지 않게 수정

--- app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
+++ app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
@@ -3,10 +3,10 @@ package woowacourse.movie.view.extension
 import android.content.Intent
 import android.os.Build
 
-inline fun <reified T> Intent.getParcelableCompat(key: String): T? =
+inline fun <reified T> Intent.getParcelableCompat(key: String): T =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        this.getParcelableExtra(key, T::class.java)
+        this.getParcelableExtra(key, T::class.java) ?: throw IllegalArgumentException()
     } else {
         @Suppress("DEPRECATION")
-        this.getParcelableExtra(key) as? T
+        getParcelableExtra(key) as? T ?: throw IllegalArgumentException()
     }
