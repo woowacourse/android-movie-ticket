4dfd1985bb723ef1252cf37126aefc9e519f0c3c
moondev03 <moondev03@gmail.com>
2025-04-28 16:19:47 +0900

    refactor: Screen 개선
    - row, column 시작 index 통일
    - 상수화

--- app/src/main/java/woowacourse/movie/domain/model/cinema/screen/Screen.kt
+++ app/src/main/java/woowacourse/movie/domain/model/cinema/screen/Screen.kt
@@ -4,21 +4,25 @@ class Screen(
     size: ScreenSize,
 ) {
     val seats: List<Seat> =
-        (0 until size.rowSize).flatMap { rowIndex ->
+        (STARTING_INDEX until size.rowSize).flatMap { rowIndex ->
             val seatType = calculateSeatType(rowIndex)
-            (1..size.colSize).map { colIndex ->
+            (STARTING_INDEX until size.colSize).map { colIndex ->
                 Seat(rowIndex, colIndex, seatType)
             }
         }
 
     private fun calculateSeatType(row: Int): SeatType =
         when (row) {
-            in 0..1 -> SeatType.B_CLASS
-            in 2..3 -> SeatType.S_CLASS
+            in B_CLASS_ROW_RANGE -> SeatType.B_CLASS
+            in S_CLASS_ROW_RANGE -> SeatType.S_CLASS
             else -> SeatType.A_CLASS
         }
 
     companion object {
         val DEFAULT_SCREEN = Screen(ScreenSize(5, 4))
+
+        private const val STARTING_INDEX = 0
+        private val B_CLASS_ROW_RANGE = 0..1
+        private val S_CLASS_ROW_RANGE = 2..3
     }
 }
