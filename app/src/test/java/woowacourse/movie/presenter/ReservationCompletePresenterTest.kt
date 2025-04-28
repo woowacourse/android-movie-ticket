a6cb98fbd08037701ae232e3af2b799826847137
Ham BeomJoon <hbj0802@naver.com>
2025-04-28 14:05:25 +0900

    test: 예매 결과 화면 capturing test

--- app/src/test/java/woowacourse/movie/presenter/ReservationCompletePresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/ReservationCompletePresenterTest.kt
@@ -6,6 +6,7 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import woowacourse.movie.domain.model.dummyReservationInfo
@@ -25,14 +26,19 @@ class ReservationCompletePresenterTest {
 
     @Test
     fun `데이터를 가져오면 예매 정보를 화면에 표시한다`() {
+        val reservationInfoSlot = slot<ReservationInfo>()
+
         // given
-        val slot1 = slot<ReservationInfo>()
-        every { view.updateReservationInfo(capture(slot1)) } just Runs
+        every { view.updateReservationInfo(capture(reservationInfoSlot)) } just Runs
 
         // when
         presenter.fetchData { dummyReservationInfo }
 
         // then
         verify { view.updateReservationInfo(any()) }
+
+        assertThat(reservationInfoSlot.captured.title).isEqualTo("라라랜드")
+        assertThat(reservationInfoSlot.captured.time).isEqualTo("14:00")
+        assertThat(reservationInfoSlot.captured.price).isEqualTo(20000)
     }
 }
