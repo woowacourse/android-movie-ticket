03d7414374aec456a0e95d8e2955edda93230b29
leeyerin0210 <lyl8661@gmail.com>
2025-04-28 17:12:55 +0900

    fix : 포맷이 다르게 나타나는 현상 수정

--- app/src/main/java/woowacourse/movie/model/Ticket.kt
+++ app/src/main/java/woowacourse/movie/model/Ticket.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.model
 import woowacourse.movie.uiModel.TicketUIModel
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class Ticket(
     val title: String,
@@ -11,5 +12,7 @@ class Ticket(
     val seats: MutableList<String>,
     var money: Int,
 ) {
-    fun toUIModel(): TicketUIModel = TicketUIModel(title, date.toString(), time.toString(), seats, seats.size, money)
+    private val formatter = DateTimeFormatter.ofPattern("yyyy.M.d")
+
+    fun toUIModel(): TicketUIModel = TicketUIModel(title, date.format(formatter).toString(), time.toString(), seats, seats.size, money)
 }
