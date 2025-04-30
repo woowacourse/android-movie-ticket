066dc179c9f4e29d64948675ecb7cb51342a2976
m6z1 <sonmyungj1zz@gmail.com>
2025-05-01 01:41:31 +0900

    test: UI 테스트 관련 로직 확장 함수 추가

--- app/src/androidTest/java/woowacourse/movie/reservation/ReservationActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/reservation/ReservationActivityTest.kt
@@ -4,16 +4,15 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
 import org.junit.Test
+import woowacourse.checkMatchText
 import woowacourse.movie.MOVIE
 import woowacourse.movie.R
 import woowacourse.movie.fakeContext
 import woowacourse.movie.view.reservation.ReservationActivity
+import woowacourse.performClick
 
 class ReservationActivityTest {
     private lateinit var intent: Intent
@@ -35,27 +34,22 @@ class ReservationActivityTest {
     fun 영화_티켓_개수_플러스_버튼을_누르면_티켓_개수가_1_증가한다() {
         // given: 영화 개수 초기값은 1이고
         // when: 사용자가 플러스 버튼을 누르면
-        onView(withId(R.id.btn_reservation_plus_ticket_count))
-            .perform(click())
+        onView(withId(R.id.btn_reservation_plus_ticket_count)).performClick()
 
         // then: 화면의 영화 개수 값은 2가 된다
-        onView(withId(R.id.tv_reservation_ticket_count))
-            .check(matches(withText("2")))
+        onView(withId(R.id.tv_reservation_ticket_count)).checkMatchText("2")
     }
 
     @Test
     fun 영화_티켓_개수_마이너스_버튼을_누르면_티켓_개수가_1_감소한다() {
         // given: 선택한 영화 개수가 2이고
-        onView(withId(R.id.btn_reservation_plus_ticket_count))
-            .perform(click())
+        onView(withId(R.id.btn_reservation_plus_ticket_count)).performClick()
 
         // when: 사용자가 마이너스 버튼을 누르면
-        onView(withId(R.id.btn_reservation_minus_ticket_count))
-            .perform(click())
+        onView(withId(R.id.btn_reservation_minus_ticket_count)).performClick()
 
         // then: 화면의 영화 개수 값은 1이 된다
-        onView(withId(R.id.tv_reservation_ticket_count))
-            .check(matches(withText("1")))
+        onView(withId(R.id.tv_reservation_ticket_count)).checkMatchText("1")
     }
 
     @Test
@@ -69,15 +63,13 @@ class ReservationActivityTest {
         scenario = ActivityScenario.launch(intent)
 
         // when: 티켓 개수를 1증가 시키고 가로로 회전 시켰을 때
-        onView(withId(R.id.btn_reservation_plus_ticket_count))
-            .perform(click())
+        onView(withId(R.id.btn_reservation_plus_ticket_count)).performClick()
 
         scenario.onActivity { activity ->
             activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         }
 
         // then: 티켓 개수 2가 유지 된다
-        onView(withId(R.id.tv_reservation_ticket_count))
-            .check(matches(withText("2")))
+        onView(withId(R.id.tv_reservation_ticket_count)).checkMatchText("2")
     }
 }
--- app/src/androidTest/java/woowacourse/movie/seatSelection/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/seatSelection/SeatSelectionActivityTest.kt
@@ -3,19 +3,17 @@ package woowacourse.movie.seatSelection
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Before
 import org.junit.Test
+import woowacourse.checkIsDisplayed
+import woowacourse.checkIsEnabled
 import woowacourse.movie.MOVIE_TO_RESERVE
 import woowacourse.movie.R
 import woowacourse.movie.fakeContext
 import woowacourse.movie.view.seatSelection.SeatSelectionActivity
+import woowacourse.performClick
 
 class SeatSelectionActivityTest {
     private lateinit var intent: Intent
@@ -35,88 +33,69 @@ class SeatSelectionActivityTest {
     @Test
     fun 확인_버튼을_누르면_예매_확인_다이얼로그에_타이틀이_표시된다() {
         // given: 좌석 선택 페이지에서 좌석 선택 후
-        onView(withText("A1"))
-            .perform(click())
+        onView(withText("A1")).performClick()
 
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // when: 확인 버튼을 누르면
-        onView(withId(R.id.tv_seat_selection_complete_button))
-            .perform(click())
+        onView(withId(R.id.tv_seat_selection_complete_button)).performClick()
 
         // then: 예매 확인 다이얼로그에 타이틀이 표시된다
-        onView(withText("예매 확인"))
-            .check(matches(isDisplayed()))
+        onView(withText("예매 확인")).checkIsDisplayed()
     }
 
     @Test
     fun 확인_버튼을_누르면_예매_확인_다이얼로그에_메시지가_표시된다() {
         // given: 좌석 선택 페이지에서 좌석 선택 후
-        onView(withText("A1"))
-            .perform(click())
+        onView(withText("A1")).performClick()
 
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // when: 확인 버튼을 누르면
-        onView(withId(R.id.tv_seat_selection_complete_button))
-            .perform(click())
+        onView(withId(R.id.tv_seat_selection_complete_button)).performClick()
 
         // then: 예약 확인 다이얼로그에 메시지가 표시된다
-        onView(withText("정말 예매하시겠습니까?"))
-            .check(matches(isDisplayed()))
+        onView(withText("정말 예매하시겠습니까?")).checkIsDisplayed()
     }
 
     @Test
     fun 확인_버튼을_누르면_예매_확인_다이얼로그에_취소_버튼이_표시된다() {
         // given: 좌석 선택 페이지에서 좌석 선택 후
-        onView(withText("A1"))
-            .perform(click())
+        onView(withText("A1")).performClick()
 
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // when: 확인 버튼을 누르면
-        onView(withId(R.id.tv_seat_selection_complete_button))
-            .perform(click())
+        onView(withId(R.id.tv_seat_selection_complete_button)).performClick()
 
         // then: 예약 확인 다이얼로그에 메시지가 표시된다
-        onView(withText("취소"))
-            .check(matches(isDisplayed()))
+        onView(withText("취소")).checkIsDisplayed()
     }
 
     @Test
     fun 완료_버튼을_누르면_예매_확인_다이얼로그에_예매완료_버튼이_표시된다() {
         // given: 좌석 선택 페이지에서 좌석 선택 후
-        onView(withText("A1"))
-            .perform(click())
+        onView(withText("A1")).performClick()
 
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // when: 확인 버튼을 누르면
-        onView(withId(R.id.tv_seat_selection_complete_button))
-            .perform(click())
+        onView(withId(R.id.tv_seat_selection_complete_button)).performClick()
 
         // then: 예약 확인 다이얼로그에 예매 완료 버튼이 표시된다
-        onView(withText("예매 완료"))
-            .check(matches(isDisplayed()))
+        onView(withText("예매 완료")).checkIsDisplayed()
     }
 
     @Test
     fun 좌석을_선택하면_선택한_좌석에_대해_총_금액이_표시된다() {
         // given: 좌석 선택 페이지에서
         // when: 좌석을 선택하면
-        onView(withText("A1"))
-            .perform(click())
+        onView(withText("A1")).performClick()
 
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // then: 선택한 좌석에 대해 총 금액이 표시된다
-        onView(withText("20,000원"))
-            .check(matches(isDisplayed()))
+        onView(withText("20,000원")).checkIsDisplayed()
     }
 
     @Test
@@ -124,52 +103,42 @@ class SeatSelectionActivityTest {
         // given:
         // when: 좌석 선택뷰가 뜨면
         // then: 예약할_영화_정보에_대한_영화_제목이_표시된다
-        onView(withText("라라랜드"))
-            .check(matches(isDisplayed()))
+        onView(withText("라라랜드")).checkIsDisplayed()
     }
 
     @Test
     fun 예매할_좌석만큼_좌석을_선택하지_않으면_확인_버튼이_활성화_되지_않는다() {
         // given: 좌석 선택뷰가 뜨고
         // when: 예매할 좌석만큼 좌석을 선택하지 않으면
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
         // then: 확인 버튼이 활성화 되지 않는다
-        onView(withText("확인"))
-            .check(matches(isNotEnabled()))
+        onView(withText("확인")).checkIsDisplayed()
     }
 
     @Test
     fun 예매할_좌석만큼_좌석을_선택하면_확인_버튼이_활성화_된다() {
         // given: 좌석 선택뷰가 뜨고
         // when: 예매할 좌석만큼 좌석을 선택하면
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
-        onView(withText("C2"))
-            .perform(click())
+        onView(withText("C2")).performClick()
 
         // then: 확인 버튼이 활성화 된다.
-        onView(withText("확인"))
-            .check(matches(isEnabled()))
+        onView(withText("확인")).checkIsDisplayed()
     }
 
     @Test
     fun 예매할_좌석만큼_좌석을_선택하면_선택한_좌석이_아닌_좌석은_클릭이_비활성화_된다() {
         // given: 좌석 선택뷰가 뜨고
         // when: 예매할 좌석만큼 좌석을 선택하면
-        onView(withText("A2"))
-            .perform(click())
+        onView(withText("A2")).performClick()
 
-        onView(withText("C2"))
-            .perform(click())
+        onView(withText("C2")).performClick()
 
         // then: 선택한_좌석이_아닌_좌석은_클릭이_비활성화_된다
-        onView(withText("C3"))
-            .check(matches(isNotEnabled()))
+        onView(withText("C3")).checkIsEnabled(false)
 
-        onView(withText("D1"))
-            .check(matches(isNotEnabled()))
+        onView(withText("D1")).checkIsEnabled(false)
     }
 }
