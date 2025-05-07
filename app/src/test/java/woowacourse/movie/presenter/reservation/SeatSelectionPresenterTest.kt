62b4c1c70e12d43ef7561ec16961935e791678c8
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-07 20:03:07 +0900

    chore: remove Companion

--- app/src/test/java/woowacourse/movie/presenter/reservation/SeatSelectionPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/reservation/SeatSelectionPresenterTest.kt
@@ -35,7 +35,7 @@ class SeatSelectionPresenterTest {
     fun `좌석들을 보여줄 수 있다`() {
         // given
         every {
-            view.setSeats(Seat.Companion.seats(), emptySet())
+            view.setSeats(Seat.seats(), emptySet())
             view.setConfirmEnabled(false)
         } just Runs
 
@@ -43,7 +43,7 @@ class SeatSelectionPresenterTest {
         presenter.fetchAvailableSeats()
 
         // then
-        verify { view.setSeats(Seat.Companion.seats(), emptySet()) }
+        verify { view.setSeats(Seat.seats(), emptySet()) }
     }
 
     @Test
@@ -67,15 +67,15 @@ class SeatSelectionPresenterTest {
     @Test
     fun `좌석을 선택할 수 있다`() {
         // given
-        every { view.setSeatIsSelected(Seat.Companion(1, 1), true) } just Runs
+        every { view.setSeatIsSelected(Seat(1, 1), true) } just Runs
         every { view.setPrice(10000) } just Runs
         every { view.setConfirmEnabled(false) } just Runs
 
         // when
-        presenter.selectSeat(Seat.Companion(1, 1))
+        presenter.selectSeat(Seat(1, 1))
 
         // then
-        verify { view.setSeatIsSelected(Seat.Companion(1, 1), true) }
+        verify { view.setSeatIsSelected(Seat(1, 1), true) }
     }
 
     @Test
@@ -89,29 +89,29 @@ class SeatSelectionPresenterTest {
                     count = 2,
                     showtime = LocalDateTime.of(2025, 4, 15, 11, 0),
                 ),
-                selectedSeats = setOf(Seat.Companion(1, 1)),
+                selectedSeats = setOf(Seat(1, 1)),
             )
 
-        every { view.setSeatIsSelected(Seat.Companion(1, 1), false) } just Runs
+        every { view.setSeatIsSelected(Seat(1, 1), false) } just Runs
         every { view.setPrice(0) } just Runs
         every { view.setConfirmEnabled(false) } just Runs
 
         // when
-        presenter.selectSeat(Seat.Companion(1, 1))
+        presenter.selectSeat(Seat(1, 1))
 
         // then
-        verify { view.setSeatIsSelected(Seat.Companion(1, 1), false) }
+        verify { view.setSeatIsSelected(Seat(1, 1), false) }
     }
 
     @Test
     fun `좌석을 선택하면 하단에 선택한 좌석 수를 반영한 최종 가격이 표시된다`() {
         // given
-        every { view.setSeatIsSelected(Seat.Companion(1, 1), true) } just Runs
+        every { view.setSeatIsSelected(Seat(1, 1), true) } just Runs
         every { view.setPrice(10_000) } just Runs
         every { view.setConfirmEnabled(false) } just Runs
 
         // when
-        presenter.selectSeat(Seat.Companion(1, 1))
+        presenter.selectSeat(Seat(1, 1))
 
         // then
         verify { view.setPrice(10_000) }
